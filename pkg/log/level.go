e8809df1292aa669320aa93cabb0f29de201fe06
Romeo Van Snick <romeovs@gmail.com>
2017-08-09 11:02:52 +0200

    all: Add log levels

---
+++ pkg/log/level.go
@@ -0,0 +1,85 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package log
+
+import (
+	"errors"
+	"strings"
+)
+
+var ErrInvalidLevel = errors.New("Invalid log level")
+
+// Level is the level of logging
+type Level int8
+
+const (
+	// Debug is the log level for debug messages, usually turned of in production.
+	Debug Level = iota
+
+	// Info is the log level for informational messages.
+	Info
+
+	// Warn is the log level for warnings. Warning are more important than info but
+	// do not need individual human review.
+	Warn
+
+	// Error is the log level for high priority error messages. When everything is running smoothly,
+	// an application should not be logging error level messages.
+	Error
+
+	// Fatal the log level for unrecoverable errors.
+	Fatal Level = iota
+)
+
+// String implements fmt.Stringer
+func (l Level) String() string {
+	switch l {
+	case Debug:
+		return "debug"
+	case Info:
+		return "info"
+	case Warn:
+		return "warn"
+	case Error:
+		return "error"
+	case Fatal:
+		return "fatal"
+	default:
+		return "invalid"
+	}
+}
+
+// ParseLevel parses a string into a log level
+func ParseLevel(str string) (Level, error) {
+	switch strings.ToLower(str) {
+	case "debug":
+		return Debug, nil
+	case "info":
+		return Info, nil
+	case "warn":
+		return Warn, nil
+	case "error":
+		return Error, nil
+	case "fatal":
+		return Fatal, nil
+	default:
+		return Fatal, ErrInvalidLevel
+	}
+}
+
+// MarshalText implments encoding.TextMarshaller
+func (l Level) MarshalText() ([]byte, error) {
+	return []byte(l.String()), nil
+}
+
+// UnmarshalText implments encoding.TextMarshaller
+func (l *Level) UnmarshalText(text []byte) error {
+	level, err := ParseLevel(string(text))
+	if err != nil {
+		return err
+	}
+
+	*l = level
+
+	return nil
+}
---
+++ pkg/log/level_test.go
@@ -0,0 +1,77 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package log
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+
+	. "github.com/smartystreets/assertions"
+)
+
+var levels = []Level{Debug, Info, Warn, Error, Fatal}
+
+type o struct {
+	Level Level `json:"level"`
+}
+
+func TestLevelParse(t *testing.T) {
+	a := New(t)
+	for _, level := range levels {
+		str := level.String()
+
+		{
+			parsed, err := ParseLevel(str)
+			a.So(err, ShouldBeNil)
+			a.So(parsed, ShouldEqual, level)
+		}
+
+		{
+			parsed, err := ParseLevel(strings.ToUpper(str))
+			a.So(err, ShouldBeNil)
+			a.So(parsed, ShouldEqual, level)
+		}
+	}
+}
+
+func TestLevelOrder(t *testing.T) {
+	a := New(t)
+	a.So(Info > Debug, ShouldBeTrue)
+	a.So(Warn > Info, ShouldBeTrue)
+
+	for _, level := range levels {
+		a.So(level >= Debug, ShouldBeTrue)
+		a.So(level <= Fatal, ShouldBeTrue)
+		a.So(level < Debug, ShouldBeFalse)
+		a.So(level > Fatal, ShouldBeFalse)
+	}
+}
+
+func TestLevelJSONUnmarshal(t *testing.T) {
+	for _, level := range levels {
+		a := New(t)
+		raw := []byte(`{ "level": "` + level.String() + `" }`)
+
+		res := new(o)
+		err := json.Unmarshal(raw, res)
+
+		a.So(err, ShouldBeNil)
+		a.So(res.Level, ShouldEqual, level)
+	}
+}
+
+func TestLevelJSONMarshal(t *testing.T) {
+	for _, level := range levels {
+		a := New(t)
+
+		raw := `{"level":"` + level.String() + `"}`
+
+		res, err := json.Marshal(o{
+			Level: level,
+		})
+
+		a.So(err, ShouldBeNil)
+		a.So(string(res), ShouldEqual, raw)
+	}
+}
