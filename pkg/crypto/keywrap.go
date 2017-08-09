736d952a8dc2393839a59f86b2c0783cd9ad4b0f
Hylke Visser <hylke@thethingsnetwork.org>
2017-08-09 18:13:08 +0200

    util: Implement RFC 3394 Keywrap algorithm
    The RFC 3394 Keywrap algorithm is used to encrypt AES keys in the KeyEnvelope proto message

---
+++ pkg/crypto/keywrap.go
@@ -0,0 +1,120 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package crypto
+
+import (
+	"crypto/aes"
+	"encoding/binary"
+	"errors"
+)
+
+var iv = [8]byte{0xA6, 0xA6, 0xA6, 0xA6, 0xA6, 0xA6, 0xA6, 0xA6}
+
+func concat(a, b [8]byte) []byte {
+	c := make([]byte, aes.BlockSize)
+	copy(c[:8], a[:])
+	copy(c[8:], b[:])
+	return c
+}
+func xor(b [8]byte, t uint64) (c [8]byte) {
+	val := binary.BigEndian.Uint64(b[:]) ^ t
+	binary.BigEndian.PutUint64(c[:], val)
+	return
+}
+func msb(b [16]byte) (c [8]byte) { copy(c[:], b[:8]); return }
+func lsb(b [16]byte) (c [8]byte) { copy(c[:], b[8:]); return }
+
+// WrapKey implements the RFC 3394 Wrap algorithm
+func WrapKey(plaintext, kek []byte) (ciphertext []byte, err error) {
+	if len(plaintext)%8 != 0 {
+		return nil, errors.New("pkg/keywrap: invalid plaintext length")
+	}
+
+	var n = len(plaintext) / 8
+	if n < 2 {
+		return nil, errors.New("pkg/keywrap: no key present")
+	}
+
+	cipher, err := aes.NewCipher(kek)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set A to initial value
+	var a = iv
+
+	// Fill R blocks
+	var r = make([][8]byte, n)
+	for i := 0; i < n; i++ {
+		copy(r[i][:], plaintext[i*8:(i+1)*8])
+	}
+
+	// Run the algorithm
+	for j := 0; j <= 5; j++ {
+		for i := 1; i <= n; i++ {
+			var b [aes.BlockSize]byte
+			cipher.Encrypt(b[:], concat(a, r[i-1]))
+			a = xor(msb(b), uint64((n*j)+i))
+			r[i-1] = lsb(b)
+		}
+	}
+
+	// Build the result
+	ciphertext = make([]byte, 0, 8*(n+1))
+	ciphertext = append(ciphertext, a[:]...)
+	for i := 0; i < n; i++ {
+		ciphertext = append(ciphertext, r[i][:]...)
+	}
+
+	return ciphertext, nil
+}
+
+// UnwrapKey implements the RFC 3394 Unwrap algorithm
+func UnwrapKey(ciphertext, kek []byte) (plaintext []byte, err error) {
+	if len(ciphertext)%8 != 0 {
+		return nil, errors.New("pkg/keywrap: invalid ciphertext length")
+	}
+
+	var n = (len(ciphertext) / 8) - 1
+	if n < 2 {
+		return nil, errors.New("pkg/keywrap: no key present")
+	}
+
+	cipher, err := aes.NewCipher(kek)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set A to C[0]
+	var a [8]byte
+	copy(a[:], ciphertext[:8])
+
+	// Fill R blocks
+	var r = make([][8]byte, n)
+	for i := 0; i < n; i++ {
+		copy(r[i][:], ciphertext[(i+1)*8:(i+2)*8])
+	}
+
+	// Run the algorithm
+	for j := 5; j >= 0; j-- {
+		for i := n; i >= 1; i-- {
+			var b [aes.BlockSize]byte
+			cipher.Decrypt(b[:], concat(xor(a, uint64(n*j+i)), r[i-1]))
+			a = msb(b)
+			r[i-1] = lsb(b)
+		}
+	}
+
+	// Check for corruption
+	if a != iv {
+		return nil, errors.New("pkg/keywrap: corrupt key data")
+	}
+
+	// Build the result
+	plaintext = make([]byte, 0, 8*n)
+	for i := 0; i < n; i++ {
+		plaintext = append(plaintext, r[i][:]...)
+	}
+
+	return plaintext, nil
+}
---
+++ pkg/crypto/keywrap_test.go
@@ -0,0 +1,72 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package crypto
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+)
+
+func TestKEK(t *testing.T) {
+	a := assertions.New(t)
+
+	var table = []struct {
+		plaintext  string
+		kek        string
+		ciphertext string
+	}{
+		{"00112233445566778899AABBCCDDEEFF", "000102030405060708090A0B0C0D0E0F", "1FA68B0A8112B447AEF34BD8FB5A7B829D3E862371D2CFE5"},
+		{"00112233445566778899AABBCCDDEEFF", "000102030405060708090A0B0C0D0E0F1011121314151617", "96778B25AE6CA435F92B5B97C050AED2468AB8A17AD84E5D"},
+		{"00112233445566778899AABBCCDDEEFF", "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F", "64E8C3F9CE0F5BA263E9777905818A2A93C8191E7D6E8AE7"},
+		{"00112233445566778899AABBCCDDEEFF0001020304050607", "000102030405060708090A0B0C0D0E0F1011121314151617", "031D33264E15D33268F24EC260743EDCE1C6C7DDEE725A936BA814915C6762D2"},
+		{"00112233445566778899AABBCCDDEEFF0001020304050607", "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F", "A8F9BC1612C68B3FF6E6F4FBE30E71E4769C8B80A32CB8958CD5D17D6B254DA1"},
+		{"00112233445566778899AABBCCDDEEFF000102030405060708090A0B0C0D0E0F", "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F", "28C9F404C4B810F4CBCCB35CFB87F8263F5786E2D80ED326CBC7F0E71A99F43BFB988B9B7A02DD21"},
+	}
+
+	for _, tt := range table {
+		plaintext, _ := hex.DecodeString(tt.plaintext)
+		kek, _ := hex.DecodeString(tt.kek)
+		ciphertext, _ := hex.DecodeString(tt.ciphertext)
+
+		wrapped, err := WrapKey(plaintext, kek)
+		a.So(err, should.BeNil)
+		a.So(wrapped, should.Resemble, ciphertext)
+
+		unwrapped, err := UnwrapKey(ciphertext, kek)
+		a.So(err, should.BeNil)
+		a.So(unwrapped, should.Resemble, plaintext)
+	}
+
+	var err error
+
+	kek, _ := hex.DecodeString("101112131415161718191A1B1C1D1E1F")
+	noBlock, _ := hex.DecodeString("10111213141516")
+	tooShort, _ := hex.DecodeString("1011121314151617")
+
+	_, err = WrapKey(tooShort, kek)
+	a.So(err, should.NotBeNil)
+
+	_, err = UnwrapKey(tooShort, kek)
+	a.So(err, should.NotBeNil)
+
+	_, err = WrapKey(noBlock, kek)
+	a.So(err, should.NotBeNil)
+
+	_, err = UnwrapKey(noBlock, kek)
+	a.So(err, should.NotBeNil)
+
+	data, _ := hex.DecodeString("1FA68B0A8112B447AEF34BD8FB5A7B829D3E862371D2CFE5")
+
+	_, err = WrapKey(data, tooShort)
+	a.So(err, should.NotBeNil)
+
+	_, err = UnwrapKey(data, tooShort)
+	a.So(err, should.NotBeNil)
+
+	_, err = UnwrapKey(data, kek)
+	a.So(err, should.NotBeNil)
+
+}
