475f631991907c618f9098e4606366052557d1fc
Romeo Van Snick <romeovs@gmail.com>
2017-08-15 11:05:42 +0200

    util: Add shared config definitions

---
+++ cmd/shared/config.go
@@ -0,0 +1,38 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package shared
+
+import (
+	"github.com/TheThingsNetwork/ttn/pkg/config"
+)
+
+// DefaultBaseConfig is the default base component configuration.
+var DefaultBaseConfig = config.Base{}
+
+// DefaultTLSConfig is the default TLS config.
+var DefaultTLSConfig = config.TLS{
+	Certificate: "cert.pem",
+	Key:         "key.pem",
+}
+
+// DefaultIdentityConfig is the default Identity config
+var DefaultIdentityConfig = config.Identity{
+	Servers: map[string]string{"ttn-account-v2": "https://account.thethingsnetwork.org"},
+}
+
+// DefaultGRPCConfig is the default config for GRPC.
+var DefaultGRPCConfig = config.GRPC{}
+
+// DefaultRedisConfig is the default config for Redis.
+var DefaultRedisConfig = config.Redis{
+	Address:  "localhost:3479",
+	Database: 0,
+}
+
+// DefaultServiceBase is the default base config for a service.
+var DefaultServiceBase = config.ServiceBase{
+	Base:     DefaultBaseConfig,
+	GRPC:     DefaultGRPCConfig,
+	TLS:      DefaultTLSConfig,
+	Identity: DefaultIdentityConfig,
+}
---
+++ pkg/config/shared.go
@@ -0,0 +1,63 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package config
+
+// Base represents base component configuration
+type Base struct {
+	Debug  bool   `name:"debug" description:"Enable bug mode"`
+	Config string `name:"config" shorthand:"c" description:"Location of the config file"`
+}
+
+// TLS represents TLS configuration
+type TLS struct {
+	Certificate string `name:"certificate" description:"Location of TLS certificate"`
+	Key         string `name:"key" description:"Location of TLS private key"`
+}
+
+// GRPC represents gRPC listener configuration
+type GRPC struct {
+	TCP string `name:"tcp" description:"Address for the TCP gRPC server to listen on"`
+	TLS string `name:"tls" description:"Address for the TLS gRPC server to listen on"`
+}
+
+// HTTP represents the HTTP and HTTPS server configuration
+type HTTP struct {
+	HTTP  string `name:"http" description:"Address for the HTTP server to listen on"`
+	HTTPS string `name:"https" description:"Address for the HTTPS server to listen on"`
+}
+
+// Identity represents identity configuration
+type Identity struct {
+	Servers map[string]string `name:"servers" description:"TTN Identity Servers (id=https://...)"`
+	Keys    map[string]string `name:"keys" description:"TTN Identity Server Public Keys (id=/path/to/...)"`
+}
+
+// Redis represents Redis configuration
+type Redis struct {
+	Address  string `name:"address" description:"Address of the Redis server"`
+	Database int    `name:"database" description:"Redis database to use"`
+}
+
+// RemoteProviderConfig represents remote config provider configuration(see Viper documentation)
+type RemoteProviderConfig struct {
+	Name     string `name:"name" description:"Name of the config on the remote without the extension"`
+	Provider string `name:"provider" description:"Remote config provider name"`
+	Endpoint string `name:"endpoint" description:"Endpoint where the remote config provider is accessible"`
+	Path     string `name:"path" description:"Path where to look for the config on the remote"`
+	KeyRing  string `name:"keyring" description:"Optional path to secret keyring for initializing a secure connection to remote config provider"`
+}
+
+// ServiceBase represents base service configuration
+type ServiceBase struct {
+	Base         `name:",squash"`
+	GRPC         GRPC                  `name:"grpc"`
+	HTTP         HTTP                  `name:"http"`
+	TLS          TLS                   `name:"tls"`
+	Identity     Identity              `name:"identity"`
+	RemoteConfig *RemoteProviderConfig `name:"remote-config"`
+}
+
+// IsValid returns wether or not the remote config is valid or not
+func (c RemoteProviderConfig) IsValid() bool {
+	return c.Provider != "" && c.Endpoint != "" && c.Path != ""
+}
