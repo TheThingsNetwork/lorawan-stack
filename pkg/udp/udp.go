dd6e86652069b1c496e338ae9055205f15535b36
Eric Gourlaouen <eric@thethingsindustries.com>
2017-08-22 15:06:32 +0200

    api: Add package documentation for pkg/udp

---
+++ pkg/udp/udp.go
@@ -0,0 +1,8 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+/*
+Package udp contains a framework for interacting with UDP Semtech packet
+forwarders. It provides structs and methods to do so, as well as some other
+structs of convenience.
+*/
+package udp
