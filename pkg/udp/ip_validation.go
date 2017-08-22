ef4a187e92ff22d04ff408e991e60ab71c535be2
Eric Gourlaouen <eric@thethingsindustries.com>
2017-08-22 13:20:33 +0200

    api: Move Validation to pkg/udp

--- pkg/udp/ip_validation.go
+++ pkg/udp/ip_validation.go
@@ -1,11 +1,9 @@
 // Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
 
-package validation
-
-import "github.com/TheThingsNetwork/ttn/pkg/udp"
+package udp
 
 // Validator is an object that can validate UDP packets.
 type Validator interface {
 	// Valid returns true if the sent packet is considered valid.
-	Valid(packet udp.Packet) bool
+	Valid(packet Packet) bool
 }
