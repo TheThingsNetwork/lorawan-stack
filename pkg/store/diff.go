2aa6397cc102fe9b3cce4e1301bdbd08bf074224
Roman Volosatovs <rvolosatovs@riseup.net>
2017-08-29 10:00:41 +0200

    util: Simplify Diff

--- pkg/store/diff.go
+++ pkg/store/diff.go
@@ -2,57 +2,32 @@
 
 package store
 
-import (
-	"reflect"
-	"sort"
-)
-
-func keys(v map[string]interface{}) []string {
-	keys := make([]string, 0, len(v))
-	for k := range v {
-		keys = append(keys, k)
-	}
-	return keys
-}
+import "reflect"
 
 // Diff the new and the old value and return the changed fields
 func Diff(new, old map[string]interface{}) (diff map[string]interface{}) {
-	allKeys := append(keys(old), keys(new)...)
-
-	// Remove duplicate keys
-	if len(allKeys) > 1 {
-		sort.Strings(allKeys)
-		var p int
-		for i := 1; i < len(allKeys); i++ {
-			if allKeys[i] == allKeys[p] {
-				continue
-			}
-			p++
-			if p < i {
-				allKeys[p], allKeys[i] = allKeys[i], allKeys[p]
-			}
-		}
-		allKeys = allKeys[:p+1]
+	keys := make(map[string]struct{}, len(new))
+	for k := range old {
+		keys[k] = struct{}{}
+	}
+	for k := range new {
+		keys[k] = struct{}{}
 	}
 
-	// The actual diff
-	diff = make(map[string]interface{})
-	for _, key := range allKeys {
-		oldVal, oldOK := old[key]
-		newVal, newOK := new[key]
-		if oldOK && newOK {
-			if !reflect.DeepEqual(oldVal, newVal) {
-				diff[key] = newVal
+	diff = make(map[string]interface{}, len(keys))
+	for k := range keys {
+		newVal, newOK := new[k]
+		oldVal, oldOK := old[k]
+		switch {
+		case newOK && oldOK:
+			if !reflect.DeepEqual(newVal, oldVal) {
+				diff[k] = newVal
 			}
-			continue
-		}
-		if newOK {
-			diff[key] = newVal
-		}
-		if oldOK {
-			diff[key] = nil
+		case newOK:
+			diff[k] = newVal
+		case oldOK:
+			diff[k] = nil
 		}
 	}
-
 	return diff
 }
