20ec23c4a44fd3a1fee73ab60c2cda3a59ffa2a4
Roman Volosatovs <rvolosatovs@riseup.net>
2017-08-31 13:27:59 +0200

    util: Add storetest store testing library

--- pkg/store/mapstore/mapstore_test.go
+++ pkg/store/mapstore/mapstore_test.go
@@ -5,69 +5,10 @@ package mapstore
 import (
 	"testing"
 
-	"github.com/TheThingsNetwork/ttn/pkg/store"
-	"github.com/smartystreets/assertions"
-	"github.com/smartystreets/assertions/should"
+	"github.com/TheThingsNetwork/ttn/pkg/store/storetest"
 )
 
 func TestMapStore(t *testing.T) {
-	a := assertions.New(t)
-
-	old := map[string]interface{}{
-		"foo": "foo",
-		"bar": "bar",
-		"baz": "baz",
-	}
-	new := map[string]interface{}{
-		"foo": "baz",
-		"bar": "bar",
-		"qux": "qux",
-	}
-
 	s := New()
-
-	id, err := s.Create(old)
-	a.So(err, should.BeNil)
-
-	other := map[string]interface{}{
-		"hello": "world",
-	}
-	otherID, _ := s.Create(other)
-
-	a.So(otherID, should.NotEqual, id)
-
-	fields, err := s.Find(id)
-	a.So(err, should.BeNil)
-	a.So(fields, should.Resemble, old)
-
-	fields, err = s.Find(otherID)
-	a.So(err, should.BeNil)
-	a.So(fields, should.Resemble, other)
-
-	err = s.Update(id, store.Diff(new, old))
-	a.So(err, should.BeNil)
-
-	fields, err = s.Find(id)
-	a.So(err, should.BeNil)
-	a.So(fields, should.Resemble, new)
-
-	matches, err := s.FindBy(map[string]interface{}{
-		"foo": "baz",
-		"bar": "bar",
-	})
-	a.So(err, should.BeNil)
-	a.So(matches, should.HaveLength, 1)
-
-	matches, err = s.FindBy(map[string]interface{}{
-		"foo": "foo",
-		"bar": "bar",
-	})
-	a.So(err, should.BeNil)
-	a.So(matches, should.HaveLength, 0)
-
-	err = s.Delete(id)
-	a.So(err, should.BeNil)
-
-	fields, err = s.Find(id)
-	a.So(err, should.Equal, store.ErrNotFound)
+	storetest.TestStore(t, s)
 }
---
+++ pkg/store/storetest/test.go
@@ -0,0 +1,74 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package storetest
+
+import (
+	"github.com/TheThingsNetwork/ttn/pkg/store"
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+)
+
+type testingT interface {
+	Error(args ...interface{})
+}
+
+func TestStore(t testingT, v store.Interface) {
+	a := assertions.New(t)
+
+	old := map[string]interface{}{
+		"foo": "foo",
+		"bar": "bar",
+		"baz": "baz",
+	}
+	new := map[string]interface{}{
+		"foo": "baz",
+		"bar": "bar",
+		"qux": "qux",
+	}
+
+	id, err := v.Create(old)
+	a.So(err, should.BeNil)
+
+	other := map[string]interface{}{
+		"hello": "world",
+	}
+	otherID, _ := v.Create(other)
+
+	a.So(otherID, should.NotEqual, id)
+
+	fields, err := v.Find(id)
+	a.So(err, should.BeNil)
+	a.So(fields, should.Resemble, old)
+
+	fields, err = v.Find(otherID)
+	a.So(err, should.BeNil)
+	a.So(fields, should.Resemble, other)
+
+	err = v.Update(id, store.Diff(new, old))
+	a.So(err, should.BeNil)
+
+	fields, err = v.Find(id)
+	a.So(err, should.BeNil)
+	a.So(fields, should.Resemble, new)
+
+	matches, err := v.FindBy(map[string]interface{}{
+		"foo": "baz",
+		"bar": "bar",
+	})
+	a.So(err, should.BeNil)
+	a.So(matches, should.HaveLength, 1)
+
+	matches, err = v.FindBy(map[string]interface{}{
+		"foo": "foo",
+		"bar": "bar",
+	})
+	a.So(err, should.BeNil)
+	a.So(matches, should.HaveLength, 0)
+
+	err = v.Delete(id)
+	a.So(err, should.BeNil)
+
+	fields, err = v.Find(id)
+	a.So(err, should.Equal, store.ErrNotFound)
+	a.So(fields, should.Equal, nil)
+}
