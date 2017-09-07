97cc6e60fb93dcaa353ae55a3da03058a47008bf
Daniel Gómez Jurado <danielgomjur@gmail.com>
2017-09-07 12:59:06 +0200

    is: Remove stale headers

--- pkg/identityserver/utils/panic.go
+++ pkg/identityserver/utils/panic.go
@@ -1,7 +1,5 @@
 // Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
 
-// Copyright © 2017 The Things Industries B.V.
-
 package utils
 
 // Panic panics if the error is not nil
