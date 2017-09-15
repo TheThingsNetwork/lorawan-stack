12ff5751e3dd3caca0fb3281b140d83cf915bf36
Hylke Visser <hylke@thethingsnetwork.org>
2017-09-15 14:05:34 +0200

    dev: Add Cockroach to Concourse CI

--- ci/go-test.sh
+++ ci/go-test.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+if [[ ! -f ./deps/cockroach-v1.0.6 ]]
+then
+  wget -qO- https://binaries.cockroachdb.com/cockroach-v1.0.6.linux-amd64.tgz | tar xvz
+  mkdir -p ./deps
+  mv ./cockroach-v1.0.6.linux-amd64/cockroach ./deps/cockroach-v1.0.6
+  chmod +x ./deps/cockroach-v1.0.6
+fi
+./deps/cockroach-v1.0.6 start --insecure &
 
 export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:/usr/local/bin:$PATH
