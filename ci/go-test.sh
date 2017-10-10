5eead7970e1a22b336f2403a1500f3971f25987f
Hylke Visser <hylke@thethingsnetwork.org>
2017-10-10 14:12:13 +0200

    dev: Add redis test dependency to Concourse CI

--- ci/go-test.sh
+++ ci/go-test.sh
@@ -2,14 +2,31 @@
 
 set -e
 
+# Kill background jobs on exit
+trap 'kill $(jobs -p)' EXIT
+
+if [[ ! -d ./deps ]]
+then
+  mkdir -p ./deps
+fi
+
 if [[ ! -f ./deps/cockroach-v1.0.6 ]]
 then
   wget -qO- https://binaries.cockroachdb.com/cockroach-v1.0.6.linux-amd64.tgz | tar xvz
-  mkdir -p ./deps
   mv ./cockroach-v1.0.6.linux-amd64/cockroach ./deps/cockroach-v1.0.6
   chmod +x ./deps/cockroach-v1.0.6
 fi
 ./deps/cockroach-v1.0.6 start --insecure &
+
+if [[ ! -f ./deps/redis-v4.0.2 ]]
+then
+  wget -qO- http://download.redis.io/releases/redis-4.0.2.tar.gz | tar xvz
+  pushd redis-4.0.2/src > /dev/null
+  make redis-server
+  popd > /dev/null
+  mv ./redis-4.0.2/src/redis-server ./deps/redis-v4.0.2
+fi
+./deps/redis-v4.0.2 &
 
 export GOPATH=$PWD/go
 export PATH=$GOPATH/bin:/usr/local/bin:$PATH
