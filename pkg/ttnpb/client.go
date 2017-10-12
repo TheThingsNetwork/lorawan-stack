96a7d6a7d6d28e51947f9abb3745ecd2e881a1ff
Romeo Van Snick <romeovs@gmail.com>
2017-10-12 13:15:14 +0200

    is: Implement osin.Client in client

--- pkg/ttnpb/client.go
+++ pkg/ttnpb/client.go
@@ -6,3 +6,18 @@ package ttnpb
 func (c *Client) GetClient() *Client {
 	return c
 }
+
+// GetId implements osin.Client.
+func (c *Client) GetId() string {
+	return c.ClientIdentifier.GetClientID()
+}
+
+// GetRedirectUri implements osin.Client.
+func (c *Client) GetRedirectUri() string {
+	return c.CallbackURI
+}
+
+// GetUserData implements osin.Client.
+func (c *Client) GetUserData() interface{} {
+	return nil
+}
