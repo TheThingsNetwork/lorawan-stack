784c0e8fb7f44d540eee4f482824285fd078a67a
Daniel Gómez Jurado <danielgomjur@gmail.com>
2017-10-20 10:24:00 +0200

    is: Add NOT NULL constraint to OAuth migration

--- pkg/identityserver/store/sql/migrations/5_oauth_initial_schema.go
+++ pkg/identityserver/store/sql/migrations/5_oauth_initial_schema.go
@@ -5,22 +5,22 @@ package migrations
 func init() {
 	const forwards = `
 		CREATE TABLE IF NOT EXISTS authorization_codes (
-			authorization_code STRING(64) PRIMARY KEY,
-			client_id          STRING(36) REFERENCES clients(client_id) NOT NULL,
-			created_at         TIMESTAMP DEFAULT current_timestamp(),
-			expires_in         INTEGER,
-			scope              STRING,
-			redirect_uri       STRING,
-			state              STRING,
-			user_id            STRING REFERENCES users(user_id)
+			authorization_code   STRING(64) PRIMARY KEY,
+			client_id            STRING(36) REFERENCES clients(client_id) NOT NULL,
+			created_at           TIMESTAMP DEFAULT current_timestamp(),
+			expires_in           INTEGER,
+			scope                STRING,
+			redirect_uri         STRING,
+			state                STRING,
+			user_id              STRING(36) REFERENCES users(user_id) NOT NULL
 		);
 
 		CREATE TABLE IF NOT EXISTS refresh_tokens (
-			refresh_token STRING(64) PRIMARY KEY,
-			client_id    STRING(36) REFERENCES clients(client_id) NOT NULL,
-			created_at   TIMESTAMP DEFAULT current_timestamp(),
-			scope		 STRING,
-			redirect_uri STRING
+			refresh_token   STRING(64) PRIMARY KEY,
+			client_id       STRING(36) REFERENCES clients(client_id) NOT NULL,
+			created_at      TIMESTAMP DEFAULT current_timestamp(),
+			scope		    STRING,
+			redirect_uri    STRING
 		);
 	`
 
