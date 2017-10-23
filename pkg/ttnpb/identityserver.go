aa627a34960f6a76f577aa1fc38791839cb97dd6
Daniel Gómez Jurado <danielgomjur@gmail.com>
2017-10-23 16:55:06 +0200

    api: Move UpdateMask errors to a main file inside the package

--- pkg/ttnpb/identityserver.go
+++ pkg/ttnpb/identityserver.go
@@ -7,27 +7,6 @@ import (
 	"github.com/TheThingsNetwork/ttn/pkg/validate"
 )
 
-func init() {
-	ErrInvalidPathUpdateMask.Register()
-	ErrUpdateMaskNotFound.Register()
-}
-
-// ErrInvalidPathUpdateMask is returned when including in the `update_mask` a
-// path that does not exist or is not allowed.
-var ErrInvalidPathUpdateMask = &errors.ErrDescriptor{
-	MessageFormat: "Invalid update_mask: `{path}` is not a valid path",
-	Code:          1,
-	Type:          errors.InvalidArgument,
-}
-
-// ErrUpdateMaskNotFound is returned on update operations where `update_mask`
-// field was not specfied or it was empty.
-var ErrUpdateMaskNotFound = &errors.ErrDescriptor{
-	MessageFormat: "update_mask must be specified and non-empty",
-	Code:          2,
-	Type:          errors.NotFound,
-}
-
 // Validate is used as validator function by the GRPC validator interceptor.
 func (req *CreateUserRequest) Validate() error {
 	return validate.All(
