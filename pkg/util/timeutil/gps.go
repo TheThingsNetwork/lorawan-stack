a0f5b1f7c1d2f0d260a1ef6add3e38de68cd2532
Roman Volosatovs <rvolosatovs@riseup.net>
2017-10-24 22:09:39 +0200

    util: Add GPS time translation

---
+++ pkg/util/timeutil/gps.go
@@ -0,0 +1,76 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+// Package timeutil provides utilities to work with time.
+package timeutil
+
+import (
+	"time"
+)
+
+// 1980-01-06T00:00:00+00:00
+const gpsEpochSec = 315964800
+
+// Leap seconds in GPS time
+var leaps = [...]int64{
+	46828800,
+	78364801,
+	109900802,
+	173059203,
+	252028804,
+	315187205,
+	346723206,
+	393984007,
+	425520008,
+	457056009,
+	504489610,
+	551750411,
+	599184012,
+	820108813,
+	914803214,
+	1025136015,
+	1119744016,
+	1167264017,
+	1341118800,
+}
+
+// IsLeap reports whether the given GPS time,
+// sec seconds since January 6, 1980 UTC,
+// is a leap second in UTC.
+func IsLeap(sec int64) bool {
+	i := int64(len(leaps)) - 1
+	for ; i >= 0; i-- {
+		if sec > leaps[i] {
+			return false
+		}
+		if sec == leaps[i] {
+			return true
+		}
+	}
+	return false
+}
+
+// GPS returns the local Time corresponding to the given GPS time,
+// sec seconds since January 6, 1980 UTC.
+func GPS(sec int64) time.Time {
+	i := int64(len(leaps))
+	for ; i > 0; i-- {
+		if sec > leaps[i-1] {
+			break
+		}
+	}
+	return time.Unix(sec+gpsEpochSec-i, 0)
+}
+
+// TimeToGPS returns t as a GPS time, the number of seconds elapsed
+// since January 6, 1980 UTC.
+func TimeToGPS(t time.Time) int64 {
+	sec := t.Unix() - gpsEpochSec
+
+	i := int64(len(leaps))
+	for ; i > 0; i-- {
+		if sec > leaps[i-1]-i {
+			break
+		}
+	}
+	return sec + i
+}
---
+++ pkg/util/timeutil/gps_test.go
@@ -0,0 +1,104 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package timeutil_test
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/TheThingsNetwork/ttn/pkg/util/timeutil"
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+)
+
+var (
+	epoch          = time.Date(1980, time.January, 6, 0, 0, 0, 0, time.UTC)
+	leap1          = time.Date(1981, time.June, 30, 23, 59, 59, 0, time.UTC).Unix() - epoch.Unix() + 1
+	leap5          = time.Date(1987, time.December, 31, 23, 59, 59, 0, time.UTC).Unix() - epoch.Unix() + 1 + 4
+	now            = time.Date(2017, time.October, 24, 23, 53, 30, 0, time.UTC)
+	nowLeaps int64 = 18
+)
+
+func TestGPSConversion(t *testing.T) {
+	t.Logf("Leap 1: %d Leap 5: %d", leap1, leap5)
+
+	for _, tc := range []struct {
+		GPS  int64
+		Time time.Time
+	}{
+		{
+			// From LoRaWAN 1.1 specification
+			1139322288,
+			time.Date(2016, time.February, 12, 14, 24, 31, 0, time.UTC),
+		},
+		{
+			now.Unix() - epoch.Unix() + nowLeaps,
+			now,
+		},
+		{
+			42,
+			epoch.Add(42 * time.Second),
+		},
+		{
+			0,
+			epoch,
+		},
+		{
+			-1,
+			epoch.Add(-1 * time.Second),
+		},
+
+		{
+			leap1 - 2,
+			epoch.Add(time.Second * time.Duration(leap1-2)),
+		},
+		{
+			leap1 - 1,
+			epoch.Add(time.Second * time.Duration(leap1-1)),
+		},
+		{
+			leap1,
+			epoch.Add(time.Second * time.Duration(leap1)),
+		},
+		{
+			leap1 + 1,
+			epoch.Add(time.Second * time.Duration(leap1)),
+		},
+		{
+			leap1 + 2,
+			epoch.Add(time.Second * time.Duration(leap1+1)),
+		},
+
+		{
+			leap5 - 2,
+			epoch.Add(time.Second * time.Duration(leap5-6)),
+		},
+		{
+			leap5 - 1,
+			epoch.Add(time.Second * time.Duration(leap5-5)),
+		},
+		{
+			leap5,
+			epoch.Add(time.Second * time.Duration(leap5-4)),
+		},
+		{
+			leap5 + 1,
+			epoch.Add(time.Second * time.Duration(leap5-4)),
+		},
+		{
+			leap5 + 2,
+			epoch.Add(time.Second * time.Duration(leap5-3)),
+		},
+	} {
+		a := assertions.New(t)
+		a.So(GPS(tc.GPS).UnixNano(), should.Resemble, tc.Time.UnixNano())
+		if IsLeap(tc.GPS) {
+			a.So(TimeToGPS(tc.Time), should.Equal, tc.GPS+1)
+		} else {
+			a.So(TimeToGPS(tc.Time), should.Equal, tc.GPS)
+		}
+		if a.Failed() {
+			t.Errorf("Time: %s, Unix: %d, GPS: %d", tc.Time, tc.Time.Unix(), tc.GPS)
+		}
+	}
+}
