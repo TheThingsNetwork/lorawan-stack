fb8c2f05ee86ceadc2d5d943224139fe525dcc03
Hylke Visser <hylke@thethingsnetwork.org>
2017-11-20 17:35:17 +0200

    util: Implement gRPC error interceptors

---
+++ pkg/errors/grpcerrors/client_interceptors.go
@@ -0,0 +1,50 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package grpcerrors
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+// UnaryClientInterceptor converts gRPC errors to regular errors.
+func UnaryClientInterceptor() grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		err := invoker(ctx, method, req, reply, cc, opts...)
+		if err != nil {
+			err = FromGRPC(err)
+		}
+		return err
+	}
+}
+
+type wrappedStream struct {
+	grpc.ClientStream
+}
+
+func (w wrappedStream) SendMsg(m interface{}) error {
+	err := w.ClientStream.SendMsg(m)
+	if err != nil {
+		err = FromGRPC(err)
+	}
+	return err
+}
+func (w wrappedStream) RecvMsg(m interface{}) error {
+	err := w.ClientStream.RecvMsg(m)
+	if err != nil {
+		err = FromGRPC(err)
+	}
+	return err
+}
+
+// StreamClientInterceptor converts gRPC errors to regular errors.
+func StreamClientInterceptor() grpc.StreamClientInterceptor {
+	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+		s, err := streamer(ctx, desc, cc, method, opts...)
+		if err != nil {
+			return nil, FromGRPC(err)
+		}
+		return wrappedStream{s}, nil
+	}
+}
---
+++ pkg/errors/grpcerrors/server_interceptors.go
@@ -0,0 +1,31 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package grpcerrors
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+// UnaryServerInterceptor converts errors to gRPC errors.
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		res, err := handler(ctx, req)
+		if err != nil {
+			err = ToGRPC(err)
+		}
+		return res, err
+	}
+}
+
+// StreamServerInterceptor converts errors to gRPC errors.
+func StreamServerInterceptor() grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		err := handler(srv, stream)
+		if err != nil {
+			err = ToGRPC(err)
+		}
+		return err
+	}
+}
