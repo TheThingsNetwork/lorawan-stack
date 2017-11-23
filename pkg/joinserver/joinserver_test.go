cbeb4dfaea869a8179657297019731f88ceae2ed
Roman Volosatovs <rvolosatovs@riseup.net>
2017-11-23 19:45:07 +0100

    js: Test non-zero nonces, add invalid cases

--- pkg/joinserver/joinserver_test.go
+++ pkg/joinserver/joinserver_test.go
@@ -94,6 +94,7 @@ func TestHandleJoin(t *testing.T) {
 
 		NextNextDevNonce  uint32
 		NextNextJoinNonce uint32
+		NextUsedDevNonces []uint32
 
 		JoinRequest  *ttnpb.JoinRequest
 		JoinResponse *ttnpb.JoinResponse
@@ -101,7 +102,7 @@ func TestHandleJoin(t *testing.T) {
 		Error error
 	}{
 		{
-			"1.1",
+			"1.1 new device",
 			&ttnpb.EndDevice{
 				NextDevNonce:  0,
 				NextJoinNonce: 0,
@@ -123,6 +124,7 @@ func TestHandleJoin(t *testing.T) {
 			},
 			1,
 			1,
+			[]uint32{0},
 			&ttnpb.JoinRequest{
 				SelectedMacVersion: ttnpb.MAC_V1_1,
 				RawPayload: []byte{
@@ -210,9 +212,177 @@ func TestHandleJoin(t *testing.T) {
 			nil,
 		},
 		{
-			"1.0.2",
+			"1.1 existing device",
 			&ttnpb.EndDevice{
-				NextDevNonce:  42,
+				NextDevNonce:  0x2442,
+				UsedDevNonces: []uint32{0, 42, 0x2441},
+				NextJoinNonce: 0x424242,
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+					JoinEUI: &types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+				},
+				RootKeys: &ttnpb.RootKeys{
+					AppKey: &ttnpb.KeyEnvelope{
+						Key:      &appKey,
+						KekLabel: "",
+					},
+					NwkKey: &ttnpb.KeyEnvelope{
+						Key:      &nwkKey,
+						KekLabel: "",
+					},
+				},
+				LoRaWANVersion: ttnpb.MAC_V1_1,
+			},
+			0x2443,
+			0x424243,
+			[]uint32{0, 42, 0x2441, 0x2442},
+			&ttnpb.JoinRequest{
+				SelectedMacVersion: ttnpb.MAC_V1_1,
+				RawPayload: []byte{
+					/* MHDR */
+					0x00,
+
+					/* MACPayload */
+					/** JoinEUI **/
+					0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevEUI **/
+					0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevNonce **/
+					0x42, 0x24,
+
+					/* MIC */
+					0x79, 0x8, 0xfd, 0x3d,
+				},
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevAddr: &types.DevAddr{0x42, 0xff, 0xff, 0xff},
+				},
+				NetID: types.NetID{0x42, 0xff, 0xff},
+				DownlinkSettings: ttnpb.DLSettings{
+					OptNeg:      true,
+					Rx1DROffset: 0x7,
+					Rx2DR:       0xf,
+				},
+				RxDelay: 0x42,
+				CFList:  nil,
+			},
+			&ttnpb.JoinResponse{
+				RawPayload: append([]byte{
+					/* MHDR */
+					0x20},
+					mustEncryptJoinAccept(nwkKey, []byte{
+						/* JoinNonce */
+						0x42, 0x42, 0x42,
+						/* NetID */
+						0x42, 0xff, 0xff,
+						/* DevAddr */
+						0x42, 0xff, 0xff, 0xff,
+						/* DLSettings */
+						0xff,
+						/* RxDelay */
+						0x42,
+
+						/* MIC */
+						0xa5, 0x2c, 0x95, 0x4c,
+					})...),
+				SessionKeys: ttnpb.SessionKeys{
+					AppSKey: &ttnpb.KeyEnvelope{
+						Key: KeyPointer(crypto.DeriveAppSKey(
+							appKey,
+							types.JoinNonce{0x42, 0x42, 0x42},
+							types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+							types.DevNonce{0x42, 0x24})),
+						KekLabel: "",
+					},
+					SNwkSIntKey: &ttnpb.KeyEnvelope{
+						Key: KeyPointer(crypto.DeriveSNwkSIntKey(
+							nwkKey,
+							types.JoinNonce{0x42, 0x42, 0x42},
+							types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+							types.DevNonce{0x42, 0x24})),
+						KekLabel: "",
+					},
+					FNwkSIntKey: &ttnpb.KeyEnvelope{
+						Key: KeyPointer(crypto.DeriveFNwkSIntKey(
+							nwkKey,
+							types.JoinNonce{0x42, 0x42, 0x42},
+							types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+							types.DevNonce{0x42, 0x24})),
+						KekLabel: "",
+					},
+					NwkSEncKey: &ttnpb.KeyEnvelope{
+						Key: KeyPointer(crypto.DeriveNwkSEncKey(
+							nwkKey,
+							types.JoinNonce{0x42, 0x42, 0x42},
+							types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+							types.DevNonce{0x42, 0x24})),
+						KekLabel: "",
+					},
+				},
+				Lifetime: nil,
+			},
+			nil,
+		},
+		{
+			"1.1 DevNonce too small",
+			&ttnpb.EndDevice{
+				NextDevNonce:  0x2443,
+				UsedDevNonces: []uint32{0, 42, 0x2441, 0x2442},
+				NextJoinNonce: 0x424242,
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+					JoinEUI: &types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+				},
+				RootKeys: &ttnpb.RootKeys{
+					AppKey: &ttnpb.KeyEnvelope{
+						Key:      &appKey,
+						KekLabel: "",
+					},
+					NwkKey: &ttnpb.KeyEnvelope{
+						Key:      &nwkKey,
+						KekLabel: "",
+					},
+				},
+				LoRaWANVersion: ttnpb.MAC_V1_1,
+			},
+			0x2442,
+			0x424242,
+			[]uint32{0, 42, 0x2441, 0x2442},
+			&ttnpb.JoinRequest{
+				SelectedMacVersion: ttnpb.MAC_V1_1,
+				RawPayload: []byte{
+					/* MHDR */
+					0x00,
+
+					/* MACPayload */
+					/** JoinEUI **/
+					0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevEUI **/
+					0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevNonce **/
+					0x42, 0x24,
+
+					/* MIC */
+					0x79, 0x8, 0xfd, 0x3d,
+				},
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevAddr: &types.DevAddr{0x42, 0xff, 0xff, 0xff},
+				},
+				NetID: types.NetID{0x42, 0xff, 0xff},
+				DownlinkSettings: ttnpb.DLSettings{
+					OptNeg:      true,
+					Rx1DROffset: 0x7,
+					Rx2DR:       0xf,
+				},
+				RxDelay: 0x42,
+				CFList:  nil,
+			},
+			nil,
+			ErrDevNonceTooSmall.New(nil),
+		},
+		{
+			"1.0.2 new device",
+			&ttnpb.EndDevice{
+				UsedDevNonces: []uint32{23, 41, 42, 52},
 				NextJoinNonce: 0,
 				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
 					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
@@ -226,8 +396,9 @@ func TestHandleJoin(t *testing.T) {
 				},
 				LoRaWANVersion: ttnpb.MAC_V1_0_2,
 			},
-			42,
+			0,
 			1,
+			[]uint32{23, 41, 42, 52, 0},
 			&ttnpb.JoinRequest{
 				SelectedMacVersion: ttnpb.MAC_V1_0_2,
 				RawPayload: []byte{
@@ -300,9 +471,9 @@ func TestHandleJoin(t *testing.T) {
 			nil,
 		},
 		{
-			"1.0.1",
+			"1.0.1 new device",
 			&ttnpb.EndDevice{
-				NextDevNonce:  42,
+				UsedDevNonces: []uint32{23, 41, 42, 52},
 				NextJoinNonce: 0,
 				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
 					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
@@ -316,8 +487,9 @@ func TestHandleJoin(t *testing.T) {
 				},
 				LoRaWANVersion: ttnpb.MAC_V1_0_1,
 			},
-			42,
+			0,
 			1,
+			[]uint32{23, 41, 42, 52, 0},
 			&ttnpb.JoinRequest{
 				SelectedMacVersion: ttnpb.MAC_V1_0_1,
 				RawPayload: []byte{
@@ -390,9 +562,9 @@ func TestHandleJoin(t *testing.T) {
 			nil,
 		},
 		{
-			"1.0",
+			"1.0 new device",
 			&ttnpb.EndDevice{
-				NextDevNonce:  42,
+				UsedDevNonces: []uint32{23, 41, 42, 52},
 				NextJoinNonce: 0,
 				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
 					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
@@ -406,8 +578,9 @@ func TestHandleJoin(t *testing.T) {
 				},
 				LoRaWANVersion: ttnpb.MAC_V1_0,
 			},
-			42,
+			0,
 			1,
+			[]uint32{23, 41, 42, 52, 0},
 			&ttnpb.JoinRequest{
 				SelectedMacVersion: ttnpb.MAC_V1_0,
 				RawPayload: []byte{
@@ -479,6 +652,150 @@ func TestHandleJoin(t *testing.T) {
 			},
 			nil,
 		},
+		{
+			"1.0 existing device",
+			&ttnpb.EndDevice{
+				UsedDevNonces: []uint32{23, 41, 42, 52},
+				NextJoinNonce: 0x424242,
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+					JoinEUI: &types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+				},
+				RootKeys: &ttnpb.RootKeys{
+					AppKey: &ttnpb.KeyEnvelope{
+						Key:      &appKey,
+						KekLabel: "",
+					},
+				},
+				LoRaWANVersion: ttnpb.MAC_V1_0,
+			},
+			0,
+			0x424243,
+			[]uint32{23, 41, 42, 52, 0x2442},
+			&ttnpb.JoinRequest{
+				SelectedMacVersion: ttnpb.MAC_V1_0,
+				RawPayload: []byte{
+					/* MHDR */
+					0x00,
+
+					/* MACPayload */
+					/** JoinEUI **/
+					0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevEUI **/
+					0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevNonce **/
+					0x42, 0x24,
+
+					/* MIC */
+					0x8, 0xc4, 0x4, 0x4a,
+				},
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+					DevAddr: &types.DevAddr{0x42, 0xff, 0xff, 0xff},
+				},
+				NetID: types.NetID{0x42, 0xff, 0xff},
+				DownlinkSettings: ttnpb.DLSettings{
+					OptNeg:      true,
+					Rx1DROffset: 0x7,
+					Rx2DR:       0xf,
+				},
+				RxDelay: 0x42,
+				CFList:  nil,
+			},
+			&ttnpb.JoinResponse{
+				RawPayload: append([]byte{
+					/* MHDR */
+					0x20},
+					mustEncryptJoinAccept(appKey, []byte{
+						/* JoinNonce */
+						0x42, 0x42, 0x42,
+						/* NetID */
+						0x42, 0xff, 0xff,
+						/* DevAddr */
+						0x42, 0xff, 0xff, 0xff,
+						/* DLSettings */
+						0xff,
+						/* RxDelay */
+						0x42,
+
+						/* MIC */
+						0x54, 0x90, 0x4, 0xb6,
+					})...),
+				SessionKeys: ttnpb.SessionKeys{
+					FNwkSIntKey: &ttnpb.KeyEnvelope{
+						Key: KeyPointer(crypto.DeriveLegacyNwkSKey(
+							appKey,
+							types.JoinNonce{0x42, 0x42, 0x42},
+							types.NetID{0x42, 0xff, 0xff},
+							types.DevNonce{0x42, 0x24})),
+						KekLabel: "",
+					},
+					AppSKey: &ttnpb.KeyEnvelope{
+						Key: KeyPointer(crypto.DeriveLegacyAppSKey(
+							appKey,
+							types.JoinNonce{0x42, 0x42, 0x42},
+							types.NetID{0x42, 0xff, 0xff},
+							types.DevNonce{0x42, 0x24})),
+						KekLabel: "",
+					},
+				},
+				Lifetime: nil,
+			},
+			nil,
+		},
+		{
+			"1.0 repeated DevNonce",
+			&ttnpb.EndDevice{
+				UsedDevNonces: []uint32{23, 41, 42, 52, 0x2442},
+				NextJoinNonce: 0x424242,
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+					JoinEUI: &types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+				},
+				RootKeys: &ttnpb.RootKeys{
+					AppKey: &ttnpb.KeyEnvelope{
+						Key:      &appKey,
+						KekLabel: "",
+					},
+				},
+				LoRaWANVersion: ttnpb.MAC_V1_0,
+			},
+			0,
+			0x424242,
+			[]uint32{23, 41, 42, 52, 0x2442},
+			&ttnpb.JoinRequest{
+				SelectedMacVersion: ttnpb.MAC_V1_0,
+				RawPayload: []byte{
+					/* MHDR */
+					0x00,
+
+					/* MACPayload */
+					/** JoinEUI **/
+					0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevEUI **/
+					0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+					/** DevNonce **/
+					0x42, 0x24,
+
+					/* MIC */
+					0x8, 0xc4, 0x4, 0x4a,
+				},
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DevEUI:  &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+					DevAddr: &types.DevAddr{0x42, 0xff, 0xff, 0xff},
+				},
+				NetID: types.NetID{0x42, 0xff, 0xff},
+				DownlinkSettings: ttnpb.DLSettings{
+					OptNeg:      true,
+					Rx1DROffset: 0x7,
+					Rx2DR:       0xf,
+				},
+				RxDelay: 0x42,
+				CFList:  nil,
+			},
+			nil,
+			ErrDevNonceReused.New(nil),
+		},
 	} {
 		t.Run(tc.Name, func(t *testing.T) {
 			a := assertions.New(t)
@@ -498,28 +815,36 @@ func TestHandleJoin(t *testing.T) {
 			dev, err := reg.FindDeviceByIdentifiers(&tc.Device.EndDeviceIdentifiers)
 			a.So(err, should.BeNil)
 			if a.So(dev, should.NotBeNil) && a.So(dev, should.HaveLength, 1) {
-				a.So(dev[0].EndDevice, should.Resemble, tc.Device)
+				a.So(pretty.Diff(dev[0].EndDevice, tc.Device), should.BeEmpty)
 			}
 
 			resp, err := js.HandleJoin(context.Background(), tc.JoinRequest)
 			if tc.Error != nil {
-				a.So(err, should.Resemble, tc.Error)
+				a.So(errors.From(err).Attributes(), should.Resemble, errors.From(tc.Error).Attributes())
+				a.So(errors.From(err).Code(), should.Resemble, errors.From(tc.Error).Code())
 				a.So(resp, should.BeNil)
-			} else {
-				a.So(err, should.BeNil)
-				if a.So(resp, should.NotBeNil) {
-					a.So(pretty.Diff(resp, tc.JoinResponse), should.BeEmpty)
-				}
+				return
 			}
 
-			// ensure the stored device nonce(s) are updated
+			a.So(err, should.BeNil)
+			if !a.So(resp, should.Resemble, tc.JoinResponse) {
+				pretty.Ldiff(t, resp, tc.JoinResponse)
+				return
+			}
+
+			// ensure the stored device nonces are updated
 			time.Sleep(time.Millisecond)
+
+			resp, err = js.HandleJoin(context.Background(), tc.JoinRequest)
+			a.So(err, should.BeError)
+			a.So(resp, should.BeNil)
 
 			dev, err = reg.FindDeviceByIdentifiers(&tc.Device.EndDeviceIdentifiers)
 			a.So(err, should.BeNil)
 			if a.So(dev, should.NotBeNil) && a.So(dev, should.HaveLength, 1) {
 				a.So(dev[0].GetNextDevNonce(), should.Equal, tc.NextNextDevNonce)
 				a.So(dev[0].GetNextJoinNonce(), should.Equal, tc.NextNextJoinNonce)
+				a.So(pretty.Diff(dev[0].GetUsedDevNonces(), tc.NextUsedDevNonces), should.BeEmpty)
 			}
 		})
 	}
