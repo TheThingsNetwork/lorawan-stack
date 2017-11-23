4323d846f18b61329d1b24e1274fa4e7e5262f02
Roman Volosatovs <rvolosatovs@riseup.net>
2017-11-23 20:26:22 +0100

    util: Add device registry tests

--- pkg/deviceregistry/registry_grpc_test.go
+++ pkg/deviceregistry/registry_grpc_test.go
@@ -3,110 +3,287 @@
 package deviceregistry_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/TheThingsNetwork/ttn/cmd/shared"
 	"github.com/TheThingsNetwork/ttn/pkg/component"
 	. "github.com/TheThingsNetwork/ttn/pkg/deviceregistry"
 	"github.com/TheThingsNetwork/ttn/pkg/errors"
-	"github.com/TheThingsNetwork/ttn/pkg/log"
+	"github.com/TheThingsNetwork/ttn/pkg/store"
+	"github.com/TheThingsNetwork/ttn/pkg/store/mapstore"
 	"github.com/TheThingsNetwork/ttn/pkg/ttnpb"
+	"github.com/TheThingsNetwork/ttn/pkg/util/test"
+	"github.com/kr/pretty"
 	"github.com/smartystreets/assertions"
 	"github.com/smartystreets/assertions/should"
 )
 
-var logger log.Stack
+func TestRegistryRPC(t *testing.T) {
+	a := assertions.New(t)
+	dr := NewRPC(component.New(test.GetLogger(t), &component.Config{shared.DefaultServiceBase}), NewRegistry(store.NewTypedStoreClient(mapstore.New())))
 
-func init() {
-	var err error
-	logger, err = log.NewLogger(log.WithLevel(log.DebugLevel))
-	if err != nil {
-		panic(err)
+	dev := ttnpb.NewPopulatedEndDevice(test.Randy, false)
+
+	v, err := dr.SetDevice(context.Background(), dev)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+	a.So(v, should.NotBeNil)
+
+	devs, err := dr.ListDevices(context.Background(), &dev.EndDeviceIdentifiers)
+	if !a.So(err, should.BeNil) ||
+		!a.So(devs.EndDevices, should.HaveLength, 1) ||
+		!a.So(devs.EndDevices[0], should.Resemble, dev) {
+		return
+	}
+
+	v, err = dr.DeleteDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+	a.So(v, should.NotBeNil)
+
+	devs, err = dr.ListDevices(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.NotBeNil)
+	a.So(devs, should.BeNil)
+}
+
+func TestSetDeviceNoCheck(t *testing.T) {
+	a := assertions.New(t)
+	dr := NewRPC(component.New(test.GetLogger(t), &component.Config{shared.DefaultServiceBase}), NewRegistry(store.NewTypedStoreClient(mapstore.New())))
+
+	dev := ttnpb.NewPopulatedEndDevice(test.Randy, false)
+
+	v, err := dr.SetDevice(context.Background(), dev)
+	a.So(err, should.BeNil)
+	a.So(v, should.NotBeNil)
+
+	v, err = dr.SetDevice(context.Background(), dev)
+	a.So(err, should.BeNil)
+	a.So(v, should.NotBeNil)
+
+	_, err = dr.Interface.Create(dev)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	v, err = dr.SetDevice(context.Background(), dev)
+	a.So(err, should.NotBeNil)
+	a.So(v, should.BeNil)
+}
+
+func TestListDevicesNoCheck(t *testing.T) {
+	a := assertions.New(t)
+	dr := NewRPC(component.New(test.GetLogger(t), &component.Config{shared.DefaultServiceBase}), NewRegistry(store.NewTypedStoreClient(mapstore.New())))
+
+	devs, err := dr.ListDevices(context.Background(), nil)
+	a.So(err, should.NotBeNil)
+	a.So(devs, should.BeNil)
+
+	dev1, err := dr.Interface.Create(ttnpb.NewPopulatedEndDevice(test.Randy, false))
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	dev2, err := dr.Interface.Create(ttnpb.NewPopulatedEndDevice(test.Randy, false))
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	devs, err = dr.ListDevices(context.Background(), &dev1.EndDeviceIdentifiers)
+	if !a.So(err, should.BeNil) ||
+		!a.So(devs.EndDevices, should.HaveLength, 1) ||
+		!a.So(devs.EndDevices[0], should.Resemble, dev1.EndDevice) {
+		return
+	}
+
+	devs, err = dr.ListDevices(context.Background(), &dev2.EndDeviceIdentifiers)
+	if !a.So(err, should.BeNil) ||
+		!a.So(devs.EndDevices, should.HaveLength, 1) ||
+		!a.So(devs.EndDevices[0], should.Resemble, dev2.EndDevice) {
+		return
+	}
+
+	devs, err = dr.ListDevices(context.Background(), &ttnpb.EndDeviceIdentifiers{})
+	if !a.So(err, should.BeNil) ||
+		!a.So(devs.EndDevices, should.HaveLength, 2) {
+		return
 	}
 }
 
-type findDeviceByIdentifiersOut struct {
-	devs []*Device
-	err  error
+func TestGetDeviceNoCheck(t *testing.T) {
+	a := assertions.New(t)
+	dr := NewRPC(component.New(test.GetLogger(t), &component.Config{shared.DefaultServiceBase}), NewRegistry(store.NewTypedStoreClient(mapstore.New())))
+
+	dev := ttnpb.NewPopulatedEndDevice(test.Randy, false)
+
+	v, err := dr.GetDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.NotBeNil)
+	a.So(v, should.BeNil)
+
+	_, err = dr.Interface.Create(dev)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	v, err = dr.GetDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.BeNil)
+	a.So(v, should.NotBeNil)
+
+	_, err = dr.Interface.Create(dev)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	v, err = dr.GetDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.NotBeNil)
+	a.So(v, should.BeNil)
 }
 
-type mockRegistry struct {
-	Interface
-	findDeviceByIdentifiersOut findDeviceByIdentifiersOut
+func TestDeleteDeviceNoCheck(t *testing.T) {
+	a := assertions.New(t)
+	dr := NewRPC(component.New(test.GetLogger(t), &component.Config{shared.DefaultServiceBase}), NewRegistry(store.NewTypedStoreClient(mapstore.New())))
+
+	dev := ttnpb.NewPopulatedEndDevice(test.Randy, false)
+
+	v, err := dr.DeleteDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.NotBeNil)
+	a.So(v, should.BeNil)
+
+	_, err = dr.Interface.Create(dev)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	v, err = dr.DeleteDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.BeNil)
+	a.So(v, should.NotBeNil)
+
+	_, err = dr.Interface.Create(dev)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	_, err = dr.Interface.Create(dev)
+	if !a.So(err, should.BeNil) {
+		return
+	}
+
+	v, err = dr.DeleteDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.NotBeNil)
+	a.So(v, should.BeNil)
 }
 
-func (r *mockRegistry) FindDeviceByIdentifiers(ids ...*ttnpb.EndDeviceIdentifiers) ([]*Device, error) {
-	out := r.findDeviceByIdentifiersOut
-	return out.devs, out.err
-}
-
-func TestListDevices(t *testing.T) {
+func TestCheck(t *testing.T) {
 	a := assertions.New(t)
 
-	reg := &mockRegistry{}
-	dr := NewRPC(component.New(logger, &component.Config{shared.DefaultServiceBase}), reg)
-	for _, tc := range []struct {
-		findDeviceByIdentifiersOut findDeviceByIdentifiersOut
-		devs                       *ttnpb.EndDevices
-		err                        error
-	}{
-		{
-			findDeviceByIdentifiersOut{[]*Device{{EndDevice: &ttnpb.EndDevice{}}}, nil},
-			&ttnpb.EndDevices{[]*ttnpb.EndDevice{&ttnpb.EndDevice{}}},
-			nil,
-		},
-		{
-			findDeviceByIdentifiersOut{},
-			nil,
-			ErrDeviceNotFound.New(nil),
-		},
-		{
-			findDeviceByIdentifiersOut{nil, errors.New("test")},
-			nil,
-			errors.New("test"),
-		},
-	} {
-		reg.findDeviceByIdentifiersOut = tc.findDeviceByIdentifiersOut
-		devs, err := dr.ListDevices(nil, nil)
-		a.So(err, should.Resemble, tc.err)
-		a.So(devs, should.Resemble, tc.devs)
+	errTest := &errors.ErrDescriptor{
+		MessageFormat: "Test",
+		Type:          errors.Internal,
+		Code:          1,
 	}
-}
+	errTest.Register()
 
-func TestGetDevice(t *testing.T) {
-	a := assertions.New(t)
+	var checkErr error
 
-	reg := &mockRegistry{}
-	dr := NewRPC(nil, reg)
-	for _, tc := range []struct {
-		findDeviceByIdentifiersOut findDeviceByIdentifiersOut
-		dev                        *ttnpb.EndDevice
-		err                        error
-	}{
-		{
-			findDeviceByIdentifiersOut{[]*Device{{EndDevice: &ttnpb.EndDevice{}}}, nil},
-			&ttnpb.EndDevice{},
-			nil,
-		},
-		{
-			findDeviceByIdentifiersOut{},
-			nil,
-			ErrDeviceNotFound.New(nil),
-		},
-		{
-			findDeviceByIdentifiersOut{nil, errors.New("test")},
-			nil,
-			errors.New("test"),
-		},
-		{
-			findDeviceByIdentifiersOut{[]*Device{{EndDevice: &ttnpb.EndDevice{}}, {EndDevice: &ttnpb.EndDevice{}}}, nil},
-			nil,
-			ErrTooManyDevices.New(nil),
-		},
-	} {
-		reg.findDeviceByIdentifiersOut = tc.findDeviceByIdentifiersOut
-		dev, err := dr.GetDevice(nil, nil)
-		a.So(err, should.Resemble, tc.err)
-		a.So(dev, should.Resemble, tc.dev)
+	listCheck := func(context.Context, *ttnpb.EndDeviceIdentifiers) error {
+		return checkErr
 	}
+	getCheck := func(context.Context, *ttnpb.EndDeviceIdentifiers) error {
+		return checkErr
+	}
+	deleteCheck := func(context.Context, *ttnpb.EndDeviceIdentifiers) error {
+		return checkErr
+	}
+	setCheck := func(context.Context, *ttnpb.EndDevice) error {
+		return checkErr
+	}
+
+	dr := NewRPC(component.New(test.GetLogger(t), &component.Config{shared.DefaultServiceBase}), NewRegistry(store.NewTypedStoreClient(mapstore.New())),
+		WithListDevicesCheck(listCheck),
+		WithGetDeviceCheck(getCheck),
+		WithSetDeviceCheck(setCheck),
+		WithDeleteDeviceCheck(deleteCheck),
+	)
+
+	dev := ttnpb.NewPopulatedEndDevice(test.Randy, false)
+
+	// Set
+
+	checkErr = errors.New("err")
+	v, err := dr.SetDevice(context.Background(), dev)
+	a.So(errors.From(err).Code(), should.Equal, ErrCheckFailed.Code)
+	a.So(errors.From(err).Type(), should.Equal, ErrCheckFailed.Type)
+	a.So(v, should.BeNil)
+
+	checkErr = errTest.New(nil)
+	v, err = dr.SetDevice(context.Background(), dev)
+	a.So(err, should.Equal, checkErr)
+	a.So(v, should.BeNil)
+
+	checkErr = nil
+	v, err = dr.SetDevice(context.Background(), dev)
+	a.So(err, should.BeNil)
+	a.So(v, should.NotBeNil)
+
+	// Get
+
+	checkErr = errors.New("err")
+	ret, err := dr.GetDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(errors.From(err).Code(), should.Equal, ErrCheckFailed.Code)
+	a.So(errors.From(err).Type(), should.Equal, ErrCheckFailed.Type)
+	a.So(ret, should.BeNil)
+
+	checkErr = errTest.New(nil)
+	ret, err = dr.GetDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.Equal, checkErr)
+	a.So(ret, should.BeNil)
+
+	checkErr = nil
+	ret, err = dr.GetDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.BeNil)
+	if !a.So(ret, should.Resemble, dev) {
+		pretty.Ldiff(t, ret, dev)
+	}
+
+	// List
+
+	checkErr = errors.New("err")
+	devs, err := dr.ListDevices(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(errors.From(err).Code(), should.Equal, ErrCheckFailed.Code)
+	a.So(errors.From(err).Type(), should.Equal, ErrCheckFailed.Type)
+	a.So(devs, should.BeNil)
+
+	checkErr = errTest.New(nil)
+	devs, err = dr.ListDevices(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.Equal, checkErr)
+	a.So(devs, should.BeNil)
+
+	checkErr = nil
+	devs, err = dr.ListDevices(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.BeNil)
+	if a.So(devs, should.NotBeNil) && a.So(devs.EndDevices, should.HaveLength, 1) {
+		if !a.So(devs.EndDevices[0], should.Resemble, dev) {
+			pretty.Ldiff(t, devs.EndDevices[0], dev)
+		}
+	}
+
+	// Delete
+
+	checkErr = errors.New("err")
+	v, err = dr.DeleteDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(errors.From(err).Code(), should.Equal, ErrCheckFailed.Code)
+	a.So(errors.From(err).Type(), should.Equal, ErrCheckFailed.Type)
+	a.So(v, should.BeNil)
+
+	checkErr = errTest.New(nil)
+	v, err = dr.DeleteDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.Equal, checkErr)
+	a.So(v, should.BeNil)
+
+	checkErr = nil
+	v, err = dr.DeleteDevice(context.Background(), &dev.EndDeviceIdentifiers)
+	a.So(err, should.BeNil)
+	a.So(v, should.NotBeNil)
 }
