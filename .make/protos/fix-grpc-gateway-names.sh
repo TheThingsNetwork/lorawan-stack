b1eb127e7502be2d365caa7e796bbd9926c7b231
Roman Volosatovs <rvolosatovs@riseup.net>
2017-11-28 01:14:07 +0100

    make: Fix perl regexp in grpc-gateway fix script

--- .make/protos/fix-grpc-gateway-names.sh
+++ .make/protos/fix-grpc-gateway-names.sh
@@ -21,7 +21,8 @@ for f in ${protos[@]}; do
   if grep -q '(google.api.http)' ${f}; then
     path=${f%".proto"}".pb.gw.go"
     if grep -q 'option go_package' ${f}; then
-      goPackage=`grep 'option go_package' ${f} | perl -pe 's/[[:space:]]*option[[:space:]]\+go_package[[:space:]]\+=[[:space:]]*"\([[:alnum:]./]\+\)".*/\1/'`
+      goPackage=`grep 'option go_package' ${f} | perl \
+        -pe 's![[:space:]]*option[[:space:]]+go_package[[:space:]]*=[[:space:]]*"([[:alnum:]./]+)".*!\1!'`
       newPath=${GOPATH:-"${HOME}/go"}"/src/"${goPackage}/`basename ${path}`
       mv ${path} ${newPath}
       path=${newPath}
@@ -31,9 +32,12 @@ for f in ${protos[@]}; do
 
   if grep -q '(gogoproto.customname)' ${f}; then
     for l in `grep '(gogoproto.customname)' ${f}`; do
-      from=`echo $l | perl -pe 's/[[:space:]]*\(repeated[[:space:]]\+\)\?[[:alnum:]_.]\+[[:space:]]\+\([[:alnum:]_]\+\)[[:space:]]*=[[:space:]]*[0-9]\+.*/\2/' | perl -pe 's/_\([a-z]\)/\u\1/g' | tr -d ' ' | perl -pe 's/\(^[:a-z:]\)\(.*\)/\u\1\2/' | tr -d '_' `
-      to=`echo $l | perl -pe 's/.*(gogoproto.customname)[[:space:]]*=[[:space:]]*"\([[:alnum:]]\+\)".*/\1/' | tr -d ' '`
-      perlArgs+=("-e s/\([^[:alnum:]]\)${from}\([^[:alnum:]]\)/\1${to}\2/")
+      from=`echo $l | perl \
+        -pe 's![[:space:]]*(repeated[[:space:]]+)?[[:alnum:]_.]+[[:space:]]+([[:alnum:]_]+)[[:space:]]*=[[:space:]]*[0-9]+.*!\2!;' \
+        -pe 's!(^[[:alnum:]])([[:alnum:]]*)|_([[:alnum:]])([[:alnum:]]*)!\U\1\3\E\2\4!g;'`
+      to=`echo $l | perl \
+        -pe 's!.*\(gogoproto.customname\)[[:space:]]*=[[:space:]]*"([[:alnum:]]+)".*!\1!'`
+      ! [ "${from}" = "${to}" ] && perlArgs+=("-pe s!([^[:alnum:]])${from}([^[:alnum:]])!\1${to}\2!g;")
     done
   fi
 done
