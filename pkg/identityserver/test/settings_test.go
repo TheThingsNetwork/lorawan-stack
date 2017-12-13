3e2eeabfb8a20b0a97c1682f19dc9bb23db66a09
Daniel Gómez Jurado <danielgomjur@gmail.com>
2017-12-13 11:25:48 +0100

    is: Add test helpers for settings

---
+++ pkg/identityserver/test/settings_test.go
@@ -0,0 +1,45 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/TheThingsNetwork/ttn/pkg/ttnpb"
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+)
+
+var now = time.Now()
+
+func testSettings() *ttnpb.IdentityServerSettings {
+	return &ttnpb.IdentityServerSettings{
+		BlacklistedIDs:     []string{"admin"},
+		AllowedEmails:      []string{},
+		UpdatedAt:          now,
+		ValidationTokenTTL: time.Duration(time.Hour),
+	}
+}
+
+func TestShouldBeSettings(t *testing.T) {
+	a := assertions.New(t)
+
+	a.So(ShouldBeSettings(testSettings(), testSettings()), should.Equal, success)
+
+	modified := testSettings()
+	modified.ValidationTokenTTL = time.Duration(time.Hour * 2)
+
+	a.So(ShouldBeSettings(modified, testSettings()), should.NotEqual, success)
+}
+
+func TestShouldBeSettingsIgnoringAutoFields(t *testing.T) {
+	a := assertions.New(t)
+
+	a.So(ShouldBeSettingsIgnoringAutoFields(testSettings(), testSettings()), should.Equal, success)
+
+	modified := testSettings()
+	modified.AllowedEmails = nil
+
+	a.So(ShouldBeSettingsIgnoringAutoFields(modified, testSettings()), should.NotEqual, success)
+}
