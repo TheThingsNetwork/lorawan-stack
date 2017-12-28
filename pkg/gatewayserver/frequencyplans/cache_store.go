ba012da665387e9cb6ee04edcb6a28562f49d3ae
Eric Gourlaouen <eric@thethingsnetwork.org>
2017-12-28 13:08:01 +0100

    gs: Add cache feature for frequency plans store

---
+++ pkg/gatewayserver/frequencyplans/cache_store.go
@@ -0,0 +1,62 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package frequencyplans
+
+import (
+	"time"
+
+	"github.com/TheThingsNetwork/ttn/pkg/ttnpb"
+)
+
+const DefaultCacheExpiry = time.Hour
+
+type cacheEntry struct {
+	fp      ttnpb.FrequencyPlan
+	lastHit time.Time
+}
+
+type cache struct {
+	s Store
+
+	fps    map[string]cacheEntry
+	expiry time.Duration
+
+	idsCache   []string
+	idsLastHit time.Time
+}
+
+// Cache wraps the given store with a cache, so that all returned frequency plans are cached for `expiry`.
+func Cache(s Store, expiry time.Duration) Store {
+	return &cache{
+		s:      s,
+		fps:    make(map[string]cacheEntry),
+		expiry: expiry,
+	}
+}
+
+func (c *cache) GetAllIDs() []string {
+	if c.idsLastHit.After(time.Now().Add(-1 * c.expiry)) {
+		return c.idsCache
+	}
+
+	ids := c.s.GetAllIDs()
+	c.idsCache = ids
+	c.idsLastHit = time.Now()
+	return ids
+}
+
+func (c *cache) GetByID(id string) (ttnpb.FrequencyPlan, error) {
+	entry, hit := c.fps[id]
+	if hit && entry.lastHit.After(time.Now().Add(-1*c.expiry)) {
+		return entry.fp, nil
+	}
+
+	fp, err := c.s.GetByID(id)
+	if err == nil {
+		c.fps[id] = cacheEntry{
+			fp:      fp,
+			lastHit: time.Now(),
+		}
+	}
+	return fp, err
+}
---
+++ pkg/gatewayserver/frequencyplans/cache_store_test.go
@@ -0,0 +1,112 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package frequencyplans_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/TheThingsNetwork/ttn/pkg/gatewayserver/frequencyplans"
+	"github.com/TheThingsNetwork/ttn/pkg/ttnpb"
+)
+
+var (
+	validFrequencyPlanID   = "validID"
+	invalidFrequencyPlanID = "invalidID"
+
+	allIDs = []string{validFrequencyPlanID, invalidFrequencyPlanID}
+)
+
+type DummyStore struct {
+	hitIDs chan bool
+	hitFP  chan bool
+}
+
+func (d DummyStore) GetAllIDs() []string {
+	select {
+	case d.hitIDs <- true:
+	default:
+	}
+	return allIDs
+}
+
+func (d DummyStore) GetByID(id string) (ttnpb.FrequencyPlan, error) {
+	if id == validFrequencyPlanID {
+		select {
+		case d.hitFP <- true:
+		default:
+		}
+
+		return ttnpb.FrequencyPlan{}, nil
+	}
+
+	return ttnpb.FrequencyPlan{}, errors.New("Invalid frequency plan ID")
+}
+
+func TestCacheStore(t *testing.T) {
+	dummy := DummyStore{
+		hitIDs: make(chan bool),
+		hitFP:  make(chan bool),
+	}
+	store := frequencyplans.Cache(dummy, frequencyplans.DefaultCacheExpiry)
+
+	stored := make(chan bool)
+
+	var ids []string
+	go func() {
+		ids = store.GetAllIDs()
+		stored <- true
+	}()
+
+	select {
+	case <-dummy.hitIDs:
+		<-stored
+	case <-stored:
+		t.Log("Cache did not hit on the original store to retrieve the list of frequency plan IDs, even though there should not be any stored value yet")
+		t.Fail()
+	}
+
+	go func() {
+		store.GetAllIDs()
+		stored <- true
+	}()
+
+	select {
+	case <-dummy.hitIDs:
+		t.Log("Cache hit on the original store to retrieve the list of frequency plan IDs, whereas it should have used cached value")
+		t.Fail()
+	case <-stored:
+	}
+
+	for _, id := range ids {
+		if id != validFrequencyPlanID && id != invalidFrequencyPlanID {
+			t.Log("Unknown frequency plan ID returned by cache")
+			t.Fail()
+		}
+	}
+
+	go func() {
+		store.GetByID(validFrequencyPlanID)
+		stored <- true
+	}()
+
+	select {
+	case <-dummy.hitFP:
+		<-stored
+	case <-stored:
+		t.Log("Cache did not hit on the original store to retrieve the frequency plan, even though there should not be any stored value yet")
+		t.Fail()
+	}
+
+	go func() {
+		store.GetByID(validFrequencyPlanID)
+		stored <- true
+	}()
+
+	select {
+	case <-dummy.hitFP:
+		t.Log("Cache hit on the original store to retrieve the frequency plan, whereas it should have used cached value")
+		t.Fail()
+	case <-stored:
+	}
+}
