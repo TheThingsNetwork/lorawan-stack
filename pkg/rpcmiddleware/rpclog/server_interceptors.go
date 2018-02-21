a5db46728a60e74784f3518dc248833c659aa493
Hylke Visser <hylke@thethingsnetwork.org>
2018-02-21 16:33:43 +0100

    all: Make gRPC interceptor log to grpc namespace

--- pkg/rpcmiddleware/rpclog/server_interceptors.go
+++ pkg/rpcmiddleware/rpclog/server_interceptors.go
@@ -3,9 +3,9 @@
 package rpclog
 
 import (
+	"context"
 	"time"
 
-	"context"
 	"github.com/TheThingsNetwork/ttn/pkg/log"
 	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
@@ -14,7 +14,7 @@ import (
 // UnaryServerInterceptor returns a new unary server interceptors that adds the logger from the global context to the call context.
 func UnaryServerInterceptor(ctx context.Context, opts ...Option) grpc.UnaryServerInterceptor {
 	o := evaluateServerOpt(opts)
-	logger := log.FromContext(ctx)
+	logger := log.FromContext(ctx).WithField("namespace", "grpc")
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		newCtx := newLoggerForCall(ctx, logger, info.FullMethod)
 		startTime := time.Now()
@@ -36,7 +36,7 @@ func UnaryServerInterceptor(ctx context.Context, opts ...Option) grpc.UnaryServe
 // StreamServerInterceptor returns a new streaming server interceptor that adds the logger from the global context to the call context.
 func StreamServerInterceptor(ctx context.Context, opts ...Option) grpc.StreamServerInterceptor {
 	o := evaluateServerOpt(opts)
-	logger := log.FromContext(ctx)
+	logger := log.FromContext(ctx).WithField("namespace", "grpc")
 	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		newCtx := newLoggerForCall(stream.Context(), logger, info.FullMethod)
 		wrapped := grpc_middleware.WrapServerStream(stream)
