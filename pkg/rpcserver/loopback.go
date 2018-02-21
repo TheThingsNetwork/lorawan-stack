255bff6ed620e2a92a9d52c902dd8025b4624144
Hylke Visser <hylke@thethingsnetwork.org>
2018-02-21 16:04:30 +0100

    all: Remove interceptors from Loopback gRPC conn

--- pkg/rpcserver/loopback.go
+++ pkg/rpcserver/loopback.go
@@ -6,16 +6,16 @@ import (
 	"context"
 	"net"
 
-	"github.com/TheThingsNetwork/ttn/pkg/rpcclient"
 	"google.golang.org/grpc"
 )
 
-// StartLoopback starts the server on a local address and returns a connection to that address
+// StartLoopback starts the server on a local address and returns a connection to that address.
+// This function does not add the default DialOptions.
 func StartLoopback(ctx context.Context, s *grpc.Server, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	lis, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		return nil, err
 	}
 	go s.Serve(lis)
-	return grpc.Dial(lis.Addr().String(), append(append(rpcclient.DefaultDialOptions(ctx), grpc.WithInsecure()), opts...)...)
+	return grpc.Dial(lis.Addr().String(), append(append([]grpc.DialOption{}, grpc.WithInsecure()), opts...)...)
 }
