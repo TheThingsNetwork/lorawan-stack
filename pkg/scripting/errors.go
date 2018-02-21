7ffd3c59b1ea09d71c47acefd458b13d8d39ed16
Johan Stokking <johan@stokking.nl>
2018-02-21 10:32:56 +0100

    all: Add runtime error

--- pkg/scripting/errors.go
+++ pkg/scripting/errors.go
@@ -4,14 +4,23 @@ package scripting
 
 import "github.com/TheThingsNetwork/ttn/pkg/errors"
 
-// ErrRuntime represents the ErrDescriptor of the error returned when there is a runtime
-// error.
+// ErrRuntime represents the ErrDescriptor of the error returned when
+// there is a runtime error.
 var ErrRuntime = &errors.ErrDescriptor{
 	MessageFormat: "Runtime error",
 	Type:          errors.External,
 	Code:          1,
 }
 
+// ErrInvalidOutput represents the ErrDescriptor of the error returned when
+// the output of the script does not have a valid output.
+var ErrInvalidOutput = &errors.ErrDescriptor{
+	MessageFormat: "Invalid script output",
+	Type:          errors.External,
+	Code:          2,
+}
+
 func init() {
 	ErrRuntime.Register()
+	ErrInvalidOutput.Register()
 }
