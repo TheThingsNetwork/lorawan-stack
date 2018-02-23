dee266b376f3c60137b51386d1cdd6b5e19ea33d
Johan Stokking <johan@stokking.nl>
2018-02-23 10:20:35 +0100

    all: Add marshaling for gogo protobuf types

---
+++ pkg/gogoproto/struct.go
@@ -0,0 +1,178 @@
+// Copyright © 2018 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package gogoproto
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/TheThingsNetwork/ttn/pkg/errors"
+	"github.com/gogo/protobuf/types"
+)
+
+// Map returns the Struct proto as a map[string]interface{}.
+func Map(p *types.Struct) (map[string]interface{}, error) {
+	m := make(map[string]interface{}, len(p.Fields))
+	for k, v := range p.Fields {
+		if v == nil {
+			continue
+		}
+		gv, err := Interface(v)
+		if err != nil {
+			return nil, err
+		}
+		m[k] = gv
+	}
+	return m, nil
+}
+
+// Slice returns the ListValue proto as a []interface{}.
+func Slice(l *types.ListValue) ([]interface{}, error) {
+	s := make([]interface{}, len(l.Values))
+	for i, v := range l.Values {
+		gv, err := Interface(v)
+		if err != nil {
+			return nil, err
+		}
+		s[i] = gv
+	}
+	return s, nil
+}
+
+// Interface returns the Value proto as an interface{}.
+func Interface(v *types.Value) (interface{}, error) {
+	switch v := v.GetKind().(type) {
+	case *types.Value_NullValue:
+		return nil, nil
+	case *types.Value_NumberValue:
+		return v.NumberValue, nil
+	case *types.Value_StringValue:
+		return v.StringValue, nil
+	case *types.Value_BoolValue:
+		return v.BoolValue, nil
+	case *types.Value_StructValue:
+		return Map(v.StructValue)
+	case *types.Value_ListValue:
+		return Slice(v.ListValue)
+	default:
+		return nil, errors.Errorf("unmatched types type: %T", v)
+	}
+}
+
+// Struct returns the map as a Struct proto.
+func Struct(m map[string]interface{}) (*types.Struct, error) {
+	p := &types.Struct{
+		Fields: make(map[string]*types.Value),
+	}
+	for k, v := range m {
+		pv, err := Value(v)
+		if err != nil {
+			return nil, err
+		}
+		p.Fields[k] = pv
+	}
+	return p, nil
+}
+
+// List returns the slice as a ListValue proto.
+func List(s []interface{}) (*types.ListValue, error) {
+	l := &types.ListValue{
+		Values: make([]*types.Value, len(s)),
+	}
+	for i, v := range s {
+		pv, err := Value(v)
+		if err != nil {
+			return nil, err
+		}
+		l.Values[i] = pv
+	}
+	return l, nil
+}
+
+func valueFromReflect(rv reflect.Value) (*types.Value, error) {
+	switch k := rv.Kind(); k {
+	case reflect.Ptr:
+		if rv.IsNil() {
+			return &types.Value{Kind: &types.Value_NullValue{}}, nil
+		}
+		return valueFromReflect(rv.Elem())
+	case reflect.String:
+		return &types.Value{Kind: &types.Value_StringValue{rv.String()}}, nil
+
+	case reflect.Bool:
+		return &types.Value{Kind: &types.Value_BoolValue{rv.Bool()}}, nil
+
+	case reflect.Slice, reflect.Array:
+		if k == reflect.Slice && rv.IsNil() {
+			return &types.Value{Kind: &types.Value_NullValue{}}, nil
+		}
+		s := make([]interface{}, rv.Len())
+		for i := 0; i < rv.Len(); i++ {
+			s[i] = rv.Index(i).Interface()
+		}
+		pv, err := List(s)
+		if err != nil {
+			return nil, err
+		}
+		return &types.Value{Kind: &types.Value_ListValue{pv}}, nil
+
+	case reflect.Map:
+		if rv.IsNil() {
+			return &types.Value{Kind: &types.Value_NullValue{}}, nil
+		}
+		m := make(map[string]interface{}, rv.Len())
+		for _, key := range rv.MapKeys() {
+			m[fmt.Sprint(key.Interface())] = rv.MapIndex(key).Interface()
+		}
+		pv, err := Struct(m)
+		if err != nil {
+			return nil, err
+		}
+		return &types.Value{Kind: &types.Value_StructValue{pv}}, nil
+
+	case reflect.Struct:
+		n := rv.NumField()
+		fields := make(map[string]*types.Value, n)
+		for i := 0; i < n; i++ {
+			f := rv.Field(i)
+			ft := f.Type()
+			if f.Type().PkgPath() != "" {
+				continue
+			}
+			pv, err := valueFromReflect(f)
+			if err != nil {
+				return nil, err
+			}
+			fields[ft.Name()] = pv
+		}
+		return &types.Value{Kind: &types.Value_StructValue{&types.Struct{fields}}}, nil
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return &types.Value{Kind: &types.Value_NumberValue{float64(rv.Int())}}, nil
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return &types.Value{Kind: &types.Value_NumberValue{float64(rv.Uint())}}, nil
+
+	case reflect.Float32, reflect.Float64:
+		return &types.Value{Kind: &types.Value_NumberValue{rv.Float()}}, nil
+
+	case reflect.Complex64, reflect.Complex128:
+		return &types.Value{Kind: &types.Value_StringValue{fmt.Sprint(rv.Complex())}}, nil
+
+	default:
+		// either Invalid, Chan. Func, Interface or UnsafePointer.
+		return nil, errors.Errorf("Can not map a value of kind %s to a *types.Value", k)
+	}
+}
+
+// Value returns the value as a Value proto
+func Value(v interface{}) (*types.Value, error) {
+	if v == nil {
+		return &types.Value{Kind: &types.Value_NullValue{}}, nil
+	}
+	pv, err := valueFromReflect(reflect.Indirect(reflect.ValueOf(v)))
+	if err != nil {
+		return nil, err
+	}
+	return pv, nil
+}
---
+++ pkg/gogoproto/struct_test.go
@@ -0,0 +1,116 @@
+// Copyright © 2018 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package gogoproto
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	"github.com/TheThingsNetwork/ttn/pkg/types"
+	pbtypes "github.com/gogo/protobuf/types"
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+	"github.com/spf13/cast"
+)
+
+type jsonMarshaler struct {
+	Text string
+}
+
+func (m jsonMarshaler) MarshalJSON() ([]byte, error) {
+	return bytes.ToUpper([]byte(`"` + m.Text + `"`)), nil
+}
+
+func (m *jsonMarshaler) UnmarshalJSON(b []byte) error {
+	m.Text = string(bytes.ToLower(bytes.Trim(b, `"`)))
+	return nil
+}
+
+func TestStructProto(t *testing.T) {
+	a := assertions.New(t)
+
+	ptr := "ptr"
+	m := map[string]interface{}{
+		"foo":            "bar",
+		"ptr":            &ptr,
+		"answer":         42,
+		"answer.precise": 42.0,
+		"works":          true,
+		"empty":          nil,
+		"list":           []string{"a", "b", "c"},
+		"map":            map[string]string{"foo": "bar"},
+		"eui":            types.EUI64{1, 2, 3, 4, 5, 6, 7, 8},
+		"jsonMarshaler":  &jsonMarshaler{Text: "testtext"},
+	}
+	s, err := Struct(m)
+	a.So(err, should.BeNil)
+	sm, err := Map(s)
+	a.So(err, should.BeNil)
+	for k, v := range m {
+		a.So(s.Fields, should.ContainKey, k)
+		a.So(sm, should.ContainKey, k)
+		if v == nil {
+			a.So(s.Fields[k].Kind, should.HaveSameTypeAs, &pbtypes.Value_NullValue{})
+			a.So(sm[k], should.BeNil)
+			continue
+		}
+
+		rv := reflect.Indirect(reflect.ValueOf(v))
+
+		switch kind := rv.Kind(); kind {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+			reflect.Float32, reflect.Float64:
+
+			var vt float64
+			a.So(s.Fields[k].Kind, should.HaveSameTypeAs, &pbtypes.Value_NumberValue{})
+			a.So(sm[k], should.HaveSameTypeAs, vt)
+			a.So(sm[k], should.Equal, cast.ToFloat64(rv.Interface()))
+
+		case reflect.Bool:
+			var vt bool
+			a.So(s.Fields[k].Kind, should.HaveSameTypeAs, &pbtypes.Value_BoolValue{})
+			a.So(sm[k], should.HaveSameTypeAs, vt)
+			a.So(sm[k], should.Equal, rv.Bool())
+
+		case reflect.String:
+			var vt string
+			a.So(s.Fields[k].Kind, should.HaveSameTypeAs, &pbtypes.Value_StringValue{})
+			a.So(sm[k], should.HaveSameTypeAs, vt)
+			a.So(sm[k], should.Equal, rv.String())
+
+		case reflect.Slice, reflect.Array:
+			var vt []interface{}
+			a.So(s.Fields[k].Kind, should.HaveSameTypeAs, &pbtypes.Value_ListValue{})
+			a.So(sm[k], should.HaveSameTypeAs, vt)
+			if a.So(sm[k], should.HaveLength, rv.Len()) {
+				// TODO find a way to compare these values
+				//smv := reflect.ValueOf(sm[k])
+				//for i := 0; i < rv.Len(); i++ {
+				//a.So(smv.Index(i).Interface(), should.Resemble, rv.Index(i).Interface())
+				//}
+			}
+
+		case reflect.Struct, reflect.Map:
+			var vt map[string]interface{}
+			a.So(s.Fields[k].Kind, should.HaveSameTypeAs, &pbtypes.Value_StructValue{})
+			a.So(sm[k], should.HaveSameTypeAs, vt)
+			if kind == reflect.Map {
+				a.So(sm[k], should.HaveLength, rv.Len())
+			}
+
+		default:
+			panic("Unmatched kind: " + rv.Kind().String())
+		}
+		pv, err := Value(rv.Interface())
+		if a.So(err, should.BeNil) {
+			a.So(s.Fields[k], should.Resemble, pv)
+
+			gv, err := Interface(pv)
+			if a.So(err, should.BeNil) {
+				a.So(sm[k], should.Resemble, gv)
+			}
+		}
+	}
+}
