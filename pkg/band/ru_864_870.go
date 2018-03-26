5a7ca5c40fa0c18e6fd9a27f057e9d6027504e89
Eric Gourlaouen <eric@thethingsnetwork.org>
2018-03-26 14:33:51 +0200

    util: Add russian band

---
+++ pkg/band/ru_864_870.go
@@ -0,0 +1,85 @@
+// Copyright © 2018 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package band
+
+import "github.com/TheThingsNetwork/ttn/pkg/types"
+
+var ru_864_870 Band
+
+const (
+	// RU_864_870 is the ID of the Russian frequency plan
+	RU_864_870 ID = "RU_864_870"
+)
+
+func init() {
+	defaultChannels := []Channel{
+		{Frequency: 868900000, DataRateIndexes: []int{0, 1, 2, 3, 4, 5}},
+		{Frequency: 869100000, DataRateIndexes: []int{0, 1, 2, 3, 4, 5}},
+	}
+	ru_864_870 = Band{
+		ID: RU_864_870,
+
+		UplinkChannels:   defaultChannels,
+		DownlinkChannels: defaultChannels,
+
+		BandDutyCycles: []DutyCycle{
+			{
+				MinFrequency: 864000000,
+				MaxFrequency: 870000000,
+				DutyCycle:    0.01,
+			},
+		},
+
+		DataRates: [16]DataRate{
+			{Rate: types.DataRate{LoRa: "SF12BW125"}, DefaultMaxSize: maxPayloadSize{59, 51}, NoRepeaterMaxSize: maxPayloadSize{59, 51}},
+			{Rate: types.DataRate{LoRa: "SF11BW125"}, DefaultMaxSize: maxPayloadSize{59, 51}, NoRepeaterMaxSize: maxPayloadSize{59, 51}},
+			{Rate: types.DataRate{LoRa: "SF10BW125"}, DefaultMaxSize: maxPayloadSize{59, 51}, NoRepeaterMaxSize: maxPayloadSize{59, 51}},
+			{Rate: types.DataRate{LoRa: "SF9BW125"}, DefaultMaxSize: maxPayloadSize{123, 115}, NoRepeaterMaxSize: maxPayloadSize{123, 115}},
+			{Rate: types.DataRate{LoRa: "SF8BW125"}, DefaultMaxSize: maxPayloadSize{230, 222}, NoRepeaterMaxSize: maxPayloadSize{250, 242}},
+			{Rate: types.DataRate{LoRa: "SF7BW125"}, DefaultMaxSize: maxPayloadSize{230, 222}, NoRepeaterMaxSize: maxPayloadSize{250, 242}},
+			{Rate: types.DataRate{LoRa: "SF7BW250"}, DefaultMaxSize: maxPayloadSize{230, 222}, NoRepeaterMaxSize: maxPayloadSize{250, 242}},
+			{Rate: types.DataRate{FSK: 50000}, DefaultMaxSize: maxPayloadSize{}},
+			{}, {}, {}, {}, {}, {}, {},
+			{}, // Used by LinkADRReq starting from LoRaWAN 1.1, RFU before
+		},
+
+		ReceiveDelay1:    defaultReceiveDelay1,
+		ReceiveDelay2:    defaultReceiveDelay2,
+		JoinAcceptDelay1: defaultJoinAcceptDelay2,
+		JoinAcceptDelay2: defaultJoinAcceptDelay2,
+		MaxFCntGap:       defaultMaxFCntGap,
+		AdrAckLimit:      defaultAdrAckLimit,
+		AdrAckDelay:      defaultAdrAckDelay,
+		MinAckTimeout:    defaultAckTimeout - defaultAckTimeoutMargin,
+		MaxAckTimeout:    defaultAckTimeout + defaultAckTimeoutMargin,
+
+		DefaultMaxEIRP: 14,
+		TxOffset: [16]float32{0, -2, -4, -6, -8, -10, -12, -14,
+			0, 0, 0, 0, 0, 0, 0, // RFU
+			0, // Used by LinkADRReq starting from LoRaWAN 1.1, RFU before
+		},
+
+		Rx1Parameters: func(frequency uint64, dataRateIndex, rx1DROffset int, _ bool) (int, uint64) {
+			outDataRateIndex := dataRateIndex - rx1DROffset
+			if outDataRateIndex < 0 {
+				outDataRateIndex = 0
+			}
+			return outDataRateIndex, frequency
+		},
+
+		DefaultRx2Parameters: Rx2Parameters{0, 869100000},
+
+		Beacon: Beacon{
+			DataRateIndex:    3,
+			CodingRate:       "4/5",
+			PingSlotChannels: []uint32{868900000},
+			BroadcastChannel: func(_ float64) uint32 { return 869100000 },
+		},
+
+		// No LoRaWAN 1.0
+		// No LoRaWAN 1.0.1
+		// No LoRaWAN 1.0.2
+		// No LoRaWAN 1.1A
+	}
+	All = append(All, ru_864_870)
+}
