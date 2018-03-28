2de3a5bc0f8fadd73a55d51ad180d9661175b5c9
Hylke Visser <hylke@thethingsnetwork.org>
2018-03-28 14:50:25 +0200

    util: Add test cases for IsZero

--- pkg/validate/required_test.go
+++ pkg/validate/required_test.go
@@ -3,98 +3,137 @@
 package validate
 
 import (
+	"fmt"
 	"reflect"
 	"strconv"
 	"testing"
 	"time"
 	"unsafe"
 
+	"github.com/TheThingsNetwork/ttn/pkg/types"
 	"github.com/smartystreets/assertions"
 	"github.com/smartystreets/assertions/should"
 )
+
+type isZeroer struct {
+	isZero bool
+}
+
+var izCalled int
+
+func (iz isZeroer) IsZero() bool {
+	izCalled++
+	return iz.isZero
+}
+
+type stringType string
+type boolType bool
+type int64Type int64
+type uint64Type uint64
+type float64Type float64
+type structType struct {
+	field bool
+}
 
 var isZeroCases = []struct {
 	v      interface{}
 	isZero bool
 }{
-	{
-		(*time.Time)(nil),
-		true,
-	},
-	{
-		time.Time{},
-		true,
-	},
-	{
-		&time.Time{},
-		true,
-	},
-	{
-		[]int{},
-		true,
-	},
-	{
-		"",
-		true,
-	},
-	{
-		"42",
-		false,
-	},
-	{
-		[]int{0},
-		false,
-	},
-	{
-		[]interface{}{nil, nil, nil},
-		false,
-	},
-	{
-		map[string]interface{}{
-			"empty": struct{}{},
-			"map":   nil,
-		},
-		false,
-	},
-	{
-		map[string]interface{}{
-			"nonempty": struct{ A int }{42},
-			"map":      nil,
-		},
-		false,
-	},
-	{
-		([]int)(nil),
-		true,
-	},
-	{
-		nil,
-		true,
-	},
-	{
-		(interface{})(nil),
-		true,
-	},
-	{
-		struct{ a int }{42},
-		false,
-	},
-	{
-		unsafe.Pointer(nil),
-		true,
-	},
-	{
-		unsafe.Pointer(&([]byte{42})[0]),
-		false,
-	},
+	{nil, true},
+	{(interface{})(nil), true},
+
+	{false, true},
+	{int(0), true},
+	{int64(0), true},
+	{int32(0), true},
+	{int16(0), true},
+	{int8(0), true},
+	{uint(0), true},
+	{uint64(0), true},
+	{uint32(0), true},
+	{uint16(0), true},
+	{uint8(0), true},
+	{float64(0), true},
+	{float32(0), true},
+	{float32(0), true},
+	{"", true},
+
+	{[]bool{}, true},
+	{[]int{}, true},
+	{[]int64{}, true},
+	{[]int32{}, true},
+	{[]int16{}, true},
+	{[]int8{}, true},
+	{[]uint{}, true},
+	{[]uint64{}, true},
+	{[]uint32{}, true},
+	{[]uint16{}, true},
+	{[]uint8{}, true},
+	{[]float64{}, true},
+	{[]float32{}, true},
+	{[]float32{}, true},
+	{[]string{}, true},
+
+	{(*time.Time)(nil), true},
+	{time.Time{}, true},
+	{&time.Time{}, true},
+
+	{map[string]interface{}{}, true},
+	{map[string]interface{}{"foo": "bar"}, false},
+
+	{map[string]string{}, true},
+	{map[string]string{"foo": "bar"}, false},
+
+	{types.AES128Key{}, true},
+	{types.EUI64{}, true},
+	{types.NetID{}, true},
+	{types.DevAddr{}, true},
+	{types.DevNonce{}, true},
+	{types.JoinNonce{}, true},
+
+	{(*types.AES128Key)(nil), true},
+	{(*types.EUI64)(nil), true},
+	{(*types.NetID)(nil), true},
+	{(*types.DevAddr)(nil), true},
+	{(*types.DevNonce)(nil), true},
+	{(*types.JoinNonce)(nil), true},
+
+	{isZeroer{isZero: false}, false},
+	{isZeroer{isZero: true}, true},
+
+	{(*isZeroer)(nil), true},
+	{&isZeroer{isZero: false}, false},
+	{&isZeroer{isZero: true}, true},
+
+	{stringType(""), true},
+	{stringType("foo"), false},
+
+	{boolType(false), true},
+	{boolType(true), false},
+
+	{int64Type(0), true},
+	{int64Type(42), false},
+
+	{uint64Type(0), true},
+	{uint64Type(42), false},
+
+	{float64Type(0), true},
+	{float64Type(42), false},
+
+	{structType{true}, false},
+	{structType{false}, true},
+
+	{unsafe.Pointer(nil), true},
+	{unsafe.Pointer(&([]byte{42})[0]), false},
 }
 
 func TestIsZero(t *testing.T) {
 	for i, tc := range isZeroCases {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d %T(%v)", i, tc.v, tc.v), func(t *testing.T) {
 			assertions.New(t).So(isZero(tc.v), should.Equal, tc.isZero)
 		})
 	}
+	assertions.New(t).So(izCalled, should.Equal, 4)
 }
 
 func TestRequired(t *testing.T) {
