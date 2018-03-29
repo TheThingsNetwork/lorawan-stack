f088571fbcf7339dd2714aafa3c71a6b033ffd02
Hylke Visser <hylke@thethingsnetwork.org>
2018-03-29 15:21:15 +0200

    util: Upgrade gRPC logger to v2

--- pkg/rpcmiddleware/rpclog/rpclog.go
+++ pkg/rpcmiddleware/rpclog/rpclog.go
@@ -13,34 +13,23 @@ import (
 // ReplaceGrpcLogger sets the given log.Interface as a gRPC-level logger.
 // This should be called *before* any other initialization, preferably from init() functions.
 func ReplaceGrpcLogger(logger log.Interface) {
-	zgl := &ttnGrpcLogger{logger}
-	grpclog.SetLogger(zgl)
+	grpclog.SetLoggerV2(&ttnGrpcLogger{logger})
 }
 
 type ttnGrpcLogger struct {
 	logger log.Interface
 }
 
-func (l *ttnGrpcLogger) Fatal(args ...interface{}) {
-	l.logger.Fatal(fmt.Sprint(args...))
-}
-
-func (l *ttnGrpcLogger) Fatalf(format string, args ...interface{}) {
-	l.logger.Fatal(fmt.Sprintf(format, args...))
-}
-
-func (l *ttnGrpcLogger) Fatalln(args ...interface{}) {
-	l.logger.Fatal(fmt.Sprint(args...))
-}
-
-func (l *ttnGrpcLogger) Print(args ...interface{}) {
-	l.logger.Debug(fmt.Sprint(args...))
-}
-
-func (l *ttnGrpcLogger) Printf(format string, args ...interface{}) {
-	l.logger.Debug(fmt.Sprintf(format, args...))
-}
-
-func (l *ttnGrpcLogger) Println(args ...interface{}) {
-	l.logger.Debug(fmt.Sprint(args...))
-}
+func (l *ttnGrpcLogger) Info(args ...interface{})                    { l.logger.Debug(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Infoln(args ...interface{})                  { l.logger.Debug(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Infof(format string, args ...interface{})    { l.logger.Debugf(format, args...) }
+func (l *ttnGrpcLogger) Warning(args ...interface{})                 { l.logger.Warn(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Warningln(args ...interface{})               { l.logger.Warn(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Warningf(format string, args ...interface{}) { l.logger.Warnf(format, args...) }
+func (l *ttnGrpcLogger) Error(args ...interface{})                   { l.logger.Error(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Errorln(args ...interface{})                 { l.logger.Error(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Errorf(format string, args ...interface{})   { l.logger.Errorf(format, args...) }
+func (l *ttnGrpcLogger) Fatal(args ...interface{})                   { l.logger.Fatal(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Fatalln(args ...interface{})                 { l.logger.Fatal(fmt.Sprint(args...)) }
+func (l *ttnGrpcLogger) Fatalf(format string, args ...interface{})   { l.logger.Fatalf(format, args...) }
+func (l *ttnGrpcLogger) V(int) bool                                  { return true } // TODO: Use when log.Interface supports this
