6e3912bee6f393b693f08005e971e02d71f33cfe
Eric Gourlaouen <eric@thethingsnetwork.org>
2018-04-23 18:06:01 +0200

    util: Add utility functions to get CFList

---
+++ pkg/frequencyplans/cflist_test.go
@@ -0,0 +1,121 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package frequencyplans_test
+
+import (
+	"testing"
+
+	"github.com/TheThingsNetwork/ttn/pkg/band"
+	"github.com/TheThingsNetwork/ttn/pkg/frequencyplans"
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+
+	"github.com/TheThingsNetwork/ttn/pkg/ttnpb"
+)
+
+func TestFrequenciesCFList(t *testing.T) {
+	a := assertions.New(t)
+
+	euFP := ttnpb.FrequencyPlan{
+		BandID: "EU_863_870",
+		Channels: []*ttnpb.FrequencyPlan_Channel{
+			{Frequency: 867100000},
+			{Frequency: 867300000},
+			{Frequency: 867500000},
+			{Frequency: 867700000},
+			{Frequency: 867900000},
+			{Frequency: 868100000},
+			{Frequency: 868300000},
+			{Frequency: 868500000},
+		},
+	}
+
+	cfList := frequencyplans.CFList(euFP, ttnpb.PHY_V1_1)
+
+	a.So(cfList.Type, should.Equal, ttnpb.CFListType_FREQUENCIES)
+
+	euBand, err := band.GetByID(euFP.BandID)
+	a.So(err, should.BeNil)
+
+fpChannels:
+	for _, channel := range euFP.Channels {
+		for _, bandChannel := range euBand.UplinkChannels {
+			if bandChannel.Frequency == uint64(channel.Frequency) {
+				continue fpChannels
+			}
+		}
+
+		var found bool
+		for _, cfListFrequency := range cfList.Freq {
+			if cfListFrequency*100 == channel.Frequency {
+				found = true
+			}
+		}
+		a.So(found, should.BeTrue)
+	}
+}
+
+func TestChannelMasksCFList(t *testing.T) {
+	a := assertions.New(t)
+
+	usFP := ttnpb.FrequencyPlan{
+		BandID: "US_902_928",
+		Channels: []*ttnpb.FrequencyPlan_Channel{
+			{Frequency: 903900000},
+			{Frequency: 904100000},
+			{Frequency: 904300000},
+			{Frequency: 904500000},
+			{Frequency: 904700000},
+			{Frequency: 904900000},
+			{Frequency: 905100000},
+			{Frequency: 905300000},
+		},
+	}
+
+	cfList := frequencyplans.CFList(usFP, ttnpb.PHY_V1_1)
+
+	enabledChannels := []int{8, 9, 10, 11, 12, 13, 14, 15}
+chMaskLoop:
+	for index, chMaskEntry := range cfList.ChMasks {
+		for _, enabledChannel := range enabledChannels {
+			if enabledChannel == index {
+				a.So(chMaskEntry, should.BeTrue)
+				continue chMaskLoop
+			}
+		}
+		a.So(chMaskEntry, should.BeFalse)
+	}
+}
+
+func TestUnimplementedCFList(t *testing.T) {
+	a := assertions.New(t)
+
+	usFP := ttnpb.FrequencyPlan{
+		BandID: "US_902_928",
+		Channels: []*ttnpb.FrequencyPlan_Channel{
+			{Frequency: 903900000},
+			{Frequency: 904100000},
+			{Frequency: 904300000},
+			{Frequency: 904500000},
+			{Frequency: 904700000},
+			{Frequency: 904900000},
+			{Frequency: 905100000},
+			{Frequency: 905300000},
+		},
+	}
+
+	cfList := frequencyplans.CFList(usFP, ttnpb.PHY_V1_0)
+	a.So(cfList, should.BeNil)
+}
