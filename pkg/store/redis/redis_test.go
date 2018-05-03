933ab04bcbaea0d929b921a92d41bcf811c81a58
Eric Gourlaouen <eric@thethingsnetwork.org>
2018-05-03 17:09:24 +0200

    util: Move Redis config generation for tests

--- pkg/store/redis/redis_test.go
+++ pkg/store/redis/redis_test.go
@@ -15,12 +15,14 @@
 package redis_test
 
 import (
+	"os"
+	"strconv"
 	"testing"
 
+	"github.com/TheThingsNetwork/ttn/pkg/config"
 	"github.com/TheThingsNetwork/ttn/pkg/store"
 	. "github.com/TheThingsNetwork/ttn/pkg/store/redis"
 	"github.com/TheThingsNetwork/ttn/pkg/store/storetest"
-	"github.com/TheThingsNetwork/ttn/pkg/util/test"
 )
 
 var (
@@ -29,9 +31,32 @@ var (
 	_ store.ByteSetStore  = &Store{}
 )
 
+// redisConfig returns a new redis config for testing
+func redisConfig() config.Redis {
+	var err error
+	config := config.Redis{
+		Address:  "localhost:6379",
+		Database: 1,
+		Prefix:   "test",
+	}
+	if address := os.Getenv("REDIS_ADDRESS"); address != "" {
+		config.Address = address
+	}
+	if db := os.Getenv("REDIS_DB"); db != "" {
+		config.Database, err = strconv.Atoi(db)
+		if err != nil {
+			panic(err)
+		}
+	}
+	if prefix := os.Getenv("REDIS_PREFIX"); prefix != "" {
+		config.Prefix = prefix
+	}
+	return config
+}
+
 func newStore() *Store {
 	conf := &Config{
-		Redis:     test.RedisConfig(),
+		Redis:     redisConfig(),
 		IndexKeys: storetest.IndexedFields,
 	}
 
