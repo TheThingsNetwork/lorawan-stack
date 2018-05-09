9b2935460b55bb03f5d68bb614a3df1cc631fff1
Johan Stokking <johan@stokking.nl>
2018-05-09 15:13:03 +0200

    util: Remove obsolete documentation file

--- pkg/deviceregistry/registry.go
+++ pkg/deviceregistry/registry.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package deviceregistry contains the implementation of a device registry service.
 package deviceregistry
 
 import (
