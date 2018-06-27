eadb569bee533f1dd7cda53ee3c3202d31f850d6
Kevin Schiffer <mail@kevinschiffer.de>
2018-06-27 10:43:54 +0200

    console: Add initial views

---
+++ pkg/webui/views/console/app/index.js
@@ -0,0 +1,40 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import Header from '../../../components/header'
+import Login from '../login'
+import Landing from '../landing'
+import store from '../../../store'
+
+export default class Console extends React.PureComponent {
+  render () {
+    return (
+      <Provider store={store}>
+        <div>
+          <Header />
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/console" component={Landing} />
+              <Route path="/console/login" component={Login} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </Provider>
+    )
+  }
+}
