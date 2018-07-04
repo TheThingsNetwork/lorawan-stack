8152dcef4f50d86c4ddab17f8b805a060d8b2fff
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2018-07-04 11:04:45 +0200

    console: Add basic notification component

---
+++ pkg/webui/components/notification/index.js
@@ -0,0 +1,52 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import classnames from 'classnames'
+
+import Icon from '../icon'
+
+import style from './notification.styl'
+
+const Notification = function ({
+  className,
+  error,
+  warning,
+  info,
+  small,
+  message,
+}) {
+
+  const classname = classnames(style.notification, className, {
+    [style.error]: error,
+    [style.warning]: warning,
+    [style.info]: info,
+    [style.small]: small,
+  })
+
+  let icon = 'info'
+  if (error) {
+    icon = 'error'
+  } else if (warning) {
+    icon = 'warning'
+  }
+
+  return (
+    <div className={classname}>
+      <Icon icon={icon} /><span>{message || error || warning || info}</span>
+    </div>
+  )
+}
+
+export default Notification
