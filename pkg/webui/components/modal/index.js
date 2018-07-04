47e6e8f253dbf2c44802eeb61c359d0d55d585b9
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2018-07-04 17:32:37 +0200

    console: Add modal component

---
+++ pkg/webui/components/modal/index.js
@@ -0,0 +1,126 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import bind from 'autobind-decorator'
+import PropTypes from 'prop-types'
+
+import Button from '../button'
+import Logo from '../logo'
+
+import style from './modal.styl'
+
+@bind
+export default class Modal extends React.PureComponent {
+  static propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.element),
+      PropTypes.element,
+    ]),
+    message: PropTypes.string,
+    subtitle: PropTypes.string,
+    bottomLine: PropTypes.string,
+    approval: PropTypes.bool,
+    buttonMessage: PropTypes.string,
+    cancelButtonMessage: PropTypes.string,
+    method: PropTypes.string,
+    buttonName: PropTypes.string,
+  }
+
+  handleApprove () {
+    const { onComplete } = this.props
+    if (onComplete) {
+      onComplete(true)
+    }
+  }
+
+  handleCancel () {
+    const { onComplete } = this.props
+    if (onComplete) {
+      onComplete(false)
+    }
+  }
+
+  render () {
+    const {
+      title,
+      subtitle,
+      children,
+      message,
+      bottomLine,
+      logo,
+      approval = false,
+      formName,
+      buttonMessage = this.props.approval ? 'Approve' : 'Ok',
+      cancelButtonMessage = 'Cancel',
+      onComplete,
+      ...rest
+    } = this.props
+
+    const name = formName ? { name: formName } : {}
+    const RootComponent = this.props.method ? 'form' : 'div'
+    const messageElement = (<span className={style.message}>{message}</span>)
+
+    let buttons = <div><Button message={buttonMessage} onClick={this.handleApprove} icon="check" /></div>
+
+
+    if (approval) {
+      buttons = (
+        <div>
+          <Button
+            secondary
+            message={cancelButtonMessage}
+            onClick={this.handleCancel}
+            name={formName}
+            icon="clear"
+            value="false"
+            {...name}
+          />
+          <Button
+            message={buttonMessage}
+            onClick={this.handleApprove}
+            name={formName}
+            icon="check"
+            value="true"
+            {...name}
+          />
+        </div>
+      )
+    }
+
+    return [
+      <div key="shadow" className={style.shadow} />,
+      <RootComponent key="modal" className={style.modal} {...rest}>
+        { title
+          && <div className={style.titleSection}>
+            <div>
+              <h1>{title}</h1>
+              { subtitle && (<span>{subtitle}</span>) }
+            </div>
+            { logo && (<Logo className={style.logo} />)}
+          </div>
+        }
+        { title && <div className={style.line} /> }
+        <div className={style.body}>
+          {children || messageElement}
+        </div>
+        <div className={style.controlBar}>
+          <div><span>{ bottomLine }</span></div>
+          {buttons}
+        </div>
+      </RootComponent>,
+    ]
+  }
+}
---
+++ pkg/webui/components/modal/story.js
@@ -0,0 +1,58 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Icon from '../icon'
+
+import style from './story.styl'
+import Modal from '.'
+
+storiesOf('Modal', module)
+  .add('Basic Modal', () => (
+    <Modal
+      title="Example Modal"
+      message="This is something you need to know!"
+    />
+  ))
+  .add('No Title', () => (
+    <Modal
+      message="This modal has no title. Might be useful in some situations."
+    />
+  ))
+  .add('OAuth Authorize Example', () => (
+    <Modal
+      title="Request for Permission"
+      subtitle="Console is requesting permission to do the following:"
+      bottomLine="Logged in as John Doe"
+      buttonMessage="Allow"
+      approval
+      logo
+    >
+      <div className={style.left}>
+        <ul>
+          <li><Icon icon="check" className={style.icon} /> View your profile</li>
+          <li><Icon icon="check" className={style.icon} /> Make changes to your profile</li>
+          <li><Icon icon="check" className={style.icon} /> Perform administrative action</li>
+          <li><Icon icon="check" className={style.icon} /> List your applications</li>
+          <li><Icon icon="check" className={style.icon} /> Register new gateways in your account</li>
+          <li><Icon icon="check" className={style.icon} /> Create and edit devices of your applications</li>
+        </ul>
+      </div>
+      <div className={style.right}>
+        <h3>Console</h3>
+        <p>The Console is the official The Things Network web application.</p>
+      </div>
+    </Modal>
+  ))
