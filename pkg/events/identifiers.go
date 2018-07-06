60049d76ed7d9c5be421079fdafc6defe38c6dab
Hylke Visser <hylke@thethingsnetwork.org>
2018-07-06 11:17:38 +0200

    all: Add event filtering on Identifiers

---
+++ pkg/events/identifiers.go
@@ -0,0 +1,205 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package events
+
+import (
+	"context"
+	"sync"
+
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+)
+
+// IdentifierFilter can be used as a layer on top of a PubSub to filter events
+// based on the identifiers they contain.
+type IdentifierFilter interface {
+	Handler
+	Subscribe(ctx context.Context, ids ttnpb.Identifiers, handler Handler)
+	Unsubscribe(ctx context.Context, ids ttnpb.Identifiers, handler Handler)
+}
+
+// NewIdentifierFilter returns a new IdentifierFilter (see interface).
+func NewIdentifierFilter() IdentifierFilter {
+	return &identifierFilter{
+		applicationIDs:  make(map[string][]Handler),
+		clientIDs:       make(map[string][]Handler),
+		deviceIDs:       make(map[string][]Handler),
+		gatewayIDs:      make(map[string][]Handler),
+		organizationIDs: make(map[string][]Handler),
+		userIDs:         make(map[string][]Handler),
+	}
+}
+
+// identifierFilter is a simple implementation of the IdentifierFilter.
+//
+// It uses a single RWMutex to protect the maps as well as the slices that are
+// contained within those maps. In the future it could be optimized by batching
+// subscribes and unsubscribes into a single Lock/Unlock.
+//
+// Using a sync.Map will likely not help much here, as we also need to protect
+// the slice from concurrent writes. Additionally it creates more complexity
+// when cleaning up (deleting empty []Handler from the map).
+type identifierFilter struct {
+	mu              sync.RWMutex
+	applicationIDs  map[string][]Handler
+	clientIDs       map[string][]Handler
+	deviceIDs       map[string][]Handler
+	gatewayIDs      map[string][]Handler
+	organizationIDs map[string][]Handler
+	userIDs         map[string][]Handler
+}
+
+func (f *identifierFilter) Subscribe(ctx context.Context, ids ttnpb.Identifiers, handler Handler) {
+	cids := ids.CombinedIdentifiers()
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	for _, id := range cids.ApplicationIDs {
+		uid := id.UniqueID(ctx)
+		f.applicationIDs[uid] = append(f.applicationIDs[uid], handler)
+	}
+	for _, id := range cids.ClientIDs {
+		uid := id.UniqueID(ctx)
+		f.clientIDs[uid] = append(f.clientIDs[uid], handler)
+	}
+	for _, id := range cids.DeviceIDs {
+		uid := id.UniqueID(ctx)
+		f.deviceIDs[uid] = append(f.deviceIDs[uid], handler)
+	}
+	for _, id := range cids.GatewayIDs {
+		uid := id.UniqueID(ctx)
+		f.gatewayIDs[uid] = append(f.gatewayIDs[uid], handler)
+	}
+	for _, id := range cids.OrganizationIDs {
+		uid := id.UniqueID(ctx)
+		f.organizationIDs[uid] = append(f.organizationIDs[uid], handler)
+	}
+	for _, id := range cids.UserIDs {
+		uid := id.UniqueID(ctx)
+		f.userIDs[uid] = append(f.userIDs[uid], handler)
+	}
+}
+
+func (f *identifierFilter) Unsubscribe(ctx context.Context, ids ttnpb.Identifiers, handler Handler) {
+	cids := ids.CombinedIdentifiers()
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	for _, id := range cids.ApplicationIDs {
+		uid := id.UniqueID(ctx)
+		for i, registered := range f.applicationIDs[uid] {
+			if registered == handler {
+				f.applicationIDs[uid] = append(f.applicationIDs[uid][:i], f.applicationIDs[uid][i+1:]...)
+				if len(f.applicationIDs[uid]) == 0 {
+					delete(f.applicationIDs, uid)
+				}
+				break
+			}
+		}
+	}
+	for _, id := range cids.ClientIDs {
+		uid := id.UniqueID(ctx)
+		for i, registered := range f.clientIDs[uid] {
+			if registered == handler {
+				f.clientIDs[uid] = append(f.clientIDs[uid][:i], f.clientIDs[uid][i+1:]...)
+				if len(f.clientIDs[uid]) == 0 {
+					delete(f.clientIDs, uid)
+				}
+				break
+			}
+		}
+	}
+	for _, id := range cids.DeviceIDs {
+		uid := id.UniqueID(ctx)
+		for i, registered := range f.deviceIDs[uid] {
+			if registered == handler {
+				f.deviceIDs[uid] = append(f.deviceIDs[uid][:i], f.deviceIDs[uid][i+1:]...)
+				if len(f.deviceIDs[uid]) == 0 {
+					delete(f.deviceIDs, uid)
+				}
+				break
+			}
+		}
+	}
+	for _, id := range cids.GatewayIDs {
+		uid := id.UniqueID(ctx)
+		for i, registered := range f.gatewayIDs[uid] {
+			if registered == handler {
+				f.gatewayIDs[uid] = append(f.gatewayIDs[uid][:i], f.gatewayIDs[uid][i+1:]...)
+				if len(f.gatewayIDs[uid]) == 0 {
+					delete(f.gatewayIDs, uid)
+				}
+				break
+			}
+		}
+	}
+	for _, id := range cids.OrganizationIDs {
+		uid := id.UniqueID(ctx)
+		for i, registered := range f.organizationIDs[uid] {
+			if registered == handler {
+				f.organizationIDs[uid] = append(f.organizationIDs[uid][:i], f.organizationIDs[uid][i+1:]...)
+				if len(f.organizationIDs[uid]) == 0 {
+					delete(f.organizationIDs, uid)
+				}
+				break
+			}
+		}
+	}
+	for _, id := range cids.UserIDs {
+		uid := id.UniqueID(ctx)
+		for i, registered := range f.userIDs[uid] {
+			if registered == handler {
+				f.userIDs[uid] = append(f.userIDs[uid][:i], f.userIDs[uid][i+1:]...)
+				if len(f.userIDs[uid]) == 0 {
+					delete(f.userIDs, uid)
+				}
+				break
+			}
+		}
+	}
+}
+
+func (f *identifierFilter) Notify(evt Event) {
+	var matched []Handler
+	ids := evt.Identifiers()
+	if ids == nil {
+		return
+	}
+	f.mu.RLock()
+	for _, id := range ids.ApplicationIDs {
+		matched = append(matched, f.applicationIDs[id.UniqueID(evt.Context())]...)
+	}
+	for _, id := range ids.ClientIDs {
+		matched = append(matched, f.clientIDs[id.UniqueID(evt.Context())]...)
+	}
+	for _, id := range ids.DeviceIDs {
+		matched = append(matched, f.deviceIDs[id.UniqueID(evt.Context())]...)
+	}
+	for _, id := range ids.GatewayIDs {
+		matched = append(matched, f.gatewayIDs[id.UniqueID(evt.Context())]...)
+	}
+	for _, id := range ids.OrganizationIDs {
+		matched = append(matched, f.organizationIDs[id.UniqueID(evt.Context())]...)
+	}
+	for _, id := range ids.UserIDs {
+		matched = append(matched, f.userIDs[id.UniqueID(evt.Context())]...)
+	}
+	f.mu.RUnlock()
+	notified := make(map[Handler]struct{}, len(matched))
+	for _, handler := range matched {
+		if _, ok := notified[handler]; ok {
+			continue
+		}
+		handler.Notify(evt)
+		notified[handler] = struct{}{}
+	}
+}
---
+++ pkg/events/identifiers_test.go
@@ -0,0 +1,126 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package events_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/smartystreets/assertions"
+	"go.thethings.network/lorawan-stack/pkg/events"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
+)
+
+func TestIdentifierFilter(t *testing.T) {
+	a := assertions.New(t)
+
+	ch := make(events.Channel, 10)
+
+	filter := events.NewIdentifierFilter()
+
+	evtAppFoo := events.New(context.Background(), "test", ttnpb.ApplicationIdentifiers{ApplicationID: "foo"}, "hello foo")
+	evtAppBar := events.New(context.Background(), "test", ttnpb.ApplicationIdentifiers{ApplicationID: "bar"}, "hello bar")
+
+	evtCliFoo := events.New(context.Background(), "test", ttnpb.ClientIdentifiers{ClientID: "foo"}, "hello foo")
+	evtCliBar := events.New(context.Background(), "test", ttnpb.ClientIdentifiers{ClientID: "bar"}, "hello bar")
+
+	evtDevFoo := events.New(context.Background(), "test", ttnpb.EndDeviceIdentifiers{
+		DeviceID:               "foo",
+		ApplicationIdentifiers: ttnpb.ApplicationIdentifiers{ApplicationID: "foo"},
+	}, "hello foo")
+	evtDevBar := events.New(context.Background(), "test", ttnpb.EndDeviceIdentifiers{
+		DeviceID:               "bar",
+		ApplicationIdentifiers: ttnpb.ApplicationIdentifiers{ApplicationID: "bar"},
+	}, "hello bar")
+
+	evtGtwFoo := events.New(context.Background(), "test", ttnpb.GatewayIdentifiers{GatewayID: "foo"}, "hello foo")
+	evtGtwBar := events.New(context.Background(), "test", ttnpb.GatewayIdentifiers{GatewayID: "bar"}, "hello bar")
+
+	evtOrgFoo := events.New(context.Background(), "test", ttnpb.OrganizationIdentifiers{OrganizationID: "foo"}, "hello foo")
+	evtOrgBar := events.New(context.Background(), "test", ttnpb.OrganizationIdentifiers{OrganizationID: "bar"}, "hello bar")
+
+	evtUsrFoo := events.New(context.Background(), "test", ttnpb.UserIdentifiers{UserID: "foo"}, "hello foo")
+	evtUsrBar := events.New(context.Background(), "test", ttnpb.UserIdentifiers{UserID: "bar"}, "hello bar")
+
+	fooIDs := &ttnpb.CombinedIdentifiers{
+		ApplicationIDs:  []*ttnpb.ApplicationIdentifiers{{ApplicationID: "foo"}},
+		ClientIDs:       []*ttnpb.ClientIdentifiers{{ClientID: "foo"}},
+		DeviceIDs:       []*ttnpb.EndDeviceIdentifiers{{DeviceID: "foo", ApplicationIdentifiers: ttnpb.ApplicationIdentifiers{ApplicationID: "foo"}}},
+		GatewayIDs:      []*ttnpb.GatewayIdentifiers{{GatewayID: "foo"}},
+		OrganizationIDs: []*ttnpb.OrganizationIdentifiers{{OrganizationID: "foo"}},
+		UserIDs:         []*ttnpb.UserIdentifiers{{UserID: "foo"}},
+	}
+
+	filter.Subscribe(context.Background(), fooIDs, ch)
+
+	filter.Notify(evtAppBar)
+	filter.Notify(evtAppFoo)
+
+	a.So(<-ch, should.Equal, evtAppFoo)
+	a.So(ch, should.BeEmpty)
+
+	filter.Notify(evtCliBar)
+	filter.Notify(evtCliFoo)
+
+	a.So(<-ch, should.Equal, evtCliFoo)
+	a.So(ch, should.BeEmpty)
+
+	filter.Notify(evtDevBar)
+	filter.Notify(evtDevFoo)
+
+	a.So(<-ch, should.Equal, evtDevFoo)
+	a.So(ch, should.BeEmpty)
+
+	filter.Notify(evtGtwBar)
+	filter.Notify(evtGtwFoo)
+
+	a.So(<-ch, should.Equal, evtGtwFoo)
+	a.So(ch, should.BeEmpty)
+
+	filter.Notify(evtOrgBar)
+	filter.Notify(evtOrgFoo)
+
+	a.So(<-ch, should.Equal, evtOrgFoo)
+	a.So(ch, should.BeEmpty)
+
+	filter.Notify(evtUsrBar)
+	filter.Notify(evtUsrFoo)
+
+	a.So(<-ch, should.Equal, evtUsrFoo)
+	a.So(ch, should.BeEmpty)
+
+	filter.Unsubscribe(context.Background(), fooIDs, ch)
+
+	filter.Notify(evtAppFoo)
+	filter.Notify(evtCliFoo)
+	filter.Notify(evtDevFoo)
+	filter.Notify(evtGtwFoo)
+	filter.Notify(evtOrgFoo)
+	filter.Notify(evtUsrFoo)
+
+	if !a.So(ch, should.BeEmpty) {
+	loop:
+		for {
+			select {
+			case evt := <-ch:
+				fmt.Println(evt)
+			default:
+				break loop
+			}
+		}
+	}
+}
