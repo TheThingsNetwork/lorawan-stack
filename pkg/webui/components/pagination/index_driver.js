ff175c695df31ead0ddac23ef1f6a4a1a3d1b2c5
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2018-07-19 13:50:14 +0200

    console: Add tests for pagination component

---
+++ pkg/webui/components/pagination/index_driver.js
@@ -0,0 +1,84 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+
+import Pagination from '.'
+
+export default function () {
+  const driver = {
+    component: undefined,
+    when: {
+      created (props) {
+        driver.component = shallow(
+          <Pagination {...props} />
+        ).dive()
+
+        return driver
+      },
+      navigatedNextPage () {
+        driver.get.nextNavigationButton().simulate('click', {
+          preventDefault: () => undefined,
+        })
+      },
+      navigatedLastPage () {
+        driver.get.lastPage().simulate('click', {
+          preventDefault: () => undefined,
+        })
+      },
+    },
+    is: {
+      pageSelected (page) {
+        return driver.get.page(page).props().selected
+      },
+      firstPageSelected () {
+        return driver.is.pageSelected(0)
+      },
+      lastPageSelected () {
+        return driver.is.pageSelected(driver.get.pages().length - 1)
+      },
+      prevNavigationDisabled () {
+        return driver.get.prevNavigation().hasClass('disabled')
+      },
+      nextNavigationDisabled () {
+        return driver.get.nextNavigation().hasClass('disabled')
+      },
+    },
+    get: {
+      pages () {
+        return driver.component.find('PageView')
+      },
+      page (page) {
+        return driver.get.pages().at(page)
+      },
+      lastPage () {
+        return driver.get.page(driver.get.pages().length - 1)
+      },
+      prevNavigation () {
+        return driver.component.find('li').first()
+      },
+      nextNavigation () {
+        return driver.component.find('li').last()
+      },
+      prevNavigationButton () {
+        return driver.get.prevNavigation().find('a').first()
+      },
+      nextNavigationButton () {
+        return driver.get.nextNavigation().find('a').first()
+      },
+    },
+  }
+
+  return driver
+}
---
+++ pkg/webui/components/pagination/index_test.js
@@ -0,0 +1,86 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import getPaginationDriver from './index_driver'
+
+describe('Pagination', function () {
+  let driver = null
+
+  beforeEach(function () {
+    driver = getPaginationDriver()
+  })
+
+  describe('has only a single page', function () {
+    beforeEach(function () {
+      driver.when.created({ pageCount: 1 })
+    })
+    it('should match snapshot', function () {
+      expect(driver.component).toMatchSnapshot()
+    })
+
+    it('should disable the previous page navigation', function () {
+      expect(driver.is.prevNavigationDisabled()).toBeTruthy()
+    })
+
+    it('should disable the next page navigation', function () {
+      expect(driver.is.nextNavigationDisabled()).toBeTruthy()
+    })
+  })
+
+  describe('has several pages', function () {
+    beforeEach(function () {
+      driver.when.created({ pageCount: 3 })
+    })
+
+    it('should match snapshot', function () {
+      expect(driver.component).toMatchSnapshot()
+    })
+
+    it('should select the first page', function () {
+      expect(driver.is.firstPageSelected()).toBeTruthy()
+    })
+
+    it('should disable the previous navigation', function () {
+      expect(driver.is.prevNavigationDisabled()).toBeTruthy()
+    })
+
+    describe('the user moves to the next page', function () {
+      beforeEach(function () {
+        driver.when.navigatedNextPage()
+      })
+
+      it('should select the second page', function () {
+        expect(driver.is.pageSelected(1)).toBeTruthy()
+      })
+
+      it('should enable the previous navigation', function () {
+        expect(driver.is.prevNavigationDisabled()).toBeFalsy()
+      })
+    })
+
+    describe('the user moves to the last page', function () {
+      beforeEach(function () {
+        driver.when.navigatedLastPage()
+      })
+
+      it('should select the last page', function () {
+        expect(driver.is.lastPageSelected()).toBeTruthy()
+      })
+
+      it('should disable the the next navigation', function () {
+        expect(driver.is.nextNavigationDisabled()).toBeTruthy()
+      })
+    })
+  })
+})
