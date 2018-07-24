36b6dd986115c9873c6a90faf7ece2c2fbaab2f0
Hylke Visser <hylke@thethingsnetwork.org>
2018-07-24 14:40:21 +0200

    dev: Bypass log stack and handlers in test logger

--- pkg/util/test/logger.go
+++ pkg/util/test/logger.go
@@ -15,14 +15,126 @@
 package test
 
 import (
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+
 	"go.thethings.network/lorawan-stack/pkg/log"
-	"go.thethings.network/lorawan-stack/pkg/log/test"
 )
 
-// GetLogger returns a logger for tests.
-func GetLogger(logger test.Logger) log.Stack {
-	return &log.Logger{
-		Level:   log.DebugLevel,
-		Handler: test.NewTestingHandler(logger),
+type kv struct {
+	k string
+	v interface{}
+}
+
+type fields []kv
+
+func (f fields) Len() int           { return len(f) }
+func (f fields) Swap(i, j int)      { f[i], f[j] = f[j], f[i] }
+func (f fields) Less(i, j int) bool { return f[i].k < f[j].k }
+
+func (f fields) unique() fields {
+	uniqueMap := make(map[string]interface{}, len(f))
+	for _, kv := range f {
+		uniqueMap[kv.k] = kv.v
 	}
+	unique := make(fields, 0, len(uniqueMap))
+	for k, v := range uniqueMap {
+		unique = append(unique, kv{k, v})
+	}
+	return unique
+}
+
+func (f fields) sorted() fields {
+	clone := make(fields, len(f))
+	copy(clone, f)
+	sort.Sort(clone)
+	return clone
+}
+
+// GetLogger returns a logger for tests.
+func GetLogger(t testing.TB) log.Stack {
+	return &testLogger{t: t}
+}
+
+type testLogger struct {
+	t      testing.TB
+	fields fields
+}
+
+func (l *testLogger) Use(m log.Middleware) {}
+
+func (l *testLogger) format(level log.Level, msg string, v ...interface{}) string {
+	if len(v) > 0 {
+		msg = fmt.Sprintf(msg, v...)
+	}
+	formatted := fmt.Sprintf("\033[%dm%6s\033[0m %-40s", log.Colors[level], strings.ToUpper(level.String()), msg)
+	if len(l.fields) > 0 {
+		for _, kv := range l.fields.unique().sorted() {
+			formatted += fmt.Sprintf(" \033[%dm%s\033[0m=%v", log.Colors[level], kv.k, kv.v)
+		}
+	}
+	return formatted
+}
+
+func (l *testLogger) Debug(msg string) {
+	l.t.Helper()
+	l.t.Log(l.format(log.DebugLevel, msg))
+}
+func (l *testLogger) Info(msg string) {
+	l.t.Helper()
+	l.t.Log(l.format(log.InfoLevel, msg))
+}
+func (l *testLogger) Warn(msg string) {
+	l.t.Helper()
+	l.t.Log(l.format(log.WarnLevel, msg))
+}
+func (l *testLogger) Error(msg string) {
+	l.t.Helper()
+	l.t.Log(l.format(log.ErrorLevel, msg))
+}
+func (l *testLogger) Fatal(msg string) {
+	l.t.Helper()
+	l.t.Log(l.format(log.FatalLevel, msg))
+}
+func (l *testLogger) Debugf(msg string, v ...interface{}) {
+	l.t.Helper()
+	l.t.Log(l.format(log.DebugLevel, msg, v...))
+}
+func (l *testLogger) Infof(msg string, v ...interface{}) {
+	l.t.Helper()
+	l.t.Log(l.format(log.InfoLevel, msg, v...))
+}
+func (l *testLogger) Warnf(msg string, v ...interface{}) {
+	l.t.Helper()
+	l.t.Log(l.format(log.WarnLevel, msg, v...))
+}
+func (l *testLogger) Errorf(msg string, v ...interface{}) {
+	l.t.Helper()
+	l.t.Log(l.format(log.ErrorLevel, msg, v...))
+}
+func (l *testLogger) Fatalf(msg string, v ...interface{}) {
+	l.t.Helper()
+	l.t.Log(l.format(log.FatalLevel, msg, v...))
+}
+func (l *testLogger) WithField(k string, v interface{}) log.Interface {
+	return &testLogger{
+		t:      l.t,
+		fields: append(l.fields, kv{k, v}),
+	}
+}
+func (l *testLogger) WithFields(f log.Fielder) log.Interface {
+	m := f.Fields()
+	fields := make([]kv, 0, len(m))
+	for k, v := range m {
+		fields = append(fields, kv{k, v})
+	}
+	return &testLogger{
+		t:      l.t,
+		fields: append(l.fields, fields...),
+	}
+}
+func (l *testLogger) WithError(err error) log.Interface {
+	return l.WithField("error", err)
 }
--- pkg/util/test/logger_test.go
+++ pkg/util/test/logger_test.go
@@ -14,11 +14,29 @@
 
 package test
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"go.thethings.network/lorawan-stack/pkg/log"
+)
 
 func TestGetLogger(t *testing.T) {
-	logger := GetLogger(t)
-	logger.Debug("abcabcabc - Hi!")
-	logger.Info("Fooz")
-	logger.Errorf("Nope %d", 1234)
+	var logger log.Interface = GetLogger(t)
+
+	logger = logger.WithField("foo", "bar")
+
+	logger = logger.WithError(errors.New("example error"))
+
+	logger = logger.WithFields(log.Fields("k1", "v1", "k2", "v2"))
+
+	logger.Debug("This is a debug log")
+	logger.Info("This is a info log")
+	logger.Warn("This is a warn log")
+	logger.Error("This is an error log")
+
+	logger.Debugf("This is a %s log", "debug")
+	logger.Infof("This is a %s log", "info")
+	logger.Warnf("This is a %s log", "warn")
+	logger.Errorf("This is an %s log", "error")
 }
