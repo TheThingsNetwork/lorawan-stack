29956159f4b4f6feceb704afaa3fa84edf2c87a4
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2018-08-02 11:13:10 +0200

    console,oauth: Add error-message component

---
+++ pkg/webui/components/error-message/index.js
@@ -0,0 +1,50 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+
+import Message from '../message'
+
+import PropTypes from '../../lib/prop-types'
+
+const ErrorMessage = function ({ content, ...rest }) {
+
+  const props = {
+    content: {},
+    ...rest,
+  }
+
+  // Check if it is a error message and transform it to a intl message
+  if (typeof content === 'object' && !('id' in content) && content.message && content.details) {
+    props.content.id = content.message.split(' ')[0]
+    props.content.defaultMessage = content.details[0].message_format || content.message.replace(/^.*\s/, '')
+    props.values = content.details[0].attributes
+  } else {
+    // Fall back to normal message
+    props.content = content
+  }
+
+  return <Message {...props} />
+}
+
+ErrorMessage.propTypes = {
+  /**
+   * Content contains the error message data, returned from the backend. It will
+   * be marshalled into a `react-intl` message and then output as such. Can also
+   * be a usual message type, in case of frontend-defined errors.
+   */
+  content: PropTypes.error,
+}
+
+export default ErrorMessage
---
+++ pkg/webui/components/error-message/story.js
@@ -0,0 +1,52 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { IntlProvider } from 'react-intl'
+
+import ErrorMessage from '.'
+
+const exampleError = {
+  code: 2,
+  message: 'error:pkg/assets:http (HTTP error: `` is not a valid ID. Must be at least 2 and at most 36 characters long and may consist of only letters, numbers, dashes and underscores. It may not start or end with a dash or an underscore)',
+  details: [{
+    '@type': 'type.googleapis.com/ttn.lorawan.v3.ErrorDetails',
+    namespace: 'pkg/assets',
+    name: 'http',
+    message_format: 'HTTP error: {message}',
+    attributes: {
+      message: '`` is not a valid ID. Must be at least 2 and at most 36 characters long and may consist of only letters, numbers, dashes and underscores. It may not start or end with a dash or an underscore',
+    },
+  }],
+}
+
+const IntlDecorator = storyFn => (
+  <IntlProvider key="key" messages={{}} locale="en-US">
+    { storyFn() }
+  </IntlProvider>
+)
+
+storiesOf('ErrorMessage', module)
+  .addDecorator((story, context) => withInfo({
+    inline: true,
+    header: false,
+    propTables: [ ErrorMessage ],
+  })(story)(context))
+  .addDecorator(IntlDecorator)
+  .add('Default', () =>
+    (
+      <ErrorMessage content={exampleError} />
+    ))
