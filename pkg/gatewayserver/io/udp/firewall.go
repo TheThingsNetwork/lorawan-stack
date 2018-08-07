e20e262af418496b58787a9d3ea621f8ca6f8169
Johan Stokking <johan@thethingsnetwork.org>
2018-08-07 14:13:19 +0200

    gs: Add in memory traffic firewall for UDP

---
+++ pkg/gatewayserver/io/udp/firewall.go
@@ -0,0 +1,107 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package udp
+
+import (
+	"context"
+	"net"
+	"sync"
+	"time"
+
+	encoding "go.thethings.network/lorawan-stack/pkg/gatewayserver/io/udp/encoding"
+)
+
+// Firewall filters packets by tracking addresses and time.
+type Firewall interface {
+	Filter(packet encoding.Packet) bool
+}
+
+type addrTime struct {
+	net.UDPAddr
+	lastSeen time.Time
+}
+
+type memoryFirewall struct {
+	push            sync.Map
+	pull            sync.Map
+	addrChangeBlock time.Duration
+}
+
+// NewMemoryFirewall returns an in-memory Firewall.
+func NewMemoryFirewall(ctx context.Context, addrChangeBlock time.Duration) Firewall {
+	v := &memoryFirewall{
+		addrChangeBlock: addrChangeBlock,
+	}
+	go func() {
+		ticker := time.NewTicker(addrChangeBlock)
+		for {
+			select {
+			case <-ctx.Done():
+				ticker.Stop()
+				return
+			case <-ticker.C:
+				v.gc()
+			}
+		}
+	}()
+	return v
+}
+
+func (v *memoryFirewall) filter(packet encoding.Packet, store *sync.Map) bool {
+	if packet.GatewayEUI == nil || packet.GatewayAddr == nil {
+		return false
+	}
+	now := time.Now().UTC()
+	eui := *packet.GatewayEUI
+	val, ok := store.Load(eui)
+	if ok {
+		a := val.(addrTime)
+		if a.UDPAddr.String() != packet.GatewayAddr.String() {
+			if a.lastSeen.Add(v.addrChangeBlock).After(now) {
+				return false
+			}
+		}
+	}
+	store.Store(eui, addrTime{
+		UDPAddr:  *packet.GatewayAddr,
+		lastSeen: now,
+	})
+	return true
+}
+
+func (v *memoryFirewall) Filter(packet encoding.Packet) bool {
+	switch packet.PacketType {
+	case encoding.PullData, encoding.TxAck:
+		return v.filter(packet, &v.pull)
+	case encoding.PushData:
+		return v.filter(packet, &v.push)
+	}
+	return false
+}
+
+func (v *memoryFirewall) gc() {
+	now := time.Now().UTC()
+	gcStore := func(store *sync.Map) {
+		store.Range(func(k, val interface{}) bool {
+			a := val.(addrTime)
+			if a.lastSeen.Add(v.addrChangeBlock).Before(now) {
+				store.Delete(k)
+			}
+			return true
+		})
+	}
+	gcStore(&v.pull)
+	gcStore(&v.push)
+}
---
+++ pkg/gatewayserver/io/udp/firewall_test.go
@@ -0,0 +1,132 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package udp_test
+
+import (
+	"net"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/smartystreets/assertions"
+	. "go.thethings.network/lorawan-stack/pkg/gatewayserver/io/udp"
+	encoding "go.thethings.network/lorawan-stack/pkg/gatewayserver/io/udp/encoding"
+	"go.thethings.network/lorawan-stack/pkg/types"
+	"go.thethings.network/lorawan-stack/pkg/util/test"
+	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
+)
+
+func TestMemoryFirewall(t *testing.T) {
+	ctx := test.Context()
+
+	block := 10 * time.Millisecond
+	v := NewMemoryFirewall(ctx, block)
+
+	eui1 := types.EUI64{0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01}
+	eui2 := types.EUI64{0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02}
+	addr1 := net.UDPAddr{
+		IP:   []byte{0x01, 0x01, 0x01, 0x01},
+		Port: 1,
+	}
+	addr2 := net.UDPAddr{
+		IP:   []byte{0x02, 0x02, 0x02, 0x02},
+		Port: 2,
+	}
+	addr3 := net.UDPAddr{
+		IP:   []byte{0x03, 0x03, 0x03, 0x03},
+		Port: 3,
+	}
+
+	for i, tc := range []struct {
+		Packet    encoding.Packet
+		OK        bool
+		WaitAfter time.Duration
+	}{
+		{
+			Packet: encoding.Packet{
+				GatewayAddr: &addr1,
+				PacketType:  encoding.PullData,
+			},
+			OK: false, // no EUI
+		},
+		{
+			Packet: encoding.Packet{
+				GatewayEUI: &eui1,
+				PacketType: encoding.PullData,
+			},
+			OK: false, // no address
+		},
+		{
+			Packet: encoding.Packet{
+				GatewayEUI:  &eui1,
+				GatewayAddr: &addr1,
+				PacketType:  encoding.PullData,
+			},
+			OK: true, // first time 1
+		},
+		{
+			Packet: encoding.Packet{
+				GatewayEUI:  &eui1,
+				GatewayAddr: &addr1,
+				PacketType:  encoding.PullData,
+			},
+			OK: true, // second time 1 with same address
+		},
+		{
+			Packet: encoding.Packet{
+				GatewayEUI:  &eui2,
+				GatewayAddr: &addr2,
+				PacketType:  encoding.PullData,
+			},
+			OK: true, // first time downlink from 2
+		},
+		{
+			Packet: encoding.Packet{
+				GatewayEUI:  &eui2,
+				GatewayAddr: &addr3,
+				PacketType:  encoding.PushData,
+			},
+			OK: true, // first time uplink from 2
+		},
+		{
+			Packet: encoding.Packet{
+				GatewayEUI:  &eui2,
+				GatewayAddr: &addr3,
+				PacketType:  encoding.PullData,
+			},
+			WaitAfter: block * 2,
+			OK:        false, // block change of downlink address
+		},
+		{
+			Packet: encoding.Packet{
+				GatewayEUI:  &eui2,
+				GatewayAddr: &addr3,
+				PacketType:  encoding.PullData,
+			},
+			OK: true, // permit change of downlink address after block time
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			a := assertions.New(t)
+
+			actual := v.Filter(tc.Packet)
+			if !a.So(actual, should.Equal, tc.OK) {
+				t.FailNow()
+			}
+
+			time.Sleep(tc.WaitAfter)
+		})
+	}
+}
