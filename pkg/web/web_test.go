f867a162c7c969e457ee558cc654ebdb3321c2e8
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2018-08-10 18:31:47 +0200

    dev: Increase test coverage in web package

--- pkg/web/web_test.go
+++ pkg/web/web_test.go
@@ -15,13 +15,20 @@
 package web
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo"
 	"github.com/smartystreets/assertions"
 	"go.thethings.network/lorawan-stack/pkg/config"
+	"go.thethings.network/lorawan-stack/pkg/random"
 	"go.thethings.network/lorawan-stack/pkg/util/test"
 	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
+	"go.thethings.network/lorawan-stack/pkg/web/middleware"
 )
 
 func handler(c echo.Context) error {
@@ -61,5 +68,117 @@ func TestGroup(t *testing.T) {
 		a.So(ggrp.prefix, should.Equal, "/group/quu")
 		ggrp.GET("/q", handler)
 		a.So(s, ShouldHaveRoute, "GET", "/group/quu/q")
+	}
+}
+
+func TestIsZeros(t *testing.T) {
+	a := assertions.New(t)
+	{
+		res := isZeros([]byte{0, 0, 0, 0, 0})
+		a.So(res, assertions.ShouldBeTrue)
+	}
+	{
+		res := isZeros([]byte{0, 0, 0, 1, 0})
+		a.So(res, assertions.ShouldBeFalse)
+	}
+
+}
+
+func TestServeHTTP(t *testing.T) {
+	a := assertions.New(t)
+	s, err := New(test.Context(), config.HTTP{})
+	if !a.So(err, should.BeNil) {
+		t.Fatal("Could not create a web instance")
+	}
+
+	// HTTP server returns 200 on valid route
+	{
+		req := httptest.NewRequest(echo.GET, "/", nil)
+		rec := httptest.NewRecorder()
+
+		s.GET("/", handler)
+
+		s.ServeHTTP(rec, req)
+
+		resp := rec.Result()
+		a.So(resp.StatusCode, should.Equal, http.StatusOK)
+	}
+}
+
+func TestRootGroup(t *testing.T) {
+	a := assertions.New(t)
+	s, err := New(test.Context(), config.HTTP{})
+	if !a.So(err, should.BeNil) {
+		t.Fatal("Could not create a web instance")
+	}
+
+	s.RootGroup("/sub")
+	a.So(s, ShouldNotHaveRoute, "GET", "/")
+	a.So(s, ShouldNotHaveRoute, "GET", "/sub/another")
+	a.So(s, ShouldHaveRoute, "GET", "/sub")
+}
+
+func TestStatic(t *testing.T) {
+	a := assertions.New(t)
+	s, err := New(test.Context(), config.HTTP{})
+	if !a.So(err, should.BeNil) {
+		t.Fatal("Could not create a web instance")
+	}
+
+	dir, err := os.Getwd()
+
+	if !a.So(err, should.BeNil) {
+		t.Fatal("Could not create resolve testing directory")
+	}
+
+	s.Static("/assets", http.Dir(dir), middleware.Immutable)
+
+	// HTTP server returns 200 on valid file request
+	{
+		req := httptest.NewRequest(echo.GET, "/assets/web_test.go", nil)
+		rec := httptest.NewRecorder()
+
+		s.ServeHTTP(rec, req)
+
+		resp := rec.Result()
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		a.So(resp.StatusCode, should.Equal, http.StatusOK)
+		a.So(strings.HasPrefix(string(body), "//"), should.BeTrue)
+	}
+
+	// HTTP server returns 404 on invalid file request
+	{
+		req := httptest.NewRequest(echo.GET, "/assets/null.txt", nil)
+		rec := httptest.NewRecorder()
+
+		s.Static("/assets", http.Dir(dir+"/teststatic"), middleware.Immutable)
+
+		s.ServeHTTP(rec, req)
+
+		resp := rec.Result()
+		a.So(resp.StatusCode, should.Equal, http.StatusNotFound)
+	}
+}
+
+func TestCookies(t *testing.T) {
+	a := assertions.New(t)
+	// Errors on illegal hash key byte size
+	{
+		c := config.HTTP{}
+		c.Cookie.HashKey = random.Bytes(2)
+
+		_, err := New(test.Context(), c)
+
+		a.So(err, should.NotBeNil)
+	}
+	// Errors on non 32bit block key
+	{
+		c := config.HTTP{}
+		c.Cookie.BlockKey = random.Bytes(31)
+
+		_, err := New(test.Context(), c)
+
+		a.So(err, should.NotBeNil)
 	}
 }
