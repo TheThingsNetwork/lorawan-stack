f2b4b477a3b504384f9ffde0cea806393fca2771
Johan Stokking <johan@thethingsnetwork.org>
2018-08-15 13:37:43 +0200

    gs: Harmonize events and metrics

--- pkg/gatewayserver/observability.go
+++ pkg/gatewayserver/observability.go
@@ -18,32 +18,40 @@ import (
 	"context"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"go.thethings.network/lorawan-stack/pkg/cluster"
+	errors "go.thethings.network/lorawan-stack/pkg/errorsv3"
 	"go.thethings.network/lorawan-stack/pkg/events"
 	"go.thethings.network/lorawan-stack/pkg/metrics"
 	"go.thethings.network/lorawan-stack/pkg/ttnpb"
 )
 
 var (
-	evtStartGatewayLink = events.Define("gs.gateway.start_link", "start gateway link")
-	evtEndGatewayLink   = events.Define("gs.gateway.end_link", "end gateway link")
+	evtGatewayConnect    = events.Define("gs.gateway.connect", "gateway connect")
+	evtGatewayDisconnect = events.Define("gs.gateway.disconnect", "gateway disconnect")
+	evtGatewayStatus     = events.Define("gs.gateway.status", "gateway status")
 
-	evtReceiveUp     = events.Define("gs.up.receive", "receive uplink message")
-	evtReceiveStatus = events.Define("gs.status.receive", "receive status message")
-	evtSendDown      = events.Define("gs.down.send", "send downlink message")
+	evtReceiveUp = events.Define("gs.up.receive", "receive uplink message")
+	evtDropUp    = events.Define("gs.up.drop", "drop uplink message")
+	evtForwardUp = events.Define("gs.up.forward", "forward uplink message")
+
+	evtSendDown = events.Define("gs.down.send", "send downlink message")
 )
 
 const (
 	subsystem = "gs"
+	unknown   = "unknown"
 	gatewayID = "gateway_id"
+	peer      = "peer"
 )
 
 var gsMetrics = &messageMetrics{
-	gatewayLinks: metrics.NewContextualGaugeVec(
-		prometheus.GaugeOpts{
+	statusReceived: metrics.NewContextualCounterVec(
+		prometheus.CounterOpts{
 			Subsystem: subsystem,
-			Name:      "gateway_links",
-			Help:      "Number of gateway links",
+			Name:      "status_received_total",
+			Help:      "Total number of received gateway statuses",
 		},
+		// TODO: Remove label (https://github.com/TheThingsIndustries/lorawan-stack/issues/1039)
 		[]string{gatewayID},
 	),
 	uplinkReceived: metrics.NewContextualCounterVec(
@@ -52,15 +60,24 @@ var gsMetrics = &messageMetrics{
 			Name:      "uplink_received_total",
 			Help:      "Total number of received uplinks",
 		},
+		// TODO: Remove label (https://github.com/TheThingsIndustries/lorawan-stack/issues/1039)
 		[]string{gatewayID},
 	),
-	statusReceived: metrics.NewContextualCounterVec(
+	uplinkForwarded: metrics.NewContextualCounterVec(
 		prometheus.CounterOpts{
 			Subsystem: subsystem,
-			Name:      "status_received_total",
-			Help:      "Total number of received statuses",
+			Name:      "uplink_forwarded_total",
+			Help:      "Total number of forwarded uplinks",
 		},
-		[]string{gatewayID},
+		[]string{peer},
+	),
+	uplinkDropped: metrics.NewContextualCounterVec(
+		prometheus.CounterOpts{
+			Subsystem: subsystem,
+			Name:      "uplink_dropped_total",
+			Help:      "Total number of dropped uplinks",
+		},
+		[]string{"error"},
 	),
 	downlinkSent: metrics.NewContextualCounterVec(
 		prometheus.CounterOpts{
@@ -68,6 +85,7 @@ var gsMetrics = &messageMetrics{
 			Name:      "downlink_sent_total",
 			Help:      "Total number of sent downlinks",
 		},
+		// TODO: Remove label (https://github.com/TheThingsIndustries/lorawan-stack/issues/1039)
 		[]string{gatewayID},
 	),
 }
@@ -77,47 +95,54 @@ func init() {
 }
 
 type messageMetrics struct {
-	gatewayLinks   *metrics.ContextualGaugeVec
-	uplinkReceived *metrics.ContextualCounterVec
-	statusReceived *metrics.ContextualCounterVec
-	downlinkSent   *metrics.ContextualCounterVec
+	statusReceived  *metrics.ContextualCounterVec
+	uplinkReceived  *metrics.ContextualCounterVec
+	uplinkForwarded *metrics.ContextualCounterVec
+	uplinkDropped   *metrics.ContextualCounterVec
+	downlinkSent    *metrics.ContextualCounterVec
 }
 
 func (m messageMetrics) Describe(ch chan<- *prometheus.Desc) {
-	m.gatewayLinks.Describe(ch)
-	m.uplinkReceived.Describe(ch)
 	m.statusReceived.Describe(ch)
+	m.uplinkReceived.Describe(ch)
+	m.uplinkForwarded.Describe(ch)
+	m.uplinkDropped.Describe(ch)
 	m.downlinkSent.Describe(ch)
 }
 
 func (m messageMetrics) Collect(ch chan<- prometheus.Metric) {
-	m.gatewayLinks.Collect(ch)
-	m.uplinkReceived.Collect(ch)
 	m.statusReceived.Collect(ch)
+	m.uplinkReceived.Collect(ch)
+	m.uplinkForwarded.Collect(ch)
+	m.uplinkDropped.Collect(ch)
 	m.downlinkSent.Collect(ch)
 }
 
-func registerStartGatewayLink(ctx context.Context, gtw ttnpb.GatewayIdentifiers) {
-	events.Publish(evtStartGatewayLink(ctx, gtw, nil))
-	gsMetrics.gatewayLinks.WithLabelValues(ctx, gtw.GatewayID).Inc()
-}
-
-func registerEndGatewayLink(ctx context.Context, gtw ttnpb.GatewayIdentifiers) {
-	events.Publish(evtEndGatewayLink(ctx, gtw, nil))
-	gsMetrics.gatewayLinks.WithLabelValues(ctx, gtw.GatewayID).Dec()
-}
-
-func registerReceiveUplink(ctx context.Context, gtw ttnpb.GatewayIdentifiers, msg *ttnpb.UplinkMessage) {
-	events.Publish(evtReceiveUp(ctx, ttnpb.CombineIdentifiers(gtw, msg.EndDeviceIdentifiers), nil))
-	gsMetrics.uplinkReceived.WithLabelValues(ctx, gtw.GatewayID).Inc()
-}
-
-func registerReceiveStatus(ctx context.Context, gtw ttnpb.GatewayIdentifiers, msg *ttnpb.GatewayStatus) {
-	events.Publish(evtReceiveStatus(ctx, gtw, nil))
+func registerReceiveStatus(ctx context.Context, gtw *ttnpb.Gateway, status *ttnpb.GatewayStatus) {
+	events.Publish(evtGatewayStatus(ctx, gtw.GatewayIdentifiers, nil))
 	gsMetrics.statusReceived.WithLabelValues(ctx, gtw.GatewayID).Inc()
 }
 
-func registerSendDownlink(ctx context.Context, gtw ttnpb.GatewayIdentifiers, msg *ttnpb.DownlinkMessage) {
-	events.Publish(evtSendDown(ctx, ttnpb.CombineIdentifiers(gtw, msg.EndDeviceIdentifiers), nil))
+func registerReceiveUplink(ctx context.Context, gtw *ttnpb.Gateway, msg *ttnpb.UplinkMessage) {
+	events.Publish(evtReceiveUp(ctx, gtw.GatewayIdentifiers, nil))
+	gsMetrics.uplinkReceived.WithLabelValues(ctx, gtw.GatewayID).Inc()
+}
+
+func registerForwardUplink(ctx context.Context, gtw *ttnpb.Gateway, msg *ttnpb.UplinkMessage, peer cluster.Peer) {
+	events.Publish(evtForwardUp(ctx, gtw.GatewayIdentifiers, nil))
+	gsMetrics.uplinkForwarded.WithLabelValues(ctx, peer.Name()).Inc()
+}
+
+func registerDropUplink(ctx context.Context, gtw *ttnpb.Gateway, msg *ttnpb.UplinkMessage, err error) {
+	events.Publish(evtDropUp(ctx, gtw.GatewayIdentifiers, err))
+	if ttnErr, ok := errors.From(err); ok {
+		gsMetrics.uplinkDropped.WithLabelValues(ctx, ttnErr.String()).Inc()
+	} else {
+		gsMetrics.uplinkDropped.WithLabelValues(ctx, unknown).Inc()
+	}
+}
+
+func registerSendDownlink(ctx context.Context, gtw *ttnpb.Gateway, msg *ttnpb.DownlinkMessage) {
+	events.Publish(evtSendDown(ctx, msg.EndDeviceIdentifiers, nil))
 	gsMetrics.downlinkSent.WithLabelValues(ctx, gtw.GatewayID).Inc()
 }
