bab94f435f1dc83f5658634bf623f5d2d5fec161
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2018-08-17 16:15:31 +0200

    console,dev: Add profile dropdown component

---
+++ pkg/webui/components/profile-dropdown/index.js
@@ -0,0 +1,139 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import bind from 'autobind-decorator'
+import { Link } from 'react-router-dom'
+
+import Icon from '../icon'
+import Message from '../message'
+import PropTypes from '../../lib/prop-types'
+
+import styles from './profile-dropdown.styl'
+
+@bind
+export default class ProfileDropdown extends React.PureComponent {
+
+  static propTypes = {
+    /**
+    * The User object returned from the API
+    */
+    user: PropTypes.PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+    /**
+    * A list of items for the dropdown
+    * @param {(string|Object)} title - The title to be displayed
+    * @param {string} icon - The icon name to be displayed next to the title
+    * @param {string} path - The path for a navigation tab
+    * @param {function} action - Alternatively, the function to be called on click
+    */
+    dropdownItems: PropTypes.arrayOf(PropTypes.shape({
+      title: PropTypes.message.isRequired,
+      icon: PropTypes.string,
+      path: PropTypes.string,
+      action: PropTypes.func,
+    })),
+  }
+
+  state = {
+    expanded: false,
+  }
+
+  showDropdown () {
+    document.addEventListener('mousedown', this.handleClickOutside)
+    this.setState({ expanded: true })
+  }
+
+  hideDropdown () {
+    document.removeEventListener('mousedown', this.handleClickOutside)
+    this.setState({ expanded: false })
+  }
+
+  handleClickOutside (e) {
+    if (!this.node.contains(e.target)) {
+      this.hideDropdown()
+    }
+  }
+
+  toggleDropdown () {
+    let { expanded } = this.state
+    expanded = !expanded
+    if (expanded) {
+      this.showDropdown()
+    } else {
+      this.hideDropdown()
+    }
+  }
+
+  ref (node) {
+    this.node = node
+  }
+
+  render () {
+    const { user, dropdownItems, ...rest } = this.props
+
+    return (
+      <div
+        className={styles.container}
+        onClick={this.toggleDropdown}
+        onKeyPress={this.toggleDropdown}
+        ref={this.ref}
+        tabIndex="0"
+        role="button"
+        {...rest}
+      >
+        <div className={styles.profilePicture} />
+        <span className={styles.username}>{user.name}</span>
+        <Icon icon="arrow_drop_down" />
+        { this.state.expanded && <Dropdown items={dropdownItems} />}
+      </div>
+    )
+  }
+}
+
+const Dropdown = ({ items }) => (
+  <ul className={styles.dropdown}>
+    { items.map( function (item) {
+      const icon = item.icon && <Icon className={styles.icon} icon={item.icon} />
+      return (
+        <li className={styles.dropdownItem} key={item.title.id || item.title}>
+          { item.action
+            ? <button onClick={item.action} onKeyPress={item.action} role="tab" tabIndex="0">{icon}<Message content={item.title} /></button>
+            : <Link to={item.link}>{icon}<Message content={item.title} /></Link>
+          }
+        </li>
+      )
+    }
+    )}
+  </ul>
+)
+
+Dropdown.propTypes = {
+  /**
+  * A list of items for the dropdown
+  * @param {(string|Object)} title - The title to be displayed
+  * @param {string} icon - The icon name to be displayed next to the title
+  * @param {string} path - The path for a navigation tab
+  * @param {function} action - Alternatively, the function to be called on click
+  */
+  items: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.message.isRequired,
+    icon: PropTypes.string,
+    path: PropTypes.string,
+    action: PropTypes.func,
+  })),
+}
+
+export { Dropdown }
---
+++ pkg/webui/components/profile-dropdown/story.js
@@ -0,0 +1,54 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import 'focus-visible/dist/focus-visible'
+import { withInfo } from '@storybook/addon-info'
+
+import ProfileDropdown from '.'
+
+const user = {
+  name: 'kschiffer',
+}
+
+const items = [
+  {
+    title: 'Profile Settings',
+    icon: 'settings',
+    link: '/profile-settings',
+  },
+  {
+    title: 'Logout',
+    icon: 'power_settings_new',
+    action: () => null,
+  },
+]
+
+storiesOf('Profile Dropdown', module)
+  .addDecorator((story, context) => withInfo({
+    inline: true,
+    header: false,
+    source: false,
+    propTables: [ ProfileDropdown ],
+  })(story)(context))
+  .add('Default', function () {
+    return (
+      <ProfileDropdown
+        style={{ marginLeft: '60px' }}
+        user={user}
+        dropdownItems={items}
+      />
+    )
+  })
