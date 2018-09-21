b4078cb5fec23a7c8c714bf816102e6772f875d1
Johan Stokking <johan@thethingsnetwork.org>
2018-09-21 10:55:30 +0200

    console: Fix story error message about password validation

--- pkg/webui/lib/components/error-message/story.js
+++ pkg/webui/lib/components/error-message/story.js
@@ -20,14 +20,14 @@ import ErrorMessage from '.'
 
 const exampleError = {
   code: 2,
-  message: 'error:pkg/assets:http (HTTP error: `` is not a valid ID. Must be at least 2 and at most 36 characters long and may consist of only letters, numbers, dashes and underscores. It may not start or end with a dash or an underscore)',
+  message: 'error:pkg/assets:http (HTTP error: `` is not a valid ID. Must be at least 2 and at most 36 characters long and may consist of only letters, numbers and dashes. It may not start or end with a dash)',
   details: [{
     '@type': 'type.googleapis.com/ttn.lorawan.v3.ErrorDetails',
     namespace: 'pkg/assets',
     name: 'http',
     message_format: 'HTTP error: {message}',
     attributes: {
-      message: '`` is not a valid ID. Must be at least 2 and at most 36 characters long and may consist of only letters, numbers, dashes and underscores. It may not start or end with a dash or an underscore',
+      message: '`` is not a valid ID. Must be at least 2 and at most 36 characters long and may consist of only letters, numbers and dashes. It may not start or end with a dash',
     },
   }],
 }
