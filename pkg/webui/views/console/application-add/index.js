bd8836ec6b9e57312058ca4760f2f51638e0a2cb
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2018-10-28 18:19:03 +0100

    console: Improve add application view

--- pkg/webui/views/console/application-add/index.js
+++ pkg/webui/views/console/application-add/index.js
@@ -14,6 +14,11 @@
 
 import React from 'react'
 import { Col, Row } from 'react-grid-system'
+import * as Yup from 'yup'
+import { push } from 'connected-react-router'
+import { connect } from 'react-redux'
+import bind from 'autobind-decorator'
+import { defineMessages } from 'react-intl'
 
 import Form from '../../../components/form'
 import Field from '../../../components/field'
@@ -25,11 +30,32 @@ import sharedMessages from '../../../lib/shared-messages'
 
 import style from './application-add.styl'
 
+const m = defineMessages({
+  applicationName: 'Application Name',
+  appIdPlaceholder: 'my-new-application',
+  appNamePlaceholder: 'My New Application',
+  appDescPlaceholder: 'Description for my new application',
+  createApplication: 'Create Application',
+})
+
 const initialValues = {
   app_id: '',
   name: '',
   description: '',
 }
+
+const validationSchema = Yup.object().shape({
+  app_id: Yup.string()
+    .matches(/^[0-9a-z]+$/i, sharedMessages.validateAlphanum)
+    .min(2, sharedMessages.validateTooShort)
+    .max(25, sharedMessages.validateTooLong)
+    .required(sharedMessages.validateRequired),
+  name: Yup.string()
+    .min(2, sharedMessages.validateTooShort)
+    .max(50, sharedMessages.validateTooLong)
+    .required(sharedMessages.validateRequired),
+  description: Yup.string(),
+})
 
 @withBreadcrumb('apps.add', function (props) {
   return (
@@ -40,50 +66,66 @@ const initialValues = {
     />
   )
 })
+@connect()
+@bind
 export default class Add extends React.Component {
 
   handleSubmit (e) {
-    console.log(e)
+    // Add API call and redirect to applications page go here
+  }
+
+  handleCancel () {
+    this.props.dispatch(push('/console/applications'))
   }
 
   render () {
     return (
-      <Row>
-        <Col className={style.description} sm={12} md={4}>
-          <h2>Add Application</h2>
-          <p>
-            Here is a text that sort of explains the process of adding an application.
-            This is to help users making sense of what is actually happening.
-            <br />
-            We could also provide links and resources from our documentation.
-            Lorem Ipsum dolor sit amet.
-          </p>
-        </Col>
-        <Col className={style.form} sm={12} md={8}>
+      <Row className={style.wrapper}>
+        <Col sm={12}><h2>Add Application</h2></Col>
+        <Col className={style.form} sm={12} md={8} lg={8} xl={8}>
           <Form
             onSubmit={this.handleSubmit}
             initialValues={initialValues}
+            validationSchema={validationSchema}
+            horizontal
           >
             <Field
-              title="Application ID"
+              title={sharedMessages.appId}
               name="app_id"
               type="text"
+              placeholder={m.appIdPlaceholder}
+              autoFocus
               required
             />
             <Field
-              title="Application Name"
+              title={m.applicationName}
               name="name"
               type="text"
+              placeholder={m.appNamePlaceholder}
               required
             />
             <Field
-              title="Description"
+              title={sharedMessages.description}
               name="description"
+              placeholder={m.appDescPlaceholder}
               type="text"
             />
-            <Button type="submit" message="Create Application" />
-            <Button naked message="Cancel" />
+            <div className={style.submitBar}>
+              <Button type="submit" message={m.createApplication} />
+              <Button type="button" naked message={sharedMessages.cancel} onClick={this.handleCancel} />
+            </div>
           </Form>
+        </Col>
+        <Col className={style.description} sm={12} md={4} xl={3}>
+          <aside>
+            <p>
+              Here is a text that sort of explains the process of adding an application.
+              This is to help users making sense of what is actually happening.
+              <br />
+              We could also provide links and resources from our documentation.
+              Lorem Ipsum dolor sit amet.
+            </p>
+          </aside>
         </Col>
       </Row>
     )
