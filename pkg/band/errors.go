4e917839eed86e00333a40059ca824bd11839b82
Roman Volosatovs <roman@thethingsnetwork.org>
2018-10-29 11:33:51 +0100

    util: Add channel count error to band

--- pkg/band/errors.go
+++ pkg/band/errors.go
@@ -20,6 +20,7 @@ var (
 	errBandNotFound                         = errors.DefineNotFound("band_not_found", "band `{band_id}` not found")
 	errDataRateIndexTooHigh                 = errors.DefineInvalidArgument("data_rate_index_too_high", "data rate index must be lower or equal to {max}")
 	errDataRateOffsetTooHigh                = errors.DefineInvalidArgument("data_rate_offset_too_high", "data rate offset must be lower or equal to {max}")
+	errInvalidChannelCount                  = errors.DefineInvalidArgument("invalid_channel_count", "invalid number of channels defined")
 	errUnknownLoRaWANRegionalParameters     = errors.DefineNotFound("unknown_lorawan_version", "unknown LoRaWAN version")
 	errUnsupportedChMaskCntl                = errors.DefineInvalidArgument("chmaskcntl_unsupported", "ChMaskCntl `{chmaskcntl}` unsupported")
 	errUnsupportedLoRaWANRegionalParameters = errors.DefineInvalidArgument("lorawan_version_unsupported", "LoRaWAN Regional Parameters version not supported; supported versions: {supported}")
