fbb5e2964b84465d775e58ad6e4baf6fdb2e021a
Johan Stokking <johan@thethingsnetwork.org>
2018-11-14 17:09:12 +0100

    util: Remove wrong package comment

--- pkg/util/test/redis.go
+++ pkg/util/test/redis.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package redistest provides a Redis client for testing.
 package test
 
 import (
