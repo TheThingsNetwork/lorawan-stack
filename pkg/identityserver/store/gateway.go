d6c115fb20e5539b815afdc59327529fb1a0909b
Hylke Visser <hylke@thethingsnetwork.org>
2018-11-27 17:00:09 +0100

    is: Add missing fields to Gateway model

--- pkg/identityserver/store/gateway.go
+++ pkg/identityserver/store/gateway.go
@@ -37,13 +37,24 @@ type Gateway struct {
 	Memberships []Membership  `gorm:"polymorphic:Entity;polymorphic_value:gateway"`
 	// END common fields
 
+	BrandID         string `gorm:"type:VARCHAR"`
+	ModelID         string `gorm:"type:VARCHAR"`
+	HardwareVersion string `gorm:"type:VARCHAR"`
+	FirmwareVersion string `gorm:"type:VARCHAR"`
+
 	GatewayServerAddress string `gorm:"type:VARCHAR"`
-	AutoUpdate           bool
-	UpdateChannel        string `gorm:"type:VARCHAR"`
-	FrequencyPlanID      string `gorm:"type:VARCHAR"`
-	ScheduleDownlinkLate bool
-	StatusPublic         bool
-	LocationPublic       bool
+
+	AutoUpdate    bool
+	UpdateChannel string `gorm:"type:VARCHAR"`
+
+	FrequencyPlanID string `gorm:"type:VARCHAR"`
+
+	StatusPublic   bool
+	LocationPublic bool
+
+	ScheduleDownlinkLate   bool
+	EnforceDutyCycle       bool
+	DownlinkPathConstraint int
 }
 
 func init() {
@@ -57,13 +68,21 @@ var gatewayPBSetters = map[string]func(*ttnpb.Gateway, *Gateway){
 	descriptionField:          func(pb *ttnpb.Gateway, gtw *Gateway) { pb.Description = gtw.Description },
 	attributesField:           func(pb *ttnpb.Gateway, gtw *Gateway) { pb.Attributes = attributes(gtw.Attributes).toMap() },
 	contactInfoField:          func(pb *ttnpb.Gateway, gtw *Gateway) { pb.ContactInfo = contactInfos(gtw.ContactInfo).toPB() },
+	brandIDField:              func(pb *ttnpb.Gateway, gtw *Gateway) { pb.BrandID = gtw.BrandID },
+	modelIDField:              func(pb *ttnpb.Gateway, gtw *Gateway) { pb.ModelID = gtw.ModelID },
+	hardwareVersionField:      func(pb *ttnpb.Gateway, gtw *Gateway) { pb.HardwareVersion = gtw.HardwareVersion },
+	firmwareVersionField:      func(pb *ttnpb.Gateway, gtw *Gateway) { pb.FirmwareVersion = gtw.FirmwareVersion },
 	gatewayServerAddressField: func(pb *ttnpb.Gateway, gtw *Gateway) { pb.GatewayServerAddress = gtw.GatewayServerAddress },
 	autoUpdateField:           func(pb *ttnpb.Gateway, gtw *Gateway) { pb.AutoUpdate = gtw.AutoUpdate },
 	updateChannelField:        func(pb *ttnpb.Gateway, gtw *Gateway) { pb.UpdateChannel = gtw.UpdateChannel },
 	frequencyPlanIDField:      func(pb *ttnpb.Gateway, gtw *Gateway) { pb.FrequencyPlanID = gtw.FrequencyPlanID },
-	scheduleDownlinkLateField: func(pb *ttnpb.Gateway, gtw *Gateway) { pb.ScheduleDownlinkLate = gtw.ScheduleDownlinkLate },
 	statusPublicField:         func(pb *ttnpb.Gateway, gtw *Gateway) { pb.StatusPublic = gtw.StatusPublic },
 	locationPublicField:       func(pb *ttnpb.Gateway, gtw *Gateway) { pb.LocationPublic = gtw.LocationPublic },
+	scheduleDownlinkLateField: func(pb *ttnpb.Gateway, gtw *Gateway) { pb.ScheduleDownlinkLate = gtw.ScheduleDownlinkLate },
+	enforceDutyCycleField:     func(pb *ttnpb.Gateway, gtw *Gateway) { pb.EnforceDutyCycle = gtw.EnforceDutyCycle },
+	downlinkPathConstraintField: func(pb *ttnpb.Gateway, gtw *Gateway) {
+		pb.DownlinkPathConstraint = ttnpb.DownlinkPathConstraint(gtw.DownlinkPathConstraint)
+	},
 }
 
 // functions to set fields from the gateway proto into the gateway model.
@@ -77,13 +96,19 @@ var gatewayModelSetters = map[string]func(*Gateway, *ttnpb.Gateway){
 	contactInfoField: func(gtw *Gateway, pb *ttnpb.Gateway) {
 		gtw.ContactInfo = contactInfos(gtw.ContactInfo).updateFromPB(pb.ContactInfo)
 	},
-	gatewayServerAddressField: func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.GatewayServerAddress = pb.GatewayServerAddress },
-	autoUpdateField:           func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.AutoUpdate = pb.AutoUpdate },
-	updateChannelField:        func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.UpdateChannel = pb.UpdateChannel },
-	frequencyPlanIDField:      func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.FrequencyPlanID = pb.FrequencyPlanID },
-	scheduleDownlinkLateField: func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.ScheduleDownlinkLate = pb.ScheduleDownlinkLate },
-	statusPublicField:         func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.StatusPublic = pb.StatusPublic },
-	locationPublicField:       func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.LocationPublic = pb.LocationPublic },
+	brandIDField:                func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.BrandID = pb.BrandID },
+	modelIDField:                func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.ModelID = pb.ModelID },
+	hardwareVersionField:        func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.HardwareVersion = pb.HardwareVersion },
+	firmwareVersionField:        func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.FirmwareVersion = pb.FirmwareVersion },
+	gatewayServerAddressField:   func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.GatewayServerAddress = pb.GatewayServerAddress },
+	autoUpdateField:             func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.AutoUpdate = pb.AutoUpdate },
+	updateChannelField:          func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.UpdateChannel = pb.UpdateChannel },
+	frequencyPlanIDField:        func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.FrequencyPlanID = pb.FrequencyPlanID },
+	statusPublicField:           func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.StatusPublic = pb.StatusPublic },
+	locationPublicField:         func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.LocationPublic = pb.LocationPublic },
+	scheduleDownlinkLateField:   func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.ScheduleDownlinkLate = pb.ScheduleDownlinkLate },
+	enforceDutyCycleField:       func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.EnforceDutyCycle = pb.EnforceDutyCycle },
+	downlinkPathConstraintField: func(gtw *Gateway, pb *ttnpb.Gateway) { gtw.DownlinkPathConstraint = int(pb.DownlinkPathConstraint) },
 }
 
 // fieldMask to use if a nil or empty fieldmask is passed.
@@ -99,19 +124,25 @@ func init() {
 
 // fieldmask path to column name in gateways table.
 var gatewayColumnNames = map[string]string{
-	"ids.gateway_id":          "gateway_id",
-	"ids.eui":                 "gateway_eui",
-	attributesField:           "",
-	contactInfoField:          "",
-	nameField:                 nameField,
-	descriptionField:          descriptionField,
-	gatewayServerAddressField: gatewayServerAddressField,
-	autoUpdateField:           autoUpdateField,
-	updateChannelField:        updateChannelField,
-	frequencyPlanIDField:      frequencyPlanIDField,
-	scheduleDownlinkLateField: scheduleDownlinkLateField,
-	statusPublicField:         statusPublicField,
-	locationPublicField:       locationPublicField,
+	"ids.gateway_id":            "gateway_id",
+	"ids.eui":                   "gateway_eui",
+	attributesField:             "",
+	contactInfoField:            "",
+	nameField:                   nameField,
+	descriptionField:            descriptionField,
+	gatewayServerAddressField:   gatewayServerAddressField,
+	brandIDField:                "brand_id",
+	modelIDField:                "model_id",
+	hardwareVersionField:        "hardware_version",
+	firmwareVersionField:        "firmware_version",
+	autoUpdateField:             autoUpdateField,
+	updateChannelField:          updateChannelField,
+	frequencyPlanIDField:        frequencyPlanIDField,
+	statusPublicField:           statusPublicField,
+	locationPublicField:         locationPublicField,
+	scheduleDownlinkLateField:   scheduleDownlinkLateField,
+	enforceDutyCycleField:       enforceDutyCycleField,
+	downlinkPathConstraintField: downlinkPathConstraintField,
 }
 
 func (gtw Gateway) toPB(pb *ttnpb.Gateway, fieldMask *pbtypes.FieldMask) {
