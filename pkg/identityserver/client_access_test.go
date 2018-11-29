97babb89dcdf3018bf8ce29c5cb127678b324b24
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2018-11-29 21:19:10 +0100

    is: Add client access tests

---
+++ pkg/identityserver/client_access_test.go
@@ -0,0 +1,132 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identityserver
+
+import (
+	"testing"
+
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+	"go.thethings.network/lorawan-stack/pkg/errors"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/util/test"
+	"google.golang.org/grpc"
+)
+
+func init() {
+	clientAccessUser.Admin = false
+	clientAccessUser.State = ttnpb.STATE_APPROVED
+	APIKeys := userAPIKeys(&clientAccessUser.UserIdentifiers)
+	for _, APIKey := range APIKeys.APIKeys {
+		APIKey.Rights = []ttnpb.Right{
+			ttnpb.RIGHT_CLIENT_ALL,
+		}
+	}
+}
+
+func TestClientAccessRightsPermissionDenied(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, creds := clientAccessUser.UserIdentifiers, userCreds(clientAccessUserIdx)
+		clientID := userClients(&userID).Clients[0].ClientIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+
+		reg := ttnpb.NewClientAccessClient(cc)
+
+		_, err := reg.SetCollaborator(ctx, &ttnpb.SetClientCollaboratorRequest{
+			ClientIdentifiers: clientID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_ALL},
+			},
+		}, creds)
+
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+	})
+}
+
+func TestClientAccessPermissionDenied(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID := defaultUser.UserIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+		clientID := userClients(&userID).Clients[0].ClientIdentifiers
+
+		reg := ttnpb.NewClientAccessClient(cc)
+
+		rights, err := reg.ListRights(ctx, &clientID)
+
+		a.So(rights, should.NotBeNil)
+		a.So(rights.Rights, should.BeEmpty)
+		a.So(err, should.BeNil)
+
+		collaborators, err := reg.ListCollaborators(ctx, &clientID)
+
+		a.So(collaborators, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsUnauthenticated(err), should.BeTrue)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetClientCollaboratorRequest{
+			ClientIdentifiers: clientID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_CLIENT_ALL},
+			},
+		})
+
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+	})
+}
+
+func TestClientAccessCRUD(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, creds := defaultUser.UserIdentifiers, userCreds(defaultUserIdx)
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+		clientID := userClients(&userID).Clients[0].ClientIdentifiers
+
+		reg := ttnpb.NewClientAccessClient(cc)
+
+		rights, err := reg.ListRights(ctx, &clientID, creds)
+
+		a.So(rights, should.NotBeNil)
+		a.So(rights.Rights, should.Contain, ttnpb.RIGHT_CLIENT_ALL)
+		a.So(err, should.BeNil)
+
+		collaborators, err := reg.ListCollaborators(ctx, &clientID, creds)
+
+		a.So(collaborators, should.NotBeNil)
+		a.So(collaborators.Collaborators, should.NotBeEmpty)
+		a.So(err, should.BeNil)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetClientCollaboratorRequest{
+			ClientIdentifiers: clientID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_CLIENT_ALL},
+			},
+		}, creds)
+
+		a.So(err, should.BeNil)
+	})
+}
