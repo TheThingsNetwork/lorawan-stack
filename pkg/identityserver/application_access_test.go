0f3b5a5216aa6e807783cc35e1d450ba258c06db
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2018-11-29 19:46:31 +0100

    is: Add application access tests

---
+++ pkg/identityserver/application_access_test.go
@@ -0,0 +1,242 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identityserver
+
+import (
+	"testing"
+
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+	"go.thethings.network/lorawan-stack/pkg/errors"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/util/test"
+	"google.golang.org/grpc"
+)
+
+func init() {
+	applicationAccessUser.Admin = false
+	applicationAccessUser.State = ttnpb.STATE_APPROVED
+	APIKeys := userAPIKeys(&applicationAccessUser.UserIdentifiers)
+	for _, APIKey := range APIKeys.APIKeys {
+		APIKey.Rights = []ttnpb.Right{
+			ttnpb.RIGHT_APPLICATION_SETTINGS_API_KEYS,
+			ttnpb.RIGHT_APPLICATION_SETTINGS_COLLABORATORS,
+		}
+	}
+}
+
+func TestApplicationAccessNotFound(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, creds := defaultUser.UserIdentifiers, userCreds(defaultUserIdx)
+		applicationID := userApplications(&userID).Applications[0].ApplicationIdentifiers
+
+		reg := ttnpb.NewApplicationAccessClient(cc)
+
+		APIKey := ttnpb.APIKey{
+			ID:   "does-not-exist-id",
+			Name: "test-application-api-key-name",
+		}
+
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateApplicationAPIKeyRequest{
+			ApplicationIdentifiers: applicationID,
+			APIKey:                 APIKey,
+		}, creds)
+
+		a.So(updated, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsNotFound(err), should.BeTrue)
+	})
+}
+
+func TestApplicationAccessRightsPermissionDenied(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, creds := applicationAccessUser.UserIdentifiers, userCreds(applicationAccessUserIdx)
+		applicationID := userApplications(&userID).Applications[0].ApplicationIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+
+		reg := ttnpb.NewApplicationAccessClient(cc)
+
+		APIKeyName := "test-application-api-key-name"
+		APIKey, err := reg.CreateAPIKey(ctx, &ttnpb.CreateApplicationAPIKeyRequest{
+			ApplicationIdentifiers: applicationID,
+			Name:                   APIKeyName,
+			Rights:                 []ttnpb.Right{ttnpb.RIGHT_APPLICATION_ALL},
+		}, creds)
+
+		a.So(APIKey, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		APIKey = applicationAPIKeys(&applicationID).APIKeys[0]
+
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateApplicationAPIKeyRequest{
+			ApplicationIdentifiers: applicationID,
+			APIKey:                 *APIKey,
+		}, creds)
+
+		a.So(updated, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetApplicationCollaboratorRequest{
+			ApplicationIdentifiers: applicationID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_APPLICATION_ALL},
+			},
+		}, creds)
+
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+	})
+}
+
+func TestApplicationAccessPermissionDenied(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID := defaultUser.UserIdentifiers
+		applicationID := userApplications(&userID).Applications[0].ApplicationIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+
+		reg := ttnpb.NewApplicationAccessClient(cc)
+
+		rights, err := reg.ListRights(ctx, &applicationID)
+
+		a.So(rights, should.NotBeNil)
+		a.So(rights.Rights, should.BeEmpty)
+		a.So(err, should.BeNil)
+
+		APIKeys, err := reg.ListAPIKeys(ctx, &applicationID)
+
+		a.So(APIKeys, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		collaborators, err := reg.ListCollaborators(ctx, &applicationID)
+
+		a.So(collaborators, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		APIKeyName := "test-application-api-key-name"
+		APIKey, err := reg.CreateAPIKey(ctx, &ttnpb.CreateApplicationAPIKeyRequest{
+			ApplicationIdentifiers: applicationID,
+			Name:                   APIKeyName,
+			Rights:                 []ttnpb.Right{ttnpb.RIGHT_APPLICATION_ALL},
+		})
+
+		a.So(APIKey, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		APIKey = applicationAPIKeys(&applicationID).APIKeys[0]
+
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateApplicationAPIKeyRequest{
+			ApplicationIdentifiers: applicationID,
+			APIKey:                 *APIKey,
+		})
+
+		a.So(updated, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetApplicationCollaboratorRequest{
+			ApplicationIdentifiers: applicationID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_APPLICATION_ALL},
+			},
+		})
+
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+	})
+}
+
+func TestApplicationAccessCRUD(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, creds := defaultUser.UserIdentifiers, userCreds(defaultUserIdx)
+		applicationID := userApplications(&userID).Applications[0].ApplicationIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+
+		reg := ttnpb.NewApplicationAccessClient(cc)
+
+		rights, err := reg.ListRights(ctx, &applicationID, creds)
+
+		a.So(rights, should.NotBeNil)
+		a.So(rights.Rights, should.NotBeEmpty)
+		a.So(err, should.BeNil)
+
+		applicationAPIKeys := applicationAPIKeys(&applicationID)
+		APIKeys, err := reg.ListAPIKeys(ctx, &applicationID, creds)
+
+		a.So(APIKeys, should.NotBeNil)
+		a.So(err, should.BeNil)
+		a.So(len(APIKeys.APIKeys), should.Equal, len(applicationAPIKeys.APIKeys))
+		for i, APIkey := range APIKeys.APIKeys {
+			a.So(APIkey.Name, should.Equal, applicationAPIKeys.APIKeys[i].Name)
+			a.So(APIkey.ID, should.Equal, applicationAPIKeys.APIKeys[i].ID)
+		}
+
+		collaborators, err := reg.ListCollaborators(ctx, &applicationID, creds)
+
+		a.So(collaborators, should.NotBeNil)
+		a.So(collaborators.Collaborators, should.NotBeEmpty)
+		a.So(err, should.BeNil)
+
+		APIKeyName := "test-application-api-key-name"
+		APIKey, err := reg.CreateAPIKey(ctx, &ttnpb.CreateApplicationAPIKeyRequest{
+			ApplicationIdentifiers: applicationID,
+			Name:                   APIKeyName,
+			Rights:                 []ttnpb.Right{ttnpb.RIGHT_APPLICATION_ALL},
+		}, creds)
+
+		a.So(APIKey, should.NotBeNil)
+		a.So(APIKey.Name, should.Equal, APIKeyName)
+		a.So(err, should.BeNil)
+
+		newAPIKeyName := "test-new-api-key"
+		APIKey.Name = newAPIKeyName
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateApplicationAPIKeyRequest{
+			ApplicationIdentifiers: applicationID,
+			APIKey:                 *APIKey,
+		}, creds)
+
+		a.So(updated, should.NotBeNil)
+		a.So(updated.Name, should.Equal, newAPIKeyName)
+		a.So(err, should.BeNil)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetApplicationCollaboratorRequest{
+			ApplicationIdentifiers: applicationID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_APPLICATION_ALL},
+			},
+		}, creds)
+
+		a.So(err, should.BeNil)
+	})
+}
