d30e9721f094c2576bc127f203701c90e33e55bb
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2018-11-30 10:21:18 +0100

    is: Add gateway access tests

---
+++ pkg/identityserver/gateway_access_test.go
@@ -0,0 +1,242 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identityserver
+
+import (
+	"testing"
+
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+	"go.thethings.network/lorawan-stack/pkg/errors"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/util/test"
+	"google.golang.org/grpc"
+)
+
+func init() {
+	gatewayAccessUser.Admin = false
+	gatewayAccessUser.State = ttnpb.STATE_APPROVED
+	APIKeys := userAPIKeys(&gatewayAccessUser.UserIdentifiers)
+	for _, APIKey := range APIKeys.APIKeys {
+		APIKey.Rights = []ttnpb.Right{
+			ttnpb.RIGHT_GATEWAY_SETTINGS_API_KEYS,
+			ttnpb.RIGHT_GATEWAY_SETTINGS_COLLABORATORS,
+		}
+	}
+}
+
+func TestGatewayAccessNotFound(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, creds := defaultUser.UserIdentifiers, userCreds(defaultUserIdx)
+		gatewayID := userGateways(&userID).Gateways[0].GatewayIdentifiers
+
+		reg := ttnpb.NewGatewayAccessClient(cc)
+
+		APIKey := ttnpb.APIKey{
+			ID:   "does-not-exist-id",
+			Name: "test-gateway-api-key-name",
+		}
+
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateGatewayAPIKeyRequest{
+			GatewayIdentifiers: gatewayID,
+			APIKey:             APIKey,
+		}, creds)
+
+		a.So(updated, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsNotFound(err), should.BeTrue)
+	})
+}
+
+func TestGatewayAccessRightsPermissionDenied(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, creds := gatewayAccessUser.UserIdentifiers, userCreds(gatewayAccessUserIdx)
+		gatewayID := userGateways(&userID).Gateways[0].GatewayIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+
+		reg := ttnpb.NewGatewayAccessClient(cc)
+
+		APIKeyName := "test-gateway-api-key-name"
+		APIKey, err := reg.CreateAPIKey(ctx, &ttnpb.CreateGatewayAPIKeyRequest{
+			GatewayIdentifiers: gatewayID,
+			Name:               APIKeyName,
+			Rights:             []ttnpb.Right{ttnpb.RIGHT_GATEWAY_ALL},
+		}, creds)
+
+		a.So(APIKey, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		APIKey = gatewayAPIKeys(&gatewayID).APIKeys[0]
+
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateGatewayAPIKeyRequest{
+			GatewayIdentifiers: gatewayID,
+			APIKey:             *APIKey,
+		}, creds)
+
+		a.So(updated, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetGatewayCollaboratorRequest{
+			GatewayIdentifiers: gatewayID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_GATEWAY_ALL},
+			},
+		}, creds)
+
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+	})
+}
+
+func TestGatewayAccessPermissionDenied(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID := defaultUser.UserIdentifiers
+		gatewayID := userGateways(&userID).Gateways[0].GatewayIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+
+		reg := ttnpb.NewGatewayAccessClient(cc)
+
+		rights, err := reg.ListRights(ctx, &gatewayID)
+
+		a.So(rights, should.NotBeNil)
+		a.So(rights.Rights, should.BeEmpty)
+		a.So(err, should.BeNil)
+
+		APIKeys, err := reg.ListAPIKeys(ctx, &gatewayID)
+
+		a.So(APIKeys, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		collaborators, err := reg.ListCollaborators(ctx, &gatewayID)
+
+		a.So(collaborators, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		APIKeyName := "test-gateway-api-key-name"
+		APIKey, err := reg.CreateAPIKey(ctx, &ttnpb.CreateGatewayAPIKeyRequest{
+			GatewayIdentifiers: gatewayID,
+			Name:               APIKeyName,
+			Rights:             []ttnpb.Right{ttnpb.RIGHT_GATEWAY_ALL},
+		})
+
+		a.So(APIKey, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		APIKey = gatewayAPIKeys(&gatewayID).APIKeys[0]
+
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateGatewayAPIKeyRequest{
+			GatewayIdentifiers: gatewayID,
+			APIKey:             *APIKey,
+		})
+
+		a.So(updated, should.BeNil)
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetGatewayCollaboratorRequest{
+			GatewayIdentifiers: gatewayID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_GATEWAY_ALL},
+			},
+		})
+
+		a.So(err, should.NotBeNil)
+		a.So(errors.IsPermissionDenied(err), should.BeTrue)
+	})
+}
+
+func TestGatewayAccessCRUD(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+
+	testWithIdentityServer(t, func(is *IdentityServer, cc *grpc.ClientConn) {
+		userID, userCreds := defaultUser.UserIdentifiers, userCreds(defaultUserIdx)
+		gatewayID := userGateways(&userID).Gateways[0].GatewayIdentifiers
+		collaboratorID := collaboratorUser.UserIdentifiers.OrganizationOrUserIdentifiers()
+
+		reg := ttnpb.NewGatewayAccessClient(cc)
+
+		rights, err := reg.ListRights(ctx, &gatewayID, userCreds)
+
+		a.So(rights, should.NotBeNil)
+		a.So(rights.Rights, should.Contain, ttnpb.RIGHT_GATEWAY_ALL)
+		a.So(err, should.BeNil)
+
+		gatewayAPIKeys := gatewayAPIKeys(&gatewayID)
+		APIKeys, err := reg.ListAPIKeys(ctx, &gatewayID, userCreds)
+
+		a.So(APIKeys, should.NotBeNil)
+		a.So(len(APIKeys.APIKeys), should.Equal, len(gatewayAPIKeys.APIKeys))
+		a.So(err, should.BeNil)
+		for i, APIkey := range APIKeys.APIKeys {
+			a.So(APIkey.Name, should.Equal, gatewayAPIKeys.APIKeys[i].Name)
+			a.So(APIkey.ID, should.Equal, gatewayAPIKeys.APIKeys[i].ID)
+		}
+
+		collaborators, err := reg.ListCollaborators(ctx, &gatewayID, userCreds)
+
+		a.So(collaborators, should.NotBeNil)
+		a.So(collaborators.Collaborators, should.NotBeEmpty)
+		a.So(err, should.BeNil)
+
+		APIKeyName := "test-gateway-api-key-name"
+		APIKey, err := reg.CreateAPIKey(ctx, &ttnpb.CreateGatewayAPIKeyRequest{
+			GatewayIdentifiers: gatewayID,
+			Name:               APIKeyName,
+			Rights:             []ttnpb.Right{ttnpb.RIGHT_GATEWAY_ALL},
+		}, userCreds)
+
+		a.So(APIKey, should.NotBeNil)
+		a.So(APIKey.Name, should.Equal, APIKeyName)
+		a.So(err, should.BeNil)
+
+		newAPIKeyName := "test-new-gateway-api-key"
+		APIKey.Name = newAPIKeyName
+		updated, err := reg.UpdateAPIKey(ctx, &ttnpb.UpdateGatewayAPIKeyRequest{
+			GatewayIdentifiers: gatewayID,
+			APIKey:             *APIKey,
+		}, userCreds)
+
+		a.So(updated, should.NotBeNil)
+		a.So(updated.Name, should.Equal, newAPIKeyName)
+		a.So(err, should.BeNil)
+
+		_, err = reg.SetCollaborator(ctx, &ttnpb.SetGatewayCollaboratorRequest{
+			GatewayIdentifiers: gatewayID,
+			Collaborator: ttnpb.Collaborator{
+				OrganizationOrUserIdentifiers: *collaboratorID,
+				Rights:                        []ttnpb.Right{ttnpb.RIGHT_GATEWAY_ALL},
+			},
+		}, userCreds)
+
+		a.So(err, should.BeNil)
+	})
+}
