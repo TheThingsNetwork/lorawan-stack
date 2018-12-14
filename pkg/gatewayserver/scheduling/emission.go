60238bc75905a552273b99980e87dd27e7ab18df
Johan Stokking <johan@thethingsnetwork.org>
2018-12-14 15:44:59 +0800

    gs: Add emission for scheduling

---
+++ pkg/gatewayserver/scheduling/emission.go
@@ -0,0 +1,88 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scheduling
+
+import (
+	"time"
+
+	"go.thethings.network/lorawan-stack/pkg/frequencyplans"
+)
+
+// NewEmission returns a new Emission with the given values.
+func NewEmission(starts, duration time.Duration) Emission {
+	return Emission{t: starts, d: duration}
+}
+
+// Emission contains the scheduled time and duration of an emission.
+type Emission struct {
+	t, d time.Duration
+}
+
+// Starts returns the time when the emission starts.
+func (em Emission) Starts() time.Duration { return em.t }
+
+// Ends returns the time when the emission ends.
+func (em Emission) Ends() time.Duration { return em.t + em.d }
+
+// Duration returns the duration of the emission.
+func (em Emission) Duration() time.Duration { return em.d }
+
+// OffAir returns the time-off-air of the emission.
+func (em Emission) OffAir(toa frequencyplans.TimeOffAir) time.Duration {
+	d := time.Duration(float32(em.d) * toa.Fraction)
+	if d < toa.Duration {
+		d = toa.Duration
+	}
+	return d
+}
+
+// Within returns the duration of the emission that happens within the given window.
+func (em Emission) Within(from, to time.Duration) time.Duration {
+	if em.Ends() < from || em.t > to {
+		return 0
+	}
+	if em.t < from {
+		return em.Ends() - from
+	}
+	return em.d
+}
+
+// EndsWithOffAir returns the time when the emission ends plus the time-off-air.
+func (em Emission) EndsWithOffAir(toa frequencyplans.TimeOffAir) time.Duration {
+	return em.Ends() + em.OffAir(toa)
+}
+
+// BeforeWithOffAir returns the time between the end of this emission to the start of the given other emission, considering time-off-air.
+func (em Emission) BeforeWithOffAir(other Emission, toa frequencyplans.TimeOffAir) time.Duration {
+	return other.Starts() - em.EndsWithOffAir(toa)
+}
+
+// AfterWithOffAir returns the time between the end of the given other emission to the start of this emission, considering time-off-air.
+func (em Emission) AfterWithOffAir(other Emission, toa frequencyplans.TimeOffAir) time.Duration {
+	return em.Starts() - other.EndsWithOffAir(toa)
+}
+
+// Emissions is an list of emissions.
+type Emissions []Emission
+
+// Insert inserts the given emission to the emissions by preserving order.
+func (ems Emissions) Insert(em Emission) Emissions {
+	for i := range ems {
+		if ems[i].t > em.t {
+			return append(ems[:i], append([]Emission{em}, ems[i:]...)...)
+		}
+	}
+	return append(ems, em)
+}
---
+++ pkg/gatewayserver/scheduling/emission_test.go
@@ -0,0 +1,77 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scheduling_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/smartystreets/assertions"
+	"go.thethings.network/lorawan-stack/pkg/frequencyplans"
+	"go.thethings.network/lorawan-stack/pkg/gatewayserver/scheduling"
+	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
+)
+
+func TestEmission(t *testing.T) {
+	a := assertions.New(t)
+	em := scheduling.NewEmission(10*time.Second, time.Second)
+
+	a.So(em.Starts(), should.Equal, 10*time.Second)
+	a.So(em.Ends(), should.Equal, 11*time.Second)
+	a.So(em.Duration(), should.Equal, time.Second)
+
+	a.So(em.Within(10*time.Second, 20*time.Second), should.Equal, time.Second)
+	a.So(em.Within(11*time.Second, 20*time.Second), should.Equal, 0)
+	a.So(em.Within(0*time.Second, 10*time.Second), should.Equal, time.Second)
+	a.So(em.Within(0*time.Second, 11*time.Second), should.Equal, time.Second)
+
+	a.So(em.OffAir(frequencyplans.TimeOffAir{Fraction: 0.5, Duration: 2 * time.Second}), should.Equal, 2*time.Second)
+	a.So(em.OffAir(frequencyplans.TimeOffAir{Fraction: 0.5, Duration: 0}), should.Equal, 500*time.Millisecond)
+
+	toa := frequencyplans.TimeOffAir{Duration: 2 * time.Second}
+
+	// No conflicts.
+	a.So(em.BeforeWithOffAir(scheduling.NewEmission(13*time.Second, time.Second), toa), should.Equal, 0)
+	a.So(em.AfterWithOffAir(scheduling.NewEmission(13*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.BeforeWithOffAir(scheduling.NewEmission(20*time.Second, time.Second), toa), should.Equal, 7*time.Second)
+	a.So(em.AfterWithOffAir(scheduling.NewEmission(20*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.BeforeWithOffAir(scheduling.NewEmission(7*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.AfterWithOffAir(scheduling.NewEmission(7*time.Second, time.Second), toa), should.Equal, 0)
+	a.So(em.BeforeWithOffAir(scheduling.NewEmission(5*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.AfterWithOffAir(scheduling.NewEmission(5*time.Second, time.Second), toa), should.Equal, 2*time.Second)
+
+	// Conflicts.
+	a.So(em.BeforeWithOffAir(scheduling.NewEmission(9*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.AfterWithOffAir(scheduling.NewEmission(9*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.BeforeWithOffAir(scheduling.NewEmission(10*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.AfterWithOffAir(scheduling.NewEmission(10*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.BeforeWithOffAir(scheduling.NewEmission(11*time.Second, time.Second), toa), should.BeLessThan, 0)
+	a.So(em.AfterWithOffAir(scheduling.NewEmission(11*time.Second, time.Second), toa), should.BeLessThan, 0)
+}
+
+func TestEmissions(t *testing.T) {
+	a := assertions.New(t)
+	var ems scheduling.Emissions
+	ems = ems.Insert(scheduling.NewEmission(10*time.Second, time.Second))
+	ems = ems.Insert(scheduling.NewEmission(6*time.Second, time.Second))
+	ems = ems.Insert(scheduling.NewEmission(8*time.Second, time.Second))
+	ems = ems.Insert(scheduling.NewEmission(12*time.Second, time.Second))
+
+	a.So(ems, should.HaveLength, 4)
+	a.So(ems[0].Starts(), should.Equal, 6*time.Second)
+	a.So(ems[1].Starts(), should.Equal, 8*time.Second)
+	a.So(ems[2].Starts(), should.Equal, 10*time.Second)
+	a.So(ems[3].Starts(), should.Equal, 12*time.Second)
+}
