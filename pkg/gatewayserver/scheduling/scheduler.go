51ee583b529af43207cd5115ab6c5dcc5e04989b
Johan Stokking <johan@thethingsnetwork.org>
2018-12-14 15:46:43 +0800

    gs: Add scheduler with conflict detection and class C support

---
+++ pkg/gatewayserver/scheduling/scheduler.go
@@ -0,0 +1,180 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package scheduling offer convenience methods to manage RF packets that must respect scheduling constraints.
+package scheduling
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"go.thethings.network/lorawan-stack/pkg/band"
+	"go.thethings.network/lorawan-stack/pkg/errors"
+	"go.thethings.network/lorawan-stack/pkg/frequencyplans"
+	"go.thethings.network/lorawan-stack/pkg/toa"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+)
+
+// QueueDelay indicates the time the gateway needs to recharge the concentrator between items in the queue.
+// This is a conservative value as implemented in the Semtech UDP Packet Forwarder reference implementation,
+// see https://github.com/Lora-net/packet_forwarder/blob/v4.0.1/lora_pkt_fwd/src/jitqueue.c#L39
+var QueueDelay = 30 * time.Millisecond
+
+// Clock represents an absolute time source.
+type Clock interface {
+	// Now returns an indication of the current concentrator time.
+	Now() time.Duration
+	// ConcentratorTime returns the concentrator time based on the given absolute time.
+	ConcentratorTime(time.Time) time.Duration
+}
+
+// NewScheduler instantiates a new Scheduler for the given clock source and frequency plan.
+func NewScheduler(ctx context.Context, clock Clock, fp *frequencyplans.FrequencyPlan) (*Scheduler, error) {
+	s := &Scheduler{
+		clock:             clock,
+		respectsDwellTime: fp.RespectsDwellTime,
+		timeOffAir:        fp.TimeOffAir,
+	}
+	band, err := band.GetByID(fp.BandID)
+	if err != nil {
+		return nil, err
+	}
+	for _, subBand := range band.BandDutyCycles {
+		sb := NewSubBand(ctx, subBand, clock, nil)
+		s.subBands = append(s.subBands, sb)
+	}
+	return s, nil
+}
+
+// Scheduler is a packet scheduler that takes time conflicts and sub-band restrictions into account.
+type Scheduler struct {
+	clock             Clock
+	respectsDwellTime func(isDownlink bool, frequency uint64, duration time.Duration) bool
+	timeOffAir        frequencyplans.TimeOffAir
+	subBands          []*SubBand
+	emissionsMu       sync.Mutex
+	emissions         Emissions
+}
+
+var errSubBandNotFound = errors.DefineFailedPrecondition("sub_band_not_found", "sub-band not found for frequency `{frequency}` Hz")
+
+func (s *Scheduler) findSubBand(frequency uint64) (*SubBand, error) {
+	for _, subBand := range s.subBands {
+		if subBand.Comprises(frequency) {
+			return subBand, nil
+		}
+	}
+	return nil, errSubBandNotFound.WithAttributes("frequency", frequency)
+}
+
+var (
+	errDwellTime = errors.DefineFailedPrecondition("dwell_time", "packet exceeds dwell time restriction")
+)
+
+func (s *Scheduler) newEmission(payloadSize int, settings ttnpb.TxSettings) (Emission, error) {
+	d, err := toa.Compute(payloadSize, settings)
+	if err != nil {
+		return Emission{}, err
+	}
+	if !s.respectsDwellTime(true, settings.Frequency, d) {
+		return Emission{}, errDwellTime
+	}
+	var relative time.Duration
+	if settings.Time != nil {
+		relative = s.clock.ConcentratorTime(*settings.Time)
+	} else {
+		relative = time.Duration(settings.Timestamp) * time.Microsecond
+	}
+	return NewEmission(relative, d), nil
+}
+
+var errConflict = errors.DefineResourceExhausted("conflict", "scheduling conflict")
+
+// ScheduleAt attempts to schedule the given Tx settings with the given priority.
+func (s *Scheduler) ScheduleAt(payloadSize int, settings ttnpb.TxSettings, priority ttnpb.TxSchedulePriority) error {
+	sb, err := s.findSubBand(settings.Frequency)
+	if err != nil {
+		return err
+	}
+	em, err := s.newEmission(payloadSize, settings)
+	if err != nil {
+		return err
+	}
+	s.emissionsMu.Lock()
+	defer s.emissionsMu.Unlock()
+	for _, other := range s.emissions {
+		if em.AfterWithOffAir(other, s.timeOffAir)-QueueDelay < 0 && em.BeforeWithOffAir(other, s.timeOffAir)-QueueDelay < 0 {
+			return errConflict
+		}
+	}
+	if err := sb.Schedule(em, priority); err != nil {
+		return err
+	}
+	s.emissions = s.emissions.Insert(em)
+	return nil
+}
+
+// ScheduleAnytime attempts to schedule the given Tx settings with the given priority from the time in the settings.
+// This method returns the emission.
+//
+// The scheduler does not support immediate scheduling, i.e. sending a message to the gateway that should be transmitted
+// immediately. The reason for this is that this scheduler cannot determine conflicts or enforce duty-cycle when the
+// emission time is unknown. Therefore, when the timestamp is set to 0, the estimated current concentrator time plus
+// ScheduleDelayLong will be used.
+func (s *Scheduler) ScheduleAnytime(ctx context.Context, payloadSize int, settings ttnpb.TxSettings, priority ttnpb.TxSchedulePriority) (Emission, error) {
+	sb, err := s.findSubBand(settings.Frequency)
+	if err != nil {
+		return Emission{}, err
+	}
+	em, err := s.newEmission(payloadSize, settings)
+	if err != nil {
+		return Emission{}, err
+	}
+	s.emissionsMu.Lock()
+	defer s.emissionsMu.Unlock()
+	i := 0
+	next := func() time.Duration {
+		if len(s.emissions) == 0 {
+			// No emissions; schedule at the requested time.
+			return em.t
+		}
+		for i < len(s.emissions)-1 {
+			// Find a window between two emissions that does not conflict with either side.
+			prevConflicts := s.emissions[i].AfterWithOffAir(em, s.timeOffAir)-QueueDelay < 0
+			if prevConflicts {
+				// Schedule right after previous to resolve conflict.
+				em.t = s.emissions[i].EndsWithOffAir(s.timeOffAir) + QueueDelay
+			}
+			nextConflicts := em.BeforeWithOffAir(s.emissions[i+1], s.timeOffAir)-QueueDelay < 0
+			if nextConflicts {
+				// If we conflict with the next, try the next window.
+				em.t = s.emissions[i+1].EndsWithOffAir(s.timeOffAir) + QueueDelay
+				i++
+				continue
+			}
+			// No conflicts, but advance counter for potential next iteration.
+			i++
+			return em.t
+		}
+		// No emissions to schedule in between; schedule after last emission.
+		return s.emissions[len(s.emissions)-1].EndsWithOffAir(s.timeOffAir) + QueueDelay
+	}
+	em, err = sb.ScheduleAnytime(em.d, next, priority)
+	if err != nil {
+		return Emission{}, err
+	}
+	s.emissions = s.emissions.Insert(em)
+	return em, nil
+}
---
+++ pkg/gatewayserver/scheduling/scheduler_test.go
@@ -0,0 +1,257 @@
+// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scheduling_test
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/smartystreets/assertions"
+	"go.thethings.network/lorawan-stack/pkg/band"
+	"go.thethings.network/lorawan-stack/pkg/errors"
+	"go.thethings.network/lorawan-stack/pkg/frequencyplans"
+	"go.thethings.network/lorawan-stack/pkg/gatewayserver/scheduling"
+	"go.thethings.network/lorawan-stack/pkg/toa"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/util/test"
+	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
+)
+
+func TestScheduleAt(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+	clock := &mockClock{}
+	fp := &frequencyplans.FrequencyPlan{
+		BandID: band.EU_863_870,
+		TimeOffAir: frequencyplans.TimeOffAir{
+			Duration: time.Second,
+		},
+		DwellTime: frequencyplans.DwellTime{
+			Downlinks: boolPtr(true),
+			Duration:  durationPtr(2 * time.Second),
+		},
+	}
+	scheduler, err := scheduling.NewScheduler(ctx, clock, fp)
+	a.So(err, should.BeNil)
+
+	for i, tc := range []struct {
+		PayloadSize int
+		Settings    ttnpb.TxSettings
+		Priority    ttnpb.TxSchedulePriority
+		ExpectedToa time.Duration
+		AssertError *errors.Definition
+	}{
+		{
+			PayloadSize: 51,
+			Settings: ttnpb.TxSettings{
+				Modulation:      ttnpb.Modulation_LORA,
+				Frequency:       869525000,
+				Bandwidth:       125000,
+				SpreadingFactor: 12,
+				CodingRate:      "4/5",
+				Timestamp:       100,
+			},
+			ExpectedToa: 2465792 * time.Microsecond,
+			Priority:    ttnpb.TxSchedulePriority_NORMAL,
+			// Exceeding dwell time of 2 seconds.
+			AssertError: &scheduling.ErrDwellTime,
+		},
+		{
+			PayloadSize: 10,
+			Settings: ttnpb.TxSettings{
+				Modulation:      ttnpb.Modulation_LORA,
+				Frequency:       869525000,
+				Bandwidth:       125000,
+				SpreadingFactor: 7,
+				CodingRate:      "4/5",
+				Timestamp:       100,
+			},
+			ExpectedToa: 41216 * time.Microsecond,
+			Priority:    ttnpb.TxSchedulePriority_NORMAL,
+		},
+		{
+			PayloadSize: 10,
+			Settings: ttnpb.TxSettings{
+				Modulation:      ttnpb.Modulation_LORA,
+				Frequency:       869525000,
+				Bandwidth:       125000,
+				SpreadingFactor: 7,
+				CodingRate:      "4/5",
+				Timestamp:       100,
+			},
+			ExpectedToa: 41216 * time.Microsecond,
+			Priority:    ttnpb.TxSchedulePriority_NORMAL,
+			// Overlapping with previous transmission.
+			AssertError: &scheduling.ErrConflict,
+		},
+		{
+			PayloadSize: 10,
+			Settings: ttnpb.TxSettings{
+				Modulation:      ttnpb.Modulation_LORA,
+				Frequency:       869525000,
+				Bandwidth:       125000,
+				SpreadingFactor: 7,
+				CodingRate:      "4/5",
+				Timestamp:       100,
+			},
+			ExpectedToa: 41216 * time.Microsecond,
+			Priority:    ttnpb.TxSchedulePriority_NORMAL,
+			// Right after previous transmission; not respecting time-off-air and queue delay.
+			AssertError: &scheduling.ErrConflict,
+		},
+		{
+			PayloadSize: 10,
+			Settings: ttnpb.TxSettings{
+				Modulation:      ttnpb.Modulation_LORA,
+				Frequency:       869525000,
+				Bandwidth:       125000,
+				SpreadingFactor: 7,
+				CodingRate:      "4/5",
+				Timestamp:       100 + 41216 + 1000000, // time-on-air + time-off-air.
+			},
+			ExpectedToa: 41216 * time.Microsecond,
+			Priority:    ttnpb.TxSchedulePriority_NORMAL,
+			// Right after previous transmission; not respecting queue delay.
+			AssertError: &scheduling.ErrConflict,
+		},
+		{
+			PayloadSize: 10,
+			Settings: ttnpb.TxSettings{
+				Modulation:      ttnpb.Modulation_LORA,
+				Frequency:       869525000,
+				Bandwidth:       125000,
+				SpreadingFactor: 7,
+				CodingRate:      "4/5",
+				Timestamp:       100 + 41216 + 1000000 + 30000, // time-on-air + time-off-air + queue delay
+			},
+			ExpectedToa: 41216 * time.Microsecond,
+			Priority:    ttnpb.TxSchedulePriority_NORMAL,
+		},
+		{
+			PayloadSize: 20,
+			Settings: ttnpb.TxSettings{
+				Modulation:      ttnpb.Modulation_LORA,
+				Frequency:       868100000,
+				Bandwidth:       125000,
+				SpreadingFactor: 12,
+				CodingRate:      "4/5",
+				Timestamp:       20000000, // In next duty-cycle window; discard previous.
+			},
+			ExpectedToa: 1318912 * time.Microsecond,
+			Priority:    ttnpb.TxSchedulePriority_NORMAL,
+			// Exceeds duty-cycle limitation of 1% in 868.0 - 868.6.
+			AssertError: &scheduling.ErrDutyCycle,
+		},
+	} {
+		tcok := t.Run(strconv.Itoa(i), func(t *testing.T) {
+			a := assertions.New(t)
+			d, err := toa.Compute(tc.PayloadSize, tc.Settings)
+			a.So(err, should.BeNil)
+			a.So(d, should.Equal, tc.ExpectedToa)
+			err = scheduler.ScheduleAt(tc.PayloadSize, tc.Settings, tc.Priority)
+			if err != nil && tc.AssertError == nil {
+				a.So(err, should.BeNil)
+			} else if err != nil {
+				if !a.So(err, should.HaveSameErrorDefinitionAs, *tc.AssertError) {
+					t.Fatalf("Unexpected error: %v", err)
+				}
+			} else {
+				a.So(err, should.BeNil)
+			}
+		})
+		if !tcok {
+			t.FailNow()
+		}
+	}
+}
+
+func TestScheduleAnytime(t *testing.T) {
+	a := assertions.New(t)
+	ctx := test.Context()
+	clock := &mockClock{}
+	fp := &frequencyplans.FrequencyPlan{
+		BandID: band.EU_863_870,
+		TimeOffAir: frequencyplans.TimeOffAir{
+			Duration: time.Second,
+		},
+		DwellTime: frequencyplans.DwellTime{
+			Downlinks: boolPtr(true),
+			Duration:  durationPtr(2 * time.Second),
+		},
+	}
+	scheduler, err := scheduling.NewScheduler(ctx, clock, fp)
+	a.So(err, should.BeNil)
+
+	settingsAt := func(frequency uint64, sf, t uint32) ttnpb.TxSettings {
+		return ttnpb.TxSettings{
+			Modulation:      ttnpb.Modulation_LORA,
+			Frequency:       frequency,
+			Bandwidth:       125000,
+			SpreadingFactor: sf,
+			CodingRate:      "4/5",
+			Timestamp:       t,
+		}
+	}
+
+	// Scheduling two items, occupying considering time-on-air, time-off-air and queue delay.
+	// Time-on-air is 41216 us, time-off-air is 1000000 us, queue delay is 30000 us.
+	// 1: [1000000, 2071216]
+	// 2: [4000000, 5071216]
+	err = scheduler.ScheduleAt(10, settingsAt(869525000, 7, 1000000), ttnpb.TxSchedulePriority_NORMAL)
+	a.So(err, should.BeNil)
+	err = scheduler.ScheduleAt(10, settingsAt(869525000, 7, 4000000), ttnpb.TxSchedulePriority_NORMAL)
+	a.So(err, should.BeNil)
+
+	// Try schedule a transmission from 1000000 us.
+	// Time-on-air is 41216 us, time-off-air is 1000000 us, queue delay is 30000 us.
+	// It fits between 1 and 2, so it should be right after 1.
+	// 1: [1000000, 2071216]
+	// 3: [2071216, 3142432]
+	// 2: [4000000, 5071216]
+	em, err := scheduler.ScheduleAnytime(10, settingsAt(869525000, 7, 1000000), ttnpb.TxSchedulePriority_NORMAL)
+	a.So(err, should.BeNil)
+	a.So(em.Starts(), should.Equal, 2071216*time.Microsecond)
+
+	// Try schedule another transmission from 1000000 us.
+	// Time-on-air is 41216 us, time-off-air is 1000000 us, queue delay is 30000 us.
+	// It does not fit between 1, 3, and 2, so it should be right after 2.
+	// 1: [1000000, 2071216]
+	// 3: [2071216, 3142432]
+	// 2: [4000000, 5071216]
+	// 4: [5071216, 6142432]
+	em, err = scheduler.ScheduleAnytime(10, settingsAt(869525000, 7, 1000000), ttnpb.TxSchedulePriority_NORMAL)
+	a.So(err, should.BeNil)
+	a.So(em.Starts(), should.Equal, 5071216*time.Microsecond)
+
+	// Try schedule another transmission from 1000000 us.
+	// Time-on-air is 991232 us, time-off-air is 1000000 us, queue delay is 30000 us.
+	// It's 9.91% in a 10% duty-cycle sub-band, almost hitting the limit, so it should be pushed to right after transmission 4.
+	// Transmission starts then at 5071216 (start of 4) + 41216 (time-on-air of 4) + 10000000 (duty-cycle window) - 991232 (this time-on-air).
+	// 1: [1000000, 2071216]
+	// 3: [2071216, 3142432]
+	// 2: [4000000, 5071216]
+	// 4: [5071216, 6142432]
+	// 5: [14121200, 15112432]
+	em, err = scheduler.ScheduleAnytime(10, settingsAt(869525000, 12, 1000000), ttnpb.TxSchedulePriority_HIGHEST)
+	a.So(err, should.BeNil)
+	a.So(em.Starts(), should.Equal, 14121200*time.Microsecond)
+
+	// Try schedule another transmission from 1000000 us.
+	// Time-on-air is 991232 us, time-off-air is 1000000 us, queue delay is 30000 us.
+	// It's 9.91% in a 1% duty-cycle sub-band, so it hits the duty-cycle limitation.
+	_, err = scheduler.ScheduleAnytime(10, settingsAt(868100000, 12, 1000000), ttnpb.TxSchedulePriority_HIGHEST)
+	a.So(err, should.HaveSameErrorDefinitionAs, scheduling.ErrDutyCycle)
+}
