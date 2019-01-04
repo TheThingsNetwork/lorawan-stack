57518f29c3bfb39819459a79b61cfd53a8c5315a
Johan Stokking <johan@thethingsnetwork.org>
2019-01-04 10:09:52 +0100

    util: Add field mask path utilities

---
+++ pkg/ttnpb/fieldmask_utils.go
@@ -0,0 +1,60 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ttnpb
+
+import "strings"
+
+// TopLevelFields returns the unique top level fields of the given paths.
+func TopLevelFields(paths []string) []string {
+	seen := make(map[string]struct{}, len(paths))
+	out := make([]string, 0, len(paths))
+	for _, path := range paths {
+		parts := strings.SplitN(path, ".", 2)
+		if _, ok := seen[parts[0]]; ok {
+			continue
+		}
+		seen[parts[0]] = struct{}{}
+		out = append(out, parts[0])
+	}
+	return out
+}
+
+// HasOnlyAllowedFields returns whether the given requested paths only contains paths that are allowed.
+// The requested fields (i.e. `a.b`) may be of a lower level than the allowed path (i.e. `a`).
+func HasOnlyAllowedFields(requested []string, allowed ...string) bool {
+nextRequested:
+	for _, requested := range requested {
+		for _, allowed := range allowed {
+			if requested == allowed || strings.HasPrefix(requested, allowed+".") {
+				continue nextRequested
+			}
+		}
+		return false
+	}
+	return true
+}
+
+// HasAnyField returns whether the given requested paths contain any of the given fields.
+// The requested fields (i.e. `a.b`) may be of a lower level than the search path (i.e. `a`).
+func HasAnyField(requested []string, search ...string) bool {
+	for _, requested := range requested {
+		for _, search := range search {
+			if requested == search || strings.HasPrefix(search, requested+".") {
+				return true
+			}
+		}
+	}
+	return false
+}
---
+++ pkg/ttnpb/fieldmask_utils_test.go
@@ -0,0 +1,82 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ttnpb_test
+
+import (
+	"testing"
+
+	"github.com/smartystreets/assertions"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
+)
+
+func TestTopLevelFields(t *testing.T) {
+	a := assertions.New(t)
+	paths := []string{
+		"a",
+		"b",
+		"b.c",
+		"b.c.d",
+	}
+	a.So(ttnpb.TopLevelFields(paths), should.Resemble, []string{"a", "b"})
+}
+
+func TestHasOnlyAllowedFields(t *testing.T) {
+	a := assertions.New(t)
+	allowed := []string{
+		"a",
+		"b.c",
+		"d.e",
+	}
+
+	{
+		requested := []string{
+			"a",
+			"b.c",
+			"b.c.d", // lower level allowed
+		}
+		a.So(ttnpb.HasOnlyAllowedFields(requested, allowed...), should.BeTrue)
+	}
+
+	{
+		requested := []string{
+			"a",
+			"e.f",
+		}
+		a.So(ttnpb.HasOnlyAllowedFields(requested, allowed...), should.BeFalse)
+	}
+
+	{
+		requested := []string{
+			"a",
+			"d", // higher level not allowed
+		}
+		a.So(ttnpb.HasOnlyAllowedFields(requested, allowed...), should.BeFalse)
+	}
+}
+
+func TestHasAnyField(t *testing.T) {
+	a := assertions.New(t)
+	requested := []string{
+		"a",
+		"b.c",
+		"d",
+	}
+	a.So(ttnpb.HasAnyField(requested, "x", "a"), should.BeTrue)
+	a.So(ttnpb.HasAnyField(requested, "x.y", "b"), should.BeFalse)
+	a.So(ttnpb.HasAnyField(requested, "x", "b.c"), should.BeTrue)
+	a.So(ttnpb.HasAnyField(requested, "d"), should.BeTrue)
+	a.So(ttnpb.HasAnyField(requested, "d.e", "b"), should.BeTrue)
+}
