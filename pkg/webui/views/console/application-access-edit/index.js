56567c0a1aa5570e3b2a16b04b3084c6c93d8dec
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-01-14 13:52:45 +0100

    console: Add app access edit page

--- pkg/webui/views/console/application-access-edit/index.js
+++ pkg/webui/views/console/application-access-edit/index.js
@@ -13,15 +13,55 @@
 // limitations under the License.
 
 import React from 'react'
+import { connect } from 'react-redux'
+import bind from 'autobind-decorator'
+import { Container, Col, Row } from 'react-grid-system'
+import { defineMessages } from 'react-intl'
+import * as Yup from 'yup'
 
 import { withBreadcrumb } from '../../../components/breadcrumbs/context'
 import Breadcrumb from '../../../components/breadcrumbs/breadcrumb'
 import sharedMessages from '../../../lib/shared-messages'
 
+import Form from '../../../components/form'
+import Field from '../../../components/field'
+import Button from '../../../components/button'
+import Spinner from '../../../components/spinner'
+import ModalButton from '../../../components/button/modal-button'
+import Message from '../../../lib/components/message'
+
+import { getApplicationsRightsList } from '../../../actions/applications'
+
+import style from './application-access-edit.styl'
+
+const m = defineMessages({
+  deleteKey: 'Delete Key',
+  modalWarning:
+    'Are you sure you want to delete "{keyName}"? Deleting an application access key cannot be undone!',
+})
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, sharedMessages.validateTooShort)
+    .max(50, sharedMessages.validateTooLong)
+    .required(sharedMessages.validateRequired),
+  description: Yup.string(),
+})
+
+@connect(function ({ apiKeys, rights }, props) {
+  const { appId, apiKeyId } = props.match.params
+
+  return {
+    apiKeyId,
+    appId,
+    apiKey: apiKeys.applications[appId].keys.find(k => k.id === apiKeyId),
+    applicationsRights: rights.applications.rights,
+    fetching: rights.applications.fetching,
+    error: rights.applications.error,
+  }
+})
 @withBreadcrumb('apps.single.access.single', function (props) {
-  const { match } = props
-  const appId = match.params.appId
-  const keyId = match.params.apiKeyId
+  const { appId, keyId } = props
 
   return (
     <Breadcrumb
@@ -31,9 +71,115 @@ import sharedMessages from '../../../lib/shared-messages'
     />
   )
 })
+@bind
 export default class ApplicationAccessEdit extends React.Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+
+    dispatch(getApplicationsRightsList())
+  }
+
+  handleSubmit () { }
+
+  handleDelete () { }
+
+  handleCancel () { }
 
   render () {
-    return <div>edit app access</div>
+    const { apiKey, applicationsRights, fetching, error } = this.props
+
+    if (fetching ) {
+      return <Spinner />
+    }
+
+    if (error) {
+      return 'ERROR'
+    }
+
+    const { rightsItems, rightsValues } = applicationsRights.reduce(
+      function (acc, right) {
+        acc.rightsItems.push(
+          <Field
+            key={right}
+            name={right}
+            type="checkbox"
+            title={{ id: `enum:${right}` }}
+            form
+          />
+        )
+        acc.rightsValues[right] = apiKey.rights.includes(right)
+
+        return acc
+      },
+      {
+        rightsItems: [],
+        rightsValues: {},
+      }
+    )
+
+    const initialFormValues = {
+      name: apiKey.name,
+      description: apiKey.description,
+      ...rightsValues,
+    }
+
+    return (
+      <Container>
+        <Row>
+          <Col lg={8} md={12}>
+            <Message component="h2" content={sharedMessages.edit} />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={8} md={12}>
+            <Form
+              onReset={this.handleCancel}
+              horizontal
+              onSubmit={this.handleSubmit}
+              initialValues={initialFormValues}
+              validationSchema={validationSchema}
+            >
+              <Message
+                component="h4"
+                content={sharedMessages.generalInformation}
+              />
+              <Field
+                title={sharedMessages.name}
+                required
+                name="name"
+                type="text"
+              />
+              <Field
+                title={sharedMessages.description}
+                name="description"
+                type="text"
+              />
+              <Message component="h4" content={sharedMessages.rights} />
+              {rightsItems}
+              <div className={style.submitBar}>
+                <div>
+                  <Button type="submit" message={sharedMessages.saveChanges} />
+                  <Button type="reset" naked secondary message={sharedMessages.cancel} />
+                </div>
+                <ModalButton
+                  type="button"
+                  icon="delete"
+                  danger
+                  naked
+                  message={m.deleteKey}
+                  modalData={{
+                    message: {
+                      values: { keyName: apiKey.name },
+                      ...m.modalWarning,
+                    },
+                  }}
+                  onApprove={this.handleDelete}
+                />
+              </div>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
