3537382c75b7ed7ab6b9758b7903c0bdb0e97bd1
KrishnaIyer <mail@krishnaiyereaswaran.com>
2019-01-16 14:21:50 +0100

    gs: Update copyright to 2019

--- pkg/gatewayserver/io/basicstation/messages/messages.go
+++ pkg/gatewayserver/io/basicstation/messages/messages.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
