517bf531634651a06794781c61e46465bbfdecd2
Johan Stokking <johan@stokking.nl>
2019-01-21 16:06:44 +0100

    gs: Fix unnecessary conversion

--- pkg/gatewayserver/scheduling/clock_test.go
+++ pkg/gatewayserver/scheduling/clock_test.go
@@ -34,14 +34,14 @@ func TestConcentratorClock(t *testing.T) {
 
 	{
 		// Test first roll-over to 4299967295 us (math.MaxUint32 + 5000000).
-		passed := time.Duration(time.Microsecond * (math.MaxUint32 + 5000000))
+		passed := time.Microsecond * (math.MaxUint32 + 5000000)
 		clock.SyncWithGateway(5000000, time.Unix(10, 0).Add(passed), time.Unix(0, 0).Add(passed))
 		a.So(clock.ServerTime(time.Unix(10, 100).Add(passed)), should.Equal, passed+100)
 	}
 
 	{
 		// Test second roll-over to 8589934590 us (2 * math.MaxUint32).
-		passed := time.Duration(time.Microsecond * 2 * math.MaxUint32)
+		passed := time.Microsecond * 2 * math.MaxUint32
 		clock.SyncWithGateway(0, time.Unix(10, 0).Add(passed), time.Unix(0, 0).Add(passed))
 		a.So(clock.ServerTime(time.Unix(10, 100).Add(passed)), should.Equal, passed+100)
 	}
