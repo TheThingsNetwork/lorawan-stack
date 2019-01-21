178d4fa24cfc408c34ecbea0bb256a2a9f9d870a
KrishnaIyer <mail@krishnaiyereaswaran.com>
2019-01-21 18:03:55 +0100

    gs: Add tests for JoinRequest handling

--- pkg/gatewayserver/io/basicstation/basicstation_test.go
+++ pkg/gatewayserver/io/basicstation/basicstation_test.go
@@ -25,6 +25,8 @@ import (
 	"github.com/smartystreets/assertions"
 	"go.thethings.network/lorawan-stack/pkg/component"
 	"go.thethings.network/lorawan-stack/pkg/config"
+	"go.thethings.network/lorawan-stack/pkg/encoding/lorawan"
+	"go.thethings.network/lorawan-stack/pkg/gatewayserver/io"
 	. "go.thethings.network/lorawan-stack/pkg/gatewayserver/io/basicstation"
 	"go.thethings.network/lorawan-stack/pkg/gatewayserver/io/basicstation/messages"
 	"go.thethings.network/lorawan-stack/pkg/gatewayserver/io/mock"
@@ -43,6 +45,8 @@ var (
 
 	discoveryEndPoint      = "ws://localhost:8100/api/v3/gs/io/basicstation/discover"
 	connectionRootEndPoint = "ws://localhost:8100/api/v3/gs/io/basicstation/traffic/"
+
+	testTrafficEndPoint = "ws://localhost:8100/api/v3/gs/io/basicstation/traffic/eui-0101010101010101"
 
 	timeout = 10 * test.Delay
 )
@@ -249,7 +253,11 @@ func TestVersion(t *testing.T) {
 	defer c.Close()
 	gs.RegisterGateway(ctx, registeredGatewayID, &registeredGateway)
 
-	testTrafficEndPoint := "ws://localhost:8100/api/v3/gs/io/basicstation/traffic/eui-0101010101010101"
+	conn, _, err := websocket.DefaultDialer.Dial(testTrafficEndPoint, nil)
+	if !a.So(err, should.BeNil) {
+		t.Fatalf("Connection failed: %v", err)
+	}
+	defer conn.Close()
 
 	for i, tc := range []struct {
 		Name                 string
@@ -313,19 +321,6 @@ func TestVersion(t *testing.T) {
 		},
 	} {
 		t.Run(fmt.Sprintf("VersionMessage/%d", i), func(t *testing.T) {
-			conn, _, err := websocket.DefaultDialer.Dial(testTrafficEndPoint, nil)
-			if !a.So(err, should.BeNil) {
-				t.Fatalf("Connection failed: %v", err)
-			}
-			defer conn.Close()
-			req, err := json.Marshal(messages.DiscoverQuery{EUI: messages.EUI{Prefix: "router", EUI64: types.EUI64{0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01}}})
-			if err != nil {
-				panic(err)
-			}
-			if err := conn.WriteMessage(websocket.TextMessage, req); err != nil {
-				t.Fatalf("Failed to write message: %v", err)
-			}
-
 			reqVersion, err := json.Marshal(tc.VersionQuery)
 			if err != nil {
 				panic(err)
@@ -358,6 +353,119 @@ func TestVersion(t *testing.T) {
 	//TODO: test hardcoded URL, connect without discover.
 }
 
-func TestTraffic(t *testing.T) {
-	// TODO: Test traffic, see gRPC frontend.
+func TestUpstream(t *testing.T) {
+	a := assertions.New(t)
+	ctx := log.NewContext(test.Context(), test.GetLogger(t))
+	ctx = newContextWithRightsFetcher(ctx)
+
+	c := component.MustNew(test.GetLogger(t), &component.Config{
+		ServiceBase: config.ServiceBase{
+			HTTP: config.HTTP{
+				Listen: ":8100",
+			},
+		},
+	})
+	gs := mock.NewServer()
+	srv := New(ctx, gs)
+	c.RegisterWeb(srv)
+	if err := c.Start(); err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	gs.RegisterGateway(ctx, registeredGatewayID, &registeredGateway)
+
+	wsConn, _, err := websocket.DefaultDialer.Dial(testTrafficEndPoint, nil)
+	if !a.So(err, should.BeNil) {
+		t.Fatalf("Connection failed: %v", err)
+	}
+	defer wsConn.Close()
+
+	var gsConn *io.Connection
+	select {
+	case gsConn = <-gs.Connections():
+	case <-time.After(timeout):
+		t.Fatal("Connection timeout")
+	}
+
+	for _, tc := range []struct {
+		Name          string
+		TimeStamp     int64
+		Message       interface{}
+		UplinkMessage ttnpb.UplinkMessage
+	}{
+		{
+			"JoinRequest",
+			12666373963464220,
+			messages.JoinRequest{
+				MHdr:     0,
+				DevEUI:   messages.EUI{Prefix: "DevEui", EUI64: types.EUI64{0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11}},
+				JoinEUI:  messages.EUI{Prefix: "JoinEui", EUI64: types.EUI64{0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22}},
+				DevNonce: 18000,
+				MIC:      12345678,
+				RadioMetaData: messages.RadioMetaData{
+					DataRate:  1,
+					Frequency: 868300000,
+					UpInfo: messages.UpInfo{
+						RxTime: 1548059982,
+						XTime:  12666373963464220,
+						RSSI:   89,
+						SNR:    9.25,
+					},
+				},
+			},
+			ttnpb.UplinkMessage{
+				Payload: &ttnpb.Message{
+					MHDR: ttnpb.MHDR{MType: ttnpb.MType_JOIN_REQUEST, Major: ttnpb.Major_LORAWAN_R1},
+					MIC:  []byte{0x4E, 0x61, 0xBC, 0x00},
+					Payload: &ttnpb.Message_JoinRequestPayload{JoinRequestPayload: &ttnpb.JoinRequestPayload{
+						JoinEUI:  types.EUI64{0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22},
+						DevEUI:   types.EUI64{0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11},
+						DevNonce: [2]byte{0x50, 0x46},
+					}}},
+				RxMetadata: []*ttnpb.RxMetadata{&ttnpb.RxMetadata{
+					GatewayIdentifiers: ttnpb.GatewayIdentifiers{GatewayID: "eui-0101010101010101"},
+					Time:               &[]time.Time{time.Unix(1548059982, 0)}[0],
+					Timestamp:          (uint32)(12666373963464220 & 0xFFFFFFFF),
+					RSSI:               89,
+					SNR:                9.25,
+				}},
+				Settings: ttnpb.TxSettings{
+					CodingRate:    "4/5",
+					Frequency:     868300000,
+					DataRateIndex: 1,
+					DataRate: ttnpb.DataRate{Modulation: &ttnpb.DataRate_LoRa{LoRa: &ttnpb.LoRaDataRate{
+						SpreadingFactor: 11,
+						Bandwidth:       125000,
+					}}},
+				},
+			},
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			req, err := json.Marshal(tc.Message)
+			if err != nil {
+				panic(err)
+			}
+			if err := wsConn.WriteMessage(websocket.TextMessage, req); err != nil {
+				t.Fatalf("Failed to write message: %v", err)
+			}
+			select {
+			case up := <-gsConn.Up():
+				a.So(time.Since(up.ReceivedAt), should.BeLessThan, timeout)
+				up.ReceivedAt = time.Time{}
+				var payload ttnpb.Message
+				a.So(lorawan.UnmarshalMessage(up.RawPayload, &payload), should.BeNil)
+				if !a.So(&payload, should.Resemble, up.Payload) {
+					t.Fatalf("Invalid RawPayload: %v", up.RawPayload)
+				}
+				up.RawPayload = nil
+				up.RxMetadata[0].UplinkToken = nil //TODO: Figure out how to test this
+				a.So(up, should.Resemble, &tc.UplinkMessage)
+			case <-time.After(timeout):
+				t.Fatalf("Read message timeout")
+			}
+		})
+	}
+
 }
