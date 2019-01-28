8fd566e435bb823d7d4134296e9a9d316263f5fa
Johan Stokking <johan@stokking.nl>
2019-01-28 21:49:52 +0100

    as: Harmonize conn correlation ID

--- pkg/applicationserver/applicationserver.go
+++ pkg/applicationserver/applicationserver.go
@@ -196,7 +196,7 @@ func (as *ApplicationServer) Subscribe(ctx context.Context, protocol string, ids
 
 	uid := unique.ID(ctx, ids)
 	logger := log.FromContext(ctx).WithField("application_uid", uid)
-	ctx = events.ContextWithCorrelationID(ctx, fmt.Sprintf("application_conn:%s", events.NewCorrelationID()))
+	ctx = events.ContextWithCorrelationID(ctx, fmt.Sprintf("as:conn:%s", events.NewCorrelationID()))
 
 	l, err := as.getLink(ctx, ids)
 	if err != nil {
