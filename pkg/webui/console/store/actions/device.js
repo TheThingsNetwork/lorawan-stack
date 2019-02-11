f641b54480b5971130e06939de3e5fc9f58d7bf9
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-02-11 11:03:40 +0100

    console: Add device actions

---
+++ pkg/webui/console/store/actions/device.js
@@ -0,0 +1,29 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export const GET_DEV = 'GET_DEVICE'
+export const GET_DEV_SUCCESS = 'GET_DEVICE_SUCCESS'
+export const GET_DEV_FAILURE = 'GET_DEVICE_FAILURE'
+
+export const getDevice = id => (
+  { type: GET_DEV, id }
+)
+
+export const getDeviceSuccess = device => (
+  { type: GET_DEV_SUCCESS, device }
+)
+
+export const getDeviceFailure = error => (
+  { type: GET_DEV_FAILURE, error }
+)
