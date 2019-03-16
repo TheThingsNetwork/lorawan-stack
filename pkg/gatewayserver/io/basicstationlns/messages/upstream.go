aaa0435c93f0bd90bb47d0c6eff469e2e126fb58
KrishnaIyer <mail@krishnaiyereaswaran.com>
2019-03-16 17:34:43 +0100

    gs: Support LNS Uplink Data Frame

--- pkg/gatewayserver/io/basicstationlns/messages/upstream.go
+++ pkg/gatewayserver/io/basicstationlns/messages/upstream.go
@@ -22,9 +22,13 @@ import (
 	"go.thethings.network/lorawan-stack/pkg/encoding/lorawan"
 	"go.thethings.network/lorawan-stack/pkg/errors"
 	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/types"
 )
 
-var errJoinRequestMessage = errors.Define("join_request_message", "invalid join-request message received")
+var (
+	errJoinRequestMessage = errors.Define("join_request_message", "invalid join-request message received")
+	errUplinkDataFrame    = errors.Define("uplink_data_Frame", "invalid uplink data frame received")
+)
 
 // DataRates encodes the available datarates of the channel plan for the Station in the format below
 // [0] -> SF (Spreading Factor; Range: 7...12 for LoRa, 0 for FSK)
@@ -49,7 +53,7 @@ type RadioMetaData struct {
 	UpInfo    UpInfo `json:"upinfo"`
 }
 
-// JoinRequest is the LoRaWAN Join Request message
+// JoinRequest is the LoRaWAN Join Request message from the BasicStation.
 type JoinRequest struct {
 	MHdr          uint             `json:"MHdr"`
 	JoinEUI       basicstation.EUI `json:"JoinEui"`
@@ -70,6 +74,32 @@ func (req JoinRequest) MarshalJSON() ([]byte, error) {
 	}{
 		Type:  TypeUpstreamJoinRequest,
 		Alias: Alias(req),
+	})
+}
+
+// UplinkDataFrame is the LoRaWAN Uplink message from the BasicStation.
+type UplinkDataFrame struct {
+	MHdr          uint    `json:"MHdr"`
+	DevAddr       int32   `json:"DevAddr"`
+	FCtrl         uint    `json:"FCtrl"`
+	FCnt          uint    `json:"Fcnt"`
+	FOpts         string  `json:"FOpts"`
+	FPort         int     `json:"FPort"`
+	FRMPayload    string  `json:"FRMPayload"`
+	MIC           int32   `json:"MIC"`
+	RefTime       float64 `json:"RefTime"`
+	RadioMetaData RadioMetaData
+}
+
+// MarshalJSON implements json.Marshaler.
+func (updf UplinkDataFrame) MarshalJSON() ([]byte, error) {
+	type Alias UplinkDataFrame
+	return json.Marshal(struct {
+		Type string `json:"msgtype"`
+		Alias
+	}{
+		Type:  TypeUpstreamUplinkDataFrame,
+		Alias: Alias(updf),
 	})
 }
 
@@ -107,7 +137,7 @@ func (req *JoinRequest) ToUplinkMessage(ids ttnpb.GatewayIdentifiers, bandID str
 	rxMetadata := &ttnpb.RxMetadata{
 		GatewayIdentifiers: ids,
 		Time:               &rxTime,
-		Timestamp:          (uint32)(req.RadioMetaData.UpInfo.XTime & 0xFFFFFFFF),
+		Timestamp:          uint32(req.RadioMetaData.UpInfo.XTime & 0xFFFFFFFF),
 		RSSI:               req.RadioMetaData.UpInfo.RSSI,
 		SNR:                req.RadioMetaData.UpInfo.SNR,
 	}
@@ -119,7 +149,91 @@ func (req *JoinRequest) ToUplinkMessage(ids ttnpb.GatewayIdentifiers, bandID str
 	}
 	up.Settings = ttnpb.TxSettings{
 		Frequency:     req.RadioMetaData.Frequency,
-		DataRateIndex: (ttnpb.DataRateIndex)(req.RadioMetaData.DataRate),
+		DataRateIndex: ttnpb.DataRateIndex(req.RadioMetaData.DataRate),
+		DataRate:      loraDR,
+	}
+	return up, nil
+}
+
+// ToUplinkMessage extracts fields from the basic station Join Request "jreq" message and converts them into an UplinkMessage for the network server.
+func (updf *UplinkDataFrame) ToUplinkMessage(ids ttnpb.GatewayIdentifiers, bandID string) (ttnpb.UplinkMessage, error) {
+	up := ttnpb.UplinkMessage{}
+	up.ReceivedAt = time.Now()
+
+	parsedMHDR := ttnpb.MHDR{}
+	err := lorawan.UnmarshalMHDR([]byte{byte(updf.MHdr)}, &parsedMHDR)
+	if err != nil {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame.WithCause(err)
+	}
+	if (parsedMHDR.MType != ttnpb.MType_UNCONFIRMED_UP) && (parsedMHDR.MType != ttnpb.MType_CONFIRMED_UP) {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame
+	}
+
+	micBytes, err := getInt32AsByteSlice(updf.MIC)
+	if err != nil {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame.WithCause(err)
+	}
+
+	var fport uint32
+	if updf.FPort == -1 {
+		fport = 0
+	} else {
+		fport = uint32(updf.FPort)
+	}
+
+	devAddrBytes, err := getInt32AsByteSlice(updf.DevAddr)
+	if err != nil {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame.WithCause(err)
+	}
+	devAddr := types.DevAddr{}
+	err = devAddr.UnmarshalBinary(devAddrBytes)
+	if err != nil {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame.WithCause(err)
+	}
+
+	fctrl := ttnpb.FCtrl{}
+	err = lorawan.UnmarshalFCtrl([]byte{byte(updf.FCtrl)}, &fctrl, true)
+	if err != nil {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame.WithCause(err)
+	}
+
+	up.Payload = &ttnpb.Message{
+		MHDR: parsedMHDR,
+		MIC:  micBytes,
+		Payload: &ttnpb.Message_MACPayload{MACPayload: &ttnpb.MACPayload{
+			FPort:      fport,
+			FRMPayload: []byte(updf.FRMPayload),
+			FHDR: ttnpb.FHDR{
+				DevAddr: devAddr,
+				FCtrl:   fctrl,
+				FCnt:    uint32(updf.FCnt),
+				FOpts:   []byte(updf.FOpts),
+			},
+		}},
+	}
+
+	up.RawPayload, err = lorawan.MarshalMessage(*up.Payload)
+	if err != nil {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame.WithCause(err)
+	}
+
+	rxTime := time.Unix(updf.RadioMetaData.UpInfo.RxTime, 0)
+	rxMetadata := &ttnpb.RxMetadata{
+		GatewayIdentifiers: ids,
+		Time:               &rxTime,
+		Timestamp:          uint32(updf.RadioMetaData.UpInfo.XTime & 0xFFFFFFFF),
+		RSSI:               updf.RadioMetaData.UpInfo.RSSI,
+		SNR:                updf.RadioMetaData.UpInfo.SNR,
+	}
+	up.RxMetadata = append(up.RxMetadata, rxMetadata)
+
+	loraDR, err := getDataRateFromIndex(bandID, updf.RadioMetaData.DataRate)
+	if err != nil {
+		return ttnpb.UplinkMessage{}, errUplinkDataFrame.WithCause(err)
+	}
+	up.Settings = ttnpb.TxSettings{
+		Frequency:     updf.RadioMetaData.Frequency,
+		DataRateIndex: ttnpb.DataRateIndex(updf.RadioMetaData.DataRate),
 		DataRate:      loraDR,
 	}
 	return up, nil
