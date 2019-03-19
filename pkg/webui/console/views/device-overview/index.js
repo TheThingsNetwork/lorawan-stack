a2ee1f8d430c06a197096615fbcdb66201f0c8a7
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-03-19 13:31:10 +0100

    console: Fix data-sheet data composing

--- pkg/webui/console/views/device-overview/index.js
+++ pkg/webui/console/views/device-overview/index.js
@@ -23,6 +23,7 @@ import sharedMessages from '../../../lib/shared-messages'
 import IntlHelmet from '../../../lib/components/intl-helmet'
 import Message from '../../../lib/components/message'
 import DataSheet from '../../../components/data-sheet'
+import DateTime from '../../../lib/components/date-time'
 
 import style from './device-overview.styl'
 
@@ -54,29 +55,36 @@ class DeviceOverview extends React.Component {
     const {
       ids,
       description,
-      version_ids,
-      root_keys,
-      session,
+      version_ids = {},
+      root_keys = {},
+      session = {},
       created_at,
     } = this.props.device
 
+    // Get session keys
+    const { keys: sessionKeys = {}} = session
+
     const {
-      f_nwk_s_int_key,
-      s_nwk_s_int_key,
-      nwk_s_enc_key,
-      app_s_key,
-    } = session.keys
+      f_nwk_s_int_key = { key: undefined },
+      s_nwk_s_int_key = { key: undefined },
+      nwk_s_enc_key = { key: undefined },
+      app_s_key = { key: undefined },
+    } = sessionKeys
 
     const sheetData = [
       {
         header: sharedMessages.generalInformation,
         items: [
           { key: sharedMessages.devID, value: ids.device_id, type: 'code', sensitive: false },
-          { key: sharedMessages.description, value: description || sharedMessages.noDesc },
-          { key: sharedMessages.createdAt, value: created_at.toLocaleDateString() },
+          { key: sharedMessages.description, value: description || <Message content={sharedMessages.noDesc} /> },
+          { key: sharedMessages.createdAt, value: <DateTime value={created_at} /> },
         ],
       },
-      {
+    ]
+
+    // Add version info, if it is available
+    if (Object.keys(version_ids).length > 0) {
+      sheetData.push({
         header: m.hardware,
         items: [
           { key: m.brand, value: version_ids.brand_id },
@@ -84,35 +92,44 @@ class DeviceOverview extends React.Component {
           { key: m.hardwareVersion, value: version_ids.hardware_version },
           { key: m.firmwareVersion, value: version_ids.firmware_version },
         ],
-      },
-      {
-        header: m.activationInfo,
-        items: [
-          { key: sharedMessages.devEUI, value: ids.dev_eui, type: 'byte', sensitive: false },
-          { key: sharedMessages.joinEUI, value: ids.join_eui, type: 'byte', sensitive: false },
-          {
-            key: m.rootKeyId,
-            value: root_keys.root_key_id,
-            type: 'code',
-            sensitive: false,
-            subItems: [
-              { key: sharedMessages.appKey, value: root_keys.app_key.key, type: 'code', sensitive: true },
-              { key: sharedMessages.networkKey, value: root_keys.nwk_key.key, type: 'code', sensitive: true },
-            ],
-          },
+      })
+    }
+
+    // Add activation info
+    const activationInfoData = {
+      header: m.activationInfo,
+      items: [
+        { key: sharedMessages.devEUI, value: ids.dev_eui, type: 'byte', sensitive: false },
+        { key: sharedMessages.joinEUI, value: ids.join_eui, type: 'byte', sensitive: false },
+      ],
+    }
+
+    // Add root keys, if available
+    if (Object.keys(root_keys).length > 0) {
+      activationInfoData.items.push({
+        key: m.rootKeyId,
+        value: root_keys.root_key_id,
+        type: 'code',
+        sensitive: false,
+        subItems: [
+          { key: sharedMessages.appKey, value: root_keys.app_key.key, type: 'code', sensitive: true },
+          { key: sharedMessages.networkKey, value: root_keys.nwk_key.key, type: 'code', sensitive: true },
         ],
-      },
-      {
-        header: m.sessionInfo,
-        items: [
-          { key: sharedMessages.devAddr, value: ids.dev_addr, type: 'byte', sensitive: false },
-          { key: m.fwdNtwkKey, value: f_nwk_s_int_key.key, type: 'code', sensitive: true },
-          { key: m.sNtwkSIKey, value: s_nwk_s_int_key.key, type: 'code', sensitive: true },
-          { key: m.ntwkSEncKey, value: nwk_s_enc_key.key, type: 'code', sensitive: true },
-          { key: m.appSKey, value: app_s_key.key, type: 'code', sensitive: true },
-        ],
-      },
-    ]
+      })
+    }
+    sheetData.push(activationInfoData)
+
+    // Add session info
+    sheetData.push({
+      header: m.sessionInfo,
+      items: [
+        { key: sharedMessages.devAddr, value: ids.dev_addr, type: 'byte', sensitive: false },
+        { key: m.fwdNtwkKey, value: f_nwk_s_int_key.key, type: 'code', sensitive: true },
+        { key: m.sNtwkSIKey, value: s_nwk_s_int_key.key, type: 'code', sensitive: true },
+        { key: m.ntwkSEncKey, value: nwk_s_enc_key.key, type: 'code', sensitive: true },
+        { key: m.appSKey, value: app_s_key.key, type: 'code', sensitive: true },
+      ],
+    })
 
     return (
       <div className={style.overviewInfo}>
