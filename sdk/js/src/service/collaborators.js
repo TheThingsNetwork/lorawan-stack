37c87e5bd970f76f2554e5df98dead2f62fafde8
Bogdans Afonins <bogdans.afonins@gmail.com>
2019-03-25 16:36:55 +0100

    util: Add collaborators service

---
+++ sdk/js/src/service/collaborators.js
@@ -0,0 +1,53 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Marshaler from '../util/marshaler'
+
+class Collaborators {
+  constructor (registry, { parentRoutes }) {
+    this._api = registry
+    this._parentRoutes = parentRoutes
+  }
+
+  async getAll (entityId) {
+    const entityIdRoute = this._parentRoutes.list
+    const result = await this._api.ListCollaborators({
+      routeParams: { [entityIdRoute]: entityId },
+    })
+
+    return Marshaler.payloadListResponse('collaborators', result)
+  }
+
+  async add (entityId, data) {
+    const entityIdRoute = this._parentRoutes.set
+    const result = await this._api.SetCollaborator({
+      routeParams: { [entityIdRoute]: entityId },
+    },
+    {
+      collaborator: data,
+    })
+
+    return Marshaler.payloadSingleResponse(result)
+  }
+
+  async update (entityId, data) {
+    return await this.add(entityId, data)
+  }
+
+  async remove (entityId, data) {
+    return await this.add(entityId, { ...data, rights: []})
+  }
+}
+
+export default Collaborators
