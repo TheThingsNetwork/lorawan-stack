2c2f826a347c3909904c1bc998d40c3bbc8b1324
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-03-26 15:33:43 +0200

    util: Add entity splitting and merging

--- sdk/js/src/service/devices.js
+++ sdk/js/src/service/devices.js
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* eslint-disable no-invalid-this */
+
+import traverse from 'traverse'
 import Marshaler from '../util/marshaler'
 import Device from '../entity/device'
+import deviceEntityMap from '../../generated/device-entity-map.json'
 
 /**
  * Devices Class provides an abstraction on all devices and manages data
@@ -28,6 +32,50 @@ class Devices {
     this._entityTransform = proxy
       ? app => new Device(this, app, false)
       : undefined
+  }
+
+  _splitEntitySetPaths (paths, base = {}) {
+    return this._splitEntityPaths(paths, 'set', base)
+  }
+
+  _splitEntityGetPaths (paths, base = {}) {
+    return this._splitEntityPaths(paths, 'get', base)
+  }
+
+  _splitEntityPaths (paths, direction, base = {}) {
+    const result = base
+    const retrieveIndex = direction === 'get' ? 0 : 1
+
+    for (const path of paths) {
+      const subtree =
+        traverse(deviceEntityMap).get(path)
+        || traverse(deviceEntityMap).get([ path[0] ])
+
+      const definition = '_root' in subtree ? subtree._root[retrieveIndex] : subtree[retrieveIndex]
+
+      if (definition) {
+        if (definition instanceof Array) {
+          for (const component of definition) {
+            result[component] = !result[component] ? [ path ] : [ ...result[component], path ]
+          }
+        } else {
+          result[definition] = !result[definition] ? [ path ] : [ ...result[definition], path ]
+        }
+      }
+    }
+    return result
+  }
+
+  _mergeEntity (parts, base = {}) {
+    const result = base
+
+    for (const part of parts) {
+      for (const path of part.paths) {
+        traverse(result).set(path, traverse(part.record).get(path))
+      }
+    }
+
+    return result
   }
 
   async getById (deviceId) {
@@ -47,10 +95,69 @@ class Devices {
   }
 
   async create (device, applicationId = this._applicationId) {
-    const result = await this._api.EndDeviceRegistry.Create(
+    const paths = traverse(device).reduce(function (acc, node) {
+      if (this.isLeaf) {
+        acc.push(this.path)
+      }
+      return acc
+    }, [])
+
+    const requestTree = this._splitEntitySetPaths(paths, {
+      ns: [[ 'ids' ], [ 'created_at' ], [ 'updated_at' ]],
+      as: [[ 'ids' ], [ 'created_at' ], [ 'updated_at' ]],
+      js: [[ 'ids' ], [ 'created_at' ], [ 'updated_at' ]],
+    })
+
+    const isResult = await this._api.EndDeviceRegistry.Create(
       { route: { 'end_device.ids.application_ids.application_id': applicationId }},
       { end_device: device }
     )
+
+    let nsResult = {}
+    let asResult = {}
+    let jsResult = {}
+
+    if ('ns' in requestTree) {
+      nsResult = await this._api.NsDeviceRegistry.Set({
+        route: {
+          'device.ids.application_ids.application_id': applicationId,
+        },
+      },
+      {
+        device,
+        field_mask: Marshaler.fieldMask(requestTree.ns),
+      })
+    }
+    if ('as' in requestTree) {
+      asResult = await this._api.AsDeviceRegistry.Set({
+        route: {
+          'device.ids.application_ids.application_id': applicationId,
+        },
+      },
+      {
+        device,
+        field_mask: Marshaler.fieldMask(requestTree.as),
+      })
+    }
+    if ('js' in requestTree) {
+      jsResult = await this._api.JsDeviceRegistry.Set({
+        route: {
+          'device.ids.application_ids.application_id': applicationId,
+        },
+      },
+      {
+        device,
+        field_mask: Marshaler.fieldMask(requestTree.js),
+      })
+    }
+
+    const result = this._mergeEntity([
+      { record: isResult.data, paths: requestTree.is },
+      { record: nsResult.data, paths: requestTree.ns },
+      { record: asResult.data, paths: requestTree.as },
+      { record: jsResult.data, paths: requestTree.js },
+    ])
+
     return Marshaler.unwrapDevice(
       result,
       this._entityTransform
