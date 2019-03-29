c6e05d63329b2996498f6c9d41d483761de7da70
Sylvain Prost <prostg.sylvain@gmail.com>
2019-03-29 11:37:53 +0100

    doc: Add version redirection

--- doc/generate-redirect.sh
+++ doc/generate-redirect.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 tag=`git describe --abbrev=0 --tags`
-cd public
 doc_url=https://github.com/TheThingsNetwork/lorawan-stack/
 echo "<!DOCTYPE html>
 <html lang=\"en\">
 <head>
-  <title>Your Page Title</title>
+  <title>Version redirect</title>
   <meta http-equiv=\"refresh\" content=\"0; url=`echo $doc_url$tag`\" />
 </head>
 <body>
@@ -15,7 +14,3 @@ echo "<!DOCTYPE html>
 <p>If you are not automatically redirected click ont this link `echo $doc_url$tag` .</p>
 </body>
 </html>" > index.html
-
-git add --all
-git commit -m "Publishing changes of `echo $tag` to gh-pages"
-git push origin gh-pages
