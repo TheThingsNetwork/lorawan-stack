5c0d4ee58b32b47b0fd9b6843657a57c3becbfb2
Johan Stokking <johan@thethingsnetwork.org>
2019-03-31 12:06:12 +0300

    ns: Log reason to add downlink task

--- pkg/networkserver/downlink.go
+++ pkg/networkserver/downlink.go
@@ -928,7 +928,7 @@ func (ns *NetworkServer) processDownlinkTask(ctx context.Context) error {
 		}
 
 		if !nextDownlinkAt.IsZero() {
-			logger.WithField("start_at", nextDownlinkAt.UTC()).Debug("Adding downlink task...")
+			logger.WithField("start_at", nextDownlinkAt.UTC()).Debug("Adding downlink task after downlink...")
 			if err := ns.downlinkTasks.Add(ctx, devID, nextDownlinkAt, true); err != nil {
 				addErr = true
 				logger.WithError(err).Error("Failed to add downlink task after downlink")
