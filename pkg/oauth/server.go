3df82c5d7f6cc1030dde3d1163b1b0001b1d055d
Hylke Visser <hylke@thethingsindustries.com>
2019-04-02 12:42:22 +0300

    oauth: Use UI mount path for path redirects

--- pkg/oauth/server.go
+++ pkg/oauth/server.go
@@ -17,6 +17,7 @@ package oauth
 import (
 	"context"
 	"net/http"
+	"strings"
 	"time"
 
 	echo "github.com/labstack/echo/v4"
@@ -141,6 +142,10 @@ func (s *server) output(c echo.Context, resp *osin.Response) error {
 		location, err := resp.GetRedirectUrl()
 		if err != nil {
 			return err
+		}
+		uiMount := strings.TrimSuffix(s.config.UI.MountPath(), "/") + "/"
+		if strings.HasPrefix(location, "/") && !strings.HasPrefix(location, uiMount) {
+			location = uiMount + location
 		}
 		return c.Redirect(http.StatusFound, location)
 	}
