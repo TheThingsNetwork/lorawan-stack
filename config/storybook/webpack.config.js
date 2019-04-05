99bd17a2b3154bfd2afb86a6f437a6744902c3b6
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-04-05 11:22:53 +0300

    dev: Move babel config

--- config/storybook/webpack.config.js
+++ config/storybook/webpack.config.js
@@ -16,7 +16,7 @@
 /* eslint-env node */
 /* eslint-disable import/no-commonjs */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-require('babel-register')
+require('@babel/register')
 
 const config = require('../webpack.config.babel.js').default
 const development = process.env.NODE_ENV !== 'production'
