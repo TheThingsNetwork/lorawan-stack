5c11edaa2e29da498b82066e4647740be99716ec
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-04-24 13:46:59 +0200

    dev: Add js sdk proto targets

--- .mage/sdkjs.go
+++ .mage/sdkjs.go
@@ -17,9 +17,12 @@ package ttnmage
 import (
 	"fmt"
 	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"github.com/magefile/mage/target"
 )
 
 // DevDeps installs the javascript SDK development dependencies.
@@ -38,7 +41,13 @@ func (sdkJs SdkJs) yarn() (func(args ...string) error, error) {
 		}
 	}
 	return func(args ...string) error {
-		return sh.Run(nodeBin("yarn"), append([]string{"--cwd=./sdk/js"}, args...)...)
+		return sh.Run(nodeBin("yarn"), append([]string{"--cwd=sdk/js"}, args...)...)
+	}, nil
+}
+
+func (sdkJs SdkJs) docker() (func(args ...string) error, error) {
+	return func(args ...string) error {
+		return sh.Run("docker", args...)
 	}, nil
 }
 
@@ -56,7 +65,7 @@ func (sdkJs SdkJs) Deps() error {
 	if err != nil {
 		return err
 	}
-	return yarn("install", "--no-progress")
+	return yarn("add", "--no-progress")
 }
 
 // Build builds the source files and output into 'dist'.
@@ -114,4 +123,55 @@ func (sdkJs SdkJs) TestWatch() error {
 // Clean clears all generated files.
 func (sdkJs SdkJs) Clean() {
 	sh.Rm("./sdk/js/dist")
+}
+
+// Protos generates the api.json for the JS SDK
+func (sdkJs SdkJs) Protos() error {
+	if mg.Verbose() {
+		fmt.Println("Extracting api definitions from protos…")
+	}
+
+	docker, err := sdkJs.docker()
+	if err != nil {
+		return err
+	}
+	PWD, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	u, err := user.Current()
+	if err != nil {
+		return err
+	}
+	PWD_PARENT := filepath.Dir(PWD)
+
+	PROTOC_DOCKER_IMAGE := "thethingsindustries/protoc:3.1.3"
+	SDK_PROTOC_FLAGS := "--doc_opt=json,api.json --doc_out=" + PWD + "/sdk/js/generated"
+	API_PROTO_FILES := PWD + "/api/*.proto"
+
+	return docker(
+		"run", "--user", u.Uid, "--rm",
+		"--mount", "type=bind,src="+PWD+"/api,dst="+PWD+"/api",
+		"--mount", "type=bind,src="+PWD+"/pkg/ttnpb,dst=/out/go.thethings.network/lorawan-stack/pkg/ttnpb",
+		"--mount", "type=bind,src="+PWD+"/sdk/js,dst="+PWD+"/sdk/js",
+		"-w", PWD, PROTOC_DOCKER_IMAGE, "-I"+PWD_PARENT, SDK_PROTOC_FLAGS, API_PROTO_FILES,
+	)
+}
+
+// Definitions extracts the api-definition.json from the proto generated api.json
+func (sdkJs SdkJs) Definitions() error {
+	mg.Deps(sdkJs.Protos)
+	changed, err := target.Path("./sdk/js/generated/api-definition.json", "./sdk/js/generated/api.json")
+	if os.IsNotExist(err) || (err == nil && changed) {
+		if mg.Verbose() {
+			fmt.Println("Extracting api definitions from protos…")
+		}
+		yarn, err := sdkJs.yarn()
+		if err != nil {
+			return err
+		}
+
+		return yarn("run", "definitions")
+	}
+	return nil
 }
