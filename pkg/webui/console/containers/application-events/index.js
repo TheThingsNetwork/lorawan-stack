41d4d15f9f5e33f81485e603cc6981cca7411473
Bogdans Afonins <bogdans.afonins@gmail.com>
2019-05-01 14:50:43 +0200

    console: Add application events component

---
+++ pkg/webui/console/containers/application-events/index.js
@@ -0,0 +1,67 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { connect } from 'react-redux'
+import bind from 'autobind-decorator'
+
+import PropTypes from '../../../lib/prop-types'
+import EventsSubscription from '../../containers/events-subscription'
+
+import {
+  clearApplicationEventsStream,
+} from '../../store/actions/application'
+
+import {
+  applicationEventsSelector,
+  applicationEventsStatusSelector,
+} from '../../store/selectors/application'
+
+@connect(
+  null,
+  (dispatch, ownProps) => ({
+    onClear: () => dispatch(clearApplicationEventsStream(ownProps.appId)),
+  }))
+@bind
+class ApplicationEvents extends React.Component {
+  render () {
+    const {
+      appId,
+      widget,
+      onClear,
+    } = this.props
+
+    return (
+      <EventsSubscription
+        id={appId}
+        widget={widget}
+        eventsSelector={applicationEventsSelector}
+        statusSelector={applicationEventsStatusSelector}
+        onClear={onClear}
+        toAllUrl={`/console/applications/${appId}/data`}
+      />
+    )
+  }
+}
+
+ApplicationEvents.propTypes = {
+  appId: PropTypes.string.isRequired,
+  widget: PropTypes.bool,
+}
+
+ApplicationEvents.defaultProps = {
+  widget: false,
+}
+
+export default ApplicationEvents
