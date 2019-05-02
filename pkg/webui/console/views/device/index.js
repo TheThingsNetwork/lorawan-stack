34dd24405798ac55e480c2ccf7f26c1c5ccecad0
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-05-02 14:03:37 +0200

    console: Add general settings route

--- pkg/webui/console/views/device/index.js
+++ pkg/webui/console/views/device/index.js
@@ -27,6 +27,7 @@ import Tabs from '../../../components/tabs'
 import IntlHelmet from '../../../lib/components/intl-helmet'
 
 import DeviceOverview from '../device-overview'
+import DeviceGeneralSettings from '../device-general-settings'
 
 import { getDevice } from '../../store/actions/device'
 
@@ -35,14 +36,6 @@ import style from './device.styl'
 const m = defineMessages({
   title: '%s - {deviceName} - The Things Network Console',
 })
-
-const tabs = [
-  { title: sharedMessages.overview, name: 'overview' },
-  { title: sharedMessages.data, name: 'data' },
-  { title: sharedMessages.location, name: 'location' },
-  { title: sharedMessages.payloadFormats, name: 'develop' },
-  { title: sharedMessages.generalSettings, name: 'general-settings' },
-]
 
 @connect(function ({ device, application }, props) {
   return {
@@ -73,12 +66,9 @@ export default class Device extends React.Component {
     dispatch(getDevice(appId, devId, 'name,description,session,version_ids,root_keys', { ignoreNotFound: true }))
   }
 
-  handleTabChange () {
-
-  }
-
   render () {
     const { fetching, error, match, devId, deviceName } = this.props
+    const { appId } = match.params
 
     if (fetching) {
       return (
@@ -93,6 +83,16 @@ export default class Device extends React.Component {
       return 'ERROR'
     }
 
+    const basePath = `/console/applications/${appId}/devices/${devId}`
+
+    const tabs = [
+      { title: sharedMessages.overview, name: 'overview', link: basePath },
+      { title: sharedMessages.data, name: 'data' },
+      { title: sharedMessages.location, name: 'location' },
+      { title: sharedMessages.payloadFormats, name: 'develop' },
+      { title: sharedMessages.generalSettings, name: 'general-settings', link: `${basePath}/general-settings` },
+    ]
+
     return (
       <React.Fragment>
         <IntlHelmet
@@ -104,16 +104,15 @@ export default class Device extends React.Component {
               <h2 className={style.title}>{devId}</h2>
               <Tabs
                 narrow
-                active="overview"
                 tabs={tabs}
-                onTabChange={this.handleTabChange}
                 className={style.tabs}
               />
             </Col>
           </Row>
         </Container>
         <Switch>
-          <Route exact path={match.path} component={DeviceOverview} />
+          <Route exact path={basePath} component={DeviceOverview} />
+          <Route exact path={`${basePath}/general-settings`} component={DeviceGeneralSettings} />
         </Switch>
       </React.Fragment>
     )
