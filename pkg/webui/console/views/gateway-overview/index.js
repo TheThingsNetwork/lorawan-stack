d419ea7aed573636be0c2b688f91be0d3df1bb76
Bogdans Afonins <bogdans.afonins@gmail.com>
2019-05-05 14:14:28 +0200

    console: Add gateway overview page

---
+++ pkg/webui/console/views/gateway-overview/index.js
@@ -0,0 +1,140 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import bind from 'autobind-decorator'
+import { connect } from 'react-redux'
+import { Container, Col, Row } from 'react-grid-system'
+
+import sharedMessages from '../../../lib/shared-messages'
+import IntlHelmet from '../../../lib/components/intl-helmet'
+import DateTime from '../../../lib/components/date-time'
+import DataSheet from '../../../components/data-sheet'
+import GatewayStatistics from '../../containers/gateway-statistics'
+import GatewayEvents from '../../containers/gateway-events'
+import Tag from '../../../components/tag'
+import Message from '../../../lib/components/message'
+
+import {
+  gatewaySelector,
+} from '../../store/selectors/gateway'
+import {
+  getGatewayId as idSelector,
+} from '../../../lib/selectors/id'
+
+import style from './gateway-overview.styl'
+
+@connect(function (state, props) {
+  const gtw = gatewaySelector(state, props)
+
+  return {
+    gtwId: idSelector(gtw),
+    gateway: gtw,
+  }
+})
+@bind
+export default class GatewayOverview extends React.Component {
+
+  get gatewayInfo () {
+    const { gtwId, gateway } = this.props
+    const {
+      ids,
+      name,
+      description,
+      created_at,
+      updated_at,
+      frequency_plan_id,
+      gateway_server_address,
+    } = gateway
+
+    const sheetData = [
+      {
+        header: sharedMessages.generalInformation,
+        items: [
+          {
+            key: sharedMessages.gatewayID,
+            value: gtwId,
+            type: 'code',
+            sensitive: false,
+          },
+          {
+            key: sharedMessages.gatewayEUI,
+            value: ids.eui,
+            type: 'code',
+            sensitive: false,
+          },
+          {
+            key: sharedMessages.gatewayDescription,
+            value: description || <Message content={sharedMessages.none} />,
+          },
+          {
+            key: sharedMessages.createdAt,
+            value: <DateTime value={created_at} />,
+          },
+          {
+            key: sharedMessages.updatedAt,
+            value: <DateTime value={updated_at} />,
+          },
+          {
+            key: sharedMessages.gatewayServerAddress,
+            value: gateway_server_address,
+            type: 'code',
+            sensitive: false,
+          },
+        ],
+      },
+      {
+        header: sharedMessages.lorawanInformation,
+        items: [
+          {
+            key: sharedMessages.frequencyPlan,
+            value: <Tag content={frequency_plan_id} />,
+          },
+        ],
+      },
+    ]
+
+    return (
+      <div className={style.overviewInfo}>
+        <h2 className={style.title}>{name || gtwId}</h2>
+        <GatewayStatistics className={style.statistics} gtwId={gtwId} />
+        <DataSheet data={sheetData} />
+      </div>
+    )
+  }
+
+  render () {
+    const { gtwId } = this.props
+
+    return (
+      <Container>
+        <IntlHelmet title={sharedMessages.overview} />
+        <Row>
+          <Col sm={12} lg={6}>
+            {this.gatewayInfo}
+          </Col>
+          <Col sm={12} lg={6}>
+            <div className={style.latestEvents}>
+              <GatewayEvents
+                gtwId={gtwId}
+                widget
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
---
+++ pkg/webui/console/views/gateway/index.js
@@ -0,0 +1,126 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { Switch, Route } from 'react-router'
+import { connect } from 'react-redux'
+
+import sharedMessages from '../../../lib/shared-messages'
+import { withSideNavigation } from '../../../components/navigation/side/context'
+import { withBreadcrumb } from '../../../components/breadcrumbs/context'
+import Breadcrumb from '../../../components/breadcrumbs/breadcrumb'
+import Spinner from '../../../components/spinner'
+import Message from '../../../lib/components/message'
+
+import GatewayOverview from '../gateway-overview'
+
+import {
+  getGateway,
+  startGatewayEventsStream,
+  stopGatewayEventsStream,
+} from '../../store/actions/gateway'
+import {
+  fetchingSelector,
+  errorSelector,
+  gatewaySelector,
+} from '../../store/selectors/gateway'
+
+@connect(function (state, props) {
+  const gtwId = props.match.params.gtwId
+
+  return {
+    gtwId,
+    gateway: gatewaySelector(state, props),
+    error: errorSelector(state, props),
+    fetching: fetchingSelector(state, props),
+  }
+},
+dispatch => ({
+  getGateway: (id, meta) => dispatch(getGateway(id, meta)),
+  startStream: id => dispatch(startGatewayEventsStream(id)),
+  stopStream: id => dispatch(stopGatewayEventsStream(id)),
+}))
+@withSideNavigation(function (props) {
+  const { match, gtwId } = props
+  const matchedUrl = match.url
+
+  return {
+    header: { title: gtwId, icon: 'gateway' },
+    entries: [
+      {
+        title: sharedMessages.overview,
+        path: matchedUrl,
+        icon: 'overview',
+      },
+    ],
+  }
+})
+@withBreadcrumb('gtws.single', function (props) {
+  const { gtwId } = props
+  return (
+    <Breadcrumb
+      path={`/console/gateways/${gtwId}`}
+      icon="gateway"
+      content={gtwId}
+    />
+  )
+})
+export default class Gateway extends React.Component {
+
+  componentDidMount () {
+    const { getGateway, startStream, match } = this.props
+    const { gtwId } = match.params
+
+    startStream(gtwId)
+    getGateway(gtwId, {
+      selectors: [
+        'name',
+        'description',
+        'enforce_duty_cycle',
+        'frequency_plan_id',
+        'gateway_server_address',
+        'enforce_duty_cycle',
+      ],
+    })
+  }
+
+  componentWillUnmount () {
+    const { stopStream, gtwId } = this.props
+
+    stopStream(gtwId)
+  }
+
+  render () {
+    const { fetching, error, match, gateway } = this.props
+
+    // show any gateway fetching error, e.g. not found, no rights, etc
+    if (error) {
+      return 'ERROR'
+    }
+
+    if (fetching || !gateway) {
+      return (
+        <Spinner center>
+          <Message content={sharedMessages.loading} />
+        </Spinner>
+      )
+    }
+
+    return (
+      <Switch>
+        <Route exact path={`${match.path}`} component={GatewayOverview} />
+      </Switch>
+    )
+  }
+}
