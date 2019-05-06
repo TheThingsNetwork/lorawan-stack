82e57b990042a2cf501e8cbe9fd8ab67e95a977b
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-05-06 16:08:14 +0200

    console: Add device list view

---
+++ pkg/webui/console/views/device-list/index.js
@@ -0,0 +1,45 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { Col, Row, Container } from 'react-grid-system'
+
+import IntlHelmet from '../../../lib/components/intl-helmet'
+import sharedMessages from '../../../lib/shared-messages'
+import DevicesTable from '../../containers/devices-table'
+
+const DEVICES_TABLE_SIZE = 25
+
+@connect(function ({ application }, props) {
+  return {
+    application: application.application,
+  }
+})
+class ApplicationDeviceList extends React.Component {
+  render () {
+    return (
+      <Container>
+        <Row>
+          <IntlHelmet title={sharedMessages.devices} />
+          <Col sm={12}>
+            <DevicesTable pageSize={DEVICES_TABLE_SIZE} />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default ApplicationDeviceList
