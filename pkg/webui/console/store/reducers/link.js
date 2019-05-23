e69f60b3e4e118f4fa46a84996cbd97cad812edd
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-05-23 11:07:24 +0200

    console: Add redux primitives for app link

---
+++ pkg/webui/console/store/reducers/link.js
@@ -0,0 +1,97 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {
+  GET_APP_LINK,
+  GET_APP_LINK_SUCCESS,
+  GET_APP_LINK_FAILURE,
+  UPDATE_APP_LINK_SUCCESS,
+  DELETE_APP_LINK_SUCCESS,
+} from '../actions/link'
+
+const defaultProps = {
+  fetching: false,
+  linked: false,
+  error: undefined,
+  link: undefined,
+  stats: undefined,
+}
+
+const getLinkSuccess = function (state, { linked, stats, link = {}}) {
+  return {
+    ...state,
+    fetching: false,
+    error: undefined,
+    linked,
+    link,
+    stats,
+  }
+}
+
+const getLinkFailure = function (state, { error }) {
+  return {
+    ...state,
+    fetching: false,
+    link: {},
+    stats: undefined,
+    linked: false,
+    error,
+  }
+}
+
+const updateLinkSuccess = function (state, { link, stats }) {
+  const newLink = { ...state.link, ...link }
+  const newStats = { ...state.stats, ...stats }
+
+  return {
+    ...state,
+    linked: true,
+    fetching: false,
+    error: undefined,
+    link: newLink,
+    stats: newStats,
+  }
+}
+
+const deleteLinkSuccess = function (state) {
+  return {
+    ...state,
+    fetching: false,
+    linked: false,
+    link: {},
+    stats: undefined,
+  }
+}
+
+const link = function (state = defaultProps, action) {
+  switch (action.type) {
+  case GET_APP_LINK:
+    return {
+      ...state,
+      fetching: true,
+    }
+  case GET_APP_LINK_SUCCESS:
+    return getLinkSuccess(state, action)
+  case UPDATE_APP_LINK_SUCCESS:
+    return updateLinkSuccess(state, action)
+  case DELETE_APP_LINK_SUCCESS:
+    return deleteLinkSuccess(state)
+  case GET_APP_LINK_FAILURE:
+    return getLinkFailure(state, action)
+  default:
+    return state
+  }
+}
+
+export default link
