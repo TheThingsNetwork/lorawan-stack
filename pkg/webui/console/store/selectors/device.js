000c48b8a240b82037f4b0f3e9dd2f1f977fc13b
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-06-06 15:40:34 +0200

    console: Add device id selector

--- pkg/webui/console/store/selectors/device.js
+++ pkg/webui/console/store/selectors/device.js
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import { GET_DEV_BASE } from '../actions/device'
+import { getDeviceId } from '../../../lib/selectors/id'
 
 import {
   eventsSelector,
@@ -31,6 +32,17 @@ export const deviceSelector = state => storeSelector(state).device
 
 export const selectDeviceFetching = createFetchingSelector(GET_DEV_BASE)
 export const selectDeviceError = createErrorSelector(GET_DEV_BASE)
+export const selectSelectedDevice = state => deviceSelector(state)
+
+export const selectSelectedDeviceId = state => getDeviceId(selectSelectedDevice(state))
+
+export const selectSelectedDeviceFormatters = state => selectSelectedDevice(state).formatters
+
+export const fetchingSelector = function (state) {
+  const store = storeSelector(state)
+
+  return store.fetching || false
+}
 
 export const errorSelector = function (state) {
   const store = storeSelector(state)
