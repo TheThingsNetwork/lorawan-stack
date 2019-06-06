7127422c6d85256a15807ef8857700e044ada7e0
KrishnaIyer <mail@krishnaiyereaswaran.com>
2019-06-06 12:14:48 +0200

    gs: Fix router config message

--- pkg/gatewayserver/io/basicstationlns/messages/messages.go
+++ pkg/gatewayserver/io/basicstationlns/messages/messages.go
@@ -18,11 +18,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
+	"time"
 
 	"go.thethings.network/lorawan-stack/pkg/band"
 	"go.thethings.network/lorawan-stack/pkg/basicstation"
 	"go.thethings.network/lorawan-stack/pkg/errors"
 	"go.thethings.network/lorawan-stack/pkg/frequencyplans"
+	pfconfig "go.thethings.network/lorawan-stack/pkg/pfconfig/shared"
 )
 
 var (
@@ -87,12 +89,12 @@ func Type(data []byte) (string, error) {
 // Version contains version information.
 // This message is sent by the gateway.
 type Version struct {
-	Station  string   `json:"station"`
-	Firmware string   `json:"firmware"`
-	Package  string   `json:"package"`
-	Model    string   `json:"model"`
-	Protocol int      `json:"protocol"`
-	Features []string `json:"features,omitempty"`
+	Station  string `json:"station"`
+	Firmware string `json:"firmware"`
+	Package  string `json:"package"`
+	Model    string `json:"model"`
+	Protocol int    `json:"protocol"`
+	Features string `json:"features,omitempty"`
 }
 
 // MarshalJSON implements json.Marshaler.
@@ -110,31 +112,25 @@ func (v Version) MarshalJSON() ([]byte, error) {
 // IsProduction checks the features field for "prod" and returns true if found.
 // This is then used to set debug options in the router config
 func (v Version) IsProduction() bool {
-	if len(v.Features) == 0 {
+	if v.Features == "" {
 		return false
 	}
-	for _, feature := range v.Features {
-		if feature == "prod" {
-			return true
-		}
+	if strings.Contains(v.Features, "prod") {
+		return true
 	}
 	return false
 }
 
-// SX1301Config contains the concentrator configuration.
-// TODO: Harmonize this with sx1301_conf from https://github.com/TheThingsNetwork/lorawan-stack/pull/276
-type SX1301Config struct{}
-
 // RouterConfig contains the router configuration.
 // This message is sent by the Gateway Server.
 type RouterConfig struct {
-	NetID          []int        `json:"NetID"`
-	JoinEUI        [][]int      `json:"JoinEui"`
-	Region         string       `json:"region"`
-	HardwareSpec   string       `json:"hwspec"`
-	FrequencyRange []int        `json:"freq_range"`
-	DataRates      DataRates    `json:"DRs"`
-	SX1301Config   SX1301Config `json:"sx1301_conf"`
+	NetID          []int                   `json:"NetID"`
+	JoinEUI        [][]int                 `json:"JoinEui"`
+	Region         string                  `json:"region"`
+	HardwareSpec   string                  `json:"hwspec"`
+	FrequencyRange []int                   `json:"freq_range"`
+	DataRates      DataRates               `json:"DRs"`
+	SX1301Config   []pfconfig.SX1301Config `json:"sx1301_conf"`
 
 	// These are debug options to be unset in production gateways
 	NoCCA       bool `json:"nocca"`
@@ -145,15 +141,25 @@ type RouterConfig struct {
 	MuxTime float64 `json:"MuxTime"`
 }
 
+// MarshalJSON implements json.Marshaler.
+func (cfg RouterConfig) MarshalJSON() ([]byte, error) {
+	type Alias RouterConfig
+	return json.Marshal(struct {
+		Type string `json:"msgtype"`
+		Alias
+	}{
+		Type:  TypeDownstreamRouterConfig,
+		Alias: Alias(cfg),
+	})
+}
+
 // GetRouterConfig returns the routerconfig message to be sent to the gateway
-func GetRouterConfig(fp frequencyplans.FrequencyPlan, isProd bool) (RouterConfig, error) {
+func GetRouterConfig(fp frequencyplans.FrequencyPlan, isProd bool, dlTime time.Time) (RouterConfig, error) {
 	if err := fp.Validate(); err != nil {
 		return RouterConfig{}, errFrequencyPlan
 	}
 
 	cfg := RouterConfig{}
-
-	// This disables filtering in the gateway.
 	cfg.JoinEUI = nil
 	cfg.NetID = nil
 
@@ -176,7 +182,7 @@ func GetRouterConfig(fp frequencyplans.FrequencyPlan, isProd bool) (RouterConfig
 		int(fp.Radios[0].TxConfiguration.MaxFrequency),
 	}
 
-	// TODO: https://github.com/TheThingsNetwork/lorawan-stack/issues/284
+	// TODO: https://github.com/TheThingsNetwork/lorawan-stack/issues/761
 	cfg.HardwareSpec = fmt.Sprintf("%s/%s", configHardwareSpecPrefix, configHardwareSpecNoOfConcentrators)
 
 	drs, err := getDataRatesFromBandID(fp.BandID)
@@ -189,7 +195,20 @@ func GetRouterConfig(fp frequencyplans.FrequencyPlan, isProd bool) (RouterConfig
 	cfg.NoDutyCycle = !isProd
 	cfg.NoDwellTime = !isProd
 
-	// TODO: Harmonize this with sx1301_conf from https://github.com/TheThingsNetwork/lorawan-stack/pull/276
-	cfg.SX1301Config = SX1301Config{}
+	sx1301Conf, err := pfconfig.BuildSX1301Config(&fp)
+	if err != nil {
+		return RouterConfig{}, err
+	}
+
+	// These fields are not defined in the v1.5 ref design https://doc.sm.tc/station/gw_v1.5.html#rfconf-object and would cause a parsing error.
+	sx1301Conf.Radios[0].TxFreqMin = 0
+	sx1301Conf.Radios[0].TxFreqMax = 0
+
+	// TODO: Extend this for > 8ch gateways https://github.com/TheThingsNetwork/lorawan-stack/issues/761
+	cfg.SX1301Config = append(cfg.SX1301Config, *sx1301Conf)
+
+	//Add the MuxTime for RTT measurement
+	cfg.MuxTime = float64(dlTime.Unix()) + float64(dlTime.Nanosecond())/(1e9)
+
 	return cfg, nil
 }
