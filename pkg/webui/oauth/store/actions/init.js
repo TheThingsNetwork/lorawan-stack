908991762136cfcb59aabd48030891d772db9389
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-06-13 15:31:17 +0200

    oauth: Adapt init actions

--- pkg/webui/oauth/store/actions/init.js
+++ pkg/webui/oauth/store/actions/init.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export const INITIALIZE = 'INITIALIZE'
+export const INITIALIZE = 'INITIALIZE_REQUEST'
 export const INITIALIZE_SUCCESS = 'INITIALIZE_SUCCESS'
 export const INITIALIZE_FAILURE = 'INITIALIZE_FAILURE'
 
