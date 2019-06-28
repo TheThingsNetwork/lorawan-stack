9532a9c02ddc192e3c8d0c94394191d19a5f932f
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-06-28 16:41:11 +0200

    console: Fix address regexp

--- pkg/webui/console/lib/regexp.js
+++ pkg/webui/console/lib/regexp.js
@@ -15,7 +15,7 @@
 // alphanumeric, dashes, lowercase
 export const id = /^[a-z0-9]+(-[a-z0-9]+)*$/
 export const apiKey = /^NNSXS.[A-Z0-9]{39}.[A-Z0-9]{52}$/
-export const address = /^(?:(?:[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*(?:[A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])(?::[0-9]{1,5})?$|^$/
+export const address = new RegExp('^(?:(?:[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*(?:[A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])(?::[0-9]{1,5})?$|^$')
 export const latitude = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/
 export const longitude = /^\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/
 export const int32 = /^-?\d+$/
