b5e9d9bdcb2841ad46cffd21ecc97f535716af76
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-08-08 17:22:09 +0200

    util: Add ns service to js sdk

--- sdk/js/src/index.js
+++ sdk/js/src/index.js
@@ -19,6 +19,7 @@ import Api from './api'
 import Token from './util/token'
 import Gateways from './service/gateways'
 import Js from './service/join-server'
+import Ns from './service/network-server'
 
 class TtnLw {
   constructor (token, {
@@ -37,6 +38,7 @@ class TtnLw {
     this.Configuration = new Configuration(this.api.Configuration)
     this.Gateways = new Gateways(this.api, { defaultUserId, proxy, stackConfig })
     this.Js = new Js(this.api.Js)
+    this.Ns = new Ns(this.api.Ns)
   }
 }
 
