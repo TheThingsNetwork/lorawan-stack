9191bdb56dcda6e7804657e1704b58b2a78e7a83
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-08-16 16:36:30 +0200

    console,oauth: Add breadcrumb view wrapper

---
+++ pkg/webui/lib/components/breadcrumb-view/index.js
@@ -0,0 +1,35 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import { Container } from 'react-grid-system'
+
+import Breadcrumbs from '../../../components/breadcrumbs'
+
+import style from './breadcrumb-view.styl'
+
+const BreadcrumbView = ({ children }) => {
+  return (
+    <div className={style.container}>
+      <div className={style.breadcrumbsContainer}>
+        <Container>
+          <Breadcrumbs />
+        </Container>
+      </div>
+      <div className={style.contentContainer}>{children}</div>
+    </div>
+  )
+}
+
+export default BreadcrumbView
