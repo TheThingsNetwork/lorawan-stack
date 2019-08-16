04a3b7a828718231dd323c90bd9d50fd2bcb367f
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-08-16 11:59:37 +0200

    console: Add steps component

--- pkg/webui/components/steps/step/index.js
+++ pkg/webui/components/steps/step/index.js
@@ -80,7 +80,6 @@ Step.defaultProps = {
   active: false,
   className: undefined,
   description: undefined,
-  status: 'wait',
   transitionFailed: false,
   vertical: PropTypes.bool,
 }
@@ -89,7 +88,7 @@ Step.propTypes = {
   active: PropTypes.bool,
   className: PropTypes.string,
   description: PropTypes.message,
-  status: PropTypes.oneOf(['success', 'failure', 'current', 'wait']),
+  status: PropTypes.oneOf(['success', 'failure', 'current', 'wait']).isRequired,
   stepNumber: PropTypes.number.isRequired,
   title: PropTypes.message.isRequired,
   transitionFailed: PropTypes.bool,
---
+++ pkg/webui/components/steps/steps.js
@@ -0,0 +1,77 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import classnames from 'classnames'
+
+import PropTypes from '../../lib/prop-types'
+
+import style from './steps.styl'
+
+const Steps = props => {
+  const { className, children, stepCountStart, currentStep, status, vertical } = props
+
+  const cls = classnames(className, style.steps, {
+    [style.vertical]: vertical,
+  })
+  const steps = React.Children.map(children, (child, index) => {
+    if (!Boolean(child)) {
+      return null
+    }
+
+    if (React.isValidElement(child) && child.type.displayName === 'Step') {
+      const stepNumber = index + stepCountStart
+      let stepStatus = status
+      if (stepNumber < currentStep) {
+        stepStatus = 'success'
+      } else if (stepNumber > currentStep) {
+        stepStatus = 'wait'
+      }
+
+      const props = {
+        ...child.props,
+        stepNumber,
+        vertical,
+        transitionFailed: status === 'failure' && stepNumber === currentStep - 1,
+        active: stepNumber === currentStep,
+        status: stepStatus,
+      }
+
+      return React.cloneElement(child, props)
+    }
+
+    return child
+  })
+
+  return <ol className={cls}>{steps}</ol>
+}
+
+Steps.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
+  className: PropTypes.string,
+  currentStep: PropTypes.number.isRequired,
+  status: PropTypes.oneOf(['success', 'failure', 'current', 'wait']),
+  stepCountStart: PropTypes.number,
+  vertical: PropTypes.bool,
+}
+
+Steps.defaultProps = {
+  className: undefined,
+  stepCountStart: 1,
+  status: 'current',
+  children: [],
+  vertical: false,
+}
+
+export default Steps
