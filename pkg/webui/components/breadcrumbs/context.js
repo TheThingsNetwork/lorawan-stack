f58a9905b128f0c7774cfbaf71d66d6554440382
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-08-22 10:55:50 +0200

    console: Fix breadcrumb mount order

--- pkg/webui/components/breadcrumbs/context.js
+++ pkg/webui/components/breadcrumbs/context.js
@@ -24,9 +24,21 @@ class BreadcrumbsProvider extends React.Component {
   }
 
   add(id, breadcrumb) {
-    this.setState(prev => ({
-      breadcrumbs: [...prev.breadcrumbs, { id, breadcrumb }],
-    }))
+    this.setState(prev => {
+      const index = prev.breadcrumbs.findIndex(({ id: breadcrumbId }) => breadcrumbId === id)
+      if (index === -1) {
+        return { breadcrumbs: [...prev.breadcrumbs, { id, breadcrumb }] }
+      }
+
+      // replace breadcrumb with existing id
+      return {
+        breadcrumbs: [
+          ...prev.breadcrumbs.slice(0, index),
+          { id, breadcrumb },
+          ...prev.breadcrumbs.slice(index + 1),
+        ],
+      }
+    })
   }
 
   remove(id) {
@@ -50,6 +62,12 @@ class BreadcrumbsProvider extends React.Component {
 const withBreadcrumb = (id, element) =>
   function(Component) {
     class BreadcrumbsConsumer extends React.Component {
+      constructor(props) {
+        super(props)
+
+        this.add()
+      }
+
       add() {
         const { add, breadcrumb } = this.props
 
@@ -60,10 +78,6 @@ const withBreadcrumb = (id, element) =>
         const { remove } = this.props
 
         remove(id)
-      }
-
-      componentDidMount() {
-        this.add()
       }
 
       componentWillUnmount() {
