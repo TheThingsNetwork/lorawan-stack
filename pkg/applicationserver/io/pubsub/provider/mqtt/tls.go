a2834526d1fe8d24939d1df057a137b0dcdbfb40
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2019-08-22 15:37:36 +0200

    fixup! as: Add MQTT PubSub provider

--- pkg/applicationserver/io/pubsub/provider/mqtt/tls.go
+++ pkg/applicationserver/io/pubsub/provider/mqtt/tls.go
@@ -21,7 +21,7 @@ import (
 	"go.thethings.network/lorawan-stack/pkg/errors"
 )
 
-var errInvalidCA = errors.DefineInvalidArgument("ca_pem_data", "CA PEM data is invalid")
+var errInvalidCAPEMData = errors.DefineInvalidArgument("ca_pem_data", "CA PEM data is invalid")
 
 func createTLSConfig(caPEM []byte, certPEM []byte, keyPEM []byte) (*tls.Config, error) {
 	// Change the CA certificate pool only if a CA has been provided.
@@ -30,7 +30,7 @@ func createTLSConfig(caPEM []byte, certPEM []byte, keyPEM []byte) (*tls.Config, 
 	if len(caPEM) != 0 {
 		certPool = x509.NewCertPool()
 		if !certPool.AppendCertsFromPEM(caPEM) {
-			return nil, errInvalidCA
+			return nil, errInvalidCAPEMData
 		}
 	}
 	cert, err := tls.X509KeyPair(certPEM, keyPEM)
