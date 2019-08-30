94d2e75558a706f454c8696c9936b3e5bb7c9307
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-08-30 12:07:12 +0300

    util: Add organizations service with getAll method

--- sdk/js/src/index.js
+++ sdk/js/src/index.js
@@ -20,6 +20,7 @@ import Token from './util/token'
 import Gateways from './service/gateways'
 import Js from './service/join-server'
 import Ns from './service/network-server'
+import Organizations from './service/organizations'
 
 class TtnLw {
   constructor(token, { stackConfig, connectionType, defaultUserId, proxy, axiosConfig }) {
@@ -33,6 +34,7 @@ class TtnLw {
     this.Gateways = new Gateways(this.api, { defaultUserId, proxy, stackConfig })
     this.Js = new Js(this.api.Js)
     this.Ns = new Ns(this.api.Ns)
+    this.Organizations = new Organizations(this.api)
   }
 }
 
