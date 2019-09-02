89c181d676697d810b035436663497926f76edd2
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-09-02 15:45:53 +0300

    console: Add organization events component

---
+++ pkg/webui/console/containers/organization-events/organization-events.js
@@ -0,0 +1,49 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+
+import PropTypes from '../../../lib/prop-types'
+import EventsSubscription from '../../containers/events-subscription'
+
+export default class OrganizationEvents extends React.Component {
+  static propTypes = {
+    errorSelector: PropTypes.func.isRequired,
+    eventsSelector: PropTypes.func.isRequired,
+    onClear: PropTypes.func.isRequired,
+    orgId: PropTypes.string.isRequired,
+    statusSelector: PropTypes.func.isRequired,
+    widget: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    widget: false,
+  }
+
+  render() {
+    const { orgId, widget, onClear, eventsSelector, errorSelector, statusSelector } = this.props
+
+    return (
+      <EventsSubscription
+        id={orgId}
+        widget={widget}
+        eventsSelector={eventsSelector}
+        statusSelector={statusSelector}
+        errorSelector={errorSelector}
+        onClear={onClear}
+        toAllUrl={`/organizations/${orgId}/data`}
+      />
+    )
+  }
+}
