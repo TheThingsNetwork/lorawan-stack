e714c3591476258ce9f31b0faa9fd1442bad4bce
KrishnaIyer <mail@krishnaiyereaswaran.com>
2019-09-12 11:27:20 +0200

    gs: Add NS upstream tests

--- pkg/gatewayserver/upstream/ns/ns_test.go
+++ pkg/gatewayserver/upstream/ns/ns_test.go
@@ -15,8 +15,91 @@
 package ns
 
 import (
+	"context"
 	"testing"
+
+	"github.com/smartystreets/assertions"
+	"go.thethings.network/lorawan-stack/pkg/component"
+	"go.thethings.network/lorawan-stack/pkg/config"
+	"go.thethings.network/lorawan-stack/pkg/gatewayserver/upstream/mock"
+	"go.thethings.network/lorawan-stack/pkg/log"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/types"
+	"go.thethings.network/lorawan-stack/pkg/util/test"
+	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
 )
 
 func TestNSHandler(t *testing.T) {
+	ctx := log.NewContext(test.Context(), test.GetLogger(t))
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	gtwIds := ttnpb.GatewayIdentifiers{GatewayID: "test-gateway"}
+	ns, nsAddr := mock.NewNS(ctx)
+	c := component.MustNew(test.GetLogger(t), &component.Config{
+		ServiceBase: config.ServiceBase{
+			GRPC: config.GRPC{
+				Listen:                      ":0",
+				AllowInsecureForCredentials: true,
+			},
+			Cluster: config.Cluster{
+				NetworkServer: nsAddr,
+			},
+		},
+	})
+	test.Must(nil, c.Start())
+	defer c.Close()
+	mustHavePeer(ctx, c, ttnpb.ClusterRole_NETWORK_SERVER)
+	h := NewHandler(ctx, "cluster", c, nil)
+	go h.ConnectGateway(ctx, gtwIds, nil)
+
+	for i, tc := range []struct {
+		Name                 string
+		UplinkMessage        *ttnpb.UplinkMessage
+		EndDeviceIdentifiers ttnpb.EndDeviceIdentifiers
+	}{
+		{
+			Name: "OneUplink",
+			UplinkMessage: &ttnpb.UplinkMessage{
+				Payload: &ttnpb.Message{
+					MHDR: ttnpb.MHDR{MType: ttnpb.MType_JOIN_REQUEST, Major: ttnpb.Major_LORAWAN_R1},
+					MIC:  []byte{0x4E, 0x61, 0xBC, 0x00},
+					Payload: &ttnpb.Message_JoinRequestPayload{JoinRequestPayload: &ttnpb.JoinRequestPayload{
+						JoinEUI:  types.EUI64{0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22},
+						DevEUI:   types.EUI64{0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11},
+						DevNonce: [2]byte{0x46, 0x50},
+					}},
+				},
+				RxMetadata: []*ttnpb.RxMetadata{{
+					GatewayIdentifiers: gtwIds,
+					RSSI:               89,
+					ChannelRSSI:        89,
+					SNR:                9.25,
+				}},
+				Settings: ttnpb.TxSettings{
+					Frequency:  868300000,
+					CodingRate: "4/5",
+					DataRate: ttnpb.DataRate{Modulation: &ttnpb.DataRate_LoRa{LoRa: &ttnpb.LoRaDataRate{
+						SpreadingFactor: 11,
+						Bandwidth:       125000,
+					}}},
+				},
+			},
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			a := assertions.New(t)
+			gtwUp := &ttnpb.GatewayUp{UplinkMessages: []*ttnpb.UplinkMessage{tc.UplinkMessage}}
+			err := h.HandleUp(ctx, gtwIds, tc.EndDeviceIdentifiers, gtwUp)
+			if !a.So(err, should.BeNil) {
+				t.Fatalf("Error sending upstream message: %v", err)
+			}
+			upmsgs := ns.GetUpMessages()
+			if !a.So(len(upmsgs), should.Equal, i+1) {
+				t.Fatalf("Unexpected number of messages received: %d", len(upmsgs))
+			}
+			if !a.So(upmsgs[i], should.Resemble, tc.UplinkMessage) {
+				t.Fatalf("Unexpected upstream message: %v", upmsgs[i])
+			}
+		})
+	}
 }
