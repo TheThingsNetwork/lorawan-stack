3dd5909d2f5be31708afb381fd088afc6aec8eef
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-09-17 18:59:16 +0200

    console: Refactor frequency plan selectors

--- pkg/webui/console/containers/freq-plans-select/index.js
+++ pkg/webui/console/containers/freq-plans-select/index.js
@@ -12,15 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react'
-import { connect as storeConnect } from 'react-redux'
 import { defineMessages } from 'react-intl'
-import bind from 'autobind-decorator'
 
-import sharedMessages from '../../../lib/shared-messages'
-import PropTypes from '../../../lib/prop-types'
-import Field from '../../../components/form/field'
-import Select from '../../../components/select'
+import CreateFetchSelect from '../fetch-select'
 
 import {
   selectGsFrequencyPlans,
@@ -32,78 +26,22 @@ import {
 import { getGsFrequencyPlans, getNsFrequencyPlans } from '../../store/actions/configuration'
 
 const m = defineMessages({
-  freqPlansFetchingFailure: 'Could not retrieve the list of available frequency plans',
+  title: 'Frequency Plan',
+  warning: 'Could not retrieve the list of available frequency plans',
 })
 
 const formatOptions = plans => plans.map(plan => ({ value: plan.id, label: plan.name }))
 
-@storeConnect(
-  function(state, props) {
-    return {
-      plansGs: selectGsFrequencyPlans(state, props),
-      plansNs: selectNsFrequencyPlans(state, props),
-      error: selectFrequencyPlansError(state, props),
-      fetching: selectFrequencyPlansFetching(state, props),
-    }
-  },
-  dispatch => ({
-    getGsFrequencyPlans: () => dispatch(getGsFrequencyPlans()),
-    getNsFrequencyPlans: () => dispatch(getNsFrequencyPlans()),
-  }),
-)
-@bind
-class FrequencyPlansSelect extends React.PureComponent {
-  componentDidMount() {
-    const { source, getNsFrequencyPlans, getGsFrequencyPlans } = this.props
+const CreateFrequencyPlansSelector = source =>
+  CreateFetchSelect({
+    fetchOptions: source === 'ns' ? getNsFrequencyPlans : getGsFrequencyPlans,
+    optionsSelector: source === 'ns' ? selectNsFrequencyPlans : selectGsFrequencyPlans,
+    fetchingSelector: selectFrequencyPlansFetching,
+    errorSelector: selectFrequencyPlansError,
+    defaultWarning: m.warning,
+    defaultTitle: m.title,
+    optionsFormatter: formatOptions,
+  })
 
-    if (source === 'ns') {
-      getNsFrequencyPlans()
-    } else {
-      getGsFrequencyPlans()
-    }
-  }
-
-  getOptions() {
-    const { source, plansGs, plansNs } = this.props
-
-    return source === 'ns' ? formatOptions(plansNs) : formatOptions(plansGs)
-  }
-
-  render() {
-    const { name, required, title, autoFocus, error, fetching, menuPlacement } = this.props
-
-    const fieldOptions = this.getOptions()
-
-    return (
-      <Field
-        component={Select}
-        options={fieldOptions}
-        name={name}
-        required={required}
-        title={title}
-        autoFocus={autoFocus}
-        isLoading={fetching}
-        warning={Boolean(error) ? m.freqPlansFetchingFailure : undefined}
-        menuPlacement={menuPlacement}
-      />
-    )
-  }
-}
-
-FrequencyPlansSelect.propTypes = {
-  source: PropTypes.oneOf(['ns', 'gs']).isRequired,
-  name: PropTypes.string.isRequired,
-  required: PropTypes.bool,
-  title: PropTypes.message,
-  autoFocus: PropTypes.bool,
-  menuPlacement: PropTypes.oneOf(['top', 'bottom', 'auto']),
-}
-
-FrequencyPlansSelect.defaultProps = {
-  title: sharedMessages.frequencyPlan,
-  autoFocus: false,
-  menuPlacement: 'auto',
-  required: false,
-}
-
-export default FrequencyPlansSelect
+export const GsFrequencyPlansSelect = CreateFrequencyPlansSelector('gs')
+export const NsFrequencyPlansSelect = CreateFrequencyPlansSelector('ns')
