aa30eaa795593b5db6c493c20a85ad484ebfda78
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-09-17 19:15:41 +0200

    console: Refactor webhook format selector

--- pkg/webui/console/containers/webhook-formats-select/index.js
+++ pkg/webui/console/containers/webhook-formats-select/index.js
@@ -12,88 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react'
-import { connect as storeConnect } from 'react-redux'
 import { defineMessages } from 'react-intl'
-import bind from 'autobind-decorator'
 
-import sharedMessages from '../../../lib/shared-messages'
-import PropTypes from '../../../lib/prop-types'
-import Field from '../../../components/form/field'
-import Select from '../../../components/select'
-
+import CreateFetchSelect from '../fetch-select'
 import {
   selectWebhookFormats,
   selectWebhookFormatsError,
   selectWebhookFormatsFetching,
 } from '../../store/selectors/webhook-formats'
-
 import { getWebhookFormats } from '../../store/actions/webhook-formats'
 
 const m = defineMessages({
-  formatFetchingFailure: 'Could not retrieve the list of available webhook formats',
+  title: 'Webhook Format',
+  warning: 'Could not retrieve the list of available webhook formats',
 })
 
-const formatOptions = formats =>
-  Object.keys(formats).map(key => ({ value: key, label: formats[key] }))
-
-@storeConnect(
-  function(state) {
-    return {
-      formats: selectWebhookFormats(state),
-      error: selectWebhookFormatsError(state),
-      fetching: selectWebhookFormatsFetching(state),
-    }
-  },
-  { getWebhookFormats },
-)
-@bind
-class WebhookFormatsSelector extends React.PureComponent {
-  componentDidMount() {
-    const { getWebhookFormats } = this.props
-    getWebhookFormats()
-  }
-
-  getOptions() {
-    const { formats } = this.props
-
-    return formatOptions(formats)
-  }
-
-  render() {
-    const { name, required, title, autoFocus, error, fetching, menuPlacement } = this.props
-
-    const fieldOptions = this.getOptions()
-
-    return (
-      <Field
-        component={Select}
-        type="select"
-        options={fieldOptions}
-        name={name}
-        required={required}
-        title={title}
-        autoFocus={autoFocus}
-        isLoading={fetching}
-        warning={Boolean(error) ? m.formatFetchingFailure : undefined}
-        menuPlacement={menuPlacement}
-      />
-    )
-  }
-}
-
-WebhookFormatsSelector.propTypes = {
-  name: PropTypes.string.isRequired,
-  required: PropTypes.bool.isRequired,
-  title: PropTypes.message,
-  autoFocus: PropTypes.bool,
-  menuPlacement: PropTypes.oneOf(['top', 'bottom', 'auto']),
-}
-
-WebhookFormatsSelector.defaultProps = {
-  title: sharedMessages.webhookFormat,
-  autoFocus: false,
-  menuPlacement: 'auto',
-}
-
-export default WebhookFormatsSelector
+export default CreateFetchSelect({
+  fetchOptions: getWebhookFormats,
+  optionsSelector: selectWebhookFormats,
+  errorSelector: selectWebhookFormatsError,
+  fetchingSelector: selectWebhookFormatsFetching,
+  defaultWarning: m.warning,
+  defaultTitle: m.title,
+})
