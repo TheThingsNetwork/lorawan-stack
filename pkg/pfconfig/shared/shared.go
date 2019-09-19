866becf7bc7ced475b71bb9d044b407433c9648d
KrishnaIyer <mail@krishnaiyereaswaran.com>
2019-09-19 13:19:09 +0200

    util: Refactor unmarshalling

--- pkg/pfconfig/shared/shared.go
+++ pkg/pfconfig/shared/shared.go
@@ -19,8 +19,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"regexp"
-	"strconv"
+	"strings"
 	"time"
 
 	"go.thethings.network/lorawan-stack/pkg/band"
@@ -29,7 +28,6 @@ import (
 )
 
 // SX1301Config contains the configuration for the SX1301 concentrator.
-// It marshals to and unmarshals from compliant JSON.
 type SX1301Config struct {
 	LoRaWANPublic       bool
 	ClockSource         uint8
@@ -107,12 +105,6 @@ func (c SX1301Config) MarshalJSON() ([]byte, error) {
 	return json.Marshal(m)
 }
 
-var (
-	radioKey = regexp.MustCompile("^radio_([0|1])$")
-	txLutKey = regexp.MustCompile("^tx_lut_([0-9]{1,2})$")
-	chanKey  = regexp.MustCompile("^chan_multiSF_([0-9]{1,2})$")
-)
-
 // UnmarshalJSON implements json.Unmarshaler.
 func (c *SX1301Config) UnmarshalJSON(msg []byte) error {
 	var root map[string]json.RawMessage
@@ -121,61 +113,57 @@ func (c *SX1301Config) UnmarshalJSON(msg []byte) error {
 	}
 	radioMap, txLutMap, chanMap := make(map[int]RFConfig), make(map[int]TxLUTConfig), make(map[int]IFConfig)
 	for key, value := range root {
-		if key == "lorawan_public" {
+		switch {
+		case key == "lorawan_public":
 			if err := json.Unmarshal(value, &c.LoRaWANPublic); err != nil {
 				return err
 			}
-		}
-		if key == "antenna_gain" {
+		case key == "antenna_gain":
 			if err := json.Unmarshal(value, &c.AntennaGain); err != nil {
 				return err
 			}
-		}
-		if key == "clksrc" {
+		case key == "clksrc":
 			if err := json.Unmarshal(value, &c.ClockSource); err != nil {
 				return err
 			}
-		}
-		if key == "lbt_cfg" {
+		case key == "lbt_cfg":
 			if err := json.Unmarshal(value, &c.LBTConfig); err != nil {
 				return err
 			}
-		}
-		if key == "chan_Lora_std" {
+		case key == "chan_Lora_std":
 			if err := json.Unmarshal(value, &c.LoRaStandardChannel); err != nil {
 				return err
 			}
-		}
-		if key == "chan_FSK" {
+		case key == "chan_FSK":
 			if err := json.Unmarshal(value, &c.FSKChannel); err != nil {
 				return err
 			}
-		}
-		if match := radioKey.FindStringSubmatch(key); len(match) == 2 {
-			var radio RFConfig
-			if err := json.Unmarshal(value, &radio); err != nil {
-				return err
-			}
-			if index, err := strconv.Atoi(match[1]); err == nil {
-				radioMap[index] = radio
-			}
-		}
-		if match := txLutKey.FindStringSubmatch(key); len(match) == 2 {
-			var txLut TxLUTConfig
-			if err := json.Unmarshal(value, &txLut); err != nil {
-				return err
-			}
-			if index, err := strconv.Atoi(match[1]); err == nil {
-				txLutMap[index] = txLut
-			}
-		}
-		if match := chanKey.FindStringSubmatch(key); len(match) == 2 {
+		case strings.HasPrefix(key, "chan_multiSF_"):
 			var channel IFConfig
 			if err := json.Unmarshal(value, &channel); err != nil {
 				return err
 			}
-			if index, err := strconv.Atoi(match[1]); err == nil {
+			var index int
+			if _, err := fmt.Sscanf(key, "chan_multiSF_%d", &index); err == nil {
 				chanMap[index] = channel
+			}
+		case strings.HasPrefix(key, "tx_lut_"):
+			var txLut TxLUTConfig
+			if err := json.Unmarshal(value, &txLut); err != nil {
+				return err
+			}
+			var index int
+			if _, err := fmt.Sscanf(key, "tx_lut_%d", &index); err == nil {
+				txLutMap[index] = txLut
+			}
+		case strings.HasPrefix(key, "radio_"):
+			var radio RFConfig
+			if err := json.Unmarshal(value, &radio); err != nil {
+				return err
+			}
+			var index int
+			if _, err := fmt.Sscanf(key, "radio_%d", &index); err == nil {
+				radioMap[index] = radio
 			}
 		}
 	}
