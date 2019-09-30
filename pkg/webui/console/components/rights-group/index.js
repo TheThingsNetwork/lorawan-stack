45170c4d446494417004fe132dfb785b437dd30b
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-09-30 15:47:51 +0200

    console: Renew rights group ui logic

--- pkg/webui/console/components/rights-group/index.js
+++ pkg/webui/console/components/rights-group/index.js
@@ -14,161 +14,238 @@
 
 import React from 'react'
 import bind from 'autobind-decorator'
-import { defineMessages } from 'react-intl'
+import { defineMessages, injectIntl } from 'react-intl'
 import classnames from 'classnames'
 
 import PropTypes from '../../../lib/prop-types'
 import { RIGHT_ALL } from '../../lib/rights'
+import withComputedProps from '../../../lib/components/with-computed-props'
 
 import Checkbox from '../../../components/checkbox'
 import Notification from '../../../components/notification'
+import Radio from '../../../components/radio-button'
 
 import style from './rights-group.styl'
 
 const m = defineMessages({
   selectAll: 'Select All',
   outOfOwnScopeRights:
-    'This entity possesses rights that are out of your scope of granted rights. These rights cannot be altered.',
-  outOfOwnScopeRightsStrict:
-    'This entity possesses rights that are out of your scope of granted rights. Modifying is hence prohibited.',
+    'This {entityType} has more rights than you have. These rights can not be modified.',
+  outOfOwnScopeUniversalRight:
+    "This {entityType} has a wildcard right that you don't have. The {entityType} can therefore only be removed entirely.",
+  grantType: 'Grant type',
+  allCurrentAndFutureRights: 'Grant all current and future rights',
+  selectIndividualRights: 'Grant individual rights',
 })
 
-const computeState = function(values, rights, universalRight) {
-  const selectedCheckboxesCount = values.length
-  const totalCheckboxesCount = rights.length
+const computeProps = function(props) {
+  const { value, universalRight: grantableUniversalRight, rights: grantableRights } = props
 
-  const universalRightIsChecked = values.includes(RIGHT_ALL) || values.includes(universalRight)
+  // Extract the universal right from own rights or granted rights
+  const derivedUniversalRight =
+    grantableUniversalRight || value.find(right => right !== RIGHT_ALL && right.endsWith('_ALL'))
+
+  // Filter out rights that the entity has but may not be granted by the user
+  const outOfOwnScopeRights = !Boolean(grantableUniversalRight)
+    ? value.filter(right => !grantableRights.includes(right))
+    : []
+
+  // Extract all rights by combining granted and grantable rights
+  const derivedRights = [...grantableRights, ...outOfOwnScopeRights].sort()
+
+  // Store whether out of scope universal rights are present
+  const hasOutOfOwnScopeUniversalRight =
+    outOfOwnScopeRights.filter(right => right.endsWith('_ALL')).length !== 0
+
+  // Store granted individual rights
+  const grantedIndividualRights = value.filter(right => right !== derivedUniversalRight)
+
+  // Store out of own scope individual rights
+  const outOfOwnScopeIndividualRights = !Boolean(grantableUniversalRight)
+    ? grantedIndividualRights.filter(right => !grantableRights.includes(right))
+    : []
+
+  // Determine whether a universal right is granted
+  const hasUniversalRightGranted =
+    value.includes(RIGHT_ALL) || value.includes(derivedUniversalRight)
+
+  // Determine the current grant type
+  const grantType = hasUniversalRightGranted ? 'universal' : 'individual'
 
   return {
-    allSelected: universalRightIsChecked || selectedCheckboxesCount === totalCheckboxesCount,
-    indeterminate:
-      !universalRightIsChecked &&
-      selectedCheckboxesCount !== 0 &&
-      selectedCheckboxesCount !== totalCheckboxesCount,
+    outOfOwnScopeIndividualRights,
+    hasOutOfOwnScopeUniversalRight,
+    derivedUniversalRight,
+    derivedRights,
+    hasUniversalRightGranted,
+    grantType,
+    ...props,
   }
 }
 
+@withComputedProps(computeProps)
+@injectIntl
 @bind
 class RightsGroup extends React.Component {
-  state = {}
-
-  static getDerivedStateFromProps(props) {
-    if ('value' in props) {
-      const { value, rights: grantableRights, universalRight: grantableUniversalRight } = props
-      let universalRight = grantableUniversalRight
-      const allGrantableRights = [...grantableRights, ...grantableUniversalRight]
-
-      // Identify given rights that are out of the scope of the current user
-      const outOfOwnScopeRights = value.filter(function(right) {
-        if (!allGrantableRights.includes(right) && right !== RIGHT_ALL) {
-          if (right.endsWith('_ALL')) {
-            universalRight = right
-          }
-          return true
-        }
-        return false
-      })
-
-      // Compose rights list
-      const rights = [...outOfOwnScopeRights, ...grantableRights]
-
-      const { allSelected, indeterminate } = computeState(value, rights, universalRight)
-
-      return {
-        allSelected,
-        indeterminate,
-        outOfOwnScopeRights,
-        rights,
-        universalRight,
-        value,
-      }
-    }
-
-    return null
+  static propTypes = {
+    /** The class to be added to the container */
+    className: PropTypes.string,
+    /** The rights derived from the granted and grantable rights **/
+    derivedRights: PropTypes.rights.isRequired,
+    /** The universal right derived from the current entity or user **/
+    derivedUniversalRight: PropTypes.string,
+    /** A flag indicating whether the whole component should be disabled **/
+    disabled: PropTypes.bool,
+    /** The message depicting the type of entity this component is setting the
+     * rights for.
+     */
+    entityTypeMessage: PropTypes.message.isRequired,
+    /** The right grant type **/
+    grantType: PropTypes.oneOf(['universal', 'individual']).isRequired,
+    /** Whether the entity has a universal right that the current use does not have **/
+    hasOutOfOwnScopeUniversalRight: PropTypes.bool.isRequired,
+    /** Whether the entity has a universal right granted **/
+    hasUniversalRightGranted: PropTypes.bool.isRequired,
+    /** The intl object provided by injectIntl of react-intl, used to translate
+     * messages
+     */
+    intl: PropTypes.shape({
+      formatMessage: PropTypes.func.isRequired,
+    }).isRequired,
+    /** Blur event hook */
+    onBlur: PropTypes.func,
+    /** Change event hook */
+    onChange: PropTypes.func,
+    /** A list of rights that are outside the scope of the current user **/
+    outOfOwnScopeIndividualRights: PropTypes.rights.isRequired,
+    /** The universal right literal comprising all other rights */
+    universalRight: PropTypes.string,
+    /** The rights value */
+    value: PropTypes.rights.isRequired,
   }
 
-  async handleChangeAll(event) {
-    const { onChange } = this.props
-    const { rights, outOfOwnScopeRights, universalRight } = this.state
+  static defaultProps = {
+    className: undefined,
+    disabled: false,
+    onBlur: () => null,
+    onChange: () => null,
+    universalRight: undefined,
+    derivedUniversalRight: undefined,
+  }
+
+  state = {
+    individualRightValue: [],
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const { individualRightValue: oldIndividualRightValue } = state
+    const { value, hasUniversalRightGranted } = props
+
+    // Store the individual right values when the grant type is changed to
+    // universal right
+    const individualRightValue = !hasUniversalRightGranted ? value : oldIndividualRightValue
+
+    return { individualRightValue }
+  }
+
+  handleChangeAll(event) {
+    const { onChange, outOfOwnScopeIndividualRights, derivedRights } = this.props
     const { checked } = event.target
 
     let value
 
-    // Determine new value based on universal rights and out of scope rights
     if (checked) {
-      if (universalRight) {
-        // Prefer universal right value, if present
-        value = [universalRight]
-      } else {
-        // Else add rights individually
-        value = [...rights]
-      }
+      // Fill up with individual rights
+      value = [...derivedRights]
     } else {
       // On uncheck, leave out of scope rights checked, if present
-      value = [...outOfOwnScopeRights]
+      value = [...outOfOwnScopeIndividualRights]
     }
 
     onChange(value)
   }
 
-  async handleChange(val) {
+  handleChange(val) {
+    const { onChange } = this.props
     const value = Object.keys(val).filter(right => val[right])
-    const { onChange, rights } = this.props
-    const { universalRight } = this.state
-    const { allSelected } = computeState(value, rights, universalRight)
 
-    // Set new right value and prefer universal right if applicable
-    const result = universalRight && allSelected ? [universalRight] : [...value]
+    onChange(value)
+  }
 
-    onChange(result)
+  handleGrantTypeChange(val) {
+    const { onChange, universalRight } = this.props
+    const { individualRightValue } = this.state
+
+    if (val === 'universal') {
+      onChange([universalRight])
+    } else {
+      onChange(individualRightValue)
+    }
   }
 
   render() {
-    const { className, name, onBlur, disabled, strict } = this.props
-
     const {
-      indeterminate,
-      outOfOwnScopeRights,
-      value,
-      allSelected,
-      rights,
-      universalRight,
-    } = this.state
+      intl,
+      className,
+      disabled,
+      entityTypeMessage,
+      onBlur,
+      outOfOwnScopeIndividualRights,
+      hasOutOfOwnScopeUniversalRight,
+      grantType,
+      derivedUniversalRight,
+      derivedRights,
+    } = this.props
+    const { individualRightValue } = this.state
 
-    const cbs = rights.map(right => (
+    const selectedCheckboxesCount = individualRightValue.filter(right => !right.endsWith('_ALL'))
+      .length
+    const totalCheckboxesCount = derivedRights.length
+    const allSelected = selectedCheckboxesCount === totalCheckboxesCount
+    const indeterminate =
+      selectedCheckboxesCount !== 0 && selectedCheckboxesCount !== totalCheckboxesCount
+    const allDisabled = grantType === 'universal' || disabled || hasOutOfOwnScopeUniversalRight
+
+    // Marshal rights to key/value for checkbox group
+    const rightsValues = derivedRights.reduce(function(acc, right) {
+      acc[right] = allSelected || individualRightValue.includes(right)
+
+      return acc
+    }, {})
+
+    const cbs = derivedRights.map(right => (
       <Checkbox
         className={style.rightLabel}
         key={right}
         name={right}
+        disabled={outOfOwnScopeIndividualRights.includes(right)}
         label={{ id: `enum:${right}` }}
-        disabled={outOfOwnScopeRights.includes(right)}
       />
     ))
 
-    const hasRightAll = Boolean(value.includes(RIGHT_ALL))
-    const hasOutOfOwnScopeRights = Boolean(outOfOwnScopeRights.length)
-    const allDisabled =
-      disabled || outOfOwnScopeRights.includes(universalRight) || (strict && hasOutOfOwnScopeRights)
-
-    // Marshal rights to key/value for checkbox group
-    const rightsValues = rights.reduce(
-      function(acc, right) {
-        acc[right] = allSelected || value.includes(right)
-
-        return acc
-      },
-      { [RIGHT_ALL]: hasRightAll },
-    )
-
     return (
       <div className={className}>
-        {hasOutOfOwnScopeRights && (
-          <Notification small info={strict ? m.outOfOwnScopeRightsStrict : m.outOfOwnScopeRights} />
+        {hasOutOfOwnScopeUniversalRight && (
+          <Notification
+            small
+            warning={m.outOfOwnScopeUniversalRight}
+            messageValues={{ entityType: intl.formatMessage(entityTypeMessage).toLowerCase() }}
+          />
         )}
+        <Radio.Group
+          className={style.grantType}
+          name="grant_type"
+          value={grantType}
+          onChange={this.handleGrantTypeChange}
+          disabled={!Boolean(derivedUniversalRight)}
+        >
+          <Radio label={m.allCurrentAndFutureRights} value="universal" />
+          <Radio label={m.selectIndividualRights} value="individual" />
+        </Radio.Group>
         <Checkbox
           className={classnames(style.selectAll, style.rightLabel)}
-          name={universalRight || 'select-all'}
-          label={universalRight ? { id: `enum:${universalRight}` } : m.selectAll}
+          name={derivedUniversalRight || 'select-all'}
+          label={derivedUniversalRight ? { id: `enum:${derivedUniversalRight}` } : m.selectAll}
           onChange={this.handleChangeAll}
           indeterminate={indeterminate}
           value={allSelected}
@@ -188,35 +265,6 @@ class RightsGroup extends React.Component {
       </div>
     )
   }
-}
-
-RightsGroup.propTypes = {
-  /** The class to be added to the container */
-  className: PropTypes.string,
-  /** The name prop, used to connect to formik */
-  name: PropTypes.string.isRequired,
-  /** The rights value */
-  value: PropTypes.array,
-  /** Change event hook */
-  onChange: PropTypes.func,
-  /** Blur event hook */
-  onBlur: PropTypes.func,
-  /** The universal right literal comprising all other rights */
-  universalRight: PropTypes.string,
-  /** The list of rights options */
-  rights: PropTypes.arrayOf(PropTypes.string),
-  /** A flag identifying whether modifying rights is allowed when out of scope
-   * rights are present. Can be used to prevent user error.
-   */
-  strict: PropTypes.bool,
-}
-
-RightsGroup.defaultProps = {
-  onChange: () => null,
-  onBlur: () => null,
-  rights: [],
-  value: [],
-  strict: false,
 }
 
 export default RightsGroup
