b0b23c4e9419473b375a4c1db44ca1eca5c9b71c
Johan Stokking <johan@thethingsindustries.com>
2019-09-30 19:29:47 +0200

    util: Support ATECC608A-TNGLORA

--- pkg/devicetemplates/microchip.go
+++ pkg/devicetemplates/microchip.go
@@ -17,16 +17,19 @@ package devicetemplates
 import (
 	"context"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
 	"fmt"
 	"io"
+	"strings"
 
 	pbtypes "github.com/gogo/protobuf/types"
 	"go.thethings.network/lorawan-stack/pkg/errors"
 	"go.thethings.network/lorawan-stack/pkg/gogoproto"
 	"go.thethings.network/lorawan-stack/pkg/provisioning"
 	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/types"
 	jose "gopkg.in/square/go-jose.v2"
 )
 
@@ -54,6 +57,18 @@ MGWjUDBOMB0GA1UdDgQWBBTtwIguUA7BijX48KEa6jJQhIwreDAfBgNVHSMEGDAW
 gBTtwIguUA7BijX48KEa6jJQhIwreDAMBgNVHRMBAf8EAjAAMAoGCCqGSM49BAMC
 A0gAMEUCIQD9/x9zxmHkeWGwjEq67QsQqBVmoY8k6PvFVr4Bz1tYOwIgYfck+fv/
 pno8+2vVTkQDhcinNrgoPLQORzV5/l/b4z4=
+-----END CERTIFICATE-----`),
+	"8VeKGdyU2d8wev6_VzNJOBOv-cA": []byte(`-----BEGIN CERTIFICATE-----
+MIIBxzCCAWygAwIBAgIQc6HyMjrkT2TOY17FAX7XVjAKBggqhkjOPQQDAjA8MSEw
+HwYDVQQKDBhNaWNyb2NoaXAgVGVjaG5vbG9neSBJbmMxFzAVBgNVBAMMDkxvZyBT
+aWduZXIgMDAyMB4XDTE5MDgxNTE5NDc1OVoXDTIwMDgxNTE5NDc1OVowPDEhMB8G
+A1UECgwYTWljcm9jaGlwIFRlY2hub2xvZ3kgSW5jMRcwFQYDVQQDDA5Mb2cgU2ln
+bmVyIDAwMjBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABLCLrgPlT3OezntD9lC2
+ShwUhlx07fiq/VETJ+ITUAwbgrPjB/Xi9GchLIM7FwZSUGOEqRA6KtH32XMpTGHK
+mCCjUDBOMB0GA1UdDgQWBBTxV4oZ3JTZ3zB6/r9XM0k4E6/5wDAfBgNVHSMEGDAW
+gBTxV4oZ3JTZ3zB6/r9XM0k4E6/5wDAMBgNVHRMBAf8EAjAAMAoGCCqGSM49BAMC
+A0kAMEYCIQDKHgctLnq/zNqfB+1v0KRhDVPvRf6Dimt8aW9WLS0NWAIhAJvUe3uJ
+pkMG4zpov9FCoj4G340idEadm7mVbAd5GOB9
 -----END CERTIFICATE-----`),
 }
 
@@ -113,15 +128,15 @@ func (m *microchipATECC608AMAHTNT) Convert(ctx context.Context, r io.Reader, ch 
 		}
 		buf, err := jws.Verify(key)
 		if err != nil {
-			return err
+			return errMicrochipData.WithCause(err)
 		}
 		m := make(map[string]interface{})
 		if err := json.Unmarshal(buf, &m); err != nil {
-			return err
+			return errMicrochipData.WithCause(err)
 		}
 		s, err := gogoproto.Struct(m)
 		if err != nil {
-			return err
+			return errMicrochipData.WithCause(err)
 		}
 		ch <- &ttnpb.EndDeviceTemplate{
 			EndDevice: ttnpb.EndDevice{
@@ -130,6 +145,103 @@ func (m *microchipATECC608AMAHTNT) Convert(ctx context.Context, r io.Reader, ch 
 			},
 			FieldMask: pbtypes.FieldMask{
 				Paths: []string{"provisioner_id", "provisioning_data"},
+			},
+			MappingKey: s.Fields["uniqueId"].GetStringValue(),
+		}
+	}
+	return nil
+}
+
+// microchipATECC608TNGLORA is a Microchip ATECC608A-TNGLORA device provisioner.
+type microchipATECC608TNGLORA struct {
+	keys map[string]interface{}
+}
+
+func (m *microchipATECC608TNGLORA) Format() *ttnpb.EndDeviceTemplateFormat {
+	return &ttnpb.EndDeviceTemplateFormat{
+		Name:        "Microchip ATECC608A-TNGLORA Manifest File",
+		Description: "JSON manifest file received through Microchip Purchasing & Client Services.",
+	}
+}
+
+// Convert decodes the given manifest data.
+// The input data is an array of JWS (JSON Web Signatures).
+func (m *microchipATECC608TNGLORA) Convert(ctx context.Context, r io.Reader, ch chan<- *ttnpb.EndDeviceTemplate) error {
+	defer close(ch)
+
+	dec := json.NewDecoder(r)
+	delim, err := dec.Token()
+	if err != nil {
+		return errMicrochipData.WithCause(err)
+	}
+	if _, ok := delim.(json.Delim); !ok {
+		return errMicrochipData
+	}
+
+	for dec.More() {
+		var jws microchipEntry
+		if err := dec.Decode(&jws); err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		kid := jws.Signatures[0].Header.KeyID
+		key, ok := m.keys[kid]
+		if !ok {
+			return errMicrochipPublicKey.WithAttributes("id", kid)
+		}
+		buf, err := jws.Verify(key)
+		if err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		// publicKeySet.keys[0].x5c[0] contains the base64 certificate of the secure element.
+		// The second value in the CN is the hex encoded IEEE issued DevEUI.
+		data := struct {
+			PublicKeySet struct {
+				Keys []struct {
+					X5C []string `json:"x5c"`
+				} `json:"keys"`
+			} `json:"publicKeySet"`
+		}{}
+		if err := json.Unmarshal(buf, &data); err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		if len(data.PublicKeySet.Keys) < 1 || len(data.PublicKeySet.Keys[0].X5C) < 1 {
+			return errMicrochipData
+		}
+		certBuf, err := base64.RawStdEncoding.DecodeString(data.PublicKeySet.Keys[0].X5C[0])
+		if err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		cert, err := x509.ParseCertificate(certBuf)
+		if err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		cnParts := strings.SplitN(cert.Subject.CommonName, " ", 3)
+		if len(cnParts) != 3 {
+			return errMicrochipData.WithCause(err)
+		}
+		var devEUI types.EUI64
+		if err := devEUI.UnmarshalText([]byte(cnParts[1])); err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		m := make(map[string]interface{})
+		if err := json.Unmarshal(buf, &m); err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		s, err := gogoproto.Struct(m)
+		if err != nil {
+			return errMicrochipData.WithCause(err)
+		}
+		ch <- &ttnpb.EndDeviceTemplate{
+			EndDevice: ttnpb.EndDevice{
+				EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+					DeviceID: strings.ToLower(fmt.Sprintf("eui-%s", devEUI)),
+					DevEUI:   &devEUI,
+				},
+				ProvisionerID:    provisioning.Microchip,
+				ProvisioningData: s,
+			},
+			FieldMask: pbtypes.FieldMask{
+				Paths: []string{"ids.device_id", "ids.dev_eui", "provisioner_id", "provisioning_data"},
 			},
 			MappingKey: s.Fields["uniqueId"].GetStringValue(),
 		}
@@ -152,6 +264,9 @@ func init() {
 	}
 
 	RegisterConverter("microchip-atecc608a-mahtn-t", &microchipATECC608AMAHTNT{
+		keys: keys,
+	})
+	RegisterConverter("microchip-atecc608a-tnglora", &microchipATECC608TNGLORA{
 		keys: keys,
 	})
 }
