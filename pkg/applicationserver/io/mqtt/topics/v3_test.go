ebc60b94347be733e9c9772bc244573dc5c8892c
Johan Stokking <johan@thethingsindustries.com>
2019-10-03 13:38:06 +0200

    as: Test MQTT topic structure

--- pkg/applicationserver/io/mqtt/topics/v3_test.go
+++ pkg/applicationserver/io/mqtt/topics/v3_test.go
@@ -15,17 +15,22 @@
 package topics_test
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/TheThingsIndustries/mystique/pkg/topic"
 	"github.com/smartystreets/assertions"
 	"go.thethings.network/lorawan-stack/pkg/applicationserver/io/mqtt/topics"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/pkg/unique"
+	"go.thethings.network/lorawan-stack/pkg/util/test"
 	"go.thethings.network/lorawan-stack/pkg/util/test/assertions/should"
 )
 
 func TestV3AcceptedTopic(t *testing.T) {
-	uid := "foo-app"
+	uid := unique.ID(test.Context(), ttnpb.ApplicationIdentifiers{ApplicationID: "foo-app"})
 	for i, tc := range []struct {
 		Requested,
 		Accepted string
@@ -39,22 +44,22 @@ func TestV3AcceptedTopic(t *testing.T) {
 		},
 		{
 			Requested: "#",
-			Accepted:  "v3/foo-app/#",
+			Accepted:  fmt.Sprintf("v3/%s/#", uid),
 			OK:        true,
 		},
 		{
 			Requested: "v3/#",
-			Accepted:  "v3/foo-app/#",
+			Accepted:  fmt.Sprintf("v3/%s/#", uid),
 			OK:        true,
 		},
 		{
 			Requested: "v3/+/uplink",
-			Accepted:  "v3/foo-app/uplink",
+			Accepted:  fmt.Sprintf("v3/%s/uplink", uid),
 			OK:        true,
 		},
 		{
-			Requested: "v3/foo-app/uplink",
-			Accepted:  "v3/foo-app/uplink",
+			Requested: fmt.Sprintf("v3/%s/uplink", uid),
+			Accepted:  fmt.Sprintf("v3/%s/uplink", uid),
 			OK:        true,
 		},
 	} {
@@ -65,6 +70,62 @@ func TestV3AcceptedTopic(t *testing.T) {
 				t.FailNow()
 			}
 			a.So(topic.Join(actual), should.Equal, tc.Accepted)
+		})
+	}
+}
+
+func TestV3Topics(t *testing.T) {
+	appUID := unique.ID(test.Context(), ttnpb.ApplicationIdentifiers{ApplicationID: "foo-app"})
+	devID := "foo-device"
+
+	for _, tc := range []struct {
+		Fn       func(applicationUID, deviceUID string) []string
+		Expected string
+	}{
+		{
+			Fn:       topics.Default.UplinkTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/up", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.JoinAcceptTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/join", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.DownlinkAckTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/down/ack", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.DownlinkNackTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/down/nack", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.DownlinkSentTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/down/sent", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.DownlinkFailedTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/down/failed", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.DownlinkQueuedTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/down/queued", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.LocationSolvedTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/location/solved", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.DownlinkPushTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/down/push", appUID, devID),
+		},
+		{
+			Fn:       topics.Default.DownlinkReplaceTopic,
+			Expected: fmt.Sprintf("v3/%s/devices/%s/down/replace", appUID, devID),
+		},
+	} {
+		t.Run(tc.Expected, func(t *testing.T) {
+			actual := strings.Join(tc.Fn(appUID, devID), "/")
+			assertions.New(t).So(actual, should.Equal, tc.Expected)
 		})
 	}
 }
