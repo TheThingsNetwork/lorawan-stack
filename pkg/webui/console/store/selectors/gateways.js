002a3d3d8aeeda92780a09648f0958634b81c62d
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-10-21 14:19:02 +0300

    console: Extend gateway selectors add collaborators selectors

--- pkg/webui/console/store/selectors/gateways.js
+++ pkg/webui/console/store/selectors/gateways.js
@@ -21,6 +21,7 @@ import {
   GET_GTWS_RIGHTS_LIST_BASE,
   START_GTW_STATS_BASE,
   GET_GTW_COLLABORATOR_BASE,
+  GET_GTW_COLLABORATORS_LIST_BASE,
 } from '../actions/gateways'
 
 import {
@@ -35,6 +36,8 @@ import {
   createPaginationTotalCountSelectorByEntity,
 } from './pagination'
 import {
+  createCollaboratorsSelector,
+  createTotalCountSelector as createCollaboratorsTotalCountSelector,
   createUserCollaboratorSelector,
   createOrganizationCollaboratorSelector,
 } from './collaborators'
@@ -112,6 +115,11 @@ export const selectGatewayStatistics = function(state) {
 }
 
 // Collaborators
+export const selectGatewayCollaborators = createCollaboratorsSelector(ENTITY)
+export const selectGatewayCollaboratorsTotalCount = createCollaboratorsTotalCountSelector(ENTITY)
+export const selectGatewayCollaboratorsFetching = createFetchingSelector(
+  GET_GTW_COLLABORATORS_LIST_BASE,
+)
 export const selectGatewayUserCollaborator = createUserCollaboratorSelector(ENTITY_SINGLE)
 export const selectGatewayOrganizationCollaborator = createOrganizationCollaboratorSelector(
   ENTITY_SINGLE,
