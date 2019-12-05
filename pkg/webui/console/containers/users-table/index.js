2fa2c6a1222a6988e18f73330e64c388e3b5ec06
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-12-05 16:22:30 +0100

    console: Add users table container

---
+++ pkg/webui/console/containers/users-table/index.js
@@ -0,0 +1,120 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React, { Component } from 'react'
+
+import sharedMessages from '../../../lib/shared-messages'
+import Message from '../../../lib/components/message'
+import PropTypes from '../../../lib/prop-types'
+import Status from '../../../components/status'
+
+import FetchTable from '../fetch-table'
+
+import { getUsersList } from '../../../console/store/actions/users'
+import {
+  selectUsers,
+  selectUsersTotalCount,
+  selectUsersFetching,
+  selectUsersError,
+} from '../../store/selectors/users'
+
+const headers = [
+  {
+    name: 'ids.user_id',
+    displayName: sharedMessages.id,
+    width: 33,
+  },
+  {
+    name: 'name',
+    displayName: sharedMessages.name,
+    width: 24,
+  },
+  {
+    name: 'primary_email_address',
+    displayName: sharedMessages.email,
+    width: 28,
+  },
+  {
+    name: 'state',
+    displayName: sharedMessages.state,
+    width: 15,
+    render(state) {
+      let indicator = 'unknown'
+      let label = sharedMessages.notSet
+      switch (state) {
+        case 'STATE_APPROVED':
+          indicator = 'good'
+          label = sharedMessages.stateApproved
+          break
+        case 'STATE_REQUESTED':
+          indicator = 'mediocre'
+          label = sharedMessages.stateRequested
+          break
+        case 'STATE_REJECTED':
+          indicator = 'bad'
+          label = sharedMessages.stateRejected
+          break
+        case 'STATE_FLAGGED':
+          indicator = 'bad'
+          label = sharedMessages.stateFlagged
+          break
+        case 'STATE_SUSPENDED':
+          indicator = 'bad'
+          label = sharedMessages.stateSuspended
+          break
+      }
+
+      return <Status status={indicator} label={label} pulse={false} />
+    },
+  },
+]
+
+export default class UsersTable extends Component {
+  static propTypes = {
+    pageSize: PropTypes.number.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.getUsersList = params => getUsersList(params, ['name', 'primary_email_address', 'state'])
+  }
+
+  baseDataSelector(state) {
+    return {
+      users: selectUsers(state),
+      totalCount: selectUsersTotalCount(state),
+      fetching: selectUsersFetching(state),
+      error: selectUsersError(state),
+      mayAdd: false,
+    }
+  }
+
+  render() {
+    const { pageSize } = this.props
+
+    return (
+      <FetchTable
+        entity="users"
+        headers={headers}
+        addMessage={sharedMessages.addOrganization}
+        tableTitle={<Message content={sharedMessages.users} />}
+        getItemsAction={this.getUsersList}
+        searchItemsAction={this.getUsersList}
+        baseDataSelector={this.baseDataSelector}
+        pageSize={pageSize}
+      />
+    )
+  }
+}
