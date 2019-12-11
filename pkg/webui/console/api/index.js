fb1e07a30eb5a9aaaa33b9de2ce16252c3f75774
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-12-11 13:32:02 +0100

    console: Add user delete function to api tree

--- pkg/webui/console/api/index.js
+++ pkg/webui/console/api/index.js
@@ -75,6 +75,7 @@ export default {
     get: ttnClient.Users.getById.bind(ttnClient.Users),
     list: ttnClient.Users.getAll.bind(ttnClient.Users),
     update: ttnClient.Users.updateById.bind(ttnClient.Users),
+    delete: ttnClient.Users.deleteById.bind(ttnClient.Users),
     search: ttnClient.Users.search.bind(ttnClient.Users),
     async authInfo() {
       return instance.get(`${isBaseUrl}/auth_info`, {
