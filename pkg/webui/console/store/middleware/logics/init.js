6ac8461ee3fab1513602c21823101ca3f2bfb39b
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-12-16 14:40:50 +0100

    console: Enhance init logic to account for users with too few rights

--- pkg/webui/console/store/middleware/logics/init.js
+++ pkg/webui/console/store/middleware/logics/init.js
@@ -12,46 +12,78 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { defineMessages } from 'react-intl'
+
 import * as user from '../../actions/user'
 import * as init from '../../actions/init'
 import api from '../../../api'
-import * as accessToken from '../../../lib/access-token'
-import { getBackendErrorName } from '../../../../lib/errors/utils'
 import createRequestLogic from './lib'
+
+const m = defineMessages({
+  errTooFewRights: 'Your account does not possess sufficient rights to use the console.',
+  errStateRequested:
+    'Your account still needs to be approved by an administrator. You will receive a confirmation email once your account is approved.',
+  errStateRejected: 'Your account has been rejected by an administrator.',
+  errStateSuspended:
+    'Your account has been suspended by an administrator. Please contact support for further information about your account status.',
+  errEmailValidation: 'Your account is restricted until your email address has been validated.',
+})
+
+// Define a minimum set of rights, without which it makes no sense to use the
+// console
+const minimumRights = ['RIGHT_APPLICATION', 'RIGHT_GATEWAY', 'RIGHT_ORGANIZATION']
 
 const consoleAppLogic = createRequestLogic({
   type: init.INITIALIZE,
   async process(_, dispatch) {
     dispatch(user.getUserRights())
 
-    let info
+    let info, rights
 
     try {
       // there is no way to retrieve the current user directly
       // within the console app, so first get the authentication info
       // and only afterwards fetch the user
       info = await api.users.authInfo()
-      const rights = info.data.oauth_access_token.rights
+      rights = info.oauth_access_token.rights
       dispatch(user.getUserRightsSuccess(rights))
     } catch (error) {
       dispatch(user.getUserRightsFailure())
-      accessToken.clear()
+      dispatch(user.logout())
+      info = undefined
     }
 
     if (info) {
       try {
         dispatch(user.getUserMe())
-        const userId = info.data.oauth_access_token.user_ids.user_id
-        const userResult = await api.users.get(userId)
-        userResult.isAdmin = info.data.is_admin || false
+        const userId = info.oauth_access_token.user_ids.user_id
+        const userResult = await api.users.get(userId, [
+          'state',
+          'name',
+          'primary_email_address_validated_at',
+        ])
+        userResult.isAdmin = info.is_admin || false
         dispatch(user.getUserMeSuccess(userResult))
-      } catch (error) {
-        if (getBackendErrorName(error) === 'user_requested') {
-          // Log unapproved users out automatically, to avoid confusion
-          dispatch(user.logout())
+
+        // Check whether the user account has sufficient rights to use the
+        // console
+        if (!info.is_admin && !rights.some(r => minimumRights.some(mr => r.startsWith(mr)))) {
+          // Provide relevant error messages if possible
+          if (userResult.state === 'STATE_REQUESTED') {
+            throw m.errStateRequested
+          } else if (userResult.state === 'STATE_REJECTED') {
+            throw m.errStateRejected
+          } else if (userResult.state === 'STATE_SUSPENDED') {
+            throw m.errStateSuspended
+          } else if (!userResult.primary_email_address_validated_at) {
+            throw m.errEmailValidation
+          }
+
+          throw m.errTooFewRights
         }
+      } catch (error) {
         dispatch(user.getUserMeFailure(error))
-        accessToken.clear()
+        dispatch(user.logout())
       }
     }
 
