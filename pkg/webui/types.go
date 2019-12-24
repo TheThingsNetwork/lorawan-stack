0727c8dd947b550fdf4f939b29557e099cceec86
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-12-24 14:20:12 +0100

    is: Remove base url from component api config when disabled

--- pkg/webui/types.go
+++ pkg/webui/types.go
@@ -14,8 +14,25 @@
 
 package webui
 
+import "encoding/json"
+
 // APIConfig for upstream APIs.
 type APIConfig struct {
 	Enabled bool   `json:"enabled" name:"enabled" description:"Enable this API"`
 	BaseURL string `json:"base_url" name:"base-url" description:"Base URL to the HTTP API"`
+}
+
+// MarshalJSON implements json.Marshaler.
+func (c APIConfig) MarshalJSON() ([]byte, error) {
+	out := struct {
+		Enabled bool   `json:"enabled"`
+		BaseURL string `json:"base_url,omitempty"`
+	}{
+		Enabled: c.Enabled,
+		BaseURL: c.BaseURL,
+	}
+	if !out.Enabled {
+		out.BaseURL = ""
+	}
+	return json.Marshal(out)
 }
