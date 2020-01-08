83305195debf048485b12ca2e90033a78811b977
Krishna Iyer Easwaran <mail@krishnaiyereaswaran.com>
2020-01-08 14:54:20 +0100

    gs: Use first fp in gRPC frontend

--- pkg/gatewayserver/gatewayserver.go
+++ pkg/gatewayserver/gatewayserver.go
@@ -391,7 +391,7 @@ func (gs *GatewayServer) Connect(ctx context.Context, frontend io.Frontend, ids 
 		return nil, err
 	}
 
-	var fps []*frequencyplans.FrequencyPlan
+	fps := make([]*frequencyplans.FrequencyPlan, len(gtw.FrequencyPlanIDs))
 	for _, fpID := range gtw.FrequencyPlanIDs {
 		fp, err := gs.FrequencyPlans.GetByID(fpID)
 		if err != nil {
@@ -604,7 +604,7 @@ func (gs *GatewayServer) handleUpstream(conn *io.Connection) {
 	}
 }
 
-// GetFrequencyPlan gets the frequency plans by the gateway identifiers.
+// GetFrequencyPlans gets the frequency plans by the gateway identifiers.
 func (gs *GatewayServer) GetFrequencyPlans(ctx context.Context, ids ttnpb.GatewayIdentifiers) ([]*frequencyplans.FrequencyPlan, error) {
 	var err error
 	var callOpt grpc.CallOption
@@ -634,7 +634,8 @@ func (gs *GatewayServer) GetFrequencyPlans(ctx context.Context, ids ttnpb.Gatewa
 	} else {
 		return nil, err
 	}
-	var fps []*frequencyplans.FrequencyPlan
+
+	fps := make([]*frequencyplans.FrequencyPlan, len(fpIDs))
 	for _, fpID := range fpIDs {
 		fp, err := gs.FrequencyPlans.GetByID(fpID)
 		if err != nil {
