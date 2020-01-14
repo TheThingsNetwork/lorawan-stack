62e62a580828a8288907221ac80b1075fc0917e3
Krishna Iyer Easwaran <mail@krishnaiyereaswaran.com>
2020-01-14 11:19:36 +0100

    gs: Add FrequencyPlanID to test downlinks

--- pkg/gatewayserver/io/grpc/grpc_test.go
+++ pkg/gatewayserver/io/grpc/grpc_test.go
@@ -391,6 +391,7 @@ func TestTraffic(t *testing.T) {
 							Rx1Frequency:     868100000,
 							Rx2DataRateIndex: 0,
 							Rx2Frequency:     869525000,
+							FrequencyPlanID:  test.EUFrequencyPlanID,
 						},
 					},
 				},
