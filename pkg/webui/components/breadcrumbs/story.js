623593ea6f60de4d7926b117fc04afc3285cfd6e
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2020-01-24 18:39:33 +0100

    console,oauth: Remove icons from breadcrumb component

--- pkg/webui/components/breadcrumbs/story.js
+++ pkg/webui/components/breadcrumbs/story.js
@@ -37,29 +37,3 @@ storiesOf('Breadcrumbs', module)
 
     return <Breadcrumbs breadcrumbs={breadcrumbs} />
   })
-  .add('With icons', function() {
-    const breadcrumbs = [
-      <Breadcrumb key="1" path="/gateways" icon="gateway" content="Gateways" />,
-      <Breadcrumb
-        key="2"
-        path="/gateways/eui-0000024b0806021d"
-        icon="devices"
-        content="eui-0000024b0806021d"
-      />,
-      <Breadcrumb
-        key="3"
-        path="/gateways/eui-0000024b0806021d/traffic"
-        icon="data"
-        content="Traffic"
-      />,
-    ]
-    return <Breadcrumbs breadcrumbs={breadcrumbs} />
-  })
-  .add('Mixed', function() {
-    const breadcrumbs = [
-      <Breadcrumb key="1" path="/applications" content="Applications" />,
-      <Breadcrumb key="2" path="/applications/test-app" icon="application" content="test-app" />,
-      <Breadcrumb key="3" path="/applications/test-app/traffix" content="Traffic" />,
-    ]
-    return <Breadcrumbs breadcrumbs={breadcrumbs} />
-  })
