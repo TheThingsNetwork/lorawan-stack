71d9134cc2cc29d86b5bb732b426502e38f69314
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2020-03-21 00:30:18 +0900

    console: Fix faulty feature check in organization collaborator view

--- pkg/webui/console/views/organization-collaborators/index.js
+++ pkg/webui/console/views/organization-collaborators/index.js
@@ -28,11 +28,11 @@ import OrganizationCollaboratorAdd from '../organization-collaborator-add'
 import OrganizationCollaboratorEdit from '../organization-collaborator-edit'
 import withFeatureRequirement from '../../lib/components/with-feature-requirement'
 
-import { mayViewOrEditOrganizationApiKeys } from '../../lib/feature-checks'
+import { mayViewOrEditOrganizationCollaborators } from '../../lib/feature-checks'
 import { selectSelectedOrganizationId } from '../../store/selectors/organizations'
 
 @connect(state => ({ orgId: selectSelectedOrganizationId(state) }))
-@withFeatureRequirement(mayViewOrEditOrganizationApiKeys, {
+@withFeatureRequirement(mayViewOrEditOrganizationCollaborators, {
   redirect: ({ orgId }) => `/organizations/${orgId}`,
 })
 @withBreadcrumb('orgs.single.collaborators', function(props) {
