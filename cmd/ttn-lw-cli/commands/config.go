5af346045e7146ee6f5e238088ac15b6c94e246b
Aggelos Kolaitis <aggelos.kolaitis@thethingsindustries.com>
2020-03-25 15:43:05 +0200

    cli: Make default config more generic, add yaml tags

--- cmd/ttn-lw-cli/commands/config.go
+++ cmd/ttn-lw-cli/commands/config.go
@@ -15,39 +15,40 @@
 package commands
 
 import (
+	"fmt"
+
 	"go.thethings.network/lorawan-stack/cmd/internal/commands"
 	conf "go.thethings.network/lorawan-stack/pkg/config"
 	"go.thethings.network/lorawan-stack/pkg/log"
 )
 
 var (
-	clusterHost        = "localhost"
-	clusterGRPCAddress = clusterHost + ":8884"
-	clusterHTTPAddress = "https://" + clusterHost + ":8885"
+	defaultClusterHost = "localhost"
+	defaultInsecure    = false
 )
 
 // Config for the ttn-lw-cli binary.
 type Config struct {
 	conf.Base                          `name:",squash"`
-	CredentialsID                      string `name:"credentials-id" description:"Credentials ID (if using multiple configurations)"`
-	InputFormat                        string `name:"input-format" description:"Input format"`
-	OutputFormat                       string `name:"output-format" description:"Output format"`
-	AllowUnknownHosts                  bool   `name:"allow-unknown-hosts" description:"Allow sending credentials to unknown hosts"`
-	OAuthServerAddress                 string `name:"oauth-server-address" description:"OAuth Server address"`
-	IdentityServerGRPCAddress          string `name:"identity-server-grpc-address" description:"Identity Server address"`
-	GatewayServerEnabled               bool   `name:"gateway-server-enabled" description:"Gateway Server enabled"`
-	GatewayServerGRPCAddress           string `name:"gateway-server-grpc-address" description:"Gateway Server address"`
-	NetworkServerEnabled               bool   `name:"network-server-enabled" description:"Network Server enabled"`
-	NetworkServerGRPCAddress           string `name:"network-server-grpc-address" description:"Network Server address"`
-	ApplicationServerEnabled           bool   `name:"application-server-enabled" description:"Application Server enabled"`
-	ApplicationServerGRPCAddress       string `name:"application-server-grpc-address" description:"Application Server address"`
-	JoinServerEnabled                  bool   `name:"join-server-enabled" description:"Join Server enabled"`
-	JoinServerGRPCAddress              string `name:"join-server-grpc-address" description:"Join Server address"`
-	DeviceTemplateConverterGRPCAddress string `name:"device-template-converter-grpc-address" description:"Device Template Converter address"`
-	DeviceClaimingServerGRPCAddress    string `name:"device-claiming-server-grpc-address" description:"Device Claiming Server address"`
-	QRCodeGeneratorGRPCAddress         string `name:"qr-code-generator-grpc-address" description:"QR Code Generator address"`
-	Insecure                           bool   `name:"insecure" description:"Connect without TLS"`
-	CA                                 string `name:"ca" description:"CA certificate file"`
+	CredentialsID                      string `name:"credentials-id" yaml:"credentials-id" description:"Credentials ID (if using multiple configurations)"`
+	InputFormat                        string `name:"input-format" yaml:"input-format" description:"Input format"`
+	OutputFormat                       string `name:"output-format" yaml:"output-format" description:"Output format"`
+	AllowUnknownHosts                  bool   `name:"allow-unknown-hosts" yaml:"allow-unknown-hosts" description:"Allow sending credentials to unknown hosts"`
+	OAuthServerAddress                 string `name:"oauth-server-address" yaml:"oauth-server-address" description:"OAuth Server address"`
+	IdentityServerGRPCAddress          string `name:"identity-server-grpc-address" yaml:"identity-server-grpc-address" description:"Identity Server address"`
+	GatewayServerEnabled               bool   `name:"gateway-server-enabled" yaml:"gateway-server-enabled" description:"Gateway Server enabled"`
+	GatewayServerGRPCAddress           string `name:"gateway-server-grpc-address" yaml:"gateway-server-grpc-address" description:"Gateway Server address"`
+	NetworkServerEnabled               bool   `name:"network-server-enabled" yaml:"network-server-enabled" description:"Network Server enabled"`
+	NetworkServerGRPCAddress           string `name:"network-server-grpc-address" yaml:"network-server-grpc-address" description:"Network Server address"`
+	ApplicationServerEnabled           bool   `name:"application-server-enabled" yaml:"application-server-enabled" description:"Application Server enabled"`
+	ApplicationServerGRPCAddress       string `name:"application-server-grpc-address" yaml:"application-server-grpc-address" description:"Application Server address"`
+	JoinServerEnabled                  bool   `name:"join-server-enabled" yaml:"join-server-enabled" description:"Join Server enabled"`
+	JoinServerGRPCAddress              string `name:"join-server-grpc-address" yaml:"join-server-grpc-address" description:"Join Server address"`
+	DeviceTemplateConverterGRPCAddress string `name:"device-template-converter-grpc-address" yaml:"device-template-converter-grpc-address" description:"Device Template Converter address"`
+	DeviceClaimingServerGRPCAddress    string `name:"device-claiming-server-grpc-address" yaml:"device-claiming-server-grpc-address" description:"Device Claiming Server address"`
+	QRCodeGeneratorGRPCAddress         string `name:"qr-code-generator-grpc-address" yaml:"qr-code-generator-grpc-address" description:"QR Code Generator address"`
+	Insecure                           bool   `name:"insecure" yaml:"insecure" description:"Connect without TLS"`
+	CA                                 string `name:"ca" yaml:"ca" description:"CA certificate file"`
 }
 
 func (c Config) getHosts() []string {
@@ -71,29 +72,42 @@ func (c Config) getHosts() []string {
 	return getHosts(hosts...)
 }
 
-// DefaultConfig contains the default config for the ttn-lw-cli binary.
-var DefaultConfig = Config{
-	Base: conf.Base{
-		Log: conf.Log{
-			Level: log.InfoLevel,
+// MakeDefaultConfig builds the default config for the ttn-lw-cli binary for a given host.
+func MakeDefaultConfig(clusterHost string, insecure bool) Config {
+	clusterGRPCAddress := fmt.Sprintf("%s:8884", clusterHost)
+	clusterHTTPAddress := fmt.Sprintf("https://%s:8885", clusterHost)
+	if insecure {
+		clusterGRPCAddress = fmt.Sprintf("%s:1884", clusterHost)
+		clusterHTTPAddress = fmt.Sprintf("http://%s:1885", clusterHost)
+	}
+
+	return Config{
+		Base: conf.Base{
+			Log: conf.Log{
+				Level: log.InfoLevel,
+			},
 		},
-	},
-	InputFormat:                        "json",
-	OutputFormat:                       "json",
-	OAuthServerAddress:                 clusterHTTPAddress + "/oauth",
-	IdentityServerGRPCAddress:          clusterGRPCAddress,
-	GatewayServerEnabled:               true,
-	GatewayServerGRPCAddress:           clusterGRPCAddress,
-	NetworkServerEnabled:               true,
-	NetworkServerGRPCAddress:           clusterGRPCAddress,
-	ApplicationServerEnabled:           true,
-	ApplicationServerGRPCAddress:       clusterGRPCAddress,
-	JoinServerEnabled:                  true,
-	JoinServerGRPCAddress:              clusterGRPCAddress,
-	DeviceTemplateConverterGRPCAddress: clusterGRPCAddress,
-	DeviceClaimingServerGRPCAddress:    clusterGRPCAddress,
-	QRCodeGeneratorGRPCAddress:         clusterGRPCAddress,
+		InputFormat:                        "json",
+		OutputFormat:                       "json",
+		OAuthServerAddress:                 clusterHTTPAddress + "/oauth",
+		IdentityServerGRPCAddress:          clusterGRPCAddress,
+		GatewayServerEnabled:               true,
+		GatewayServerGRPCAddress:           clusterGRPCAddress,
+		NetworkServerEnabled:               true,
+		NetworkServerGRPCAddress:           clusterGRPCAddress,
+		ApplicationServerEnabled:           true,
+		ApplicationServerGRPCAddress:       clusterGRPCAddress,
+		JoinServerEnabled:                  true,
+		JoinServerGRPCAddress:              clusterGRPCAddress,
+		DeviceTemplateConverterGRPCAddress: clusterGRPCAddress,
+		DeviceClaimingServerGRPCAddress:    clusterGRPCAddress,
+		QRCodeGeneratorGRPCAddress:         clusterGRPCAddress,
+		Insecure:                           insecure,
+	}
 }
+
+// DefaultConfig contains the default config for the ttn-lw-cli binary.
+var DefaultConfig = MakeDefaultConfig(defaultClusterHost, defaultInsecure)
 
 var configCommand = commands.Config(mgr)
 
