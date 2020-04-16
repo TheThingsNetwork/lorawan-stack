cfe80cbba110c4ed698f9d8a3fcaa786fedd65c6
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2020-04-16 11:09:05 +0900

    console: Fix pubsub total count selector

--- pkg/webui/console/store/selectors/pubsubs.js
+++ pkg/webui/console/store/selectors/pubsubs.js
@@ -28,6 +28,6 @@ export const selectPubsubFetching = createFetchingSelector(GET_PUBSUB_BASE)
 
 // Pubsubs
 export const selectPubsubs = state => Object.values(selectPubsubEntityStore(state))
-export const selectPubsubsTotalCount = state => selectPubsubEntityStore(state).totalCount
+export const selectPubsubsTotalCount = state => selectPubsubStore(state).totalCount
 export const selectPubsubsFetching = createFetchingSelector(GET_PUBSUBS_LIST_BASE)
 export const selectPubsubsError = createErrorSelector(GET_PUBSUBS_LIST_BASE)
