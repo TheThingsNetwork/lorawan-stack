5127ba776168a28964a7c02e709f492c2b5dbf01
Bogdan <bogdans.afonins@thethingsindustries.com>
2020-04-28 14:54:57 +0300

    console: Extend bytes utilities
    Add function to convert base64 string to hex

--- pkg/webui/console/lib/bytes.js
+++ pkg/webui/console/lib/bytes.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* eslint-disable import/prefer-default-export */
-
 export const hexToBase64 = str => {
   return btoa(
     String.fromCharCode.apply(
@@ -25,4 +23,20 @@ export const hexToBase64 = str => {
         .split(' '),
     ),
   )
+}
+
+/**
+ * Converts base64 encoded string to hex.
+ *
+ * @param {string} str - Base64 encoded string.
+ * @returns {string} - `str` hex encoded.
+ */
+export const base64ToHex = str => {
+  return Array.from(atob(str.replace(/[ \r\n]+$/, '')))
+    .map(char => {
+      const tmp = char.charCodeAt(0).toString(16)
+
+      return tmp.length > 1 ? tmp : `0${tmp}`
+    })
+    .join('')
 }
