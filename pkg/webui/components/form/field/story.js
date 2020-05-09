398f7e900fd811aee9163250defd3fb082f23b1a
Bogdan <bogdans.afonins@thethingsindustries.com>
2020-05-09 17:30:43 +0300

    console,oauth: Fix prop-types errors in storybook files

--- pkg/webui/components/form/field/story.js
+++ pkg/webui/components/form/field/story.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* eslint-disable react/prop-types */
+
 import React from 'react'
 import * as Yup from 'yup'
 import { storiesOf } from '@storybook/react'
