nejraselimovic/lorawan-stack-1

as: Replace skip_payload_crypto by override

pkg/applicationserver/applicationserver.go
@@ -127,7 +127,7 @@ func New(c *component.Component, conf *Config) (as *ApplicationServer, err error
 		config:         conf,
 		linkMode:       linkMode,
 		linkRegistry:   conf.Links,
-		deviceRegistry: conf.Devices,
+		deviceRegistry: wrapDeviceRegistryWithDeprecatedFields(conf.Devices, deprecatedDeviceFields...),
 		formatter: payloadFormatter{
 			repository: &devicerepository.Client{
 				Fetcher: drFetcher,

pkg/applicationserver/registry_test.go
@@ -18,15 +18,165 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/smartystreets/assertions"
 	"go.thethings.network/lorawan-stack/v3/pkg/applicationserver/redis"
 	"go.thethings.network/lorawan-stack/v3/pkg/errors"
 	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/v3/pkg/types"
 	"go.thethings.network/lorawan-stack/v3/pkg/unique"
 	"go.thethings.network/lorawan-stack/v3/pkg/util/test"
 	"go.thethings.network/lorawan-stack/v3/pkg/util/test/assertions/should"
 )
+
+func DeleteDevice(ctx context.Context, r DeviceRegistry, ids ttnpb.EndDeviceIdentifiers) error {
+	_, err := r.Set(ctx, ids, nil, func(*ttnpb.EndDevice) (*ttnpb.EndDevice, []string, error) { return nil, nil, nil })
+	return err
+}
+
+func handleDeviceRegistryTest(t *testing.T, reg DeviceRegistry) {
+	a := assertions.New(t)
+
+	ctx := test.Context()
+
+	pb := &ttnpb.EndDevice{
+		EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+			JoinEUI:                &types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+			DevEUI:                 &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+			ApplicationIdentifiers: ttnpb.ApplicationIdentifiers{ApplicationID: "test-app"},
+			DeviceID:               "test-dev",
+		},
+		Session: &ttnpb.Session{
+			DevAddr: types.DevAddr{0x42, 0xff, 0xff, 0xff},
+		},
+		SkipPayloadCrypto: true,
+	}
+
+	ret, err := reg.Get(ctx, pb.EndDeviceIdentifiers, ttnpb.EndDeviceFieldPathsTopLevel)
+	if !a.So(err, should.NotBeNil) || !a.So(errors.IsNotFound(err), should.BeTrue) {
+		t.Fatalf("Error received: %v", err)
+	}
+	a.So(ret, should.BeNil)
+
+	start := time.Now()
+
+	ret, err = reg.Set(ctx, pb.EndDeviceIdentifiers,
+		[]string{
+			"ids.application_ids",
+			"ids.dev_eui",
+			"ids.device_id",
+			"ids.join_eui",
+			"session",
+			"skip_payload_crypto",
+		},
+		func(stored *ttnpb.EndDevice) (*ttnpb.EndDevice, []string, error) {
+			if !a.So(stored, should.BeNil) {
+				t.Fatal("Registry is not empty")
+			}
+			return pb, []string{
+				"ids.application_ids",
+				"ids.dev_eui",
+				"ids.device_id",
+				"ids.join_eui",
+				"pending_session",
+				"session",
+				"skip_payload_crypto",
+			}, nil
+		},
+	)
+	if !a.So(err, should.BeNil) || !a.So(ret, should.NotBeNil) {
+		t.Fatalf("Failed to create device: %s", err)
+	}
+	a.So(ret.CreatedAt, should.HappenAfter, start)
+	a.So(ret.UpdatedAt, should.HappenAfter, start)
+	a.So(ret.UpdatedAt, should.Equal, ret.CreatedAt)
+	pb.CreatedAt = ret.CreatedAt
+	pb.UpdatedAt = ret.UpdatedAt
+	a.So(ret, should.HaveEmptyDiff, pb)
+
+	ret, err = reg.Set(ctx, pb.EndDeviceIdentifiers,
+		[]string{
+			"ids.application_ids",
+			"ids.dev_eui",
+			"ids.device_id",
+			"ids.join_eui",
+			"pending_session",
+			"session",
+			"skip_payload_crypto_override",
+		},
+		func(stored *ttnpb.EndDevice) (*ttnpb.EndDevice, []string, error) {
+			a.So(stored, should.HaveEmptyDiff, pb)
+			return &ttnpb.EndDevice{}, nil, nil
+		},
+	)
+	if !a.So(err, should.BeNil) || !a.So(ret, should.NotBeNil) {
+		t.Fatalf("Failed to get device via Set: %s", err)
+	}
+	a.So(ret, should.HaveEmptyDiff, pb)
+
+	ret, err = reg.Get(ctx, pb.EndDeviceIdentifiers, ttnpb.EndDeviceFieldPathsTopLevel)
+	a.So(err, should.BeNil)
+	a.So(ret, should.HaveEmptyDiff, pb)
+
+	err = DeleteDevice(ctx, reg, pb.EndDeviceIdentifiers)
+	if !a.So(err, should.BeNil) {
+		t.FailNow()
+	}
+
+	ret, err = reg.Get(ctx, pb.EndDeviceIdentifiers, ttnpb.EndDeviceFieldPathsTopLevel)
+	if !a.So(err, should.NotBeNil) || !a.So(errors.IsNotFound(err), should.BeTrue) {
+		t.Fatalf("Error received: %v", err)
+	}
+	a.So(ret, should.BeNil)
+}
+
+func TestDeviceRegistry(t *testing.T) {
+	t.Parallel()
+
+	namespace := [...]string{
+		"applicationserver_test",
+		"devices",
+	}
+	for _, tc := range []struct {
+		Name string
+		New  func(t testing.TB) (reg DeviceRegistry, closeFn func() error)
+		N    uint16
+	}{
+		{
+			Name: "Redis",
+			New: func(t testing.TB) (DeviceRegistry, func() error) {
+				cl, flush := test.NewRedis(t, namespace[:]...)
+				reg := &redis.DeviceRegistry{Redis: cl}
+				return reg, func() error {
+					flush()
+					return cl.Close()
+				}
+			},
+			N: 8,
+		},
+	} {
+		for i := 0; i < int(tc.N); i++ {
+			t.Run(fmt.Sprintf("%s/%d", tc.Name, i), func(t *testing.T) {
+				t.Parallel()
+				reg, closeFn := tc.New(t)
+				reg = wrapDeviceRegistryWithDeprecatedFields(reg, deprecatedDeviceFields...)
+				if closeFn != nil {
+					defer func() {
+						if err := closeFn(); err != nil {
+							t.Errorf("Failed to close registry: %v", err)
+						}
+					}()
+				}
+				t.Run("1st run", func(t *testing.T) { handleDeviceRegistryTest(t, reg) })
+				if t.Failed() {
+					t.Skip("Skipping 2nd run")
+				}
+				t.Run("2nd run", func(t *testing.T) { handleDeviceRegistryTest(t, reg) })
+			})
+		}
+	}
+}
 
 func handleLinkRegistryTest(t *testing.T, reg LinkRegistry) {
 	a := assertions.New(t)
@@ -105,6 +255,7 @@ func TestLinkRegistry(t *testing.T) {
 
 	namespace := [...]string{
 		"applicationserver_test",
+		"links",
 	}
 	for _, tc := range []struct {
 		Name string

TheThingsNetwork/lorawan-stack

pkg/applicationserver/applicationserver.go
@@ -127,7 +127,7 @@ func New(c *component.Component, conf *Config) (as *ApplicationServer, err error
 		config:         conf,
 		linkMode:       linkMode,
 		linkRegistry:   conf.Links,
-		deviceRegistry: conf.Devices,
+		deviceRegistry: wrapEndDeviceRegistryWithReplacedFields(conf.Devices, replacedEndDeviceFields...),
 		formatter: payloadFormatter{
 			repository: &devicerepository.Client{
 				Fetcher: drFetcher,

pkg/applicationserver/registry_test.go
@@ -18,15 +18,200 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/smartystreets/assertions"
 	"go.thethings.network/lorawan-stack/v3/pkg/applicationserver/redis"
 	"go.thethings.network/lorawan-stack/v3/pkg/errors"
 	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/v3/pkg/types"
 	"go.thethings.network/lorawan-stack/v3/pkg/unique"
 	"go.thethings.network/lorawan-stack/v3/pkg/util/test"
 	"go.thethings.network/lorawan-stack/v3/pkg/util/test/assertions/should"
 )
+
+func DeleteDevice(ctx context.Context, r DeviceRegistry, ids ttnpb.EndDeviceIdentifiers) error {
+	_, err := r.Set(ctx, ids, nil, func(*ttnpb.EndDevice) (*ttnpb.EndDevice, []string, error) { return nil, nil, nil })
+	return err
+}
+
+func handleDeviceRegistryTest(t *testing.T, reg DeviceRegistry) {
+	a := assertions.New(t)
+
+	ctx := test.Context()
+
+	pb := &ttnpb.EndDevice{
+		EndDeviceIdentifiers: ttnpb.EndDeviceIdentifiers{
+			JoinEUI:                &types.EUI64{0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+			DevEUI:                 &types.EUI64{0x42, 0x42, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+			ApplicationIdentifiers: ttnpb.ApplicationIdentifiers{ApplicationID: "test-app"},
+			DeviceID:               "test-dev",
+		},
+		Session: &ttnpb.Session{
+			DevAddr: types.DevAddr{0x42, 0xff, 0xff, 0xff},
+		},
+		SkipPayloadCrypto: true,
+	}
+
+	ret, err := reg.Get(ctx, pb.EndDeviceIdentifiers, ttnpb.EndDeviceFieldPathsTopLevel)
+	if !a.So(err, should.NotBeNil) || !a.So(errors.IsNotFound(err), should.BeTrue) {
+		t.Fatalf("Error received: %v", err)
+	}
+	a.So(ret, should.BeNil)
+
+	start := time.Now()
+
+	ret, err = reg.Set(ctx, pb.EndDeviceIdentifiers,
+		[]string{
+			"ids.application_ids",
+			"ids.dev_eui",
+			"ids.device_id",
+			"ids.join_eui",
+			"session",
+			"skip_payload_crypto",
+		},
+		func(stored *ttnpb.EndDevice) (*ttnpb.EndDevice, []string, error) {
+			if !a.So(stored, should.BeNil) {
+				t.Fatal("Registry is not empty")
+			}
+			return pb, []string{
+				"ids.application_ids",
+				"ids.dev_eui",
+				"ids.device_id",
+				"ids.join_eui",
+				"pending_session",
+				"session",
+				"skip_payload_crypto",
+			}, nil
+		},
+	)
+	if !a.So(err, should.BeNil) || !a.So(ret, should.NotBeNil) {
+		t.Fatalf("Failed to create device: %s", err)
+	}
+	a.So(ret.CreatedAt, should.HappenAfter, start)
+	a.So(ret.UpdatedAt, should.HappenAfter, start)
+	a.So(ret.UpdatedAt, should.Equal, ret.CreatedAt)
+	pb.CreatedAt = ret.CreatedAt
+	pb.UpdatedAt = ret.UpdatedAt
+	pb.SkipPayloadCrypto = true // Set because SkipPayloadCryptoOverride.GetValue() == true
+	a.So(ret, should.HaveEmptyDiff, pb)
+
+	ret, err = reg.Set(ctx, pb.EndDeviceIdentifiers,
+		[]string{
+			"ids.application_ids",
+			"ids.dev_eui",
+			"ids.device_id",
+			"ids.join_eui",
+			"session",
+			"skip_payload_crypto",
+		},
+		func(stored *ttnpb.EndDevice) (*ttnpb.EndDevice, []string, error) {
+			pb.SkipPayloadCrypto = false
+			return pb, []string{
+				"ids.application_ids",
+				"ids.dev_eui",
+				"ids.device_id",
+				"ids.join_eui",
+				"pending_session",
+				"session",
+				"skip_payload_crypto",
+			}, nil
+		},
+	)
+	if !a.So(err, should.BeNil) || !a.So(ret, should.NotBeNil) {
+		t.Fatalf("Failed to update device: %s", err)
+	}
+	a.So(ret.UpdatedAt, should.HappenAfter, start)
+	a.So(ret.UpdatedAt, should.HappenAfter, ret.CreatedAt)
+	if !a.So(ret.SkipPayloadCryptoOverride, should.NotBeNil) || !a.So(ret.SkipPayloadCryptoOverride.Value, should.BeFalse) {
+		t.Fatalf("Setting deprecated field failed to update new field")
+	}
+	pb.UpdatedAt = ret.UpdatedAt
+	pb.SkipPayloadCryptoOverride = ret.SkipPayloadCryptoOverride
+	a.So(ret, should.HaveEmptyDiff, pb)
+
+	ret, err = reg.Set(ctx, pb.EndDeviceIdentifiers,
+		[]string{
+			"ids.application_ids",
+			"ids.dev_eui",
+			"ids.device_id",
+			"ids.join_eui",
+			"pending_session",
+			"session",
+			"skip_payload_crypto_override",
+		},
+		func(stored *ttnpb.EndDevice) (*ttnpb.EndDevice, []string, error) {
+			a.So(stored, should.HaveEmptyDiff, pb)
+			return &ttnpb.EndDevice{}, nil, nil
+		},
+	)
+	if !a.So(err, should.BeNil) || !a.So(ret, should.NotBeNil) {
+		t.Fatalf("Failed to get device via Set: %s", err)
+	}
+	a.So(ret, should.HaveEmptyDiff, pb)
+
+	ret, err = reg.Get(ctx, pb.EndDeviceIdentifiers, ttnpb.EndDeviceFieldPathsTopLevel)
+	a.So(err, should.BeNil)
+	a.So(ret, should.HaveEmptyDiff, pb)
+
+	err = DeleteDevice(ctx, reg, pb.EndDeviceIdentifiers)
+	if !a.So(err, should.BeNil) {
+		t.FailNow()
+	}
+
+	ret, err = reg.Get(ctx, pb.EndDeviceIdentifiers, ttnpb.EndDeviceFieldPathsTopLevel)
+	if !a.So(err, should.NotBeNil) || !a.So(errors.IsNotFound(err), should.BeTrue) {
+		t.Fatalf("Error received: %v", err)
+	}
+	a.So(ret, should.BeNil)
+}
+
+func TestDeviceRegistry(t *testing.T) {
+	t.Parallel()
+
+	namespace := [...]string{
+		"applicationserver_test",
+		"devices",
+	}
+	for _, tc := range []struct {
+		Name string
+		New  func(t testing.TB) (reg DeviceRegistry, closeFn func() error)
+		N    uint16
+	}{
+		{
+			Name: "Redis",
+			New: func(t testing.TB) (DeviceRegistry, func() error) {
+				cl, flush := test.NewRedis(t, namespace[:]...)
+				reg := &redis.DeviceRegistry{Redis: cl}
+				return reg, func() error {
+					flush()
+					return cl.Close()
+				}
+			},
+			N: 8,
+		},
+	} {
+		for i := 0; i < int(tc.N); i++ {
+			t.Run(fmt.Sprintf("%s/%d", tc.Name, i), func(t *testing.T) {
+				t.Parallel()
+				reg, closeFn := tc.New(t)
+				reg = wrapEndDeviceRegistryWithReplacedFields(reg, replacedEndDeviceFields...)
+				if closeFn != nil {
+					defer func() {
+						if err := closeFn(); err != nil {
+							t.Errorf("Failed to close registry: %v", err)
+						}
+					}()
+				}
+				t.Run("1st run", func(t *testing.T) { handleDeviceRegistryTest(t, reg) })
+				if t.Failed() {
+					t.Skip("Skipping 2nd run")
+				}
+				t.Run("2nd run", func(t *testing.T) { handleDeviceRegistryTest(t, reg) })
+			})
+		}
+	}
+}
 
 func handleLinkRegistryTest(t *testing.T, reg LinkRegistry) {
 	a := assertions.New(t)
@@ -105,6 +290,7 @@ func TestLinkRegistry(t *testing.T) {
 
 	namespace := [...]string{
 		"applicationserver_test",
+		"links",
 	}
 	for _, tc := range []struct {
 		Name string