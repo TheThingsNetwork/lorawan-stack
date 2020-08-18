90e5285e36b9ab11281c0a6218e2131f43508c3e
Roman Volosatovs <roman@thethingsnetwork.org>
2020-08-18 19:59:29 +0200

    all: Fix EditorConfig issues

--- doc/themes/the-things-stack/assets/js/theme.js
+++ doc/themes/the-things-stack/assets/js/theme.js
@@ -13,15 +13,15 @@
 // limitations under the License.
 
 document.addEventListener('DOMContentLoaded', function () {
-	var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-	if ($navbarBurgers.length > 0) {
-		$navbarBurgers.forEach(function (el) {
-			el.addEventListener('click', function () {
-				var target = el.dataset.target;
-				var $target = document.getElementById(target);
-				el.classList.toggle('is-active');
-				$target.classList.toggle('is-active');
-			});
-		});
-	}
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function (el) {
+      el.addEventListener('click', function () {
+        var target = el.dataset.target;
+        var $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
 });
