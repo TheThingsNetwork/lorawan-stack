6fb904c85e0d23f0620fb779ee04331666f1cbb8
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2020-08-20 19:20:38 +0300

    as: Use dynamic backoff for links

--- pkg/applicationserver/linking_internal_test.go
+++ pkg/applicationserver/linking_internal_test.go
@@ -17,10 +17,10 @@ package applicationserver
 import (
 	"time"
 
-	"go.thethings.network/lorawan-stack/v3/pkg/component"
 	"go.thethings.network/lorawan-stack/v3/pkg/util/test"
 )
 
 func init() {
-	component.DialTaskBackoffConfig.Intervals = []time.Duration{(1 << 5) * test.Delay}
+	linkBackoffConfig.DynamicInterval = nil
+	linkBackoffConfig.Intervals = []time.Duration{(1 << 5) * test.Delay}
 }
