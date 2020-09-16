beff797aeb6f9aaa043d05d6322bc555eb6c9c71
Bogdan <bogdans.afonins@thethingsindustries.com>
2020-09-16 14:30:25 +0300

    dev: Add stack configuration to cypress

--- cypress/plugins/tasks.js
+++ cypress/plugins/tasks.js
@@ -12,8 +12,52 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const { execSync } = require('child_process')
+
+const yaml = require('js-yaml')
 const codeCoverageTask = require('@cypress/code-coverage/task')
 
+// `stackConfigTask` sources stack configuration entires to `Cypress` configuration while preserving
+// all entries from `cypress.json`.
+const stackConfigTask = (_, config) => {
+  try {
+    const out = execSync('go run ./cmd/ttn-lw-stack config --yml')
+    const yml = yaml.safeLoad(out)
+
+    // General.
+    config.siteName = yml.console.ui['site-name']
+    config.title = yml.console.ui.title
+    config.subTitle = yml.console.ui['sub-title']
+
+    // Cluster.
+    config.asBaseUrl = yml.console.ui.as['base-url']
+    config.asEnabled = yml.console.ui.as.enabled
+    config.nsBaseUrl = yml.console.ui.ns['base-url']
+    config.nsEnabled = yml.console.ui.ns.enabled
+    config.jsBaseUrl = yml.console.ui.js['base-url']
+    config.jsEnabled = yml.console.ui.js.enabled
+    config.isBaseUrl = yml.console.ui.is['base-url']
+    config.isEnabled = yml.console.ui.is.enabled
+    config.gsBaseUrl = yml.console.ui.gs['base-url']
+    config.gsEnabled = yml.console.ui.gs.enabled
+    config.edtcBaseUrl = yml.console.ui.edtc['base-url']
+    config.edtcEnabled = yml.console.ui.edtc.enabled
+    config.qrgBaseUrl = yml.console.ui.qrg['base-url']
+    config.qrgEnabled = yml.console.ui.qrg.enabled
+
+    // Console.
+    config.consoleAssetsRootPath = yml.console.ui['assets-base-url']
+    config.consoleRootPath = new URL(yml.console.ui['canonical-url']).pathname
+
+    // OAuth.
+    config.oauthRootPath = new URL(yml.is.oauth.ui['canonical-url']).pathname
+    config.oauthAssetsRootPath = yml.is.oauth.ui['assets-base-url']
+  } catch (err) {
+    throw err
+  }
+}
+
 module.exports = {
+  stackConfigTask,
   codeCoverageTask,
 }
