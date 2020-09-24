ffcf50f979fef295bdc2dd0336d56da4fdc0a1e9
Bogdan <bogdans.afonins@thethingsindustries.com>
2020-09-24 17:02:16 +0300

    dev: Fix oauth tests

--- cypress/integration/oauth/user-registration.spec.js
+++ cypress/integration/oauth/user-registration.spec.js
@@ -19,7 +19,9 @@ describe('OAuth user registration', () => {
   })
 
   it('displays UI elements in place', () => {
-    cy.findByText(`Create a new ${Cypress.config('siteName')} account`, { selector: 'h1' })
+    cy.findByText(`Create a new ${Cypress.config('oauthSiteName')} account`, {
+      selector: 'h1',
+    }).should('be.visible')
     cy.findByLabelText('User ID').should('be.visible')
     cy.findByLabelText('Name').should('be.visible')
     cy.findByLabelText('Email').should('be.visible')
