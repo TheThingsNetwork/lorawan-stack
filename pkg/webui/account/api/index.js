b0f3cfef0f3802a73b0a716afb0e2f7a98d6a1a5
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2020-12-23 13:56:21 +0100

    account: Add CSRF retry interceptor to axios instance

--- pkg/webui/account/api/index.js
+++ pkg/webui/account/api/index.js
@@ -19,6 +19,7 @@ import {
   selectStackConfig,
   selectCSRFToken,
 } from '@ttn-lw/lib/selectors/env'
+import { isPermissionDeniedError } from '@ttn-lw/lib/errors/utils'
 
 const appRoot = selectApplicationRootPath()
 const stackConfig = selectStackConfig()
@@ -33,7 +34,20 @@ instance.interceptors.response.use(
   response => {
     return response
   },
-  error => {
+  async error => {
+    if (isPermissionDeniedError(error) && error.response.data.includes('CSRF')) {
+      // If the CSRF token is invalid, it likely means that the CSRF cookie
+      // has been deleted or became outdated. Making a new request to the
+      // current path can then retrieve a fresh CSRF cookie, with which
+      // the request can be retried.
+      const csrfResult = await axios.get(window.location)
+      const freshCsrf = csrfResult.headers['x-csrf-token']
+      const freshRequest = {
+        ...error.config,
+        headers: { ...error.config.headers, 'X-CSRF-Token': freshCsrf },
+      }
+      return axios(freshRequest)
+    }
     if ('response' in error && error.response && 'data' in error.response) {
       throw error.response.data
     }
