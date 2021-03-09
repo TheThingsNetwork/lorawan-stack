4af7b560a930abe9a8964f04013d88ee8dd0a807
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2021-03-09 19:39:43 +0100

    as: Add LoRaCloud GLS API

---
+++ pkg/applicationserver/io/packages/loragls/v1/api/objects/objects.go
@@ -0,0 +1,144 @@
+// Copyright © 2021 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package objects
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
+)
+
+// Gateway contains the description of a LoRaWAN gateway.
+// https://www.loracloud.com/documentation/geolocation?url=v3.html#gateway
+type Gateway struct {
+	GatewayID string  `json:"gatewayId"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Altitude  float64 `json:"altitude"`
+}
+
+// Uplink contains the metadata of a LoRaWAN uplink.
+// https://www.loracloud.com/documentation/geolocation?url=v3.html#uplink
+type Uplink struct {
+	GatewayID string
+	AntennaID *uint32
+	TDOA      *uint64
+	RSSI      float64
+	SNR       float64
+}
+
+// MarshalJSON implements json.Marshaler.
+func (u *Uplink) MarshalJSON() ([]byte, error) {
+	return json.Marshal([]interface{}{
+		u.GatewayID,
+		u.AntennaID,
+		u.TDOA,
+		u.RSSI,
+		u.SNR,
+	})
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (u *Uplink) UnmarshalJSON(b []byte) error {
+	components := make([]json.RawMessage, 0, 5)
+	if err := json.Unmarshal(b, &components); err != nil {
+		return err
+	}
+	if n := len(components); n != 5 {
+		return fmt.Errorf("invalid field count %d", n)
+	}
+	for i, c := range []interface{}{
+		&u.GatewayID,
+		&u.AntennaID,
+		&u.TDOA,
+		&u.RSSI,
+		&u.SNR,
+	} {
+		if err := json.Unmarshal(components[i], c); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Frame contains the uplink metadata for each reception.
+// https://www.loracloud.com/documentation/geolocation?url=v3.html#frame
+type Frame []Uplink
+
+// SingleFrameRequest contains the location query request for a single LoRaWAN frame.
+// https://www.loracloud.com/documentation/geolocation?url=v3.html#singleframe-http-request
+type SingleFrameRequest struct {
+	Gateways []Gateway `json:"gateways"`
+	Frame    Frame     `json:"frame"`
+}
+
+// BuildSingelFrameRequest builds a SingleFrameRequest from the provided metadata.
+func BuildSingleFrameRequest(metadata []*ttnpb.RxMetadata) *SingleFrameRequest {
+	r := &SingleFrameRequest{}
+	for _, m := range metadata {
+		if m.Location == nil {
+			continue
+		}
+		var tdoa *uint64
+		if m.FineTimestamp != 0 {
+			tdoa = &m.FineTimestamp
+		}
+		r.Gateways = append(r.Gateways, Gateway{
+			GatewayID: m.GatewayID,
+			Latitude:  m.Location.Latitude,
+			Longitude: m.Location.Longitude,
+			Altitude:  float64(m.Location.Altitude),
+		})
+		r.Frame = append(r.Frame, Uplink{
+			GatewayID: m.GatewayID,
+			AntennaID: &m.AntennaIndex,
+			TDOA:      tdoa,
+			RSSI:      float64(m.RSSI),
+			SNR:       float64(m.SNR),
+		})
+	}
+	return r
+}
+
+const (
+	Algorithm_TDOA     = "Tdoa"
+	Algorithm_RSSI     = "Rssi"
+	Algorithm_RSSITDOA = "RssiTdoaCombined"
+)
+
+// Location contains the coordinates contained in a location query result.
+type Location struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Tolerance uint64  `json:"toleranceHoriz"`
+}
+
+// LocationSolverResult contains the result of a location query.
+// https://www.loracloud.com/documentation/geolocation?url=v3.html#locationsolverresult
+type LocationSolverResult struct {
+	UsedGateways uint8    `json:"numUsedGateways"`
+	HDOP         *float64 `json:"HDOP"`
+	Algorithm    string   `json:"algorithmType"`
+	Location     Location `json:"locationEst"`
+}
+
+// SingleFrameResponse contains the location query response for a single LoRaWAN frame.
+// https://www.loracloud.com/documentation/geolocation?url=v3.html#singleframe-http-request
+type SingleFrameResponse struct {
+	Result   *LocationSolverResult `json:"result"`
+	Errors   []string              `json:"errors"`
+	Warnings []string              `json:"warnings"`
+}
---
+++ pkg/applicationserver/io/packages/loragls/v1/data.go
@@ -0,0 +1,165 @@
+// Copyright © 2021 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package loracloudgeolocationv1
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/gogo/protobuf/types"
+	"go.thethings.network/lorawan-stack/v3/pkg/errors"
+)
+
+var (
+	errFieldNotFound = errors.DefineNotFound("field_not_found", "field `{field}` not found")
+	errInvalidType   = errors.DefineCorruption("invalid_type", "wrong type `{type}`")
+	errInvalidValue  = errors.DefineCorruption("invalid_value", "wrong value `{value}`")
+)
+
+// QueryType enum defines the location query types of the package.
+type QueryType uint8
+
+func (t QueryType) Value() *types.Value {
+	var s string
+	switch t {
+	case QUERY_TOARSSI:
+		s = "TOARSSI"
+	case QUERY_GNSS:
+		s = "GNSS"
+	case QUERY_BOTH:
+		s = "BOTH"
+	default:
+		panic("invalid query type")
+	}
+	return &types.Value{
+		Kind: &types.Value_StringValue{
+			StringValue: s,
+		},
+	}
+}
+
+func (t *QueryType) FromValue(v *types.Value) error {
+	sv, ok := v.Kind.(*types.Value_StringValue)
+	if !ok {
+		return errInvalidType.WithAttributes("type", fmt.Sprintf("%T", v.Kind))
+	}
+	switch sv.StringValue {
+	case "TOARSSI":
+		*t = QUERY_TOARSSI
+	case "GNSS":
+		*t = QUERY_GNSS
+	case "BOTH":
+		*t = QUERY_BOTH
+	default:
+		return errInvalidValue.WithAttributes("value", sv.StringValue)
+	}
+	return nil
+}
+
+const (
+	// QUERY_TOARSSI uses the TOA and RSSI information from the gateway metadata to compute the location of the end device.
+	QUERY_TOARSSI QueryType = 1 << 0
+	// QUERY_GNSS uses the GNSS payload to compute the location of the end device.
+	QUERY_GNSS = 2 << 0
+	// QUERY_BOTH emits both the output of QUERY_TOARSSI and QUERY_GNSS. Note that the queries are separate, and so are the end results.
+	QUERY_BOTH = QUERY_TOARSSI | QUERY_GNSS
+)
+
+// Data contains the package configuration.
+type Data struct {
+	// Query is the query type used by the package.
+	Query QueryType
+	// ServerURL represents the remote server to which the GLS queries are sent.
+	ServerURL *url.URL
+	// Token is the API token to be used when comunicating with the GLS server.
+	Token string
+}
+
+const (
+	queryField     = "query"
+	serverURLField = "server_url"
+	tokenField     = "token"
+)
+
+func toString(s string) *types.Value {
+	return &types.Value{
+		Kind: &types.Value_StringValue{
+			StringValue: s,
+		},
+	}
+}
+
+// Struct serializes the configuration to *types.Struct.
+func (d *Data) Struct() *types.Struct {
+	st := &types.Struct{
+		Fields: map[string]*types.Value{
+			queryField: d.Query.Value(),
+			tokenField: toString(d.Token),
+		},
+	}
+	if d.ServerURL != nil {
+		st.Fields[serverURLField] = toString(d.ServerURL.String())
+	}
+	return st
+}
+
+func stringFromValue(v *types.Value) (string, error) {
+	sv, ok := v.Kind.(*types.Value_StringValue)
+	if !ok {
+		return "", errInvalidType.WithAttributes("type", fmt.Sprintf("%T", v.Kind))
+	}
+	return sv.StringValue, nil
+}
+
+// FromStruct deserializes the configuration from *types.Struct.
+func (d *Data) FromStruct(st *types.Struct) error {
+	fields := st.GetFields()
+	{
+		value, ok := fields[queryField]
+		if !ok {
+			return errFieldNotFound.WithAttributes("field", queryField)
+		}
+		if err := d.Query.FromValue(value); err != nil {
+			return err
+		}
+	}
+	{
+		value, ok := fields[tokenField]
+		if !ok {
+			return errFieldNotFound.WithAttributes("field", tokenField)
+		}
+		token, err := stringFromValue(value)
+		if err != nil {
+			return err
+		}
+		d.Token = token
+	}
+	{
+		value, ok := fields[serverURLField]
+		if !ok {
+			return nil
+		}
+		serverURL, err := stringFromValue(value)
+		if err != nil {
+			return err
+		}
+		u, err := url.Parse(serverURL)
+		if err != nil {
+			return err
+		}
+		d.ServerURL = u
+	}
+	return nil
+}
