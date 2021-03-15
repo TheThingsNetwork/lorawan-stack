3ae0dd59500346fc47ca97b8d81d9c73387726df
Roman Volosatovs <roman@thethingsnetwork.org>
2021-03-15 22:52:17 +0100

    api: Test enum value wrapper encoding

--- pkg/ttnpb/ttnpb_encoding_test.go
+++ pkg/ttnpb/ttnpb_encoding_test.go
@@ -83,159 +83,175 @@ func TestStringers(t *testing.T) {
 }
 
 func TestEnumMarshalers(t *testing.T) {
-	var vals [][]fmt.Stringer
+	var vals [][]interface{}
 
-	var mTypes []fmt.Stringer
+	var mTypes []interface{}
 	for i := range MType_name {
 		mTypes = append(mTypes, MType(i))
 	}
 	vals = append(vals, mTypes)
 
-	var majors []fmt.Stringer
+	var majors []interface{}
 	for i := range Major_name {
 		majors = append(majors, Major(i))
 	}
 	vals = append(vals, majors)
 
-	var macVers []fmt.Stringer
+	var macVers []interface{}
 	for i := range MACVersion_name {
 		macVers = append(macVers, MACVersion(i))
 	}
 	vals = append(vals, macVers)
 
-	var phyVers []fmt.Stringer
+	var phyVers []interface{}
 	for i := range PHYVersion_name {
 		phyVers = append(phyVers, PHYVersion(i))
 	}
 	vals = append(vals, phyVers)
 
-	var drIdxs []fmt.Stringer
+	var drIdxs []interface{}
 	for i := range DataRateIndex_name {
 		drIdxs = append(drIdxs, DataRateIndex(i))
 	}
 	vals = append(vals, drIdxs)
 
-	var drOffsets []fmt.Stringer
+	var drIdxVals []interface{}
+	for i := range DataRateIndex_name {
+		drIdxVals = append(drIdxVals, DataRateIndexValue{
+			Value: DataRateIndex(i),
+		})
+	}
+	vals = append(vals, drIdxVals)
+
+	var drOffsets []interface{}
 	for i := range DataRateOffset_name {
 		drOffsets = append(drOffsets, DataRateOffset(i))
 	}
 	vals = append(vals, drOffsets)
 
-	var rejoins []fmt.Stringer
+	var drOffsetVals []interface{}
+	for i := range DataRateOffset_name {
+		drOffsetVals = append(drOffsetVals, DataRateOffsetValue{
+			Value: DataRateOffset(i),
+		})
+	}
+	vals = append(vals, drOffsetVals)
+
+	var rejoins []interface{}
 	for i := range RejoinType_name {
 		rejoins = append(rejoins, RejoinType(i))
 	}
 	vals = append(vals, rejoins)
 
-	var cfLists []fmt.Stringer
+	var cfLists []interface{}
 	for i := range CFListType_name {
 		cfLists = append(cfLists, CFListType(i))
 	}
 	vals = append(vals, cfLists)
 
-	var classes []fmt.Stringer
+	var classes []interface{}
 	for i := range Class_name {
 		classes = append(classes, Class(i))
 	}
 	vals = append(vals, classes)
 
-	var txSchedulePrios []fmt.Stringer
+	var txSchedulePrios []interface{}
 	for i := range TxSchedulePriority_name {
 		txSchedulePrios = append(txSchedulePrios, TxSchedulePriority(i))
 	}
 	vals = append(vals, txSchedulePrios)
 
-	var cids []fmt.Stringer
+	var cids []interface{}
 	for i := range MACCommandIdentifier_name {
 		cids = append(cids, MACCommandIdentifier(i))
 	}
 	vals = append(vals, cids)
 
-	var dutyCycles []fmt.Stringer
+	var dutyCycles []interface{}
 	for i := range AggregatedDutyCycle_name {
 		dutyCycles = append(dutyCycles, AggregatedDutyCycle(i))
 	}
 	vals = append(vals, dutyCycles)
 
-	var pingSlots []fmt.Stringer
+	var pingSlots []interface{}
 	for i := range PingSlotPeriod_name {
 		pingSlots = append(pingSlots, PingSlotPeriod(i))
 	}
 	vals = append(vals, pingSlots)
 
-	var rejoinCounts []fmt.Stringer
+	var rejoinCounts []interface{}
 	for i := range RejoinCountExponent_name {
 		rejoinCounts = append(rejoinCounts, RejoinCountExponent(i))
 	}
 	vals = append(vals, rejoinCounts)
 
-	var rejoinTimes []fmt.Stringer
+	var rejoinTimes []interface{}
 	for i := range RejoinTimeExponent_name {
 		rejoinTimes = append(rejoinTimes, RejoinTimeExponent(i))
 	}
 	vals = append(vals, rejoinTimes)
 
-	var rejoinPeriods []fmt.Stringer
+	var rejoinPeriods []interface{}
 	for i := range RejoinPeriodExponent_name {
 		rejoinPeriods = append(rejoinPeriods, RejoinPeriodExponent(i))
 	}
 	vals = append(vals, rejoinPeriods)
 
-	var deviceEIRPs []fmt.Stringer
+	var deviceEIRPs []interface{}
 	for i := range DeviceEIRP_name {
 		deviceEIRPs = append(deviceEIRPs, DeviceEIRP(i))
 	}
 	vals = append(vals, deviceEIRPs)
 
-	var ackLimitExponents []fmt.Stringer
+	var ackLimitExponents []interface{}
 	for i := range ADRAckLimitExponent_name {
 		ackLimitExponents = append(ackLimitExponents, ADRAckLimitExponent(i))
 	}
 	vals = append(vals, ackLimitExponents)
 
-	var ackDelayExponents []fmt.Stringer
+	var ackDelayExponents []interface{}
 	for i := range ADRAckDelayExponent_name {
 		ackDelayExponents = append(ackDelayExponents, ADRAckDelayExponent(i))
 	}
 	vals = append(vals, ackDelayExponents)
 
-	var rxDelays []fmt.Stringer
+	var rxDelays []interface{}
 	for i := range RxDelay_name {
 		rxDelays = append(rxDelays, RxDelay(i))
 	}
 	vals = append(vals, rxDelays)
 
-	var minors []fmt.Stringer
+	var minors []interface{}
 	for i := range Minor_name {
 		minors = append(minors, Minor(i))
 	}
 	vals = append(vals, minors)
 
-	var grants []fmt.Stringer
+	var grants []interface{}
 	for i := range GrantType_name {
 		grants = append(grants, GrantType(i))
 	}
 	vals = append(vals, grants)
 
-	var clusterRoles []fmt.Stringer
+	var clusterRoles []interface{}
 	for i := range ClusterRole_name {
 		clusterRoles = append(clusterRoles, ClusterRole(i))
 	}
 	vals = append(vals, clusterRoles)
 
-	var states []fmt.Stringer
+	var states []interface{}
 	for i := range State_name {
 		states = append(states, State(i))
 	}
 	vals = append(vals, states)
 
-	var locationSources []fmt.Stringer
+	var locationSources []interface{}
 	for i := range LocationSource_name {
 		locationSources = append(locationSources, LocationSource(i))
 	}
 	vals = append(vals, locationSources)
 
-	var rights []fmt.Stringer
+	var rights []interface{}
 	for i := range Right_name {
 		rights = append(rights, Right(i))
 	}
@@ -248,12 +264,7 @@ func TestEnumMarshalers(t *testing.T) {
 
 		t.Run(typ.String(), func(t *testing.T) {
 			for _, v := range vs {
-				a := assertions.New(t)
-				if !a.So(func() { _ = v.String() }, should.NotPanic) {
-					t.FailNow()
-				}
-
-				t.Run(v.String(), func(t *testing.T) {
+				t.Run(fmt.Sprint(v), func(t *testing.T) {
 					if m, ok := v.(encoding.TextMarshaler); ok {
 						t.Run("Text", func(t *testing.T) {
 							a := assertions.New(t)
@@ -261,7 +272,7 @@ func TestEnumMarshalers(t *testing.T) {
 							if !a.So(err, should.BeNil) {
 								t.Error(test.FormatError(err))
 							}
-							outLines = append(outLines, fmt.Sprintf(`Text: %s "%s" -> "%s"`, typ, v, b))
+							outLines = append(outLines, fmt.Sprintf(`Text: %s "%v" -> "%s"`, typ, v, b))
 
 							got, ok := newV().(encoding.TextUnmarshaler)
 							if !ok {
@@ -272,8 +283,19 @@ func TestEnumMarshalers(t *testing.T) {
 							a.So(err, should.BeNil)
 							a.So(reflect.Indirect(reflect.ValueOf(got)).Interface(), should.Resemble, v)
 
+							sv, ok := v.(fmt.Stringer)
+							if !ok {
+								// Structs (e.g. Value wrappers) implement String() on pointer types and do not
+								// require compatibility of string and text encoding
+								return
+							}
+							var s string
+							if !a.So(func() { s = sv.String() }, should.NotPanic) {
+								t.Fatalf("Failed to call String()")
+							}
+
 							got = newV().(encoding.TextUnmarshaler)
-							err = got.UnmarshalText([]byte(v.(fmt.Stringer).String()))
+							err = got.UnmarshalText([]byte(s))
 							if !a.So(err, should.BeNil) {
 								t.Error(test.FormatError(err))
 							}
@@ -288,7 +310,7 @@ func TestEnumMarshalers(t *testing.T) {
 							if !a.So(err, should.BeNil) {
 								t.Error(test.FormatError(err))
 							}
-							outLines = append(outLines, fmt.Sprintf(`Binary: %s "%s" -> %v`, typ, v, b))
+							outLines = append(outLines, fmt.Sprintf(`Binary: %s "%v" -> %v`, typ, v, b))
 
 							got, ok := newV().(encoding.BinaryUnmarshaler)
 							if !ok {
@@ -310,7 +332,7 @@ func TestEnumMarshalers(t *testing.T) {
 							if !a.So(err, should.BeNil) {
 								t.Error(test.FormatError(err))
 							}
-							outLines = append(outLines, fmt.Sprintf(`JSON: %s "%s" -> "%s"`, typ, v, b))
+							outLines = append(outLines, fmt.Sprintf(`JSON: %s "%v" -> "%s"`, typ, v, b))
 
 							got, ok := newV().(json.Unmarshaler)
 							if !ok {
