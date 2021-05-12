9c4178c84fe515533c2c0094be2426d1385af7c4
Hylke Visser <hylke@thethingsindustries.com>
2021-05-12 15:29:17 +0200

    console: Add previews for events with google.protobuf.Value data

---
+++ pkg/webui/console/components/events/previews/value.js
@@ -0,0 +1,39 @@
+// Copyright © 2021 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+
+import PropTypes from '@ttn-lw/lib/prop-types'
+
+import DescriptionList from './shared/description-list'
+import JSONPayload from './shared/json-payload'
+
+const Value = React.memo(({ event }) => {
+  const { data } = event
+  return (
+    <DescriptionList>
+      {Array.isArray(data.value) || typeof data.value === 'object' ? (
+        <JSONPayload data={data.value} />
+      ) : (
+        <DescriptionList.Item value={data.value} />
+      )}
+    </DescriptionList>
+  )
+})
+
+Value.propTypes = {
+  event: PropTypes.event.isRequired,
+}
+
+export default Value
