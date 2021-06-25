544a3cbe9846566a52a6cb713e35d02a3c845eb1
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2021-06-25 13:38:26 +0200

    util: Apply eslint --fix to js SDK files

--- sdk/js/src/service/applications.test.js
+++ sdk/js/src/service/applications.test.js
@@ -36,19 +36,17 @@ const mockApplicationData = {
   ],
 }
 
-jest.mock('../api', () => {
-  return jest.fn().mockImplementation(() => {
-    return {
-      ApplicationRegistry: {
-        Get: jest.fn().mockResolvedValue({ data: mockApplicationData }),
-        List: jest.fn().mockResolvedValue({
-          data: { applications: [mockApplicationData] },
-          headers: { 'x-total-count': 1 },
-        }),
-      },
-    }
-  })
-})
+jest.mock('../api', () =>
+  jest.fn().mockImplementation(() => ({
+    ApplicationRegistry: {
+      Get: jest.fn().mockResolvedValue({ data: mockApplicationData }),
+      List: jest.fn().mockResolvedValue({
+        data: { applications: [mockApplicationData] },
+        headers: { 'x-total-count': 1 },
+      }),
+    },
+  })),
+)
 
 describe('Applications', () => {
   let applications
