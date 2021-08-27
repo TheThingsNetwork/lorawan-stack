88a008b22fbc90d4beb35855c738dd0d7d3a48d4
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2021-08-27 17:58:38 +0200

    console,account: Use new error views in error boundaries

--- pkg/webui/account.js
+++ pkg/webui/account.js
@@ -20,10 +20,14 @@ import * as Sentry from '@sentry/browser'
 import sentryConfig from '@ttn-lw/constants/sentry'
 import store, { history } from '@account/store'
 
+import Header from '@ttn-lw/components/header'
+
 import WithLocale from '@ttn-lw/lib/components/with-locale'
 import { ErrorView } from '@ttn-lw/lib/components/error-view'
-import { FullViewErrorRaw } from '@ttn-lw/lib/components/full-view-error'
+import { FullViewError } from '@ttn-lw/lib/components/full-view-error'
 import Init from '@ttn-lw/lib/components/init'
+
+import Logo from '@account/containers/logo'
 
 import { selectSentryDsnConfig } from '@ttn-lw/lib/selectors/env'
 
@@ -34,11 +38,18 @@ if (selectSentryDsnConfig()) {
   Sentry.init(sentryConfig)
 }
 
+// Error renderer for the outermost error boundary.
+// Do not use any components that depend on context
+// e.g. Intl, Router, Redux store.
+const errorRender = error => (
+  <FullViewError error={error} header={<Header logo={<Logo safe />} />} safe />
+)
+
 const render = () => {
   const App = require('./account/views/app').default
 
   DOM.render(
-    <ErrorView ErrorComponent={FullViewErrorRaw}>
+    <ErrorView errorRender={errorRender}>
       <Provider store={store}>
         <WithLocale>
           <div id="modal-container" />
--- pkg/webui/console.js
+++ pkg/webui/console.js
@@ -21,33 +21,44 @@ import * as Sentry from '@sentry/browser'
 import sentryConfig from '@ttn-lw/constants/sentry'
 
 import { BreadcrumbsProvider } from '@ttn-lw/components/breadcrumbs/context'
+import Header from '@ttn-lw/components/header'
 
 import { EnvProvider } from '@ttn-lw/lib/components/env'
 import { ErrorView } from '@ttn-lw/lib/components/error-view'
-import { FullViewErrorRaw } from '@ttn-lw/lib/components/full-view-error'
+import { FullViewError } from '@ttn-lw/lib/components/full-view-error'
 import Init from '@ttn-lw/lib/components/init'
 import WithLocale from '@ttn-lw/lib/components/with-locale'
+
+import Logo from '@console/containers/logo'
 
 import env from '@ttn-lw/lib/env'
 import { selectApplicationRootPath, selectSentryDsnConfig } from '@ttn-lw/lib/selectors/env'
 
 import createStore from './console/store'
 
-const sentryDsn = selectSentryDsnConfig()
 const appRoot = selectApplicationRootPath()
 
-const history = createBrowserHistory({ basename: `${appRoot}/` })
 // Initialize sentry before creating store.
-if (sentryDsn) Sentry.init(sentryConfig)
-const store = createStore(history)
+if (selectSentryDsnConfig) {
+  Sentry.init(sentryConfig)
+}
 
+const history = createBrowserHistory({ basename: `${appRoot}/` })
+const store = createStore(history)
 const rootElement = document.getElementById('app')
+
+// Error renderer for the outermost error boundary.
+// Do not use any components that depend on context
+// e.g. Intl, Router, Redux store.
+const errorRender = error => (
+  <FullViewError error={error} header={<Header logo={<Logo safe />} />} safe />
+)
 
 const render = () => {
   const App = require('./console/views/app').default
 
   DOM.render(
-    <ErrorView ErrorComponent={FullViewErrorRaw}>
+    <ErrorView errorRender={errorRender}>
       <EnvProvider env={env}>
         <Provider store={store}>
           <WithLocale>
