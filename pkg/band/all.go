7e28d3cbfcb25d4237300936a45d1c05312d7152
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2021-09-02 18:03:15 +0200

    all: Add AS_923 band definition

--- pkg/band/all.go
+++ pkg/band/all.go
@@ -19,7 +19,15 @@ import (
 )
 
 // All contains all the bands available.
-var All = map[string]map[ttnpb.PHYVersion]Band{}
+var All = map[string]map[ttnpb.PHYVersion]Band{
+	AS_923: {
+		ttnpb.RP001_V1_0_2:       AS_923_RP1_v1_0_2,
+		ttnpb.RP001_V1_0_2_REV_B: AS_923_RP1_v1_0_2_RevB,
+		ttnpb.RP001_V1_0_3_REV_A: AS_923_RP1_v1_0_3_RevA,
+		ttnpb.RP001_V1_1_REV_A:   AS_923_RP1_v1_1_RevA,
+		ttnpb.RP001_V1_1_REV_B:   AS_923_RP1_v1_1_RevB,
+	},
+}
 
 // Get returns the band if it was found, and returns an error otherwise.
 func Get(id string, version ttnpb.PHYVersion) (Band, error) {
