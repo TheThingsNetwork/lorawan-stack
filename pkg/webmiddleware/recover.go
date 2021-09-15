c1b3117918215e57b20c383c27cf0275430f6b4b
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2021-09-15 12:30:58 +0200

    all: Log error on handler panic

--- pkg/webmiddleware/recover.go
+++ pkg/webmiddleware/recover.go
@@ -21,6 +21,7 @@ import (
 	"runtime/debug"
 
 	"go.thethings.network/lorawan-stack/v3/pkg/errors"
+	"go.thethings.network/lorawan-stack/v3/pkg/log"
 	"go.thethings.network/lorawan-stack/v3/pkg/webhandlers"
 )
 
@@ -41,6 +42,7 @@ func Recover() MiddlewareFunc {
 					} else {
 						err = errHTTPRecovered.WithAttributes("panic", p)
 					}
+					log.FromContext(r.Context()).WithError(err).Error("Handler panicked")
 					webhandlers.Error(w, r, err)
 				}
 			}()
