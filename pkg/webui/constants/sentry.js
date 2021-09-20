e72ed27633bfe700fc1865c8d0c62534ae205a15
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2021-09-20 17:15:24 +0100

    console,account: Add deduplicator integration to sentry config

--- pkg/webui/constants/sentry.js
+++ pkg/webui/constants/sentry.js
@@ -12,12 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import * as Integrations from '@sentry/integrations'
+
 import env from '@ttn-lw/lib/env'
 
 const sentryConfig = {
   dsn: env.sentryDsn,
   release: process.env.VERSION,
   normalizeDepth: 10,
+  integrations: [new Integrations.Dedupe()],
   beforeSend: event => {
     if (event.extra.state && event.extra.state.user) {
       delete event.extra.state.user.user.name
