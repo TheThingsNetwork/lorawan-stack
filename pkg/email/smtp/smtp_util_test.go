73ee5acd1a017110f06802ecee97bf1251e3133c
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2021-10-04 14:55:00 +0200

    all: Update smtp package tests

---
+++ pkg/email/smtp/smtp_util_test.go
@@ -0,0 +1,83 @@
+// Copyright © 2021 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package smtp
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/emersion/go-smtp"
+)
+
+type backend struct {
+	messages chan *message
+}
+
+func (bkd *backend) newSession() (smtp.Session, error) {
+	s := &session{
+		msgs: bkd.messages,
+	}
+	return s, nil
+}
+
+func (bkd *backend) Login(_ *smtp.ConnectionState, _ string, _ string) (smtp.Session, error) {
+	return bkd.newSession()
+}
+
+func (bkd *backend) AnonymousLogin(_ *smtp.ConnectionState) (smtp.Session, error) {
+	return bkd.newSession()
+}
+
+type message struct {
+	Sender     string
+	Recipients []string
+	Data       []byte
+	Opts       smtp.MailOptions
+}
+
+type session struct {
+	msg  *message
+	msgs chan *message
+}
+
+func (s *session) Mail(from string, opts smtp.MailOptions) error {
+	s.Reset()
+	s.msg.Sender = from
+	s.msg.Opts = opts
+	return nil
+}
+
+func (s *session) Rcpt(to string) error {
+	s.msg.Recipients = append(s.msg.Recipients, to)
+	return nil
+}
+
+func (s *session) Data(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	s.msg.Data = b
+	s.msgs <- s.msg
+	return nil
+}
+
+func (s *session) Reset() {
+	s.msg = &message{}
+}
+
+func (s *session) Logout() error {
+	return nil
+}
