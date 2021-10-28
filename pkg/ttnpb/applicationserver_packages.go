23c216c9144c5e5fa1ed961b1fc2d96a538837ba
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2021-10-28 13:37:45 +0200

    api: Use getters in applicationserver_packages

--- pkg/ttnpb/applicationserver_packages.go
+++ pkg/ttnpb/applicationserver_packages.go
@@ -17,103 +17,103 @@ package ttnpb
 // All EntityType methods implement the IDStringer interface.
 
 func (m *ApplicationPackageAssociationIdentifiers) EntityType() string {
-	return m.EndDeviceIds.EntityType()
+	return m.GetEndDeviceIds().EntityType()
 }
 
 func (m *ApplicationPackageAssociation) EntityType() string {
-	return m.Ids.EntityType()
+	return m.GetIds().EntityType()
 }
 
 func (m *GetApplicationPackageAssociationRequest) EntityType() string {
-	return m.Ids.EntityType()
+	return m.GetIds().EntityType()
 }
 
 func (m *ListApplicationPackageAssociationRequest) EntityType() string {
-	return m.Ids.EntityType()
+	return m.GetIds().EntityType()
 }
 
 func (m *SetApplicationPackageAssociationRequest) EntityType() string {
-	return m.Association.EntityType()
+	return m.GetAssociation().EntityType()
 }
 
 func (m *ApplicationPackageDefaultAssociationIdentifiers) EntityType() string {
-	return m.ApplicationIds.EntityType()
+	return m.GetApplicationIds().EntityType()
 }
 
 func (m *ApplicationPackageDefaultAssociation) EntityType() string {
-	return m.Ids.EntityType()
+	return m.GetIds().EntityType()
 }
 
 func (m *GetApplicationPackageDefaultAssociationRequest) EntityType() string {
-	return m.Ids.EntityType()
+	return m.GetIds().EntityType()
 }
 
 func (m *ListApplicationPackageDefaultAssociationRequest) EntityType() string {
-	return m.Ids.EntityType()
+	return m.GetIds().EntityType()
 }
 
 func (m *SetApplicationPackageDefaultAssociationRequest) EntityType() string {
-	return m.Default.EntityType()
+	return m.GetDefault().EntityType()
 }
 
 // All IDString methods implement the IDStringer interface.
 
 func (m *ApplicationPackageAssociationIdentifiers) IDString() string {
-	return m.EndDeviceIds.IDString()
+	return m.GetEndDeviceIds().IDString()
 }
 
 func (m *ApplicationPackageAssociation) IDString() string {
-	return m.Ids.IDString()
+	return m.GetIds().IDString()
 }
 
 func (m *GetApplicationPackageAssociationRequest) IDString() string {
-	return m.Ids.IDString()
+	return m.GetIds().IDString()
 }
 
 func (m *ListApplicationPackageAssociationRequest) IDString() string {
-	return m.Ids.IDString()
+	return m.GetIds().IDString()
 }
 
 func (m *SetApplicationPackageAssociationRequest) IDString() string {
-	return m.Association.IDString()
+	return m.GetAssociation().IDString()
 }
 
 func (m *ApplicationPackageDefaultAssociationIdentifiers) IDString() string {
-	return m.ApplicationIds.IDString()
+	return m.GetApplicationIds().IDString()
 }
 
 func (m *ApplicationPackageDefaultAssociation) IDString() string {
-	return m.Ids.IDString()
+	return m.GetIds().IDString()
 }
 
 func (m *GetApplicationPackageDefaultAssociationRequest) IDString() string {
-	return m.Ids.IDString()
+	return m.GetIds().IDString()
 }
 
 func (m *ListApplicationPackageDefaultAssociationRequest) IDString() string {
-	return m.Ids.IDString()
+	return m.GetIds().IDString()
 }
 
 func (m *SetApplicationPackageDefaultAssociationRequest) IDString() string {
-	return m.Default.IDString()
+	return m.GetDefault().IDString()
 }
 
 // All ExtractRequestFields methods are used by github.com/grpc-ecosystem/go-grpc-middleware/tags.
 
 func (m *ApplicationPackageAssociationIdentifiers) ExtractRequestFields(dst map[string]interface{}) {
-	m.EndDeviceIds.ExtractRequestFields(dst)
+	m.GetEndDeviceIds().ExtractRequestFields(dst)
 }
 
 func (m *ApplicationPackageAssociation) ExtractRequestFields(dst map[string]interface{}) {
-	m.Ids.ExtractRequestFields(dst)
+	m.GetIds().ExtractRequestFields(dst)
 }
 
 func (m *GetApplicationPackageAssociationRequest) ExtractRequestFields(dst map[string]interface{}) {
-	m.Ids.ExtractRequestFields(dst)
+	m.GetIds().ExtractRequestFields(dst)
 }
 
 func (m *ListApplicationPackageAssociationRequest) ExtractRequestFields(dst map[string]interface{}) {
-	m.Ids.ExtractRequestFields(dst)
+	m.GetIds().ExtractRequestFields(dst)
 }
 
 func (m *SetApplicationPackageAssociationRequest) ExtractRequestFields(dst map[string]interface{}) {
@@ -121,21 +121,21 @@ func (m *SetApplicationPackageAssociationRequest) ExtractRequestFields(dst map[s
 }
 
 func (m *ApplicationPackageDefaultAssociationIdentifiers) ExtractRequestFields(dst map[string]interface{}) {
-	m.ApplicationIds.ExtractRequestFields(dst)
+	m.GetApplicationIds().ExtractRequestFields(dst)
 }
 
 func (m *ApplicationPackageDefaultAssociation) ExtractRequestFields(dst map[string]interface{}) {
-	m.Ids.ExtractRequestFields(dst)
+	m.GetIds().ExtractRequestFields(dst)
 }
 
 func (m *GetApplicationPackageDefaultAssociationRequest) ExtractRequestFields(dst map[string]interface{}) {
-	m.Ids.ExtractRequestFields(dst)
+	m.GetIds().ExtractRequestFields(dst)
 }
 
 func (m *ListApplicationPackageDefaultAssociationRequest) ExtractRequestFields(dst map[string]interface{}) {
-	m.Ids.ExtractRequestFields(dst)
+	m.GetIds().ExtractRequestFields(dst)
 }
 
 func (m *SetApplicationPackageDefaultAssociationRequest) ExtractRequestFields(dst map[string]interface{}) {
-	m.Default.ExtractRequestFields(dst)
+	m.GetDefault().ExtractRequestFields(dst)
 }
