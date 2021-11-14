b8a98949030539216aa8ce3f0962e4419c75cce8
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2021-11-14 22:19:48 +0100

    as: Revert to Redis PubSub channel

--- pkg/applicationserver/distribution/redis/redis.go
+++ pkg/applicationserver/distribution/redis/redis.go
@@ -17,6 +17,7 @@ package redis
 import (
 	"context"
 
+	"go.thethings.network/lorawan-stack/v3/pkg/errors"
 	ttnredis "go.thethings.network/lorawan-stack/v3/pkg/redis"
 	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
 	"go.thethings.network/lorawan-stack/v3/pkg/unique"
@@ -33,39 +34,49 @@ func (ps PubSub) uidUplinkKey(uid string) string {
 
 // Publish publishes the uplink to Pub/Sub.
 func (ps PubSub) Publish(ctx context.Context, up *ttnpb.ApplicationUp) error {
-	s, err := ttnredis.MarshalProto(up)
+	msg, err := ttnredis.MarshalProto(up)
 	if err != nil {
 		return err
 	}
 	uid := unique.ID(ctx, up.ApplicationIdentifiers)
-	ch := ps.uidUplinkKey(uid)
-	if err = ps.Redis.Publish(ctx, ch, s).Err(); err != nil {
+	if err = ps.Redis.Publish(ctx, ps.uidUplinkKey(uid), msg).Err(); err != nil {
 		return ttnredis.ConvertError(err)
 	}
 	return nil
 }
 
+var errChannelClosed = errors.DefineAborted("channel_closed", "channel closed")
+
 // Subscribe subscribes to the traffic of the provided application and processes it using the handler.
 func (ps PubSub) Subscribe(ctx context.Context, ids ttnpb.ApplicationIdentifiers, handler func(context.Context, *ttnpb.ApplicationUp) error) error {
 	uid := unique.ID(ctx, ids)
-	ch := ps.uidUplinkKey(uid)
-
-	sub := ps.Redis.Subscribe(ctx, ch)
+	sub := ps.Redis.Subscribe(ctx, ps.uidUplinkKey(uid))
 	defer sub.Close()
 
+	// sub.Receive(.*) will not respect the asynchronous context
+	// cancelation, only a context deadline, if present.
+	// As such, we use the buffered channel instead and do the
+	// asynchronous select here. This allows us to close the
+	// subscription on context cancellation.
+	ch := sub.Channel()
 	for {
-		msg, err := sub.ReceiveMessage(ctx)
-		if err != nil {
-			return ttnredis.ConvertError(err)
-		}
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
 
-		up := &ttnpb.ApplicationUp{}
-		if err := ttnredis.UnmarshalProto(msg.Payload, up); err != nil {
-			return err
-		}
+		case msg, ok := <-ch:
+			if !ok {
+				return errChannelClosed.New()
+			}
 
-		if err := handler(ctx, up); err != nil {
-			return err
+			up := &ttnpb.ApplicationUp{}
+			if err := ttnredis.UnmarshalProto(msg.Payload, up); err != nil {
+				return err
+			}
+
+			if err := handler(ctx, up); err != nil {
+				return err
+			}
 		}
 	}
 }
