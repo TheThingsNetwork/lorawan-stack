32c0b14605b74b62fa8bdd181d495197adad8705
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2021-12-16 17:50:46 +0100

    dev: Avoid avoidable redundant cypress runs

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -12,12 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* eslint-disable no-invalid-this */
+
 import '@cypress/code-coverage/support'
 import '@testing-library/cypress/add-commands'
 import { configure } from '@testing-library/cypress'
 import './commands'
 
+const failedSpecsFilename = `./.cache/.failed-specs-${Cypress.env('MACHINE_NUMBER') || '0'}.txt`
+
 configure({ testIdAttribute: 'data-test-id' })
 Cypress.SelectorPlayground.defaults({
   selectorPriority: ['data-test-id', 'id', 'class', 'tag', 'attributes', 'nth-child'],
 })
+
+// Enable fail early if set.
+afterEach(function () {
+  if (this.currentTest.state === 'failed' && Cypress.env('FAIL_FAST')) {
+    cy.log('Skipping rest of run due to test failure (fail fast)')
+    cy.writeFile(failedSpecsFilename, this.currentTest.invocationDetails.relativeFile)
+  }
+})
+
+// Skip remaining runs if fail early is set.
+const skipIfNecessary = function () {
+  cy.task('fileExists', failedSpecsFilename).then(content => {
+    if (content !== '' && content !== false && Cypress.env('FAIL_FAST')) {
+      this.currentTest.pending = true
+      Cypress.runner.stop()
+    }
+  })
+}
+
+before(skipIfNecessary)
+beforeEach(skipIfNecessary)
