7ccf2c1255e0ebe98049439104a078785b10ba79
Bogdan <bogdans.afonins@thethingsindustries.com>
2021-12-17 11:06:46 +0200

    console,account: Refactor modal component
    Change class component to functional component

--- pkg/webui/components/modal/index.js
+++ pkg/webui/components/modal/index.js
@@ -1,4 +1,4 @@
-// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+// Copyright © 2021 The Things Network Foundation, The Things Industries B.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 import React from 'react'
-import bind from 'autobind-decorator'
 import classnames from 'classnames'
 
 import Button from '@ttn-lw/components/button'
@@ -25,176 +24,166 @@ import sharedMessages from '@ttn-lw/lib/shared-messages'
 
 import style from './modal.styl'
 
-class Modal extends React.PureComponent {
-  static propTypes = {
-    approval: PropTypes.bool,
-    approveButtonProps: PropTypes.shape({}),
-    bottomLine: PropTypes.oneOfType([PropTypes.element, PropTypes.message]),
-    buttonMessage: PropTypes.message,
-    buttonName: PropTypes.message,
-    cancelButtonMessage: PropTypes.message,
-    cancelButtonProps: PropTypes.shape({}),
-    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element), PropTypes.element]),
-    danger: PropTypes.bool,
-    formName: PropTypes.string,
-    inline: PropTypes.bool,
-    logo: PropTypes.node,
-    message: PropTypes.message,
-    method: PropTypes.string,
-    name: PropTypes.string,
-    onComplete: PropTypes.func,
-    subtitle: PropTypes.message,
-    title: PropTypes.message,
-  }
+const Modal = props => {
+  const {
+    buttonName,
+    buttonMessage,
+    title,
+    subtitle,
+    children,
+    message,
+    logo,
+    approval,
+    formName,
+    cancelButtonMessage,
+    onComplete,
+    bottomLine,
+    inline,
+    danger,
+    approveButtonProps,
+    cancelButtonProps,
+    ...rest
+  } = props
 
-  static defaultProps = {
-    bottomLine: undefined,
-    buttonMessage: undefined,
-    buttonName: undefined,
-    cancelButtonMessage: sharedMessages.cancel,
-    children: undefined,
-    danger: false,
-    formName: undefined,
-    logo: undefined,
-    message: undefined,
-    method: undefined,
-    onComplete: () => null,
-    inline: false,
-    approval: true,
-    subtitle: undefined,
-    title: undefined,
-    name: undefined,
-    approveButtonProps: {},
-    cancelButtonProps: {},
-  }
+  const modalClassNames = classnames(style.modal, style.modal, {
+    [style.modalInline]: inline,
+    [style.modalAbsolute]: !Boolean(inline),
+  })
 
-  @bind
-  handleApprove() {
-    this.handleComplete(true)
-  }
+  const handleComplete = React.useCallback(
+    result => {
+      onComplete(result)
+    },
+    [onComplete],
+  )
+  const handleApprove = React.useCallback(() => {
+    handleComplete(true)
+  }, [handleComplete])
 
-  @bind
-  handleCancel() {
-    this.handleComplete(false)
-  }
+  const handleCancel = React.useCallback(() => {
+    handleComplete(false)
+  }, [handleComplete])
 
-  @bind
-  handleComplete(result) {
-    const { onComplete } = this.props
+  const name = formName ? { name: formName } : {}
+  const RootComponent = props.method ? 'form' : 'div'
+  const messageElement = message && <Message content={message} className={style.message} />
+  const bottomLineElement =
+    typeof bottomLine === 'object' && Boolean(bottomLine.id) ? (
+      <Message content={bottomLine} />
+    ) : (
+      bottomLine
+    )
 
-    onComplete(result)
-  }
+  const approveButtonMessage =
+    buttonMessage !== undefined
+      ? buttonMessage
+      : approval
+      ? sharedMessages.approve
+      : sharedMessages.ok
+  let buttons = (
+    <div>
+      <Button
+        message={approveButtonMessage}
+        onClick={handleApprove}
+        icon="check"
+        {...approveButtonProps}
+      />
+    </div>
+  )
 
-  render() {
-    const {
-      buttonName,
-      buttonMessage,
-      title,
-      subtitle,
-      children,
-      message,
-      logo,
-      approval,
-      formName,
-      cancelButtonMessage,
-      onComplete,
-      bottomLine,
-      inline,
-      danger,
-      approveButtonProps,
-      cancelButtonProps,
-      ...rest
-    } = this.props
-
-    const modalClassNames = classnames(style.modal, style.modal, {
-      [style.modalInline]: inline,
-      [style.modalAbsolute]: !Boolean(inline),
-    })
-
-    const name = formName ? { name: formName } : {}
-    const RootComponent = this.props.method ? 'form' : 'div'
-    const messageElement = message && <Message content={message} className={style.message} />
-    const bottomLineElement =
-      typeof bottomLine === 'object' && Boolean(bottomLine.id) ? (
-        <Message content={bottomLine} />
-      ) : (
-        bottomLine
-      )
-
-    const approveButtonMessage =
-      buttonMessage !== undefined
-        ? buttonMessage
-        : approval
-        ? sharedMessages.approve
-        : sharedMessages.ok
-    let buttons = (
+  if (approval) {
+    buttons = (
       <div>
         <Button
+          secondary
+          message={cancelButtonMessage}
+          onClick={handleCancel}
+          name={formName}
+          icon="clear"
+          value="false"
+          {...name}
+          {...cancelButtonProps}
+        />
+        <Button
           message={approveButtonMessage}
-          onClick={this.handleApprove}
+          onClick={handleApprove}
+          name={formName}
           icon="check"
+          value="true"
+          danger={danger}
+          {...name}
           {...approveButtonProps}
         />
       </div>
     )
-
-    if (approval) {
-      buttons = (
-        <div>
-          <Button
-            secondary
-            message={cancelButtonMessage}
-            onClick={this.handleCancel}
-            name={formName}
-            icon="clear"
-            value="false"
-            {...name}
-            {...cancelButtonProps}
-          />
-          <Button
-            message={approveButtonMessage}
-            onClick={this.handleApprove}
-            name={formName}
-            icon="check"
-            value="true"
-            danger={danger}
-            {...name}
-            {...approveButtonProps}
-          />
-        </div>
-      )
-    }
-
-    return (
-      <React.Fragment>
-        {!inline && <div key="shadow" className={style.shadow} />}
-        <RootComponent
-          data-test-id="modal-window"
-          key="modal"
-          className={modalClassNames}
-          {...rest}
-        >
-          {title && (
-            <div className={style.titleSection}>
-              <div>
-                <h1>
-                  <Message content={title} />
-                </h1>
-                {subtitle && <Message component="span" content={subtitle} />}
-              </div>
-              {logo}
-            </div>
-          )}
-          {title && <div className={style.line} />}
-          <div className={style.body}>{children || messageElement}</div>
-          <div className={style.controlBar}>
-            <div>{bottomLineElement}</div>
-            {buttons}
-          </div>
-        </RootComponent>
-      </React.Fragment>
-    )
   }
+
+  return (
+    <>
+      {!inline && <div key="shadow" className={style.shadow} />}
+      <RootComponent data-test-id="modal-window" key="modal" className={modalClassNames} {...rest}>
+        {title && (
+          <div className={style.titleSection}>
+            <div>
+              <h1>
+                <Message content={title} />
+              </h1>
+              {subtitle && <Message component="span" content={subtitle} />}
+            </div>
+            {logo}
+          </div>
+        )}
+        {title && <div className={style.line} />}
+        <div className={style.body}>{children || messageElement}</div>
+        <div className={style.controlBar}>
+          <div>{bottomLineElement}</div>
+          {buttons}
+        </div>
+      </RootComponent>
+    </>
+  )
+}
+
+Modal.propTypes = {
+  approval: PropTypes.bool,
+  approveButtonProps: PropTypes.shape({}),
+  bottomLine: PropTypes.oneOfType([PropTypes.element, PropTypes.message]),
+  buttonMessage: PropTypes.message,
+  buttonName: PropTypes.message,
+  cancelButtonMessage: PropTypes.message,
+  cancelButtonProps: PropTypes.shape({}),
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element), PropTypes.element]),
+  danger: PropTypes.bool,
+  formName: PropTypes.string,
+  inline: PropTypes.bool,
+  logo: PropTypes.node,
+  message: PropTypes.message,
+  method: PropTypes.string,
+  name: PropTypes.string,
+  onComplete: PropTypes.func,
+  subtitle: PropTypes.message,
+  title: PropTypes.message,
+}
+
+Modal.defaultProps = {
+  bottomLine: undefined,
+  buttonMessage: undefined,
+  buttonName: undefined,
+  cancelButtonMessage: sharedMessages.cancel,
+  children: undefined,
+  danger: false,
+  formName: undefined,
+  logo: undefined,
+  message: undefined,
+  method: undefined,
+  onComplete: () => null,
+  inline: false,
+  approval: true,
+  subtitle: undefined,
+  title: undefined,
+  name: undefined,
+  approveButtonProps: {},
+  cancelButtonProps: {},
 }
 
 export default Modal
