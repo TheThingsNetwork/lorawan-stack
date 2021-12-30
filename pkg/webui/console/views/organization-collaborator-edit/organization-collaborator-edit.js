591655eb415fa0342acf9692ef576a5495b1a19f
Bogdan <bogdans.afonins@thethingsindustries.com>
2021-12-30 16:26:46 +0200

    console: Refactor organization collaborators edit
    Move from class components to functional components

--- pkg/webui/console/views/organization-collaborator-edit/organization-collaborator-edit.js
+++ pkg/webui/console/views/organization-collaborator-edit/organization-collaborator-edit.js
@@ -1,4 +1,4 @@
-// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+// Copyright © 2022 The Things Network Foundation, The Things Industries B.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ import React from 'react'
 import { Container, Col, Row } from 'react-grid-system'
 
 import PageTitle from '@ttn-lw/components/page-title'
-import { withBreadcrumb } from '@ttn-lw/components/breadcrumbs/context'
+import { useBreadcrumbs } from '@ttn-lw/components/breadcrumbs/context'
 import Breadcrumb from '@ttn-lw/components/breadcrumbs/breadcrumb'
 import toast from '@ttn-lw/components/toast'
 
@@ -25,70 +25,71 @@ import CollaboratorForm from '@console/components/collaborator-form'
 import sharedMessages from '@ttn-lw/lib/shared-messages'
 import PropTypes from '@ttn-lw/lib/prop-types'
 
-@withBreadcrumb('orgs.single.collaborators.edit', props => {
-  const { orgId, collaboratorId, collaboratorType } = props
+const showSuccessToast = () => {
+  toast({
+    message: sharedMessages.collaboratorUpdateSuccess,
+    type: toast.types.SUCCESS,
+  })
+}
 
-  return (
+const OrganizationCollaboratorEdit = props => {
+  const {
+    orgId,
+    collaboratorType,
+    collaboratorId,
+    collaborator,
+    rights,
+    redirectToList,
+    pseudoRights,
+    updateOrganizationCollaborator,
+    removeOrganizationCollaborator,
+  } = props
+
+  useBreadcrumbs(
+    'orgs.single.collaborators.edit',
     <Breadcrumb
       path={`/organizations/${orgId}/collaborators/${collaboratorType}/${collaboratorId}`}
       content={sharedMessages.edit}
-    />
+    />,
   )
-})
-class OrganizationCollaboratorEdit extends React.Component {
-  static propTypes = {
-    collaborator: PropTypes.collaborator.isRequired,
-    pseudoRights: PropTypes.rights,
-    redirectToList: PropTypes.func.isRequired,
-    removeOrganizationCollaborator: PropTypes.func.isRequired,
-    rights: PropTypes.rights.isRequired,
-    updateOrganizationCollaborator: PropTypes.func.isRequired,
-  }
 
-  static defaultProps = {
-    pseudoRights: [],
-  }
+  return (
+    <Container>
+      <PageTitle
+        title={sharedMessages.collaboratorEdit}
+        values={{ collaboratorId: collaborator.id }}
+      />
+      <Row>
+        <Col lg={8} md={12}>
+          <CollaboratorForm
+            onSubmit={updateOrganizationCollaborator}
+            onSubmitSuccess={showSuccessToast}
+            onDelete={removeOrganizationCollaborator}
+            onDeleteSuccess={redirectToList}
+            collaborator={collaborator}
+            pseudoRights={pseudoRights}
+            rights={rights}
+            update
+          />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+OrganizationCollaboratorEdit.propTypes = {
+  collaborator: PropTypes.collaborator.isRequired,
+  collaboratorId: PropTypes.string.isRequired,
+  collaboratorType: PropTypes.string.isRequired,
+  orgId: PropTypes.string.isRequired,
+  pseudoRights: PropTypes.rights,
+  redirectToList: PropTypes.func.isRequired,
+  removeOrganizationCollaborator: PropTypes.func.isRequired,
+  rights: PropTypes.rights.isRequired,
+  updateOrganizationCollaborator: PropTypes.func.isRequired,
+}
 
-  handleSubmitSuccess() {
-    toast({
-      message: sharedMessages.collaboratorUpdateSuccess,
-      type: toast.types.SUCCESS,
-    })
-  }
-
-  render() {
-    const {
-      collaborator,
-      rights,
-      redirectToList,
-      pseudoRights,
-      updateOrganizationCollaborator,
-      removeOrganizationCollaborator,
-    } = this.props
-
-    return (
-      <Container>
-        <PageTitle
-          title={sharedMessages.collaboratorEdit}
-          values={{ collaboratorId: collaborator.id }}
-        />
-        <Row>
-          <Col lg={8} md={12}>
-            <CollaboratorForm
-              onSubmit={updateOrganizationCollaborator}
-              onSubmitSuccess={this.handleSubmitSuccess}
-              onDelete={removeOrganizationCollaborator}
-              onDeleteSuccess={redirectToList}
-              collaborator={collaborator}
-              pseudoRights={pseudoRights}
-              rights={rights}
-              update
-            />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+OrganizationCollaboratorEdit.defaultProps = {
+  pseudoRights: [],
 }
 
 export default OrganizationCollaboratorEdit
