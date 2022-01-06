4994584811583f04822e2060c5359694ef3ddf8f
Bogdan <bogdans.afonins@thethingsindustries.com>
2022-01-06 10:50:59 +0200

    console: Refactor application collaborators add
    Move from class components to functional components

--- pkg/webui/console/views/application-collaborator-add/application-collaborator-add.js
+++ pkg/webui/console/views/application-collaborator-add/application-collaborator-add.js
@@ -1,4 +1,4 @@
-// Copyright © 2020 The Things Network Foundation, The Things Industries B.V.
+// Copyright © 2022 The Things Network Foundation, The Things Industries B.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,62 +12,57 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Container, Col, Row } from 'react-grid-system'
-import bind from 'autobind-decorator'
 
 import PageTitle from '@ttn-lw/components/page-title'
 import Breadcrumb from '@ttn-lw/components/breadcrumbs/breadcrumb'
-import { withBreadcrumb } from '@ttn-lw/components/breadcrumbs/context'
+import { useBreadcrumbs } from '@ttn-lw/components/breadcrumbs/context'
 
 import CollaboratorForm from '@console/components/collaborator-form'
 
 import sharedMessages from '@ttn-lw/lib/shared-messages'
 import PropTypes from '@ttn-lw/lib/prop-types'
 
-@withBreadcrumb('apps.single.collaborators.add', props => {
-  const appId = props.appId
-  return (
-    <Breadcrumb path={`/applications/${appId}/collaborators/add`} content={sharedMessages.add} />
+const ApplicationCollaboratorAdd = props => {
+  const { appId, rights, pseudoRights, redirectToList, addCollaborator, error } = props
+
+  useBreadcrumbs(
+    'apps.single.collaborators.add',
+    <Breadcrumb path={`/applications/${appId}/collaborators/add`} content={sharedMessages.add} />,
   )
-})
-export default class ApplicationCollaboratorAdd extends React.Component {
-  static propTypes = {
-    addCollaborator: PropTypes.func.isRequired,
-    pseudoRights: PropTypes.rights.isRequired,
-    redirectToList: PropTypes.func.isRequired,
-    rights: PropTypes.rights.isRequired,
-  }
 
-  state = {
-    error: undefined,
-  }
+  const handleSubmit = useCallback(collaborator => addCollaborator(collaborator), [addCollaborator])
 
-  @bind
-  async handleSubmit(collaborator) {
-    const { addCollaborator } = this.props
-
-    await addCollaborator(collaborator)
-  }
-
-  render() {
-    const { rights, pseudoRights, redirectToList } = this.props
-
-    return (
-      <Container>
-        <PageTitle title={sharedMessages.addCollaborator} />
-        <Row>
-          <Col lg={8} md={12}>
-            <CollaboratorForm
-              error={this.state.error}
-              onSubmit={this.handleSubmit}
-              onSubmitSuccess={redirectToList}
-              pseudoRights={pseudoRights}
-              rights={rights}
-            />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <PageTitle title={sharedMessages.addCollaborator} />
+      <Row>
+        <Col lg={8} md={12}>
+          <CollaboratorForm
+            error={error}
+            onSubmit={handleSubmit}
+            onSubmitSuccess={redirectToList}
+            pseudoRights={pseudoRights}
+            rights={rights}
+          />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
+
+ApplicationCollaboratorAdd.propTypes = {
+  addCollaborator: PropTypes.func.isRequired,
+  appId: PropTypes.string.isRequired,
+  error: PropTypes.error,
+  pseudoRights: PropTypes.rights.isRequired,
+  redirectToList: PropTypes.func.isRequired,
+  rights: PropTypes.rights.isRequired,
+}
+
+ApplicationCollaboratorAdd.defaultProps = {
+  error: undefined,
+}
+
+export default ApplicationCollaboratorAdd
