2cac7292fc46e8ed3fd7bd40520d810be270ba8b
Bogdan <bogdans.afonins@thethingsindustries.com>
2022-01-06 10:16:28 +0200

    console: Refactor application collaborators list
    Move from class components to functional components

--- pkg/webui/console/views/application-collaborators-list/index.js
+++ pkg/webui/console/views/application-collaborators-list/index.js
@@ -1,4 +1,4 @@
-// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+// Copyright © 2022 The Things Network Foundation, The Things Industries B.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,65 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react'
-import { Container, Row, Col } from 'react-grid-system'
-import bind from 'autobind-decorator'
+import connect from './connect'
+import ApplicationCollaboratorsList from './application-collaborators-list'
 
-import PAGE_SIZES from '@ttn-lw/constants/page-sizes'
+const ConnectedApplicationCollaboratorsList = connect(ApplicationCollaboratorsList)
 
-import IntlHelmet from '@ttn-lw/lib/components/intl-helmet'
-
-import CollaboratorsTable from '@console/containers/collaborators-table'
-
-import sharedMessages from '@ttn-lw/lib/shared-messages'
-import PropTypes from '@ttn-lw/lib/prop-types'
-
-import { getCollaboratorsList } from '@console/store/actions/collaborators'
-
-import {
-  selectCollaborators,
-  selectCollaboratorsTotalCount,
-  selectCollaboratorsFetching,
-} from '@console/store/selectors/collaborators'
-
-export default class ApplicationCollaborators extends React.Component {
-  static propTypes = {
-    match: PropTypes.match.isRequired,
-  }
-
-  constructor(props) {
-    super(props)
-
-    const { appId } = props.match.params
-    this.getCollaboratorsList = filters => getCollaboratorsList('application', appId, filters)
-  }
-
-  @bind
-  baseDataSelector(state) {
-    const { appId } = this.props.match.params
-    const id = { id: appId }
-
-    return {
-      collaborators: selectCollaborators(state, id),
-      fetching: selectCollaboratorsFetching(state),
-      totalCount: selectCollaboratorsTotalCount(state, id),
-    }
-  }
-
-  render() {
-    return (
-      <Container>
-        <Row>
-          <IntlHelmet title={sharedMessages.collaborators} />
-          <Col>
-            <CollaboratorsTable
-              pageSize={PAGE_SIZES.REGULAR}
-              baseDataSelector={this.baseDataSelector}
-              getItemsAction={this.getCollaboratorsList}
-            />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
-}
+export { ConnectedApplicationCollaboratorsList as default, ApplicationCollaboratorsList }
