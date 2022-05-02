32a86c94e0f63810d57f7f97cff6d88b1e07b132
Darya Plotnytska <d.plotnytska.s@gmail.com>
2022-05-02 11:37:53 +0200

    console: Update session on 'as.up.join.forward'

--- pkg/webui/console/store/middleware/logics/devices.js
+++ pkg/webui/console/store/middleware/logics/devices.js
@@ -12,14 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { createLogic } from 'redux-logic'
+import { defineMessage } from 'react-intl'
+
 import tts from '@console/api/tts'
+
+import toast from '@ttn-lw/components/toast'
 
 import createRequestLogic from '@ttn-lw/lib/store/logics/create-request-logic'
 
 import * as devices from '@console/store/actions/devices'
 import * as deviceTemplateFormats from '@console/store/actions/device-template-formats'
 
+import { selectDeviceByIds } from '@console/store/selectors/devices'
+
 import createEventsConnectLogics from './events'
+
+const m = defineMessage({
+  joinSuccess: 'The device has successfully joined the network',
+})
 
 const getDeviceLogic = createRequestLogic({
   type: devices.GET_DEV,
@@ -92,11 +103,45 @@ const getDeviceTemplateFormatsLogic = createRequestLogic({
   },
 })
 
+const getDeviceSessionLogic = createLogic({
+  type: devices.GET_DEVICE_EVENT_MESSAGE_SUCCESS,
+  process: async ({ action, getState }, dispatch, done) => {
+    const { event, id } = action
+    const appId = id.application_ids.application_id
+    const sessionSelector = ['pending_session', 'session']
+
+    const device = selectDeviceByIds(getState(), appId, id.device_id)
+
+    if (event.name === 'as.up.join.forward') {
+      const dev = await tts.Applications.Devices.getById(appId, id.device_id, sessionSelector)
+      dispatch(devices.getDeviceSuccess(dev))
+    }
+
+    if (
+      (event.name === 'ns.up.data.process' || event.name === 'as.up.data.process') &&
+      device.pending_session !== null
+    ) {
+      const dev = await tts.Applications.Devices.getById(appId, id.device_id, sessionSelector)
+      dispatch(devices.getDeviceSuccess(dev))
+      if (!('pending_session' in dev) && 'session' in dev) {
+        toast({
+          title: id.device_id,
+          message: m.joinSuccess,
+          type: toast.types.INFO,
+        })
+      }
+    }
+
+    done()
+  },
+})
+
 export default [
   getDevicesListLogic,
   getDeviceTemplateFormatsLogic,
   getDeviceLogic,
   resetDeviceLogic,
   updateDeviceLogic,
+  getDeviceSessionLogic,
   ...createEventsConnectLogics(devices.SHARED_NAME, 'devices', tts.Applications.Devices.openStream),
 ]
