310e0f88af05f19b1fca5f9d4e49edb5a4897ccf
Hylke Visser <hylke@thethingsindustries.com>
2022-05-25 09:15:26 +0200

    is: Add package documentation to migrations

--- pkg/identityserver/store/migrations/migrations.go
+++ pkg/identityserver/store/migrations/migrations.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package migrations contains Identity Server store migrations.
 package migrations
 
 import (
