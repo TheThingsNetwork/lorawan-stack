92bb8ca0a7573d3d5ca93927574334ca020474d5
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2022-05-30 11:43:14 +0200

    console: Impove API key table layout

--- pkg/webui/console/containers/api-keys-table/index.js
+++ pkg/webui/console/containers/api-keys-table/index.js
@@ -12,14 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { Component } from 'react'
-import { defineMessages } from 'react-intl'
+import React from 'react'
+import { defineMessages, useIntl } from 'react-intl'
 
 import Tag from '@ttn-lw/components/tag'
 import TagGroup from '@ttn-lw/components/tag/group'
 
 import FetchTable from '@ttn-lw/containers/fetch-table'
 
+import DateTime from '@ttn-lw/lib/components/date-time'
 import Message from '@ttn-lw/lib/components/message'
 
 import PropTypes from '@ttn-lw/lib/prop-types'
@@ -32,67 +33,72 @@ const m = defineMessages({
   grantedRights: 'Granted Rights',
 })
 
-const formatRight = right =>
-  right
-    .split('_')
-    .slice(1)
-    .map(r => r.charAt(0) + r.slice(1).toLowerCase())
-    .join(' ')
+const RIGHT_TAG_MAX_WIDTH = 160
 
-const RIGHT_TAG_MAX_WIDTH = 140
+const ApiKeysTable = props => {
+  const { pageSize, baseDataSelector, getItemsAction } = props
+  const intl = useIntl()
 
-const headers = [
-  {
-    name: 'id',
-    displayName: m.keyId,
-    width: 30,
-    render: id => <span className={style.keyId}>{id}</span>,
-  },
-  {
-    name: 'name',
-    displayName: sharedMessages.name,
-    width: 30,
-  },
-  {
-    name: 'rights',
-    displayName: m.grantedRights,
-    width: 40,
-    render: (rights = []) => {
-      if (rights.length > 0) {
-        const tags = rights.map(r => (
-          <Tag className={style.rightTag} content={formatRight(r)} key={r} />
-        ))
-
-        return (
-          <TagGroup className={style.rightTagGroup} tagMaxWidth={RIGHT_TAG_MAX_WIDTH} tags={tags} />
-        )
-      }
-
-      return <Message className={style.none} content={sharedMessages.none} lowercase />
+  const headers = [
+    {
+      name: 'id',
+      displayName: m.keyId,
+      width: 20,
+      sortKey: 'api_key_id',
+      render: id => <span className={style.keyId}>{id}</span>,
+      sortable: true,
     },
-  },
-]
+    {
+      name: 'name',
+      displayName: sharedMessages.name,
+      width: 20,
+      sortable: true,
+    },
+    {
+      name: 'rights',
+      displayName: m.grantedRights,
+      width: 50,
+      sortable: true,
+      render: (rights = []) => {
+        if (rights.length === 0) {
+          return <Message className={style.none} content={sharedMessages.none} lowercase />
+        }
+        const tags = rights.map(r => {
+          let rightLabel = intl.formatMessage({ id: `enum:${r}` })
+          rightLabel = rightLabel.charAt(0).toUpperCase() + rightLabel.slice(1)
+          return <Tag className={style.rightTag} content={rightLabel} key={r} />
+        })
 
-export default class ApiKeysTable extends Component {
-  static propTypes = {
-    baseDataSelector: PropTypes.func.isRequired,
-    getItemsAction: PropTypes.func.isRequired,
-    pageSize: PropTypes.number.isRequired,
-  }
+        return <TagGroup tagMaxWidth={RIGHT_TAG_MAX_WIDTH} tags={tags} />
+      },
+    },
+    {
+      name: 'created_at',
+      displayName: sharedMessages.createdAt,
+      sortable: true,
+      width: 10,
+      render: date => <DateTime.Relative value={date} />,
+    },
+  ]
 
-  render() {
-    const { pageSize, baseDataSelector, getItemsAction } = this.props
-
-    return (
-      <FetchTable
-        entity="keys"
-        headers={headers}
-        addMessage={sharedMessages.addApiKey}
-        pageSize={pageSize}
-        baseDataSelector={baseDataSelector}
-        getItemsAction={getItemsAction}
-        tableTitle={<Message content={sharedMessages.apiKeys} />}
-      />
-    )
-  }
+  return (
+    <FetchTable
+      entity="keys"
+      defaultOrder="-created_at"
+      headers={headers}
+      addMessage={sharedMessages.addApiKey}
+      pageSize={pageSize}
+      baseDataSelector={baseDataSelector}
+      getItemsAction={getItemsAction}
+      tableTitle={<Message content={sharedMessages.apiKeys} />}
+    />
+  )
 }
+
+ApiKeysTable.propTypes = {
+  baseDataSelector: PropTypes.func.isRequired,
+  getItemsAction: PropTypes.func.isRequired,
+  pageSize: PropTypes.number.isRequired,
+}
+
+export default ApiKeysTable
