f29c68a5e0e13fd6c0b99e7fdcc13b1dcda3f816
Hylke Visser <hylke@thethingsindustries.com>
2022-05-30 09:29:22 +0200

    api: Generate CLI flags for UpdateNotificationStatusRequest

--- pkg/ttnpb/notification_service_flags.pb.go
+++ pkg/ttnpb/notification_service_flags.pb.go
@@ -58,3 +58,41 @@ func (m *ListNotificationsRequest) SetFromFlags(flags *pflag.FlagSet, prefix str
 	}
 	return paths, nil
 }
+
+// AddSetFlagsForUpdateNotificationStatusRequest adds flags to select fields in UpdateNotificationStatusRequest.
+func AddSetFlagsForUpdateNotificationStatusRequest(flags *pflag.FlagSet, prefix string, hidden bool) {
+	AddSetFlagsForUserIdentifiers(flags, flagsplugin.Prefix("receiver-ids", prefix), hidden)
+	flags.AddFlag(flagsplugin.NewStringSliceFlag(flagsplugin.Prefix("ids", prefix), "", flagsplugin.WithHidden(hidden)))
+	flags.AddFlag(flagsplugin.NewStringFlag(flagsplugin.Prefix("status", prefix), flagsplugin.EnumValueDesc(NotificationStatus_value), flagsplugin.WithHidden(hidden)))
+}
+
+// SetFromFlags sets the UpdateNotificationStatusRequest message from flags.
+func (m *UpdateNotificationStatusRequest) SetFromFlags(flags *pflag.FlagSet, prefix string) (paths []string, err error) {
+	if changed := flagsplugin.IsAnyPrefixSet(flags, flagsplugin.Prefix("receiver_ids", prefix)); changed {
+		if m.ReceiverIds == nil {
+			m.ReceiverIds = &UserIdentifiers{}
+		}
+		if setPaths, err := m.ReceiverIds.SetFromFlags(flags, flagsplugin.Prefix("receiver_ids", prefix)); err != nil {
+			return nil, err
+		} else {
+			paths = append(paths, setPaths...)
+		}
+	}
+	if val, changed, err := flagsplugin.GetStringSlice(flags, flagsplugin.Prefix("ids", prefix)); err != nil {
+		return nil, err
+	} else if changed {
+		m.Ids = val
+		paths = append(paths, flagsplugin.Prefix("ids", prefix))
+	}
+	if val, changed, err := flagsplugin.GetString(flags, flagsplugin.Prefix("status", prefix)); err != nil {
+		return nil, err
+	} else if changed {
+		enumValue, err := flagsplugin.SetEnumString(val, NotificationStatus_value)
+		if err != nil {
+			return nil, err
+		}
+		m.Status = NotificationStatus(enumValue)
+		paths = append(paths, flagsplugin.Prefix("status", prefix))
+	}
+	return paths, nil
+}
