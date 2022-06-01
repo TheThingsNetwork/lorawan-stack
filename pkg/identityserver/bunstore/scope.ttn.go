8ae02e9af1880e435ab2abfe3a06c38c4f726524
Hylke Visser <hylke@thethingsindustries.com>
2022-06-01 14:07:10 +0200

    is: Move TTN scopes to scope.ttn.go

--- pkg/identityserver/bunstore/scope.ttn.go
+++ pkg/identityserver/bunstore/scope.ttn.go
@@ -13,3 +13,15 @@
 // limitations under the License.
 
 package store
+
+import (
+	"context"
+
+	"github.com/uptrace/bun"
+)
+
+func selectWithContext(ctx context.Context) func(*bun.SelectQuery) *bun.SelectQuery {
+	return func(q *bun.SelectQuery) *bun.SelectQuery {
+		return q
+	}
+}
