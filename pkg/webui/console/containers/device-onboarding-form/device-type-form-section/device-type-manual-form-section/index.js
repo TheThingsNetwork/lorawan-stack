808a1881a3d549fc1ab8f23c59d3532f0b8c007f
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2022-06-21 15:36:12 +0200

    console: Add new form scaffold

---
+++ pkg/webui/console/containers/device-onboarding-form/device-type-form-section/device-type-manual-form-section/index.js
@@ -0,0 +1,38 @@
+// Copyright © 2022 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React, { useEffect } from 'react'
+import { merge } from 'lodash'
+
+import { useFormContext } from '@ttn-lw/components/form'
+
+import { subtractObject } from '../../utils'
+
+const initialValues = { foo: 'bar' }
+
+const DeviceTypeManualFormSection = props => {
+  const { setValues } = useFormContext()
+
+  useEffect(() => {
+    // Set the section's initial values on mount.
+    setTimeout(() => setValues(values => merge(values, initialValues)))
+
+    // Remove initial values on on mount (on next tick).
+    return () => setValues(values => subtractObject(values, initialValues))
+  }, [setValues])
+
+  return <span>Device Type Manual Form Section</span>
+}
+
+export default DeviceTypeManualFormSection
