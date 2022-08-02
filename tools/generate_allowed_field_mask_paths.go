f73cf4c02c67d5f72b727d8f7f5dffcc8aab5193
Krishna Iyer Easwaran <krishna@thethingsindustries.com>
2022-08-02 10:36:57 +0200

    console: Force the console to use IS for CAC

--- tools/generate_allowed_field_mask_paths.go
+++ tools/generate_allowed_field_mask_paths.go
@@ -24,6 +24,8 @@ import (
 	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
 )
 
+var exceptions = make(map[string]func([]string) []string, 0)
+
 func main() {
 	messagesFile := "sdk/js/generated/allowed-field-mask-paths.json"
 	if len(os.Args) == 2 {
@@ -33,7 +35,12 @@ func main() {
 	data, err := json.MarshalIndent(func() map[string][]string {
 		ret := make(map[string][]string, len(ttnpb.RPCFieldMaskPaths))
 		for rpc, v := range ttnpb.RPCFieldMaskPaths {
-			ret[rpc] = v.Allowed
+			f, ok := exceptions[rpc]
+			if ok {
+				ret[rpc] = f(v.Allowed)
+			} else {
+				ret[rpc] = v.Allowed
+			}
 		}
 		return ret
 	}(), "", "  ")
@@ -43,5 +50,21 @@ func main() {
 
 	if err = os.WriteFile(messagesFile, data, 0o644); err != nil {
 		panic(err)
+	}
+}
+
+func init() {
+	var claimAuthenticationCodePaths = []string{
+		"claim_authentication_code",
+		"claim_authentication_code.value",
+		"claim_authentication_code.valid_from",
+		"claim_authentication_code.valid_to",
+	}
+	// Register exceptions.
+	exceptions["/ttn.lorawan.v3.JsEndDeviceRegistry/Get"] = func(allowed []string) []string {
+		return ttnpb.ExcludeFields(allowed, claimAuthenticationCodePaths...)
+	}
+	exceptions["/ttn.lorawan.v3.JsEndDeviceRegistry/Set"] = func(allowed []string) []string {
+		return ttnpb.ExcludeFields(allowed, claimAuthenticationCodePaths...)
 	}
 }
