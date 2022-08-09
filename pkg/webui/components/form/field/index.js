1d8585dc4137b4127ef00905aef915cc4b2339cd
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2022-08-09 15:36:08 +0200

    console,account: Merge composite fields instead of only spreading them

--- pkg/webui/components/form/field/index.js
+++ pkg/webui/components/form/field/index.js
@@ -14,7 +14,7 @@
 
 import React, { useCallback, useEffect, useMemo } from 'react'
 import classnames from 'classnames'
-import { isPlainObject, pick, isEmpty, at, compact, get } from 'lodash'
+import { isPlainObject, pick, isEmpty, at, compact, get, merge } from 'lodash'
 
 import Message from '@ttn-lw/lib/components/message'
 
@@ -69,7 +69,7 @@ const extractValue = value => {
 }
 
 const defaultValueSetter = ({ setFieldValue, setValues }, { name, names, value }) =>
-  names.length > 1 ? setValues(values => ({ ...values, ...value })) : setFieldValue(name, value)
+  names.length > 1 ? setValues(values => merge({}, values, value)) : setFieldValue(name, value)
 
 const FormField = props => {
   const {
