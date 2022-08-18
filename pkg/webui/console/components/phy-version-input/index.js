2cbb5b15fc44979182295e37ce6ccd79c174d35a
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2022-08-18 19:23:20 +0200

    console: Refactor phy version input

--- pkg/webui/console/components/phy-version-input/index.js
+++ pkg/webui/console/components/phy-version-input/index.js
@@ -21,28 +21,8 @@ import PropTypes from '@ttn-lw/lib/prop-types'
 import {
   LORAWAN_PHY_VERSIONS,
   parseLorawanMacVersion,
-  PHY_V1_0,
-  PHY_V1_0_1,
-  PHY_V1_0_2_REV_A,
-  PHY_V1_0_2_REV_B,
-  PHY_V1_0_3_REV_A,
-  PHY_V1_1_REV_B,
-  PHY_V1_1_REV_A,
-  RP002_V1_0_0,
-  RP002_V1_0_1,
-  RP002_V1_0_2,
-  RP002_V1_0_3,
+  LORAWAN_VERSION_PAIRS,
 } from '@console/lib/device-utils'
-
-const lorawanVersionPairs = {
-  100: [PHY_V1_0],
-  101: [PHY_V1_0_1],
-  102: [PHY_V1_0_2_REV_A, PHY_V1_0_2_REV_B],
-  103: [PHY_V1_0_3_REV_A],
-  104: LORAWAN_PHY_VERSIONS,
-  110: [PHY_V1_1_REV_A, PHY_V1_1_REV_B, RP002_V1_0_0, RP002_V1_0_1, RP002_V1_0_2, RP002_V1_0_3],
-  0: LORAWAN_PHY_VERSIONS,
-}
 
 const PhyVersionInput = props => {
   const { lorawanVersion, onChange, disabled, value, ...rest } = props
@@ -52,7 +32,7 @@ const PhyVersionInput = props => {
 
   React.useEffect(() => {
     const options =
-      lorawanVersionPairs[parseLorawanMacVersion(lorawanVersion)] || LORAWAN_PHY_VERSIONS
+      LORAWAN_VERSION_PAIRS[parseLorawanMacVersion(lorawanVersion)] || LORAWAN_PHY_VERSIONS
     if (options.length === 1) {
       onChange(options[0].value)
     } else if (lorawanVersion !== lorawanVersionRef.current) {
