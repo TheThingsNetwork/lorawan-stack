a9d5f05ba753ef3cff480bf422a89aec38840b70
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2022-09-27 14:47:09 +0200

    console: Fix crashes after deleting organizations

--- cypress/integration/smoke/organizations/index.js
+++ cypress/integration/smoke/organizations/index.js
@@ -14,5 +14,6 @@
 
 import createTests from './create'
 import subpageTests from './subpages'
+import deleteTests from './delete'
 
-export default [...createTests, ...subpageTests]
+export default [...createTests, ...subpageTests, ...deleteTests]
