249395b26628206b52b3e29822b6e0726d4541d1
Krishna Iyer Easwaran <krishna@thethingsindustries.com>
2022-09-28 10:31:51 +0200

    all: Add supporting code

--- tools/mage/certificates.go
+++ tools/mage/certificates.go
@@ -15,7 +15,9 @@
 package ttnmage
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/cloudflare/cfssl/config"
 	"github.com/cloudflare/cfssl/csr"
@@ -23,6 +25,8 @@ import (
 	"github.com/cloudflare/cfssl/signer"
 	"github.com/cloudflare/cfssl/signer/universal"
 )
+
+var gatewayCertsDir = "gateway-certs"
 
 // Certificates generates certificates for development.
 func (Dev) Certificates() error {
@@ -79,6 +83,71 @@ func (Dev) Certificates() error {
 		return err
 	}
 	if err = os.WriteFile("key.pem", key, 0o644); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GenGatewayCA generates a certificate authority to sign gateway certificates.
+func (Dev) GenGatewayCA() error {
+	caReq := csr.CertificateRequest{
+		Names:      names,
+		KeyRequest: csr.NewKeyRequest(),
+	}
+	caCert, _, caKey, err := initca.New(&caReq)
+	if err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(filepath.Join(devDir, gatewayCertsDir), 0o755); err != nil {
+		return err
+	}
+	if err = os.WriteFile(filepath.Join(devDir, gatewayCertsDir, "ca.pem"), caCert, 0o644); err != nil {
+		return err
+	}
+	if err = os.WriteFile(filepath.Join(devDir, gatewayCertsDir, "ca-key.pem"), caKey, 0o644); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GenGatewayCerts generates a client certificates for the EUI.
+func (Dev) GenGatewayCerts() error {
+	eui := os.Getenv("GATEWAY_EUI")
+	certReq := csr.CertificateRequest{
+		CN:         eui,
+		Names:      names, // Use the same names.
+		KeyRequest: csr.NewKeyRequest(),
+	}
+	g := &csr.Generator{Validator: func(req *csr.CertificateRequest) error { return nil }}
+	csr, key, err := g.ProcessRequest(&certReq)
+	if err != nil {
+		return err
+	}
+	signReq := signer.SignRequest{
+		Request: string(csr),
+		Hosts:   certReq.Hosts,
+	}
+	s, err := universal.NewSigner(universal.Root{
+		Config: map[string]string{
+			"cert-file": filepath.Join(devDir, gatewayCertsDir, "ca.pem"),
+			"key-file":  filepath.Join(devDir, gatewayCertsDir, "ca-key.pem"),
+		},
+	}, &config.Signing{
+		Profiles: map[string]*config.SigningProfile{},
+		Default:  config.DefaultConfig(),
+	})
+	if err != nil {
+		return err
+	}
+	cert, err := s.Sign(signReq)
+	if err != nil {
+		return err
+	}
+	if err = os.WriteFile(filepath.Join(devDir, gatewayCertsDir, fmt.Sprintf("%s.crt", eui)), cert, 0o644); err != nil {
+		return err
+	}
+	if err = os.WriteFile(filepath.Join(devDir, gatewayCertsDir, fmt.Sprintf("%s.key", eui)), key, 0o644); err != nil {
 		return err
 	}
 	return nil
