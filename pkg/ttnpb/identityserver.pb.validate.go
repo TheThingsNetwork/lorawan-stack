879753b5640fb7ac12b35610a027e03a4b58156b
Krishna Iyer Easwaran <krishna@thethingsindustries.com>
2022-09-28 09:10:47 +0200

    api: Add GatewayToken message

--- pkg/ttnpb/identityserver.pb.validate.go
+++ pkg/ttnpb/identityserver.pb.validate.go
@@ -67,7 +67,7 @@ func (m *AuthInfoResponse) ValidateFields(paths ...string) error {
 		case "access_method":
 			if len(subs) == 0 {
 				subs = []string{
-					"api_key", "oauth_access_token", "user_session",
+					"api_key", "oauth_access_token", "user_session", "gateway_token",
 				}
 			}
 			for name, subs := range _processPaths(subs) {
@@ -115,6 +115,22 @@ func (m *AuthInfoResponse) ValidateFields(paths ...string) error {
 						if err := v.ValidateFields(subs...); err != nil {
 							return AuthInfoResponseValidationError{
 								field:  "user_session",
+								reason: "embedded message failed validation",
+								cause:  err,
+							}
+						}
+					}
+
+				case "gateway_token":
+					w, ok := m.AccessMethod.(*AuthInfoResponse_GatewayToken_)
+					if !ok || w == nil {
+						continue
+					}
+
+					if v, ok := interface{}(m.GetGatewayToken()).(interface{ ValidateFields(...string) error }); ok {
+						if err := v.ValidateFields(subs...); err != nil {
+							return AuthInfoResponseValidationError{
+								field:  "gateway_token",
 								reason: "embedded message failed validation",
 								cause:  err,
 							}
@@ -618,6 +634,109 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = AuthInfoResponse_APIKeyAccessValidationError{}
+
+// ValidateFields checks the field values on AuthInfoResponse_GatewayToken with
+// the rules defined in the proto definition for this message. If any rules
+// are violated, an error is returned.
+func (m *AuthInfoResponse_GatewayToken) ValidateFields(paths ...string) error {
+	if m == nil {
+		return nil
+	}
+
+	if len(paths) == 0 {
+		paths = AuthInfoResponse_GatewayTokenFieldPathsNested
+	}
+
+	for name, subs := range _processPaths(append(paths[:0:0], paths...)) {
+		_ = subs
+		switch name {
+		case "gateway_ids":
+
+			if m.GetGatewayIds() == nil {
+				return AuthInfoResponse_GatewayTokenValidationError{
+					field:  "gateway_ids",
+					reason: "value is required",
+				}
+			}
+
+			if v, ok := interface{}(m.GetGatewayIds()).(interface{ ValidateFields(...string) error }); ok {
+				if err := v.ValidateFields(subs...); err != nil {
+					return AuthInfoResponse_GatewayTokenValidationError{
+						field:  "gateway_ids",
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		case "rights":
+
+		default:
+			return AuthInfoResponse_GatewayTokenValidationError{
+				field:  name,
+				reason: "invalid field path",
+			}
+		}
+	}
+	return nil
+}
+
+// AuthInfoResponse_GatewayTokenValidationError is the validation error
+// returned by AuthInfoResponse_GatewayToken.ValidateFields if the designated
+// constraints aren't met.
+type AuthInfoResponse_GatewayTokenValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e AuthInfoResponse_GatewayTokenValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e AuthInfoResponse_GatewayTokenValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e AuthInfoResponse_GatewayTokenValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e AuthInfoResponse_GatewayTokenValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e AuthInfoResponse_GatewayTokenValidationError) ErrorName() string {
+	return "AuthInfoResponse_GatewayTokenValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e AuthInfoResponse_GatewayTokenValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sAuthInfoResponse_GatewayToken.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = AuthInfoResponse_GatewayTokenValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = AuthInfoResponse_GatewayTokenValidationError{}
 
 // ValidateFields checks the field values on IsConfiguration_UserRegistration
 // with the rules defined in the proto definition for this message. If any
