7988f6bea59cae7b0ff242dfd14241158d4501aa
happyRip <aleksander.borowski.01@gmail.com>
2022-09-30 17:44:19 +0200

    util: Update `UpdateTimestamps()` method doc comment

--- pkg/ttnpb/end_device.go
+++ pkg/ttnpb/end_device.go
@@ -1553,8 +1553,7 @@ func (m *EndDevice) ExtractRequestFields(dst map[string]interface{}) {
 	m.GetIds().ExtractRequestFields(dst)
 }
 
-// Utility
-
+// UpdateTimestamps sets earliest CreatedAt and latest UpdatedAt timestamps for EndDevice based on src device.
 func (d *EndDevice) UpdateTimestamps(src *EndDevice) {
 	if d.CreatedAt == nil || (src.CreatedAt != nil && StdTime(src.CreatedAt).Before(*StdTime(d.CreatedAt))) {
 		d.CreatedAt = src.CreatedAt
