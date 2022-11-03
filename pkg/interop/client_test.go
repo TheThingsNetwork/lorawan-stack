f4b04995caabe093ad194fbd01ebf7340c8d585c
Johan Stokking <johan@stokking.nl>
2022-11-03 15:09:30 +0100

    all: Support multiple Join Server interop registrations on /64 prefix

--- pkg/interop/client_test.go
+++ pkg/interop/client_test.go
@@ -20,6 +20,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/smartystreets/assertions"
 	"go.thethings.network/lorawan-stack/v3/pkg/config"
@@ -653,6 +654,181 @@ func TestHandleJoinRequest(t *testing.T) { //nolint:paralleltest
 
 			srv := tc.NewServer(a)
 			defer srv.Close()
+
+			cl, err := NewClient(ctx, config.InteropClient{
+				ConfigSource: "directory",
+				Directory:    "testdata/client",
+			}, test.HTTPClientProvider, SelectorNetworkServer)
+			if !a.So(err, should.BeNil) {
+				t.Fatalf("Failed to create new client: %s", err)
+			}
+
+			res, err := cl.HandleJoinRequest(ctx, tc.NetID, tc.Request)
+			if a.So(tc.ErrorAssertion(a, err), should.BeTrue) {
+				a.So(tc.ResponseAssertion(a, res), should.BeTrue)
+			} else if err != nil {
+				t.Errorf("Received unexpected error: %v", errors.Stack(err))
+			}
+		})
+	}
+}
+
+func TestJoinServerRace(t *testing.T) { //nolint:paralleltest
+	for _, tc := range []struct { //nolint:paralleltest
+		Name              string
+		NewServers        func(*assertions.Assertion) []*httptest.Server
+		NetID             types.NetID
+		Request           *ttnpb.JoinRequest
+		ResponseAssertion func(*assertions.Assertion, *ttnpb.JoinResponse) bool
+		ErrorAssertion    func(*assertions.Assertion, error) bool
+	}{
+		{
+			Name: "Slowest server successful",
+			NewServers: func(a *assertions.Assertion) []*httptest.Server {
+				return []*httptest.Server{
+					newTLSServer(9183, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						time.Sleep(test.Delay << 4)
+						test.Must(nil, json.NewEncoder(w).Encode(map[string]interface{}{
+							"ProtocolVersion": "1.0",
+							"TransactionID":   0,
+							"MessageType":     "JoinAns",
+							"ReceiverToken":   "01",
+							"SenderID":        "70B3D57ED0000000",
+							"ReceiverID":      "42FFFF",
+							"PHYPayload":      "204D675073BB4153B23653EFA82C1F3A49E19C2A8696C9A34BF492674779E4BEFA",
+							"Result": map[string]interface{}{
+								"ResultCode": "Success",
+							},
+							"Lifetime": 0,
+							"NwkSKey": map[string]interface{}{
+								"KEKLabel": "ns:42ffff",
+								"AESKey":   "EB56FE6681999F25D548CFEDD4A6528B331BB5ADE1CAF17F",
+							},
+							"AppSKey": map[string]interface{}{
+								"KEKLabel": "as:010042",
+								"AESKey":   "2A195CC93CA54AD82CFB36C83D91450F3D2D523556F13E69",
+							},
+							"SessionKeyID": "016BFA7BAD4756346A674981E75CDBDC",
+						}))
+					})),
+					newTLSServer(9184, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						test.Must(nil, json.NewEncoder(w).Encode(map[string]interface{}{
+							"ProtocolVersion": "1.0",
+							"TransactionID":   0.0,
+							"MessageType":     "JoinAns",
+							"SenderID":        "EC656E0000000001",
+							"ReceiverID":      "42FFFF",
+							"Result": map[string]interface{}{
+								"ResultCode": "MICFailed",
+							},
+						}))
+					})),
+				}
+			},
+			NetID: types.NetID{0x42, 0xff, 0xff},
+			Request: &ttnpb.JoinRequest{
+				SelectedMacVersion: ttnpb.MACVersion_MAC_V1_0_3,
+				DevAddr:            types.DevAddr{0x01, 0x02, 0x03, 0x04}.Bytes(),
+				RxDelay:            ttnpb.RxDelay_RX_DELAY_5,
+				DownlinkSettings:   &ttnpb.DLSettings{},
+				Payload: &ttnpb.Message{
+					Payload: &ttnpb.Message_JoinRequestPayload{
+						JoinRequestPayload: &ttnpb.JoinRequestPayload{
+							JoinEui: types.EUI64{0xec, 0x65, 0x6e, 0x00, 0x00, 0x00, 0x00, 0x01}.Bytes(),
+							DevEui:  types.EUI64{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}.Bytes(),
+						},
+					},
+				},
+				RawPayload: []byte{0x00, 0x00, 0x00, 0x00, 0xd0, 0x7e, 0xd5, 0xb3, 0x70, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00, 0x00, 0x38, 0x51, 0xf0, 0xb6}, //nolint:lll
+			},
+			ResponseAssertion: func(a *assertions.Assertion, resp *ttnpb.JoinResponse) bool {
+				return a.So(resp, should.Resemble, &ttnpb.JoinResponse{
+					RawPayload: []byte{0x20, 0x4d, 0x67, 0x50, 0x73, 0xbb, 0x41, 0x53, 0xb2, 0x36, 0x53, 0xef, 0xa8, 0x2c, 0x1f, 0x3a, 0x49, 0xe1, 0x9c, 0x2a, 0x86, 0x96, 0xc9, 0xa3, 0x4b, 0xf4, 0x92, 0x67, 0x47, 0x79, 0xe4, 0xbe, 0xfa}, //nolint:lll
+					SessionKeys: &ttnpb.SessionKeys{
+						SessionKeyId: []byte{0x01, 0x6b, 0xfa, 0x7b, 0xad, 0x47, 0x56, 0x34, 0x6a, 0x67, 0x49, 0x81, 0xe7, 0x5c, 0xdb, 0xdc}, //nolint:lll
+						FNwkSIntKey: &ttnpb.KeyEnvelope{
+							KekLabel:     "ns:42ffff",
+							EncryptedKey: []byte{0xeb, 0x56, 0xfe, 0x66, 0x81, 0x99, 0x9f, 0x25, 0xd5, 0x48, 0xcf, 0xed, 0xd4, 0xa6, 0x52, 0x8b, 0x33, 0x1b, 0xb5, 0xad, 0xe1, 0xca, 0xf1, 0x7f}, //nolint:lll
+						},
+						AppSKey: &ttnpb.KeyEnvelope{
+							KekLabel:     "as:010042",
+							EncryptedKey: []byte{0x2a, 0x19, 0x5c, 0xc9, 0x3c, 0xa5, 0x4a, 0xd8, 0x2c, 0xfb, 0x36, 0xc8, 0x3d, 0x91, 0x45, 0x0f, 0x3d, 0x2d, 0x52, 0x35, 0x56, 0xf1, 0x3e, 0x69}, //nolint:lll
+						},
+					},
+					Lifetime: ttnpb.ProtoDurationPtr(0),
+				})
+			},
+			ErrorAssertion: func(a *assertions.Assertion, err error) bool {
+				return a.So(err, should.BeNil)
+			},
+		},
+		{
+			Name: "Fastest server error",
+			NewServers: func(a *assertions.Assertion) []*httptest.Server {
+				return []*httptest.Server{
+					newTLSServer(9183, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						// This is the slowest response.
+						time.Sleep(test.Delay << 4)
+						test.Must(nil, json.NewEncoder(w).Encode(map[string]interface{}{
+							"ProtocolVersion": "1.0",
+							"TransactionID":   0.0,
+							"MessageType":     "JoinAns",
+							"SenderID":        "EC656E0000000001",
+							"ReceiverID":      "42FFFF",
+							"Result": map[string]interface{}{
+								"ResultCode": "JoinReqFailed",
+							},
+						}))
+					})),
+					newTLSServer(9184, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						test.Must(nil, json.NewEncoder(w).Encode(map[string]interface{}{
+							"ProtocolVersion": "1.0",
+							"TransactionID":   0.0,
+							"MessageType":     "JoinAns",
+							"SenderID":        "EC656E0000000001",
+							"ReceiverID":      "42FFFF",
+							"Result": map[string]interface{}{
+								"ResultCode": "MICFailed",
+							},
+						}))
+					})),
+				}
+			},
+			NetID: types.NetID{0x42, 0xff, 0xff},
+			Request: &ttnpb.JoinRequest{
+				SelectedMacVersion: ttnpb.MACVersion_MAC_V1_0_3,
+				DevAddr:            types.DevAddr{0x01, 0x02, 0x03, 0x04}.Bytes(),
+				RxDelay:            ttnpb.RxDelay_RX_DELAY_5,
+				DownlinkSettings:   &ttnpb.DLSettings{},
+				Payload: &ttnpb.Message{
+					Payload: &ttnpb.Message_JoinRequestPayload{
+						JoinRequestPayload: &ttnpb.JoinRequestPayload{
+							JoinEui: types.EUI64{0xec, 0x65, 0x6e, 0x00, 0x00, 0x00, 0x00, 0x01}.Bytes(),
+							DevEui:  types.EUI64{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}.Bytes(),
+						},
+					},
+				},
+				RawPayload: []byte{0x00, 0x00, 0x00, 0x00, 0xd0, 0x7e, 0xd5, 0xb3, 0x70, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00, 0x00, 0x38, 0x51, 0xf0, 0xb6}, //nolint:lll
+			},
+			ResponseAssertion: func(a *assertions.Assertion, resp *ttnpb.JoinResponse) bool {
+				return a.So(resp, should.BeNil)
+			},
+			ErrorAssertion: func(a *assertions.Assertion, err error) bool {
+				return a.So(err, should.HaveSameErrorDefinitionAs, ErrMIC)
+			},
+		},
+	} {
+		tc := tc
+		t.Run(tc.Name, func(t *testing.T) {
+			a := assertions.New(t)
+
+			ctx := test.Context()
+			ctx = log.NewContext(ctx, test.GetLogger(t))
+
+			srvs := tc.NewServers(a)
+			for _, srv := range srvs {
+				defer srv.Close() //nolint:revive
+			}
 
 			cl, err := NewClient(ctx, config.InteropClient{
 				ConfigSource: "directory",
