7ebb67cdbd58454a119a36d00a6753d5315f6487
Adrian-Stefan Mares <adrian.mares@thethingsindustries.com>
2022-12-20 16:54:34 +0100

    all: Decouple pkg/webmiddleware tests

--- pkg/webmiddleware/cookie_auth_test.go
+++ pkg/webmiddleware/cookie_auth_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package webmiddleware
+package webmiddleware_test
 
 import (
 	"net/http"
@@ -21,8 +21,9 @@ import (
 
 	"github.com/gorilla/securecookie"
 	"github.com/smartystreets/assertions"
-	"github.com/smartystreets/assertions/should"
 	"go.thethings.network/lorawan-stack/v3/pkg/auth"
+	"go.thethings.network/lorawan-stack/v3/pkg/util/test/assertions/should"
+	. "go.thethings.network/lorawan-stack/v3/pkg/webmiddleware"
 )
 
 func TestCookieAuth(t *testing.T) {
--- pkg/webmiddleware/security_headers_test.go
+++ pkg/webmiddleware/security_headers_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package webmiddleware
+package webmiddleware_test
 
 import (
 	"net/http"
@@ -20,7 +20,8 @@ import (
 	"testing"
 
 	"github.com/smartystreets/assertions"
-	"github.com/smartystreets/assertions/should"
+	"go.thethings.network/lorawan-stack/v3/pkg/util/test/assertions/should"
+	. "go.thethings.network/lorawan-stack/v3/pkg/webmiddleware"
 )
 
 func TestSecurityHeaders(t *testing.T) {
