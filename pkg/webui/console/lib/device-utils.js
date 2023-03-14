ba1acf521b8cfcce86a8adebd5c327134809c78e
Darya Plotnytska <d.plotnytska.s@gmail.com>
2023-03-14 13:22:07 +0100

    console: Fix utils

--- pkg/webui/console/lib/device-utils.js
+++ pkg/webui/console/lib/device-utils.js
@@ -183,3 +183,53 @@ export const isNonZeroSessionKey = key => key !== ALL_ZERO_KEY
  * @returns {boolean} - True if the frequency value is valid, false otherwise.
  */
 export const dynamicFrequencyTest = value => value === 0 || value >= 100000
+
+export const getLorawanVersionLabel = ({ lorawan_version }) => {
+  const { label } = LORAWAN_VERSIONS.find(version => version.value === lorawan_version) || {}
+
+  return label
+}
+
+export const getLorawanPhyVersionLabel = ({ lorawan_phy_version }) => {
+  const { label } =
+    LORAWAN_PHY_VERSIONS.find(version => version.value === lorawan_phy_version) || {}
+
+  return label
+}
+
+// Always reset LW and PHY version when changing FP do avoid invalid
+// version combinations that can otherwise occur.
+export const frequencyPlanValueSetter = ({ setValues, setFieldTouched }, { value }) => {
+  setFieldTouched('lorawan_version', false)
+  setFieldTouched('lorawan_phy_version', false)
+  return setValues(values => ({
+    ...values,
+    frequency_plan_id: value,
+    lorawan_version: '',
+    lorawan_phy_version: '',
+  }))
+}
+
+// Always reset the PHY version when setting the lorawan version to avoid
+// invalid version combinations that would otherwise briefly occur until
+// the PHY version is set by the field itself.
+export const lorawanVersionValueSetter = ({ setValues, setFieldTouched }, { value }) => {
+  const phyVersions = LORAWAN_VERSION_PAIRS[parseLorawanMacVersion(value)] || []
+  setFieldTouched('lorawan_phy_version', false)
+  return setValues(values => ({
+    ...values,
+    lorawan_version: value,
+    lorawan_phy_version: phyVersions.length === 1 ? phyVersions[0].value : '',
+  }))
+}
+
+export const SELECT_UNKNOWN_HW_OPTION = '_unknown_'
+
+export const SELECT_OTHER_OPTION = '_other_'
+
+export const isUnknownHwVersion = option => option === SELECT_UNKNOWN_HW_OPTION
+
+export const isOtherOption = option => option === SELECT_OTHER_OPTION
+
+export const hasSelectedDeviceRepositoryOther = version =>
+  version && Object.values(version).some(value => isOtherOption(value))
