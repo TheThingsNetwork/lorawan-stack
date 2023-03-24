cfb39de2fcb78cd20e0bb2e121f31dc71da2e465
Oliver Cvetkovski <oliver.cvetkovski@thethingsindustries.com>
2023-03-24 17:21:20 +0100

    as: Add alcsync error attrs to definitions

--- pkg/applicationserver/io/packages/alcsync/v1/errors.go
+++ pkg/applicationserver/io/packages/alcsync/v1/errors.go
@@ -17,17 +17,25 @@ package alcsyncv1
 import "go.thethings.network/lorawan-stack/v3/pkg/errors"
 
 var (
-	errNoAssociation     = errors.DefineInternal("no_association", "no association available")
-	errUnknownCommand    = errors.DefineNotFound("unknown_command", "unknown command")
-	errUnsuportedCommand = errors.DefineUnimplemented("unsupported_command", "unsupported command")
+	errNoAssociation  = errors.DefineInternal("no_association", "no association available")
+	errUnknownCommand = errors.DefineNotFound(
+		"unknown_command", "unknown command", "command_id", "command_payload",
+	)
+	errUnsuportedCommand = errors.DefineUnimplemented(
+		"unsupported_command", "unsupported command", "command_id", "command_payload",
+	)
 	// ErrIgnoreDownlink is a sentinel error returned when the command result should be ignored.
 	errIgnoreDownlink = errors.DefineUnavailable("downlink_unavailable", "downlink unavailable")
 
-	errCommandCreationFailed  = errors.Define("command_creation_failed", "failed to create command")
+	errCommandCreationFailed = errors.Define(
+		"command_creation_failed", "failed to create command", "command_id", "command_payload", "remaining_payload",
+	)
 	errDownlinkCreationFailed = errors.Define("downlink_creation_failed", "failed to create downlink")
 
 	errInvalidFieldType = errors.DefineCorruption("invalid_field_type", "field `{field}` has the wrong type `{type}`")
 	errPkgDataMerge     = errors.DefineCorruption("pkg_data_merge", "failed to merge package data")
 
-	errInsufficientLength = errors.DefineInvalidArgument("insufficient_length", "command payload has insufficient length")
+	errInsufficientLength = errors.DefineInvalidArgument(
+		"insufficient_length", "command payload has insufficient length", "expected_length", "actual_length",
+	)
 )
