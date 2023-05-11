419aa5e0a7938229fda1949573520c60f10d2466
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2023-05-11 16:30:09 +0200

    console: Reimplement random byte module

--- pkg/webui/console/lib/random-bytes.js
+++ pkg/webui/console/lib/random-bytes.js
@@ -12,10 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import crypto from 'crypto'
-
-export default (len, type = 'hex') =>
-  crypto
-    .randomBytes(type === 'hex' ? Math.floor(len / 2) : len)
-    .toString(type)
-    .toUpperCase()
+/**
+ * Generates a random string of the given length.
+ * @param {number} len - The length of the string to generate.
+ * @param {string} [type='hex'] - The type of the string to generate.
+ * @returns {string} The generated string.
+ * @example
+ * const randomString = randomBytes(16)
+ * @example
+ * const randomString = randomBytes(16, 'base64')
+ */
+export default (len, type = 'hex') => {
+  let bytes
+  if (window.crypto) {
+    bytes = crypto.getRandomValues(new Uint8Array(Math.floor(len / 2)))
+  } else {
+    const byteLength = Math.floor(len / 2)
+    bytes = new Uint8Array(byteLength)
+    for (let i = 0; i < byteLength; i++) {
+      bytes[i] = Math.floor(Math.random() * 256)
+    }
+  }
+  switch (type) {
+    case 'base64':
+      return btoa(String.fromCharCode(...bytes))
+    case 'hex':
+    default:
+      return Array.from(bytes, byte => byte.toString(16).padStart(2, '0'))
+        .join('')
+        .toUpperCase()
+  }
+}
