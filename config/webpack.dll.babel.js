eb64364c056169e3fc307976a758d638d98bbecc
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2023-05-11 16:29:21 +0200

    dev: Upgrade to webpack 5

--- config/webpack.dll.babel.js
+++ config/webpack.dll.babel.js
@@ -24,30 +24,26 @@ const WEBPACK_GENERATE_PRODUCTION_SOURCEMAPS =
   process.env.WEBPACK_GENERATE_PRODUCTION_SOURCEMAPS === 'true'
 
 const context = path.resolve(CONTEXT)
-const library = '[name]_[hash]'
+const library = '[name]_[fullhash]'
 
 const pkg = require(path.resolve(context, 'package.json'))
 const excludeLibs = ['react-hot-loader', 'ttn-lw']
 const libs = Object.keys(pkg.dependencies || {}).filter(lib => !excludeLibs.includes(lib))
 const devtool =
   (mode === 'production' && WEBPACK_GENERATE_PRODUCTION_SOURCEMAPS) || mode === 'development'
-    ? 'module-source-map'
+    ? 'source-map'
     : false
 
 export default {
   context,
   mode,
   target: 'web',
-  node: {
-    fs: 'empty',
-    module: 'empty',
-  },
   stats: 'minimal',
   devtool,
   recordsPath: path.resolve(context, CACHE_DIR, '_libs_records'),
   entry: { libs },
   output: {
-    filename: mode === 'production' ? '[name].[hash].bundle.js' : '[name].bundle.js',
+    filename: mode === 'production' ? '[name].[fullhash].bundle.js' : '[name].bundle.js',
     hashDigest: 'hex',
     hashDigestLength: 20,
     path: path.resolve(context, PUBLIC_DIR),
@@ -66,10 +62,14 @@ export default {
     rules: [
       {
         test: /\.(woff|woff2|ttf|eot|jpg|jpeg|png|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[hash:hex:20].[ext]',
-        },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash:20].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
