bd457e7e190a692fae7b3b6142839a75ac55830e
Darya Plotnytska <d.plotnytska.s@gmail.com>
2023-06-07 15:50:05 +0200

    console: Add OS notification

--- pkg/webui/console/containers/network-information-container/index.js
+++ pkg/webui/console/containers/network-information-container/index.js
@@ -13,9 +13,39 @@
 // limitations under the License.
 
 import React from 'react'
+import { defineMessages } from 'react-intl'
+
+import Notification from '@ttn-lw/components/notification'
+import Button from '@ttn-lw/components/button'
 
 import RegistryTotals from './registry-totals'
 
-const NetworkInformationContainer = () => <RegistryTotals />
+const m = defineMessages({
+  openSourceInfo:
+    'You are currently using The Things Stack Open Source. More features can be unlocked by using The Things Stack Cloud.',
+  plansButton: 'Get started with The things Stack Cloud',
+})
+
+const NetworkInformationContainer = () => (
+  <>
+    <Notification
+      info
+      small
+      content={m.openSourceInfo}
+      children={
+        <Button.AnchorLink
+          primary
+          href={'https://www.thethingsindustries.com/stack/plans/'}
+          message={m.plansButton}
+          target="_blank"
+          external
+          className="mt-cs-s"
+        />
+      }
+      className="mt-cs-l"
+    />
+    <RegistryTotals />
+  </>
+)
 
 export default NetworkInformationContainer
