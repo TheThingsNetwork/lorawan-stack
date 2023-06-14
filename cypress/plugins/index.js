751ca3856c58fd65bf109d068673a677a9781b26
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2023-06-14 22:36:09 +0200

    dev: Remove cypress code coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-// Copyright © 2020 The Things Network Foundation, The Things Industries B.V.
+// Copyright © 2023 The Things Network Foundation, The Things Industries B.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,10 +24,6 @@ module.exports = (on, config) => {
   tasks.sqlTask(on, configWithPermissions)
   tasks.fileExistsTask(on, configWithPermissions)
   tasks.emailTask(on, configWithPermissions)
-
-  if (process.env.NODE_ENV === 'development') {
-    tasks.codeCoverageTask(on, configWithPermissions)
-  }
 
   on('before:browser:launch', (browser = {}, launchOptions) => {
     // Log console log to output when debug mode is enabled.
