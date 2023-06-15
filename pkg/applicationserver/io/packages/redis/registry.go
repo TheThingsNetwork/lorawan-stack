1261ee389cf2d36795f4faff1d791bebb6c132ab
Oliver Cvetkovski <oliver.cvetkovski@thethingsindustries.com>
2023-06-15 16:05:46 +0200

    as: Add redis pkg description

--- pkg/applicationserver/io/packages/redis/registry.go
+++ pkg/applicationserver/io/packages/redis/registry.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package redis provides implementation of the application packages registry using Redis.
 package redis
 
 import (
