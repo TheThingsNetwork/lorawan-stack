6536adfee22f7e068c4667a18ea8e1caf31510cb
Nicholas Cristofaro <nicholas.cristofaro@thethingsindustries.com>
2023-07-05 17:55:28 -0300

    is: Format client registry

--- pkg/identityserver/client_registry_test.go
+++ pkg/identityserver/client_registry_test.go
@@ -261,7 +261,9 @@ func TestClientsCRUD(t *testing.T) {
 			a.So(got.StateDescription, should.Equal, "")
 		}
 
-		for _, collaborator := range []*ttnpb.OrganizationOrUserIdentifiers{nil, usr1.GetOrganizationOrUserIdentifiers()} {
+		for _, collaborator := range []*ttnpb.OrganizationOrUserIdentifiers{
+			nil, usr1.GetOrganizationOrUserIdentifiers(),
+		} {
 			list, err := reg.List(ctx, &ttnpb.ListClientsRequest{
 				FieldMask:    ttnpb.FieldMask("name"),
 				Collaborator: collaborator,
