364b1c024f4880ddf10740dcbf163c0d2eee3d6e
Adrian-Stefan Mares <adrian.mares@thethingsindustries.com>
2023-07-11 09:58:13 +0200

    is: Replace smartystreets/assertions with smarty/assertions

--- pkg/identityserver/storetest/end_device_store.go
+++ pkg/identityserver/storetest/end_device_store.go
@@ -19,7 +19,7 @@ import (
 	. "testing"
 	"time"
 
-	"github.com/smartystreets/assertions"
+	"github.com/smarty/assertions"
 	"go.thethings.network/lorawan-stack/v3/pkg/errors"
 	"go.thethings.network/lorawan-stack/v3/pkg/identityserver/store"
 	is "go.thethings.network/lorawan-stack/v3/pkg/identityserver/store"
