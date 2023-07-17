01f893f2339ce4b96f2fcf96a3712a3e15285997
Darya Plotnytska <d.plotnytska.s@gmail.com>
2023-07-17 14:46:02 +0200

    console: Refactor device payload formatters

--- pkg/webui/console/containers/device-payload-formatters/downlink.js
+++ pkg/webui/console/containers/device-payload-formatters/downlink.js
@@ -12,26 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react'
-import bind from 'autobind-decorator'
-import { connect } from 'react-redux'
+import React, { useCallback, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { isEmpty } from 'lodash'
 
 import PAYLOAD_FORMATTER_TYPES from '@console/constants/formatter-types'
 import tts from '@console/api/tts'
 
 import Breadcrumb from '@ttn-lw/components/breadcrumbs/breadcrumb'
-import { withBreadcrumb } from '@ttn-lw/components/breadcrumbs/context'
+import { useBreadcrumbs } from '@ttn-lw/components/breadcrumbs/context'
 import toast from '@ttn-lw/components/toast'
 import Notification from '@ttn-lw/components/notification'
 
+import RequireRequest from '@ttn-lw/lib/components/require-request'
 import IntlHelmet from '@ttn-lw/lib/components/intl-helmet'
-import withRequest from '@ttn-lw/lib/components/with-request'
 
 import PayloadFormattersForm from '@console/components/payload-formatters-form'
 
 import sharedMessages from '@ttn-lw/lib/shared-messages'
-import PropTypes from '@ttn-lw/lib/prop-types'
 import attachPromise from '@ttn-lw/lib/store/actions/attach-promise'
 
 import { checkFromState } from '@account/lib/feature-checks'
@@ -49,182 +47,136 @@ import {
   selectSelectedDeviceFormatters,
   selectSelectedDevice,
 } from '@console/store/selectors/devices'
-import {
-  selectDeviceRepoPayloadFromatters,
-  selectDeviceRepoPayloadFromattersFetching,
-  selectDeviceRepoPayloadFromattersError,
-} from '@console/store/selectors/device-repository'
+import { selectDeviceRepoPayloadFromatters } from '@console/store/selectors/device-repository'
 
 import m from './messages'
 
-@connect(
-  state => ({
-    appId: selectSelectedApplicationId(state),
-    devId: selectSelectedDeviceId(state),
-    device: selectSelectedDevice(state),
-    mayViewLink: checkFromState(mayViewApplicationLink, state),
-    link: selectApplicationLink(state),
-    formatters: selectSelectedDeviceFormatters(state),
-    encodeDownlink: tts.As.encodeDownlink,
-    repositoryPayloadFormatters: selectDeviceRepoPayloadFromatters(state),
-    error: selectDeviceRepoPayloadFromattersError(state),
-    fetching: selectDeviceRepoPayloadFromattersFetching(state),
-  }),
-  {
-    updateDevice: attachPromise(updateDevice),
-    getRepositoryPayloadFormatters,
-  },
-)
-@withRequest(({ appId, device, getRepositoryPayloadFormatters }) => {
-  if (!isEmpty(device.version_ids)) {
-    return getRepositoryPayloadFormatters(appId, device.version_ids)
-  }
-})
-@withBreadcrumb('device.single.payload-formatters.downlink', props => {
-  const { appId, devId } = props
+const DevicePayloadFormatters = () => {
+  const appId = useSelector(selectSelectedApplicationId)
+  const devId = useSelector(selectSelectedDeviceId)
+  const device = useSelector(selectSelectedDevice)
+  const mayViewLink = useSelector(state => checkFromState(mayViewApplicationLink, state))
+  const link = useSelector(selectApplicationLink)
+  const formatters = useSelector(selectSelectedDeviceFormatters)
+  const encodeDownlink = tts.As.encodeDownlink
+  const repositoryPayloadFormatters = useSelector(selectDeviceRepoPayloadFromatters)
+  const [type, setType] = useState(
+    Boolean(formatters)
+      ? formatters.down_formatter || PAYLOAD_FORMATTER_TYPES.NONE
+      : PAYLOAD_FORMATTER_TYPES.DEFAULT,
+  )
+  const dispatch = useDispatch()
 
-  return (
+  useBreadcrumbs(
+    'device.single.payload-formatters.downlink',
     <Breadcrumb
       path={`/applications/${appId}/devices/${devId}/payload-formatters/downlink`}
       content={sharedMessages.downlink}
-    />
+    />,
   )
-})
-class DevicePayloadFormatters extends React.PureComponent {
-  static propTypes = {
-    appId: PropTypes.string.isRequired,
-    devId: PropTypes.string.isRequired,
-    device: PropTypes.device.isRequired,
-    encodeDownlink: PropTypes.func.isRequired,
-    formatters: PropTypes.formatters,
-    link: PropTypes.shape({
-      default_formatters: PropTypes.shape({
-        down_formatter: PropTypes.string,
-        down_formatter_parameter: PropTypes.string,
-      }),
-    }),
-    mayViewLink: PropTypes.bool.isRequired,
-    repositoryPayloadFormatters: PropTypes.shape({
-      formatter_parameter: PropTypes.string,
-    }),
-    updateDevice: PropTypes.func.isRequired,
-  }
 
-  static defaultProps = {
-    formatters: undefined,
-    repositoryPayloadFormatters: undefined,
-    link: {},
-  }
+  const onSubmit = useCallback(
+    async values => {
+      if (values.type === PAYLOAD_FORMATTER_TYPES.DEFAULT) {
+        return dispatch(
+          attachPromise(
+            updateDevice(appId, devId, {
+              formatters: null,
+            }),
+          ),
+        )
+      }
 
-  constructor(props) {
-    super(props)
+      return dispatch(
+        attachPromise(
+          updateDevice(appId, devId, {
+            formatters: {
+              down_formatter: values.type,
+              down_formatter_parameter: values.parameter,
+              up_formatter: formatters?.up_formatter || PAYLOAD_FORMATTER_TYPES.NONE,
+              up_formatter_parameter: formatters?.up_formatter_parameter,
+            },
+          }),
+        ),
+      )
+    },
+    [dispatch, appId, devId, formatters],
+  )
 
-    const { formatters } = props
-
-    this.state = {
-      type: Boolean(formatters)
-        ? formatters.down_formatter || PAYLOAD_FORMATTER_TYPES.NONE
-        : PAYLOAD_FORMATTER_TYPES.DEFAULT,
-    }
-  }
-
-  @bind
-  async onSubmit(values) {
-    const { appId, devId, formatters: initialFormatters, updateDevice } = this.props
-
-    if (values.type === PAYLOAD_FORMATTER_TYPES.DEFAULT) {
-      return updateDevice(appId, devId, {
-        formatters: null,
-      })
-    }
-
-    const formatters = { ...(initialFormatters || {}) }
-
-    return updateDevice(appId, devId, {
-      formatters: {
-        down_formatter: values.type,
-        down_formatter_parameter: values.parameter,
-        up_formatter: formatters.up_formatter || PAYLOAD_FORMATTER_TYPES.NONE,
-        up_formatter_parameter: formatters.up_formatter_parameter,
-      },
-    })
-  }
-
-  @bind
-  async onSubmitSuccess() {
-    const { devId } = this.props
+  const onSubmitSuccess = useCallback(() => {
     toast({
       title: devId,
       message: sharedMessages.payloadFormattersUpdateSuccess,
       type: toast.types.SUCCESS,
     })
-  }
+  }, [devId])
 
-  @bind
-  async onTestSubmit(data) {
-    const { appId, devId, encodeDownlink, device } = this.props
-    const { f_port, payload, formatter, parameter } = data
-    const { version_ids } = device
+  const onTestSubmit = useCallback(
+    async data => {
+      const { f_port, payload, formatter, parameter } = data
+      const { version_ids } = device
 
-    const { downlink } = await encodeDownlink(appId, devId, {
-      downlink: {
-        f_port,
-        decoded_payload: JSON.parse(payload),
-      },
-      version_ids: Object.keys(version_ids).length > 0 ? version_ids : undefined,
-      formatter,
-      parameter,
-    })
+      const { downlink } = await encodeDownlink(appId, devId, {
+        downlink: {
+          f_port,
+          decoded_payload: JSON.parse(payload),
+        },
+        version_ids: Object.keys(version_ids).length > 0 ? version_ids : undefined,
+        formatter,
+        parameter,
+      })
 
-    return downlink
-  }
+      return downlink
+    },
+    [encodeDownlink, appId, devId, device],
+  )
 
-  @bind
-  onTypeChange(type) {
-    this.setState({ type })
-  }
+  const onTypeChange = useCallback(type => {
+    setType(type)
+  }, [])
 
-  render() {
-    const { formatters, link, repositoryPayloadFormatters, mayViewLink } = this.props
-    const { type } = this.state
+  const defaultFormatters = link?.default_formatters || {}
+  const formatterType = Boolean(formatters)
+    ? formatters.down_formatter || PAYLOAD_FORMATTER_TYPES.NONE
+    : PAYLOAD_FORMATTER_TYPES.DEFAULT
+  const formatterParameter = Boolean(formatters) ? formatters.down_formatter_parameter : undefined
+  const appFormatterType = Boolean(defaultFormatters.down_formatter)
+    ? defaultFormatters.down_formatter
+    : PAYLOAD_FORMATTER_TYPES.NONE
+  const appFormatterParameter = Boolean(defaultFormatters.down_formatter_parameter)
+    ? defaultFormatters.down_formatter_parameter
+    : undefined
 
-    const defaultFormatters = link?.default_formatters || {}
-    const formatterType = Boolean(formatters)
-      ? formatters.down_formatter || PAYLOAD_FORMATTER_TYPES.NONE
-      : PAYLOAD_FORMATTER_TYPES.DEFAULT
-    const formatterParameter = Boolean(formatters) ? formatters.down_formatter_parameter : undefined
-    const appFormatterType = Boolean(defaultFormatters.down_formatter)
-      ? defaultFormatters.down_formatter
-      : PAYLOAD_FORMATTER_TYPES.NONE
-    const appFormatterParameter = Boolean(defaultFormatters.down_formatter_parameter)
-      ? defaultFormatters.down_formatter_parameter
-      : undefined
+  const isDefaultType = type === PAYLOAD_FORMATTER_TYPES.DEFAULT
 
-    const isDefaultType = type === PAYLOAD_FORMATTER_TYPES.DEFAULT
-
-    return (
-      <React.Fragment>
-        <IntlHelmet title={sharedMessages.payloadFormattersDownlink} />
-        {!mayViewLink && <Notification content={m.mayNotViewLink} small warning />}
-        <PayloadFormattersForm
-          uplink={false}
-          allowReset
-          allowTest
-          onSubmit={this.onSubmit}
-          onSubmitSuccess={this.onSubmitSuccess}
-          onTestSubmit={this.onTestSubmit}
-          title={sharedMessages.payloadFormattersDownlink}
-          initialType={formatterType}
-          initialParameter={formatterParameter}
-          defaultType={appFormatterType}
-          defaultParameter={appFormatterParameter}
-          onTypeChange={this.onTypeChange}
-          isDefaultType={isDefaultType}
-          repoFormatters={repositoryPayloadFormatters}
-        />
-      </React.Fragment>
-    )
-  }
+  return (
+    <RequireRequest
+      requestAction={
+        !isEmpty(device.version_ids)
+          ? getRepositoryPayloadFormatters(appId, device.version_ids)
+          : []
+      }
+    >
+      <IntlHelmet title={sharedMessages.payloadFormattersDownlink} />
+      {!mayViewLink && <Notification content={m.mayNotViewLink} small warning />}
+      <PayloadFormattersForm
+        uplink={false}
+        allowReset
+        allowTest
+        onSubmit={onSubmit}
+        onSubmitSuccess={onSubmitSuccess}
+        onTestSubmit={onTestSubmit}
+        title={sharedMessages.payloadFormattersDownlink}
+        initialType={formatterType}
+        initialParameter={formatterParameter}
+        defaultType={appFormatterType}
+        defaultParameter={appFormatterParameter}
+        onTypeChange={onTypeChange}
+        isDefaultType={isDefaultType}
+        repoFormatters={repositoryPayloadFormatters}
+      />
+    </RequireRequest>
+  )
 }
 
 export default DevicePayloadFormatters
--- pkg/webui/console/containers/device-payload-formatters/uplink.js
+++ pkg/webui/console/containers/device-payload-formatters/uplink.js
@@ -12,26 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from 'react'
-import bind from 'autobind-decorator'
-import { connect } from 'react-redux'
+import React, { useCallback, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { isEmpty } from 'lodash'
 
 import PAYLOAD_FORMATTER_TYPES from '@console/constants/formatter-types'
 import tts from '@console/api/tts'
 
 import Breadcrumb from '@ttn-lw/components/breadcrumbs/breadcrumb'
-import { withBreadcrumb } from '@ttn-lw/components/breadcrumbs/context'
+import { useBreadcrumbs } from '@ttn-lw/components/breadcrumbs/context'
 import toast from '@ttn-lw/components/toast'
 import Notification from '@ttn-lw/components/notification'
 
 import IntlHelmet from '@ttn-lw/lib/components/intl-helmet'
-import withRequest from '@ttn-lw/lib/components/with-request'
+import RequireRequest from '@ttn-lw/lib/components/require-request'
 
 import PayloadFormattersForm from '@console/components/payload-formatters-form'
 
 import sharedMessages from '@ttn-lw/lib/shared-messages'
-import PropTypes from '@ttn-lw/lib/prop-types'
 import attachPromise from '@ttn-lw/lib/store/actions/attach-promise'
 
 import { hexToBase64 } from '@console/lib/bytes'
@@ -50,191 +48,145 @@ import {
   selectSelectedDeviceFormatters,
   selectSelectedDevice,
 } from '@console/store/selectors/devices'
-import {
-  selectDeviceRepoPayloadFromatters,
-  selectDeviceRepoPayloadFromattersFetching,
-  selectDeviceRepoPayloadFromattersError,
-} from '@console/store/selectors/device-repository'
+import { selectDeviceRepoPayloadFromatters } from '@console/store/selectors/device-repository'
 
 import m from './messages'
 
-@connect(
-  state => ({
-    appId: selectSelectedApplicationId(state),
-    device: selectSelectedDevice(state),
-    devId: selectSelectedDeviceId(state),
-    mayViewLink: checkFromState(mayViewApplicationLink, state),
-    link: selectApplicationLink(state),
-    formatters: selectSelectedDeviceFormatters(state),
-    decodeUplink: tts.As.decodeUplink,
-    repositoryPayloadFormatters: selectDeviceRepoPayloadFromatters(state),
-    error: selectDeviceRepoPayloadFromattersError(state),
-    fetching: selectDeviceRepoPayloadFromattersFetching(state),
-  }),
-  {
-    updateDevice: attachPromise(updateDevice),
-    getRepositoryPayloadFormatters,
-  },
-)
-@withRequest(({ appId, device, getRepositoryPayloadFormatters }) => {
-  if (!isEmpty(device.version_ids)) {
-    return getRepositoryPayloadFormatters(appId, device.version_ids)
-  }
-})
-@withBreadcrumb('device.single.payload-formatters.uplink', props => {
-  const { appId, devId } = props
+const DevicePayloadFormatters = () => {
+  const appId = useSelector(selectSelectedApplicationId)
+  const devId = useSelector(selectSelectedDeviceId)
+  const device = useSelector(selectSelectedDevice)
+  const mayViewLink = useSelector(state => checkFromState(mayViewApplicationLink, state))
+  const link = useSelector(selectApplicationLink)
+  const formatters = useSelector(selectSelectedDeviceFormatters)
+  const decodeUplink = tts.As.decodeUplink
+  const repositoryPayloadFormatters = useSelector(selectDeviceRepoPayloadFromatters)
+  const [type, setType] = useState(
+    Boolean(formatters)
+      ? formatters.down_formatter || PAYLOAD_FORMATTER_TYPES.NONE
+      : PAYLOAD_FORMATTER_TYPES.DEFAULT,
+  )
+  const dispatch = useDispatch()
 
-  return (
+  useBreadcrumbs(
+    'device.single.payload-formatters.uplink',
     <Breadcrumb
       path={`/applications/${appId}/devices/${devId}/payload-formatters/uplink`}
       content={sharedMessages.uplink}
-    />
+    />,
   )
-})
-class DevicePayloadFormatters extends React.PureComponent {
-  static propTypes = {
-    appId: PropTypes.string.isRequired,
-    decodeUplink: PropTypes.func.isRequired,
-    devId: PropTypes.string.isRequired,
-    device: PropTypes.device.isRequired,
-    formatters: PropTypes.formatters,
-    link: PropTypes.shape({
-      default_formatters: PropTypes.shape({
-        up_formatter: PropTypes.string,
-        up_formatter_parameter: PropTypes.string,
-      }),
-    }),
-    mayViewLink: PropTypes.bool.isRequired,
-    repositoryPayloadFormatters: PropTypes.shape({
-      formatter_parameter: PropTypes.string,
-    }),
-    updateDevice: PropTypes.func.isRequired,
-  }
 
-  static defaultProps = {
-    formatters: undefined,
-    repositoryPayloadFormatters: undefined,
-    link: {},
-  }
+  const onSubmit = useCallback(
+    async values => {
+      if (values.type === PAYLOAD_FORMATTER_TYPES.DEFAULT) {
+        return dispatch(
+          attachPromise(
+            updateDevice(appId, devId, {
+              formatters: null,
+            }),
+          ),
+        )
+      }
 
-  constructor(props) {
-    super(props)
+      return dispatch(
+        attachPromise(
+          updateDevice(appId, devId, {
+            formatters: {
+              down_formatter: formatters?.down_formatter || PAYLOAD_FORMATTER_TYPES.NONE,
+              down_formatter_parameter: formatters?.down_formatter_parameter,
+              up_formatter: values.type,
+              up_formatter_parameter: values.parameter,
+            },
+          }),
+        ),
+      )
+    },
+    [appId, devId, dispatch, formatters],
+  )
 
-    const { formatters } = props
-
-    this.state = {
-      type: Boolean(formatters)
-        ? formatters.up_formatter || PAYLOAD_FORMATTER_TYPES.NONE
-        : PAYLOAD_FORMATTER_TYPES.DEFAULT,
-    }
-  }
-
-  @bind
-  async onSubmit(values) {
-    const { appId, devId, formatters: initialFormatters, updateDevice } = this.props
-
-    if (values.type === PAYLOAD_FORMATTER_TYPES.DEFAULT) {
-      return updateDevice(appId, devId, {
-        formatters: null,
-      })
-    }
-
-    const formatters = { ...(initialFormatters || {}) }
-
-    return updateDevice(appId, devId, {
-      formatters: {
-        down_formatter: formatters.down_formatter || PAYLOAD_FORMATTER_TYPES.NONE,
-        down_formatter_parameter: formatters.down_formatter_parameter,
-        up_formatter: values.type,
-        up_formatter_parameter: values.parameter,
-      },
-    })
-  }
-
-  @bind
-  async onSubmitSuccess() {
-    const { devId } = this.props
+  const onSubmitSuccess = useCallback(() => {
     toast({
       title: devId,
       message: sharedMessages.payloadFormattersUpdateSuccess,
       type: toast.types.SUCCESS,
     })
-  }
+  }, [devId])
 
-  @bind
-  async onTestSubmit(data) {
-    const { appId, devId, decodeUplink, device } = this.props
-    const { f_port, payload, formatter, parameter } = data
-    const { version_ids } = device
+  const onTestSubmit = useCallback(
+    async data => {
+      const { f_port, payload, formatter, parameter } = data
+      const { version_ids } = device
 
-    const { uplink } = await decodeUplink(appId, devId, {
-      uplink: {
-        f_port,
-        frm_payload: hexToBase64(payload),
-        // `rx_metadata` and `settings` fields are required by the validation middleware in AS.
-        // These fields won't affect the result of decoding an uplink message.
-        rx_metadata: [
-          { gateway_ids: { gateway_id: 'test' }, rssi: 42, channel_rssi: 42, snr: 4.2 },
-        ],
-        settings: {
-          data_rate: { lora: { bandwidth: 125000, spreading_factor: 7 } },
-          frequency: 868000000,
+      const { uplink } = await decodeUplink(appId, devId, {
+        uplink: {
+          f_port,
+          frm_payload: hexToBase64(payload),
+          // `rx_metadata` and `settings` fields are required by the validation middleware in AS.
+          // These fields won't affect the result of decoding an uplink message.
+          rx_metadata: [
+            { gateway_ids: { gateway_id: 'test' }, rssi: 42, channel_rssi: 42, snr: 4.2 },
+          ],
+          settings: {
+            data_rate: { lora: { bandwidth: 125000, spreading_factor: 7 } },
+            frequency: 868000000,
+          },
         },
-      },
-      version_ids: Object.keys(version_ids).length > 0 ? version_ids : undefined,
-      formatter,
-      parameter,
-    })
+        version_ids: Object.keys(version_ids).length > 0 ? version_ids : undefined,
+        formatter,
+        parameter,
+      })
 
-    return uplink
-  }
+      return uplink
+    },
+    [appId, devId, device, decodeUplink],
+  )
 
-  @bind
-  onTypeChange(type) {
-    this.setState({ type })
-  }
+  const onTypeChange = useCallback(type => {
+    setType(type)
+  }, [])
 
-  render() {
-    const { formatters, mayViewLink, link, repositoryPayloadFormatters } = this.props
-    const { type } = this.state
+  const defaultFormatters = link?.default_formatters || {}
+  const formatterType = Boolean(formatters)
+    ? formatters.up_formatter || PAYLOAD_FORMATTER_TYPES.NONE
+    : PAYLOAD_FORMATTER_TYPES.DEFAULT
+  const formatterParameter = Boolean(formatters) ? formatters.up_formatter_parameter : undefined
+  const appFormatterType = Boolean(defaultFormatters.up_formatter)
+    ? defaultFormatters.up_formatter
+    : PAYLOAD_FORMATTER_TYPES.NONE
+  const appFormatterParameter = Boolean(defaultFormatters.up_formatter_parameter)
+    ? defaultFormatters.up_formatter_parameter
+    : undefined
 
-    const defaultFormatters = link?.default_formatters || {}
-    const formatterType = Boolean(formatters)
-      ? formatters.up_formatter || PAYLOAD_FORMATTER_TYPES.NONE
-      : PAYLOAD_FORMATTER_TYPES.DEFAULT
-    const formatterParameter = Boolean(formatters) ? formatters.up_formatter_parameter : undefined
-    const appFormatterType = Boolean(defaultFormatters.up_formatter)
-      ? defaultFormatters.up_formatter
-      : PAYLOAD_FORMATTER_TYPES.NONE
-    const appFormatterParameter = Boolean(defaultFormatters.up_formatter_parameter)
-      ? defaultFormatters.up_formatter_parameter
-      : undefined
+  const isDefaultType = type === PAYLOAD_FORMATTER_TYPES.DEFAULT
 
-    const isDefaultType = type === PAYLOAD_FORMATTER_TYPES.DEFAULT
-
-    return (
-      <React.Fragment>
-        <IntlHelmet title={sharedMessages.payloadFormattersUplink} />
-        {!mayViewLink && <Notification content={m.mayNotViewLink} small warning />}
-        <PayloadFormattersForm
-          uplink
-          allowReset
-          allowTest
-          onSubmit={this.onSubmit}
-          onSubmitSuccess={this.onSubmitSuccess}
-          onTestSubmit={this.onTestSubmit}
-          title={sharedMessages.payloadFormattersUplink}
-          initialType={formatterType}
-          initialParameter={formatterParameter}
-          defaultType={appFormatterType}
-          defaultParameter={appFormatterParameter}
-          onTypeChange={this.onTypeChange}
-          isDefaultType={isDefaultType}
-          repoFormatters={repositoryPayloadFormatters}
-        />
-      </React.Fragment>
-    )
-  }
+  return (
+    <RequireRequest
+      requestAction={
+        !isEmpty(device.version_ids)
+          ? getRepositoryPayloadFormatters(appId, device.version_ids)
+          : []
+      }
+    >
+      <IntlHelmet title={sharedMessages.payloadFormattersUplink} />
+      {!mayViewLink && <Notification content={m.mayNotViewLink} small warning />}
+      <PayloadFormattersForm
+        uplink
+        allowReset
+        allowTest
+        onSubmit={onSubmit}
+        onSubmitSuccess={onSubmitSuccess}
+        onTestSubmit={onTestSubmit}
+        title={sharedMessages.payloadFormattersUplink}
+        initialType={formatterType}
+        initialParameter={formatterParameter}
+        defaultType={appFormatterType}
+        defaultParameter={appFormatterParameter}
+        onTypeChange={onTypeChange}
+        isDefaultType={isDefaultType}
+        repoFormatters={repositoryPayloadFormatters}
+      />
+    </RequireRequest>
+  )
 }
 
 export default DevicePayloadFormatters
