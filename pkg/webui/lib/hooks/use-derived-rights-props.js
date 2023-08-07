2eff83e8474b52574472e64925ce25c98fbe2ce8
Darya Plotnytska <d.plotnytska.s@gmail.com>
2023-08-07 13:22:54 +0200

    console: Fix hook

--- pkg/webui/lib/hooks/use-derived-rights-props.js
+++ pkg/webui/lib/hooks/use-derived-rights-props.js
@@ -16,56 +16,6 @@ import { useMemo } from 'react'
 
 import { RIGHT_ALL } from '@console/lib/rights'
 
-const computeProps = props => {
-  const { value, pseudoRight: grantablePseudoRight, rights: grantableRights } = props
-
-  // Extract the pseudo right from own rights or granted rights.
-  let derivedPseudoRight = []
-  if (grantablePseudoRight && !Array.isArray(grantablePseudoRight)) {
-    derivedPseudoRight = [grantablePseudoRight]
-  } else if (grantablePseudoRight && Array.isArray(grantablePseudoRight)) {
-    derivedPseudoRight = grantablePseudoRight
-  } else {
-    derivedPseudoRight = value.filter(right => right !== RIGHT_ALL && right.endsWith('_ALL'))
-  }
-  // Filter out rights that the entity has but may not be granted by the user.
-  const outOfOwnScopeRights = !Boolean(grantablePseudoRight)
-    ? value.filter(right => !grantableRights.includes(right))
-    : []
-
-  // Extract all rights by combining granted and grantable rights.
-  const derivedRights = [...grantableRights, ...outOfOwnScopeRights].sort()
-
-  // Store whether out of scope pseudo rights are present.
-  const hasOutOfOwnScopePseudoRight =
-    outOfOwnScopeRights.filter(right => right.endsWith('_ALL')).length !== 0
-
-  // Store granted individual rights.
-  const grantedIndividualRights = value.filter(right => !derivedPseudoRight.includes(right))
-
-  // Store out of own scope individual rights.
-  const outOfOwnScopeIndividualRights = !Boolean(grantablePseudoRight)
-    ? grantedIndividualRights.filter(right => !grantableRights.includes(right))
-    : []
-
-  // Determine whether a pseudo right is granted.
-  const hasPseudoRightGranted =
-    value.includes(RIGHT_ALL) ||
-    derivedPseudoRight.some(derivedRight => value.includes(derivedRight))
-
-  // Determine the current grant type.
-  const grantType = hasPseudoRightGranted ? 'pseudo' : 'individual'
-
-  return {
-    outOfOwnScopeIndividualRights,
-    hasOutOfOwnScopePseudoRight,
-    derivedPseudoRight,
-    derivedRights,
-    hasPseudoRightGranted,
-    grantType,
-  }
-}
-
 /**
  * `useComputedProps` is a hook that can be used to pass-in props that are
  * derived from the other props of the components. This is useful to ensure that
@@ -75,24 +25,55 @@ const computeProps = props => {
  * @returns {object} - The computed props.
  */
 const useDerivedRightProps = props => {
-  const { value, pseudoRight, rights } = props
-  const {
-    outOfOwnScopeIndividualRights,
-    hasOutOfOwnScopePseudoRight,
-    derivedPseudoRight,
-    derivedRights,
-    hasPseudoRightGranted,
-    grantType,
-  } = useMemo(computeProps({ value, pseudoRight, rights }), [value, pseudoRight, rights])
+  const { value, pseudoRight: grantablePseudoRight, rights: grantableRights } = props
 
-  return {
-    outOfOwnScopeIndividualRights,
-    hasOutOfOwnScopePseudoRight,
-    derivedPseudoRight,
-    derivedRights,
-    hasPseudoRightGranted,
-    grantType,
-  }
+  return useMemo(() => {
+    // Extract the pseudo right from own rights or granted rights.
+    let derivedPseudoRight = []
+    if (grantablePseudoRight && !Array.isArray(grantablePseudoRight)) {
+      derivedPseudoRight = [grantablePseudoRight]
+    } else if (grantablePseudoRight && Array.isArray(grantablePseudoRight)) {
+      derivedPseudoRight = grantablePseudoRight
+    } else {
+      derivedPseudoRight = value.filter(right => right !== RIGHT_ALL && right.endsWith('_ALL'))
+    }
+    // Filter out rights that the entity has but may not be granted by the user.
+    const outOfOwnScopeRights = !Boolean(grantablePseudoRight)
+      ? value.filter(right => !grantableRights.includes(right))
+      : []
+
+    // Extract all rights by combining granted and grantable rights.
+    const derivedRights = [...grantableRights, ...outOfOwnScopeRights].sort()
+
+    // Store whether out of scope pseudo rights are present.
+    const hasOutOfOwnScopePseudoRight =
+      outOfOwnScopeRights.filter(right => right.endsWith('_ALL')).length !== 0
+
+    // Store granted individual rights.
+    const grantedIndividualRights = value.filter(right => !derivedPseudoRight.includes(right))
+
+    // Store out of own scope individual rights.
+    const outOfOwnScopeIndividualRights = !Boolean(grantablePseudoRight)
+      ? grantedIndividualRights.filter(right => !grantableRights.includes(right))
+      : []
+
+    // Determine whether a pseudo right is granted.
+    const hasPseudoRightGranted =
+      value.includes(RIGHT_ALL) ||
+      derivedPseudoRight.some(derivedRight => value.includes(derivedRight))
+
+    // Determine the current grant type.
+    const grantType = hasPseudoRightGranted ? 'pseudo' : 'individual'
+
+    return {
+      outOfOwnScopeIndividualRights,
+      hasOutOfOwnScopePseudoRight,
+      derivedPseudoRight,
+      derivedRights,
+      hasPseudoRightGranted,
+      grantType,
+    }
+  }, [value, grantablePseudoRight, grantableRights])
 }
 
 export default useDerivedRightProps
