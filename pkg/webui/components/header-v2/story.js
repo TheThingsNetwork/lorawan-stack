169c330c56a525fed4ceade86cf33ca56695545a
Pavel Jankoski <paveljankoski@hotmail.com>
2023-11-28 17:34:40 +0100

    console: Added sb stories for header and profile dropdown v2

---
+++ pkg/webui/components/header-v2/story.js
@@ -0,0 +1,52 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+
+import Breadcrumb from '@ttn-lw/components/breadcrumbs/breadcrumb'
+import Dropdown from '@ttn-lw/components/dropdown'
+
+import Header from '.'
+
+const user = {
+  name: 'johndoe',
+  ids: {
+    user_id: 'jdoe300',
+  },
+}
+
+export default {
+  title: 'Header V2',
+}
+
+export const Default = () => {
+  const breadcrumbs = [
+    <Breadcrumb key="1" path="/applications" content="Applications" />,
+    <Breadcrumb key="2" path="/applications/test-app" content="test-app" />,
+    <Breadcrumb key="3" path="/applications/test-app/devices" content="Devices" />,
+  ]
+
+  const items = (
+    <React.Fragment>
+      <Dropdown.Item title="Profile Settings" icon="settings" path="/profile-settings" />
+      <Dropdown.Item title="Logout" icon="power_settings_new" path="/logout" />
+    </React.Fragment>
+  )
+
+  return (
+    <div style={{ height: '6rem' }}>
+      <Header user={user} breadcrumbs={breadcrumbs} profileDropdownItems={items} />
+    </div>
+  )
+}
---
+++ pkg/webui/components/profile-dropdown-v2/story.js
@@ -0,0 +1,38 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+
+import Dropdown from '@ttn-lw/components/dropdown'
+
+import ProfileDropdown from '.'
+
+const handleLogout = () => {
+  // eslint-disable-next-line no-console
+  console.log('Click')
+}
+
+export default {
+  title: 'Profile Dropdown V2',
+  component: ProfileDropdown,
+}
+
+export const Default = () => (
+  <div style={{ height: '6rem', marginLeft: '120px' }}>
+    <ProfileDropdown userName="johndoe">
+      <Dropdown.Item title="Profile Settings" icon="settings" path="/profile-settings" />
+      <Dropdown.Item title="Logout" icon="power_settings_new" action={handleLogout} />
+    </ProfileDropdown>
+  </div>
+)
