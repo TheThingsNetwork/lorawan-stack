b785ee440ccb59d26ce34ceace12d1f5ebcc1926
Adrian-Stefan Mares <adrian.mares@thethingsindustries.com>
2023-12-14 14:49:12 +0100

    console: Refactor subscription state

--- sdk/js/src/api/stream/subscribeToWebSocketStream.js
+++ sdk/js/src/api/stream/subscribeToWebSocketStream.js
@@ -21,47 +21,184 @@ import { notify, EVENTS, MESSAGE_TYPES } from './shared'
 
 const initialListeners = Object.values(EVENTS).reduce((acc, curr) => ({ ...acc, [curr]: {} }), {})
 
-const store = () => {
-  const connections = {}
-
+const newSubscription = (resolve, reject, unsubscribe) => {
+  const listeners = { ...initialListeners }
+  let closeRequested = false
+  const externalSubscription = {
+    on: (eventName, callback) => {
+      if (!Object.values(EVENTS).includes(eventName)) {
+        throw new Error(
+          `${eventName} event is not supported. Should be one of: open, message, error or close`,
+        )
+      }
+      listeners[eventName] = callback
+      return externalSubscription
+    },
+    close: () => {
+      closeRequested = true
+      return unsubscribe()
+    },
+  }
   return {
-    getInstance: url => traverse(connections).get([url, 'instance']),
-    setInstance: (url, instance) => {
-      traverse(connections).set([url, 'instance'], instance)
-      return instance
+    onError: err => {
+      notify(listeners[EVENTS.ERROR], err)
+      // If an error occurs while we are trying to subscribe, we should reject
+      // the promise in order to propagate the implicit subscription failure.
+      reject(err)
     },
-    deleteInstance: url => {
-      if (url in connections) {
-        delete connections[url]
+    onClose: closeEvent => {
+      notify(listeners[EVENTS.CLOSE], closeRequested)
+      // If the connection has been closed while we are trying subscribe, we should
+      // reject the promise in order to propagate the implicit subscription failure.
+      reject(new Error(`WebSocket connection closed unexpectedly with code ${closeEvent.code}`))
+    },
+    onMessage: dataParsed => {
+      if (dataParsed.type === MESSAGE_TYPES.SUBSCRIBE) {
+        notify(listeners[EVENTS.OPEN])
+        // Resolve the promise after the subscription confirmation message.
+        resolve(externalSubscription)
       }
-    },
-    getSubscriptions: url => Object.values(traverse(connections).get([url, 'subscriptions'] || {})),
-    getSubscription: (url, sid) => traverse(connections).get([url, 'subscriptions', sid]) || null,
-    setSubscription: (url, sid, subscription) => {
-      const subs = traverse(connections).get([url, 'subscriptions']) || {}
-      subs[sid] = subscription
-      traverse(connections).set([url, 'subscriptions'], subs)
-      return subs[sid]
-    },
-    markSubscriptionClosing: (url, sid) => {
-      if (traverse(connections).has([url, 'subscriptions', sid])) {
-        traverse(connections).set([url, 'subscriptions', sid, 'closeRequested'], true)
+
+      if (dataParsed.type === MESSAGE_TYPES.ERROR) {
+        notify(listeners[EVENTS.ERROR], dataParsed.error)
       }
-    },
-    getSubscriptionCount: url => {
-      const subs = traverse(connections).get([url, 'subscriptions'])
-      return subs ? Object.keys(subs).length : 0
-    },
-    deleteSubscription: (url, sid) => {
-      const subscriptions = traverse(connections).get([url, 'subscriptions'])
-      if (subscriptions && subscriptions[sid]) {
-        delete subscriptions[sid]
+
+      if (dataParsed.type === MESSAGE_TYPES.PUBLISH) {
+        notify(listeners[EVENTS.MESSAGE], dataParsed.event)
+      }
+
+      if (dataParsed.type === MESSAGE_TYPES.UNSUBSCRIBE) {
+        notify(listeners[EVENTS.CLOSE], closeRequested)
       }
     },
   }
 }
 
-const state = store()
+const newInstance = (wsInstance, onClose) => {
+  const subscriptions = {}
+  let closeRequested = false
+
+  // Broadcast connection errors to all subscriptions.
+  wsInstance.addEventListener('error', () => {
+    const err = new Error('Error in WebSocket connection')
+    for (const subscription of Object.values(subscriptions)) {
+      subscription.onError(err)
+    }
+  })
+
+  // Broadcast connection closure to all subscriptions.
+  wsInstance.addEventListener('close', closeEvent => {
+    if (closeRequested) {
+      // If the close has been requested already, the instance has been
+      // deregistered and there are no subscriptions left.
+      return
+    }
+    // TODO: Handle close event codes.
+    // https://github.com/TheThingsNetwork/lorawan-stack/issues/6752
+    for (const subscription of Object.values(subscriptions)) {
+      subscription.onClose(closeEvent)
+    }
+    onClose()
+  })
+
+  // Broadcast messages to the correct subscription.
+  wsInstance.addEventListener('message', ({ data }) => {
+    const dataParsed = JSON.parse(data)
+    const sid = dataParsed.id
+    const subscription = traverse(subscriptions).get([sid]) || null
+
+    if (!subscription) {
+      warn('Message received for closed or unknown subscription with ID', sid)
+      return
+    }
+
+    subscription.onMessage(dataParsed)
+
+    if (dataParsed.type === MESSAGE_TYPES.UNSUBSCRIBE) {
+      delete subscriptions[sid]
+      if (Object.keys(subscriptions).length === 0) {
+        closeRequested = true
+        wsInstance.close()
+        onClose()
+      }
+    }
+  })
+
+  return {
+    subscribe: (sid, resolve, reject, subscribePayload, unsubscribePayload) => {
+      if (sid in subscriptions) {
+        throw new Error(`Subscription with ID ${sid} already exists`)
+      }
+
+      let unsubscribed = null
+      const unsubscribe = () => {
+        if (unsubscribed) {
+          return unsubscribed
+        }
+
+        if (
+          wsInstance.readyState === WebSocket.CLOSED ||
+          wsInstance.readyState === WebSocket.CLOSING
+        ) {
+          warn('WebSocket was already closed')
+          return Promise.resolve()
+        }
+
+        wsInstance.send(unsubscribePayload)
+
+        // Wait for the server to confirm the unsubscribe.
+        unsubscribed = new Promise(resolve => {
+          const onMessage = ({ data }) => {
+            const { type, id } = JSON.parse(data)
+            if (id === sid && type === MESSAGE_TYPES.UNSUBSCRIBE) {
+              resolve()
+              wsInstance.removeEventListener('message', onMessage)
+            }
+          }
+          wsInstance.addEventListener('message', onMessage)
+          const onClose = () => {
+            resolve()
+            wsInstance.removeEventListener('close', onClose)
+          }
+          wsInstance.addEventListener('close', onClose)
+        })
+        return unsubscribed
+      }
+
+      const subscription = newSubscription(resolve, reject, unsubscribe)
+      subscriptions[sid] = subscription
+
+      if (wsInstance.readyState === WebSocket.OPEN) {
+        // If the WebSocket connection is already open, only add the subscription.
+        wsInstance.send(subscribePayload)
+      } else if (wsInstance.readyState === WebSocket.CONNECTING) {
+        // Otherwise wait for the connection to open and then add the subscription.
+        const onOpen = () => {
+          wsInstance.send(subscribePayload)
+          wsInstance.removeEventListener('open', onOpen)
+        }
+        wsInstance.addEventListener('open', onOpen)
+      } else {
+        delete subscriptions[sid]
+        throw new Error('WebSocket connection is closed')
+      }
+    },
+  }
+}
+
+const newStore = () => {
+  const connections = {}
+  return {
+    getInstance: url => traverse(connections).get([url]),
+    setInstance: (url, wsInstance) =>
+      traverse(connections).set(
+        [url],
+        newInstance(wsInstance, () => delete connections[url]),
+      ),
+  }
+}
+
+const state = newStore()
 
 /**
  * Opens a new stream.
@@ -111,177 +248,29 @@ export default async (
     id: subscriptionId,
   })
   const url = baseUrl + endpoint
-  let wsInstance = state.getInstance(url)
+  const token = new Token().get()
+  const tokenParsed = typeof token === 'function' ? `${(await token()).access_token}` : token
+  const baseUrlParsed = baseUrl.replace('http', 'ws')
 
-  await Promise.race([
-    new Promise(async (resolve, reject) => {
+  return await Promise.race([
+    new Promise((resolve, reject) => {
+      let instance = state.getInstance(url)
+      // Open up the WebSocket connection if it doesn't exist.
+      if (!instance) {
+        instance = state.setInstance(
+          url,
+          new WebSocket(`${baseUrlParsed}${endpoint}`, [
+            'ttn.lorawan.v3.console.internal.events.v1',
+            `ttn.lorawan.v3.header.authorization.bearer.${tokenParsed}`,
+          ]),
+        )
+      }
+
       // Add the new subscription to the subscriptions object.
       // Also add the resolver functions to the subscription object to be able
       // to resolve the promise after the subscription confirmation message.
-      if (state.getSubscription(url, subscriptionId) !== null) {
-        reject(new Error('Subscription with the same ID already exists'))
-      }
-      state.setSubscription(url, subscriptionId, {
-        ...initialListeners,
-        url,
-        resolve,
-        reject,
-        closeRequested: false,
-      })
-
-      try {
-        const token = new Token().get()
-        const tokenParsed = typeof token === 'function' ? `${(await token()).access_token}` : token
-        const baseUrlParsed = baseUrl.replace('http', 'ws')
-
-        // Open up the WebSocket connection if it doesn't exist.
-        if (!wsInstance) {
-          wsInstance = state.setInstance(
-            url,
-            new WebSocket(`${baseUrlParsed}${endpoint}`, [
-              'ttn.lorawan.v3.console.internal.events.v1',
-              `ttn.lorawan.v3.header.authorization.bearer.${tokenParsed}`,
-            ]),
-          )
-
-          // Broadcast connection errors to all listeners.
-          wsInstance.addEventListener('error', () => {
-            const err = new Error('Error in WebSocket connection')
-            const subscriptions = state.getSubscriptions(url)
-            for (const s of subscriptions) {
-              notify(s[EVENTS.ERROR], err)
-              // The error is an error event, but we should only throw proper errors.
-              // It has an optional error code that we could use to map to a proper error.
-              // However, the error codes are optional and not always used.
-              s.reject(err)
-            }
-          })
-
-          // Event listener for 'close'
-          wsInstance.addEventListener('close', closeEvent => {
-            // TODO: Handle close event codes.
-            // https://github.com/TheThingsNetwork/lorawan-stack/issues/6752
-            const subscriptions = state.getSubscriptions(url)
-            const wasClean = closeEvent?.wasClean ?? false
-
-            for (const s of subscriptions) {
-              notify(s[EVENTS.CLOSE], wasClean)
-              if (wasClean) {
-                s.resolve()
-              } else {
-                s.reject(
-                  new Error(
-                    `WebSocket connection closed unexpectedly with code ${closeEvent.code}`,
-                  ),
-                )
-              }
-            }
-
-            state.deleteInstance(url)
-          })
-
-          // After the WebSocket connection is open, add the event listeners.
-          // Wait for the subscription confirmation message before resolving.
-          wsInstance.addEventListener('message', ({ data }) => {
-            const dataParsed = JSON.parse(data)
-            const sid = dataParsed.id
-            const subscription = state.getSubscription(url, sid)
-
-            if (!subscription) {
-              warn('Message received for closed or unknown subscription with ID', sid)
-
-              return
-            }
-
-            if (dataParsed.type === MESSAGE_TYPES.SUBSCRIBE) {
-              notify(subscription[EVENTS.OPEN])
-              // Resolve the promise after the subscription confirmation message.
-              subscription.resolve()
-            }
-
-            if (dataParsed.type === MESSAGE_TYPES.ERROR) {
-              notify(subscription[EVENTS.ERROR], dataParsed)
-            }
-
-            if (dataParsed.type === MESSAGE_TYPES.PUBLISH) {
-              notify(subscription[EVENTS.MESSAGE], dataParsed.event)
-            }
-
-            if (dataParsed.type === MESSAGE_TYPES.UNSUBSCRIBE) {
-              notify(subscription[EVENTS.CLOSE], subscription.closeRequested)
-              // Remove the subscription
-              state.deleteSubscription(url, sid)
-              if (state.getSubscriptionCount(url) === 0) {
-                wsInstance.close()
-                state.deleteInstance(url)
-              }
-            }
-          })
-        }
-
-        if (wsInstance.readyState === WebSocket.OPEN) {
-          // If the WebSocket connection is already open, only add the subscription.
-          wsInstance.send(subscriptionPayload)
-        } else if (wsInstance.readyState === WebSocket.CONNECTING) {
-          // Otherwise wait for the connection to open and then add the subscription.
-          const onOpen = () => {
-            wsInstance.send(subscriptionPayload)
-            wsInstance.removeEventListener('open', onOpen)
-          }
-          wsInstance.addEventListener('open', onOpen)
-        } else {
-          reject(new Error('WebSocket connection is closed'))
-        }
-      } catch (error) {
-        const err = error instanceof Error ? error : new Error(error)
-        const subscriptions = state.getSubscriptions(url)
-        for (const s of subscriptions) {
-          notify(s[EVENTS.ERROR], err)
-          s.reject(err)
-        }
-      }
+      instance.subscribe(subscriptionId, resolve, reject, subscriptionPayload, unsubscribePayload)
     }),
-    new Promise((resolve, reject) => setTimeout(() => reject(new Error('timeout')), timeout)),
+    new Promise((_resolve, reject) => setTimeout(() => reject(new Error('timeout')), timeout)),
   ])
-
-  // Return an observer object with the `on` and `close` functions for
-  // the current subscription.
-  return {
-    on(eventName, callback) {
-      if (!Object.values(EVENTS).includes(eventName)) {
-        throw new Error(
-          `${eventName} event is not supported. Should be one of: open, message, error or close`,
-        )
-      }
-      const subscription = state.getSubscription(url, subscriptionId)
-      subscription[eventName] = callback
-
-      return this
-    },
-    close: () => {
-      if (
-        !wsInstance ||
-        wsInstance.readyState === WebSocket.CLOSED ||
-        wsInstance.readyState === WebSocket.CLOSING
-      ) {
-        warn('WebSocket was already closed')
-        return Promise.resolve()
-      }
-
-      state.markSubscriptionClosing(url, subscriptionId)
-      wsInstance.send(unsubscribePayload)
-
-      // Wait for the server to confirm the unsubscribe.
-      return new Promise(resolve => {
-        const onMessage = ({ data }) => {
-          const { type, id } = JSON.parse(data)
-          if (id === subscriptionId && type === MESSAGE_TYPES.UNSUBSCRIBE) {
-            resolve()
-          }
-          wsInstance.removeEventListener('message', onMessage)
-        }
-        wsInstance.addEventListener('message', onMessage)
-      })
-    },
-  }
 }
