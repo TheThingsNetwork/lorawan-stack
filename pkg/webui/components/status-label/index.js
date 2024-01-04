e72f9ae5864b25846bd42930a81b534d47f93e9f
Darya Plotnytska <d.plotnytska.s@gmail.com>
2024-01-04 11:13:25 +0100

    console: Add status label

---
+++ pkg/webui/components/status-label/index.js
@@ -0,0 +1,63 @@
+// Copyright © 2023 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react'
+import classnames from 'classnames'
+
+import Message from '@ttn-lw/lib/components/message'
+
+import PropTypes from '@ttn-lw/lib/prop-types'
+
+import Icon from '../icon'
+
+import style from './status-label.styl'
+
+const StatusLabel = ({ success, warning, error, info, content }) => {
+  const statusClassName = classnames(
+    style.label,
+    {
+      [style.success]: success,
+      [style.warning]: warning,
+      [style.error]: error,
+      [style.info]: info,
+    },
+    'd-flex gap-cs-xs al-center j-center p-vert-cs-s p-sides-cs-m',
+  )
+
+  const labelIcon = success ? 'check_circle' : warning ? 'warning' : error ? 'error' : 'info'
+
+  return (
+    <div className={statusClassName}>
+      <Icon icon={labelIcon} className={style.labelIcon} />
+      <Message content={content} className={style.labelContent} />
+    </div>
+  )
+}
+
+StatusLabel.propTypes = {
+  content: PropTypes.message.isRequired,
+  error: PropTypes.bool,
+  info: PropTypes.bool,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+}
+
+StatusLabel.defaultProps = {
+  success: false,
+  warning: false,
+  error: false,
+  info: false,
+}
+
+export default StatusLabel
