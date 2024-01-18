57dce28c3e9a5820835be89b46a367a1c2d1956a
Nicholas Cristofaro <nicholaspcr@gmail.com>
2024-01-18 16:26:10 -0300

    is: Add migration for removing contact info rows

---
+++ pkg/identityserver/store/migrations/20240100200000_contact_info_removal.up.sql
@@ -0,0 +1,23 @@
+delete from contact_infos
+where entity_type = 'application'
+;
+--bun:split
+delete from contact_infos
+where entity_type = 'client'
+;
+--bun:split
+delete from contact_infos
+where entity_type = 'end_device'
+;
+--bun:split
+delete from contact_infos
+where entity_type = 'gateway'
+;
+--bun:split
+delete from contact_infos
+where entity_type = 'organization'
+;
+--bun:split
+delete from contact_infos
+where entity_type = 'user'
+;
