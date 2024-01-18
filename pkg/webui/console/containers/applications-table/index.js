18b1f5fac820031a8ecd7b02713dc750a988fcf0
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2024-01-18 19:56:20 +0900

    console,account: Remove unused or duplicate global style definitions

--- pkg/webui/console/containers/applications-table/index.js
+++ pkg/webui/console/containers/applications-table/index.js
@@ -203,7 +203,7 @@ const ApplicationsTable = props => {
           align: 'center',
           render: deviceCount =>
             typeof deviceCount !== 'number' ? (
-              <Spinner micro center inline after={100} className="c-subtle-gray" />
+              <Spinner micro center inline after={100} className="c-icon" />
             ) : (
               <strong>
                 <FormattedNumber value={deviceCount} />
