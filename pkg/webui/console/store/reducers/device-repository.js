92313a62bb9b3cf5837c2d16b73120fcbd18ef34
Darya Plotnytska <d.plotnytska.s@gmail.com>
2024-02-16 09:53:12 +0100

    console: Use default import

--- pkg/webui/console/store/reducers/device-repository.js
+++ pkg/webui/console/store/reducers/device-repository.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { handleActions } from 'redux-actions'
+import handleActions from 'redux-actions'
 import { get } from 'lodash'
 
 import {
--- pkg/webui/console/store/reducers/network-server.js
+++ pkg/webui/console/store/reducers/network-server.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { handleActions } from 'redux-actions'
+import handleActions from 'redux-actions'
 
 import { GET_DEFAULT_MAC_SETTINGS_SUCCESS } from '@console/store/actions/network-server'
 
