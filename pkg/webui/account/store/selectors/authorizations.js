16f134fd86fed1606658aee909ed03b0c1874391
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2024-05-25 13:43:42 +0200

    account: Fix selectors and reducers to ensure referential equality

--- pkg/webui/account/store/selectors/authorizations.js
+++ pkg/webui/account/store/selectors/authorizations.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { createSelector } from 'reselect'
+
 import { createFetchingSelector } from '@ttn-lw/lib/store/selectors/fetching'
 import { createErrorSelector } from '@ttn-lw/lib/store/selectors/error'
 
@@ -23,23 +25,27 @@ import {
 const selectAuthorizationsStore = state => state.authorizations
 
 export const selectAuthorizations = state => selectAuthorizationsStore(state).authorizations
-export const selectSelectedAuthorization = (state, clientId) => {
-  const authorizations = selectAuthorizationsStore(state).authorizations.reduce((acc, cur) => {
-    const id = cur.client_ids.client_id
-    acc[id] = cur
 
-    return acc
-  }, {})
-
-  return authorizations[clientId]
-}
+export const selectSelectedAuthorization = createSelector(
+  [selectAuthorizationsStore, (_, clientId) => clientId],
+  (authorizationsStore, clientId) => {
+    const authorizations = authorizationsStore.authorizations.reduce((acc, cur) => {
+      const id = cur.client_ids.client_id
+      acc[id] = cur
+      return acc
+    }, {})
+    return authorizations[clientId]
+  },
+)
 export const selectAuthorizationsTotalCount = state =>
   selectAuthorizationsStore(state).authorizationsTotalCount
 export const selectAuthorizationsFetching = createFetchingSelector(GET_AUTHORIZATIONS_LIST_BASE)
 export const selectAuthorizationsError = createErrorSelector(GET_AUTHORIZATIONS_LIST_BASE)
 
 export const selectTokens = state => selectAuthorizationsStore(state).tokens
-export const selectTokenIds = state => selectTokens(state).map(token => token.id)
+export const selectTokenIds = createSelector([selectTokens], tokens =>
+  tokens.map(token => token.id),
+)
 export const selectTokensTotalCount = state => selectAuthorizationsStore(state).tokensTotalCount
 export const selectTokensFetching = createFetchingSelector(GET_ACCESS_TOKENS_LIST_BASE)
 export const selectTokensError = createErrorSelector(GET_ACCESS_TOKENS_LIST_BASE)
