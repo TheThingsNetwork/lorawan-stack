a9768f3d54cbfb976069278176794c4a247d173a
Johan Stokking <johan@stokking.nl>
2024-06-25 21:37:13 +0200

    gs: Harmonize config description

--- pkg/gatewayserver/io/semtechws/config.go
+++ pkg/gatewayserver/io/semtechws/config.go
@@ -19,7 +19,7 @@ import "time"
 // Config defines the LoRa Basics Station configuration of the Gateway Server.
 type Config struct {
 	UseTrafficTLSAddress bool          `name:"use-traffic-tls-address" description:"Use WSS for the traffic address regardless of the TLS setting"`
-	WSPingInterval       time.Duration `name:"ws-ping-interval" description:"Interval to send WS ping messages"`
+	WSPingInterval       time.Duration `name:"ws-ping-interval" description:"Interval to send websocket ping messages"`
 	MissedPongThreshold  int           `name:"missed-pong-threshold" description:"Number of consecutive missed pongs before disconnection. This value is used only if the gateway sends at least one pong."`
 	TimeSyncInterval     time.Duration `name:"time-sync-interval" description:"Interval to send time transfer messages"`
 	AllowUnauthenticated bool          `name:"allow-unauthenticated" description:"Allow unauthenticated connections"`
