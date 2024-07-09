374d5ac7d97c9eba94ad26afc313fb74f8b95f2f
Johan Stokking <johan@stokking.nl>
2024-07-09 11:00:36 +0200

    gcs: Add managed gateway event subscriber client

---
+++ pkg/gatewayconfigurationserver/managed/client/client.go
@@ -0,0 +1,201 @@
+// Copyright © 2024 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package client implements a managed gateway client.
+package client
+
+import (
+	"context"
+
+	"go.thethings.network/lorawan-stack/v3/pkg/cluster"
+	"go.thethings.network/lorawan-stack/v3/pkg/events"
+	"go.thethings.network/lorawan-stack/v3/pkg/rpcmetadata"
+	"go.thethings.network/lorawan-stack/v3/pkg/task"
+	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
+	"go.thethings.network/lorawan-stack/v3/pkg/workerpool"
+	"google.golang.org/grpc"
+)
+
+// Component is the interface to the component.
+type Component interface {
+	workerpool.Component
+	AllowInsecureForCredentials() bool
+	GetPeerConn(ctx context.Context, role ttnpb.ClusterRole, ids cluster.EntityIdentifiers) (*grpc.ClientConn, error)
+}
+
+type client struct {
+	component Component
+}
+
+// NewEvents initializes a new events subscriber.
+func NewEvents(component Component) events.Subscriber {
+	return &client{component}
+}
+
+type eventData struct {
+	*ttnpb.GatewayIdentifiers
+	*ttnpb.ManagedGatewayEventData
+}
+
+func (c *client) subscribeEventData(ctx context.Context, ids ...*ttnpb.GatewayIdentifiers) (chan *eventData, error) {
+	conn, err := c.component.GetPeerConn(ctx, ttnpb.ClusterRole_GATEWAY_CONFIGURATION_SERVER, nil)
+	if err != nil {
+		return nil, err
+	}
+	callOpt, err := rpcmetadata.WithForwardedAuth(ctx, c.component.AllowInsecureForCredentials())
+	if err != nil {
+		return nil, err
+	}
+	client := ttnpb.NewManagedGatewayConfigurationServiceClient(conn)
+	eventDataCh := make(chan *eventData)
+	for _, gtwID := range ids {
+		gtwID := gtwID
+		c.component.StartTask(&task.Config{
+			Context: ctx,
+			ID:      "managed_gateway_events",
+			Func: func(ctx context.Context) error {
+				stream, err := client.StreamEvents(ctx, gtwID, callOpt)
+				if err != nil {
+					return err
+				}
+				for {
+					data, err := stream.Recv()
+					if err != nil {
+						return err
+					}
+					select {
+					case <-ctx.Done():
+						return ctx.Err()
+					case eventDataCh <- &eventData{
+						GatewayIdentifiers:      gtwID,
+						ManagedGatewayEventData: data,
+					}:
+					}
+				}
+			},
+			Restart: task.RestartOnFailure,
+			Backoff: task.DialBackoffConfig,
+		})
+	}
+	return eventDataCh, nil
+}
+
+func mapEvent(ctx context.Context, eventData *eventData) events.Event {
+	switch data := eventData.Data.(type) {
+	case *ttnpb.ManagedGatewayEventData_Entity:
+		return evtUpdateManagedGateway.NewWithIdentifiersAndData(
+			ctx, eventData.GatewayIdentifiers, data.Entity,
+		)
+	case *ttnpb.ManagedGatewayEventData_Location:
+		return evtUpdateManagedGatewayLocation.NewWithIdentifiersAndData(
+			ctx, eventData.GatewayIdentifiers, data.Location,
+		)
+	case *ttnpb.ManagedGatewayEventData_SystemStatus:
+		return evtReceiveManagedGatewaySystemStatus.NewWithIdentifiersAndData(
+			ctx, eventData.GatewayIdentifiers, data.SystemStatus,
+		)
+	case *ttnpb.ManagedGatewayEventData_ControllerConnection:
+		if data.ControllerConnection.NetworkInterfaceType !=
+			ttnpb.ManagedGatewayNetworkInterfaceType_MANAGED_GATEWAY_NETWORK_INTERFACE_TYPE_UNSPECIFIED {
+			return evtManagedGatewayControllerUp.NewWithIdentifiersAndData(
+				ctx, eventData.GatewayIdentifiers, data.ControllerConnection,
+			)
+		}
+		return evtManagedGatewayControllerDown.New(ctx, events.WithIdentifiers(eventData.GatewayIdentifiers))
+	case *ttnpb.ManagedGatewayEventData_GatewayServerConnection:
+		if data.GatewayServerConnection.NetworkInterfaceType !=
+			ttnpb.ManagedGatewayNetworkInterfaceType_MANAGED_GATEWAY_NETWORK_INTERFACE_TYPE_UNSPECIFIED {
+			return evtManagedGatewayGatewayServerUp.NewWithIdentifiersAndData(
+				ctx, eventData.GatewayIdentifiers, data.GatewayServerConnection,
+			)
+		}
+		return evtManagedGatewayGatewayServerDown.New(ctx, events.WithIdentifiers(eventData.GatewayIdentifiers))
+	case *ttnpb.ManagedGatewayEventData_CellularBackhaul:
+		switch data.CellularBackhaul.NetworkInterface.GetStatus() {
+		case ttnpb.ManagedGatewayNetworkInterfaceStatus_MANAGED_GATEWAY_NETWORK_INTERFACE_STATUS_UP:
+			return evtManagedGatewayCellularUp.NewWithIdentifiersAndData(
+				ctx, eventData.GatewayIdentifiers, data.CellularBackhaul,
+			)
+		default:
+			return evtManagedGatewayCellularDown.New(ctx, events.WithIdentifiers(eventData.GatewayIdentifiers))
+		}
+	case *ttnpb.ManagedGatewayEventData_WifiBackhaul:
+		switch data.WifiBackhaul.NetworkInterface.GetStatus() {
+		case ttnpb.ManagedGatewayNetworkInterfaceStatus_MANAGED_GATEWAY_NETWORK_INTERFACE_STATUS_UP:
+			return evtManagedGatewayWiFiUp.NewWithIdentifiersAndData(
+				ctx, eventData.GatewayIdentifiers, data.WifiBackhaul,
+			)
+		case ttnpb.ManagedGatewayNetworkInterfaceStatus_MANAGED_GATEWAY_NETWORK_INTERFACE_STATUS_FAILED:
+			return evtManagedGatewayWiFiFail.New(ctx, events.WithIdentifiers(eventData.GatewayIdentifiers))
+		default:
+			return evtManagedGatewayWiFiDown.New(ctx, events.WithIdentifiers(eventData.GatewayIdentifiers))
+		}
+	case *ttnpb.ManagedGatewayEventData_EthernetBackhaul:
+		switch data.EthernetBackhaul.NetworkInterface.GetStatus() {
+		case ttnpb.ManagedGatewayNetworkInterfaceStatus_MANAGED_GATEWAY_NETWORK_INTERFACE_STATUS_UP:
+			return evtManagedGatewayEthernetUp.NewWithIdentifiersAndData(
+				ctx, eventData.GatewayIdentifiers, data.EthernetBackhaul,
+			)
+		default:
+			return evtManagedGatewayEthernetDown.New(ctx, events.WithIdentifiers(eventData.GatewayIdentifiers))
+		}
+	}
+	return nil
+}
+
+func matcherByNames(names []string) func(name string) bool {
+	if len(names) == 0 {
+		return func(string) bool { return true }
+	}
+	m := make(map[string]struct{}, len(names))
+	for _, name := range names {
+		m[name] = struct{}{}
+	}
+	return func(name string) bool {
+		_, ok := m[name]
+		return ok
+	}
+}
+
+// Subscribe implements events.Subscriber.
+func (c *client) Subscribe(
+	ctx context.Context, names []string, identifiers []*ttnpb.EntityIdentifiers, hdl events.Handler,
+) error {
+	ids := make([]*ttnpb.GatewayIdentifiers, 0, len(identifiers))
+	for _, id := range identifiers {
+		if id.GetGatewayIds() != nil {
+			ids = append(ids, id.GetGatewayIds())
+		}
+	}
+	ch, err := c.subscribeEventData(ctx, ids...)
+	if err != nil {
+		return err
+	}
+	match := matcherByNames(names)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case eventData := <-ch:
+				evt := mapEvent(ctx, eventData)
+				if evt == nil || !match(evt.Name()) {
+					continue
+				}
+				hdl.Notify(evt)
+			}
+		}
+	}()
+	return nil
+}
