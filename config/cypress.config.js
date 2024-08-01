19d94a7198f7e9469986049314f9193ebd23a0f5
Darya Plotnytska <d.plotnytska.s@gmail.com>
2024-08-01 13:04:46 +0200

    dev: Disable account app tests for the time being

--- config/cypress.config.js
+++ config/cypress.config.js
@@ -67,7 +67,12 @@ module.exports = defineConfig({
     },
     scrollBehavior: 'bottom',
     experimentalRunAllSpecs: true,
-    excludeSpecPattern: '!*.spec.js',
+    excludeSpecPattern: [
+      '!*.spec.js',
+      // TODO: Activate the following lines when the tests are ready.
+      'cypress/e2e/account/*.{js,jsx,ts,tsx}',
+      'cypress/e2e/account/oauth-clients/*.{js,jsx,ts,tsx}',
+    ],
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
   asBaseUrl: 'http://localhost:1885/api/v3',
