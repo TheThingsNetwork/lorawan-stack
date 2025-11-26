607bd17320534ee9d55a0a51c9e7233fb1ece5da
Vlad Vitan <23100181+vlasebian@users.noreply.github.com>
2025-11-26 13:59:14 +0200

    all: Implement HSTS headers

---
+++ pkg/webmiddleware/hsts_headers_test.go
@@ -0,0 +1,62 @@
+// Copyright © 2025 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package webmiddleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/smarty/assertions"
+	"go.thethings.network/lorawan-stack/v3/pkg/webmiddleware"
+)
+
+func TestHSTSHeaders(t *testing.T) {
+	a := assertions.New(t)
+
+	m := webmiddleware.HSTSHeaders()
+	a.So(m, assertions.ShouldNotBeNil)
+
+	t.Run("HTTPS request should add HSTS headers", func(t *testing.T) {
+		a := assertions.New(t)
+
+		req := httptest.NewRequest("GET", "https://example.com", nil)
+		rec := httptest.NewRecorder()
+
+		handler := m(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		handler.ServeHTTP(rec, req)
+
+		a.So(rec.Header().Get("Strict-Transport-Security"), assertions.ShouldNotBeEmpty)
+	})
+
+	t.Run("HTTP request should NOT add HSTS headers", func(t *testing.T) {
+		a := assertions.New(t)
+
+		req := httptest.NewRequest("GET", "http://example.com", nil)
+		rec := httptest.NewRecorder()
+
+		handler := m(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		handler.ServeHTTP(rec, req)
+
+		a.So(rec.Header().Get("Strict-Transport-Security"), assertions.ShouldBeEmpty)
+	})
+
+}
